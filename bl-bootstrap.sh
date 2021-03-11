cae1f9a21d25afc3d81902eb98e0a9b0da485218
Solomon Hykes <sh.github.6811@hykes.org>
2021-03-11 23:09:32 +0000

    more improvisation on bl-bootstrap.sh

--- bl-bootstrap.sh
+++ bl-bootstrap.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-export DAGGER_WORKSPACE=local
+FLAVOR="$1"; shift
 
-# Create a new stack based on the 'us-east' stack maintained by the blocklayer infra team.
-# Prompt interactively for remaining inputs.
-dagger new -n blocklayer-dev --base-stack blocklayer-infra.dagger.cloud/us-east --input-interactive --up
+case "$FLAVOR" in
+	base)
+		# 'bl-bootstrap.sh base': create a new infra base stack
+		dagger new -n blocklayer-dev --base-dir ./infra --input-interactive --up "$@"
+	;;
+
+	dev)
+		# 'bl-bootstrap.sh dev': create a new dev stack
+		# Create a new stack based on the 'us-east' stack maintained by the blocklayer infra team.
+		# Prompt interactively for remaining inputs.
+		dagger new -n blocklayer-dev --base-stack blocklayer-infra.dagger.cloud/us-east --input-interactive --up "$@"
+	;;
+
+esac
+
+
