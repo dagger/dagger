a79e13a35c11813810a7c7d3dea6b7af8a3a051f
Sam Alba <sam.alba@gmail.com>
2021-04-02 17:32:05 -0700

    test: fixed input_test.go
    Signed-off-by: Sam Alba <sam.alba@gmail.com>

--- dagger/input_test.go
+++ dagger/input_test.go
@@ -10,13 +10,13 @@ func TestInputDir(t *testing.T) {
 	st := &DeploymentState{
 		PlanSource: DirInput("/tmp/source", []string{}),
 	}
-	require.NoError(t, st.SetInput("www.source", DirInput(".", []string{})))
+	require.NoError(t, st.SetInput("www.source", DirInput("/", []string{})))
 
 	deployment, err := NewDeployment(st)
 	require.NoError(t, err)
 
 	localdirs := deployment.LocalDirs()
 	require.Len(t, localdirs, 2)
-	require.Contains(t, localdirs, ".")
+	require.Contains(t, localdirs, "/")
 	require.Contains(t, localdirs, "/tmp/source")
 }
