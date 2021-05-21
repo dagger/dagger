6ddb973f0366cb2e6faff8d4c2879a836728a925
Andrea Luzzardi <aluzzardi@gmail.com>
2021-05-21 10:31:25 -0700

    docs: move gendocs tool to dagger/dagger
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ tools/gendocs/src/@rocketseat/gatsby-theme-docs/components/Logos.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import logo from '../assets/dagger-logo.jpg'
+
+export default function Logo(props) {
+    return <img src={logo} alt="Logo" />
+}
---
+++ tools/gendocs/src/@rocketseat/gatsby-theme-docs/styles/theme.js
@@ -0,0 +1,15 @@
+export default {
+    colors: {
+        primary: '#1890FF',
+        background: '#FFFFFF',
+        shape: `#F2F2FA`,
+        title: `#3D3D4D`,
+        text: `#6C6C80`,
+        components: {
+            blockquote: {
+                background: `#332616`,
+                text: `#E1E1E6`,
+            },
+        },
+    },
+};
