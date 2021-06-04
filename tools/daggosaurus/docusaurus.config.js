4bce5de08be4416ae68f91838e13d8d6d778c50f
Andrea Luzzardi <aluzzardi@gmail.com>
2021-06-03 17:37:42 -0700

    docs: lint docusaurus.config.js
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- tools/daggosaurus/docusaurus.config.js
+++ tools/daggosaurus/docusaurus.config.js
@@ -1,62 +1,61 @@
-const path = require('path');
+const path = require("path");
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'Dagger',
-  tagline: 'Dagger is a programmable deployment system',
-  url: 'https://docs.dagger.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.png',
-  organizationName: 'Dagger',
-  projectName: 'Dagger',
+  title: "Dagger",
+  tagline: "Dagger is a programmable deployment system",
+  url: "https://docs.dagger.io",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.png",
+  organizationName: "Dagger",
+  projectName: "Dagger",
   stylesheets: [
-    'https://fonts.gstatic.com',
-    'https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap',
-    'https://fonts.googleapis.com/css2?family=Karla&family=Poppins:wght@700&display=swap'
+    "https://fonts.gstatic.com",
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap",
+    "https://fonts.googleapis.com/css2?family=Karla&family=Poppins:wght@700&display=swap",
   ],
   themeConfig: {
     sidebarCollapsible: false,
     prism: {
-      defaultLanguage: 'go',
+      defaultLanguage: "go",
     },
     navbar: {
       logo: {
-        alt: 'Dagger Logo',
-        src: 'img/dagger-logo.png',
-        srcDark: 'img/dagger_logo_dark.png',
+        alt: "Dagger Logo",
+        src: "img/dagger-logo.png",
+        srcDark: "img/dagger_logo_dark.png",
       },
     },
     algolia: {
-      apiKey: '66c9f1f7e6ba6617ec4e65194788bacd',
-      indexName: 'docs_dagger'
+      apiKey: "66c9f1f7e6ba6617ec4e65194788bacd",
+      indexName: "docs_dagger",
     },
     colorMode: {
       // "light" | "dark"
-      defaultMode: 'light',
+      defaultMode: "light",
 
       switchConfig: {
         darkIcon: "img/Icon_Night-mode.svg",
-        lightIcon: 'img/Icon_Day-mode.svg',
+        lightIcon: "img/Icon_Day-mode.svg",
       },
     },
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          editUrl:
-            'https://github.com/dagger/dagger/blob/main',
-          routeBasePath: '/',
+          sidebarPath: require.resolve("./sidebars.js"),
+          editUrl: "https://github.com/dagger/dagger/blob/main",
+          routeBasePath: "/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
   ],
-  plugins: [path.resolve(__dirname, './custom_plugins')],
+  plugins: [path.resolve(__dirname, "./custom_plugins")],
 };
