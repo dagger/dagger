3821d6211543e0dce1973924dd98ffd04b8ae99b
jffarge <jeanfrancois.farge@gmail.com>
2021-12-09 14:24:19 +0100

    docs: :bug: fixe iframe width + bump docusaurus to the latest version
    Signed-off-by: jffarge <jeanfrancois.farge@gmail.com>

--- website/src/theme/Heading/index.js
+++ website/src/theme/Heading/index.js
@@ -4,8 +4,6 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-
-/* eslint-disable jsx-a11y/anchor-has-content, jsx-a11y/anchor-is-valid */
 import React from 'react';
 import clsx from 'clsx';
 import {translate} from '@docusaurus/Translate';
@@ -38,18 +36,16 @@ const createAnchorHeading = (Tag) =>
     }
 
     return (
-      <Tag {...props}>
-        <a
-          aria-hidden="true"
-          tabIndex={-1}
-          className={clsx('anchor', `anchor__${Tag}`, {
-            [styles.anchorWithHideOnScrollNavbar]: hideOnScroll,
-            [styles.anchorWithStickyNavbar]: !hideOnScroll,
-          })}
-          id={id}
-        />
+      <Tag
+        {...props}
+        className={clsx('anchor', {
+          [styles.anchorWithHideOnScrollNavbar]: hideOnScroll,
+          [styles.anchorWithStickyNavbar]: !hideOnScroll,
+        })}
+        id={id}>
         {props.children}
         <a
+          aria-hidden="true"
           className="hash-link"
           href={`#${id}`}
           title={translate({
@@ -57,7 +53,7 @@ const createAnchorHeading = (Tag) =>
             message: 'Direct link to heading',
             description: 'Title for link to heading',
           })}>
-            <AnchorIcon />  
+          <AnchorIcon />
         </a>
       </Tag>
     );
