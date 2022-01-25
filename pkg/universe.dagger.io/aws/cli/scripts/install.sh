ed1e0f51894036cbb59528c4a50382d74df4a5ab
Richard Jones <richard@dagger.io>
2022-01-25 13:20:13 -0700

    fixed linting issue with sh script
    Signed-off-by: Richard Jones <richard@dagger.io>

--- pkg/universe.dagger.io/aws/cli/scripts/install.sh
+++ pkg/universe.dagger.io/aws/cli/scripts/install.sh
@@ -6,7 +6,7 @@ curl -sLO https://github.com/sgerrand/alpine-pkg-glibc/releases/download/2.31-r0
 apk add --no-cache glibc-2.31-r0.apk glibc-bin-2.31-r0.apk glibc-i18n-2.31-r0.apk 
 /usr/glibc-compat/bin/localedef -i en_US -f UTF-8 en_US.UTF-8 
 ARCH=$(uname -m)
-curl -s https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}-$1.zip -o awscliv2.zip
+curl -s "https://awscli.amazonaws.com/awscli-exe-linux-${ARCH}-$1.zip" -o awscliv2.zip
 unzip awscliv2.zip
 ./aws/install
 rm -rf awscliv2.zip aws /usr/local/aws-cli/v2/*/dist/aws_completer /usr/local/aws-cli/v2/*/dist/awscli/data/ac.index /usr/local/aws-cli/v2/*/dist/awscli/examples glibc-*.apk
