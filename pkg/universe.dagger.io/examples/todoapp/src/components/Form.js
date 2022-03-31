3703b884763823a6da3f9999675cc15aa37f2015
Tim Chan <tchan@flexport.com>
2022-03-31 11:57:18 -0700

    more urgent message

--- pkg/universe.dagger.io/examples/todoapp/src/components/Form.js
+++ pkg/universe.dagger.io/examples/todoapp/src/components/Form.js
@@ -22,7 +22,7 @@ function Form(props) {
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
         <label htmlFor="new-todo-input" className="label__lg">
-          What needs to be done?
+          What must be done today?
         </label>
       </h2>
 
