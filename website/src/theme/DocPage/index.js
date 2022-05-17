5920f93de86c8e53d65707a48d76c5dc6cca6491
user.email <jf@dagger.io>
2022-05-17 14:36:39 +0200

    Bump Docusaurus up Fix a major issue with Amplitude. Nothing has been logged during almost 2 months
    Signed-off-by: user.email <jf@dagger.io>

--- website/src/theme/DocPage/index.js
+++ website/src/theme/DocPage/index.js
@@ -1,149 +1,29 @@
-/**
- * Copyright (c) Facebook, Inc. and its affiliates.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-import React, {useState, useEffect, useCallback} from 'react';
-import {MDXProvider} from '@mdx-js/react';
-import renderRoutes from '@docusaurus/renderRoutes';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import DocSidebar from '@theme/DocSidebar';
-import MDXComponents from '@theme/MDXComponents';
+import React, {useEffect} from 'react';
 import NotFound from '@theme/NotFound';
-import IconArrow from '@theme/IconArrow';
-import BackToTopButton from '@theme/BackToTopButton';
-import {matchPath} from '@docusaurus/router';
-import {translate} from '@docusaurus/Translate';
+import DocPageLayout from '@theme/DocPage/Layout';
 import clsx from 'clsx';
-import styles from './styles.module.css';
 import {
+  HtmlClassNameProvider,
   ThemeClassNames,
   docVersionSearchTag,
   DocsSidebarProvider,
-  useDocsSidebar,
   DocsVersionProvider,
+  useDocRouteMetadata,
 } from '@docusaurus/theme-common';
-import Head from '@docusaurus/Head';
+import SearchMetadata from '@theme/SearchMetadata';
 import amplitude from 'amplitude-js';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
-function DocPageContent({
-  currentDocRoute,
-  versionMetadata,
-  children,
-  sidebarName,
-}) {
-  const sidebar = useDocsSidebar();
-  const {pluginId, version} = versionMetadata;
-  const [hiddenSidebarContainer, setHiddenSidebarContainer] = useState(false);
-  const [hiddenSidebar, setHiddenSidebar] = useState(false);
-  const toggleSidebar = useCallback(() => {
-    if (hiddenSidebar) {
-      setHiddenSidebar(false);
-    }
+export default function DocPage(props) {
+  const {versionMetadata} = props;
+  const currentDocRouteMetadata = useDocRouteMetadata(props);
 
-    setHiddenSidebarContainer((value) => !value);
-  }, [hiddenSidebar]);
-  return (
-    <Layout
-      wrapperClassName={ThemeClassNames.wrapper.docsPages}
-      pageClassName={ThemeClassNames.page.docsDocPage}
-      searchMetadata={{
-        version,
-        tag: docVersionSearchTag(pluginId, version),
-      }}>
-      <div className={styles.docPage}>
-        <BackToTopButton />
-
-        {sidebar && (
-          <aside
-            className={clsx(styles.docSidebarContainer, {
-              [styles.docSidebarContainerHidden]: hiddenSidebarContainer,
-            })}
-            onTransitionEnd={(e) => {
-              if (
-                !e.currentTarget.classList.contains(styles.docSidebarContainer)
-              ) {
-                return;
-              }
-
-              if (hiddenSidebarContainer) {
-                setHiddenSidebar(true);
-              }
-            }}>
-            <DocSidebar
-              key={
-                // Reset sidebar state on sidebar changes
-                // See https://github.com/facebook/docusaurus/issues/3414
-                sidebarName
-              }
-              sidebar={sidebar}
-              path={currentDocRoute.path}
-              onCollapse={toggleSidebar}
-              isHidden={hiddenSidebar}
-            />
-
-            {hiddenSidebar && (
-              <div
-                className={styles.collapsedDocSidebar}
-                title={translate({
-                  id: 'theme.docs.sidebar.expandButtonTitle',
-                  message: 'Expand sidebar',
-                  description:
-                    'The ARIA label and title attribute for expand button of doc sidebar',
-                })}
-                aria-label={translate({
-                  id: 'theme.docs.sidebar.expandButtonAriaLabel',
-                  message: 'Expand sidebar',
-                  description:
-                    'The ARIA label and title attribute for expand button of doc sidebar',
-                })}
-                tabIndex={0}
-                role="button"
-                onKeyDown={toggleSidebar}
-                onClick={toggleSidebar}>
-                <IconArrow className={styles.expandSidebarButtonIcon} />
-              </div>
-            )}
-          </aside>
-        )}
-        <main
-          className={clsx(styles.docMainContainer, {
-            [styles.docMainContainerEnhanced]:
-              hiddenSidebarContainer || !sidebar,
-          })}>
-          <div
-            className={clsx(
-              'container padding-top--md padding-bottom--lg',
-              styles.docItemWrapper,
-              {
-                [styles.docItemWrapperEnhanced]: hiddenSidebarContainer,
-              },
-            )}>
-            <MDXProvider components={MDXComponents}>{children}</MDXProvider>
-          </div>
-        </main>
-      </div>
-    </Layout>
-  );
-}
-
-function DocPage(props) {
-  const {
-    route: {routes: docRoutes},
-    versionMetadata,
-    location,
-  } = props;
-  const currentDocRoute = docRoutes.find((docRoute) =>
-    matchPath(location.pathname, docRoute),
-  );
-
-  // DocPage Swizzle
+    // DocPage Swizzle
   const {siteConfig} = useDocusaurusContext();
+
   useEffect(() => {
-      if(siteConfig.AMPLITUDE_ID) {
-        var instance1 = amplitude.getInstance().init(process.env.REACT_APP_AMPLITUDE_ID, null, {
+      if(siteConfig.customFields.AMPLITUDE_ID) {
+        var instance1 = amplitude.getInstance().init(siteConfig.customFields.AMPLITUDE_ID, null, {
           apiEndpoint: `${window.location.hostname}/t`
         })
         amplitude.getInstance().logEvent('Docs Viewed', { "hostname": window.location.hostname, "path": location.pathname });
@@ -151,36 +31,33 @@ function DocPage(props) {
   }, [location.pathname])
   // End DocPageSwizzle
 
-  if (!currentDocRoute) {
+  if (!currentDocRouteMetadata) {
     return <NotFound />;
-  } // For now, the sidebarName is added as route config: not ideal!
+  }
 
-  const sidebarName = currentDocRoute.sidebar;
-  const sidebar = sidebarName
-    ? versionMetadata.docsSidebars[sidebarName]
-    : null;
+  const {docElement, sidebarName, sidebarItems} = currentDocRouteMetadata;
   return (
     <>
-      <Head>
-        {/* TODO we should add a core addRoute({htmlClassName}) generic plugin option */}
-        <html className={versionMetadata.className} />
-      </Head>
-      <DocsVersionProvider version={versionMetadata}>
-        <DocsSidebarProvider sidebar={sidebar}>
-          <DocPageContent
-            currentDocRoute={currentDocRoute}
-            versionMetadata={versionMetadata}
-            sidebarName={sidebarName}>
-            <div data-cy="cy-doc-content">
-              {renderRoutes(docRoutes, {
-                versionMetadata,
-              })}
-            </div>
-          </DocPageContent>
-        </DocsSidebarProvider>
-      </DocsVersionProvider>
+      <SearchMetadata
+        version={versionMetadata.version}
+        tag={docVersionSearchTag(
+          versionMetadata.pluginId,
+          versionMetadata.version,
+        )}
+      />
+      <HtmlClassNameProvider
+        className={clsx(
+          // TODO: it should be removed from here
+          ThemeClassNames.wrapper.docsPages,
+          ThemeClassNames.page.docsDocPage,
+          props.versionMetadata.className,
+        )}>
+        <DocsVersionProvider version={versionMetadata}>
+          <DocsSidebarProvider name={sidebarName} items={sidebarItems}>
+            <DocPageLayout>{docElement}</DocPageLayout>
+          </DocsSidebarProvider>
+        </DocsVersionProvider>
+      </HtmlClassNameProvider>
     </>
   );
 }
-
-export default DocPage;
