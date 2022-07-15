561153a1f499948f9593fedf6a8b8603e1939d57
Erik Sipsma <erik@sipsma.dev>
2022-07-15 15:56:20 -0700

    re-add shell debug util
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- engine/engine.go
+++ engine/engine.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 	"time"
@@ -11,10 +12,13 @@ import (
 	bkclient "github.com/moby/buildkit/client"
 	bkgw "github.com/moby/buildkit/frontend/gateway/client"
 	"github.com/moby/buildkit/session"
+	"github.com/moby/buildkit/solver/pb"
 	"github.com/moby/buildkit/util/progress/progressui"
+	"golang.org/x/crypto/ssh/terminal"
 	"golang.org/x/sync/errgroup"
 
 	_ "github.com/moby/buildkit/client/connhelper/dockercontainer" // import the docker connection driver
+	"github.com/moby/buildkit/client/llb"
 )
 
 func Start(ctx context.Context, fn func(context.Context) error) error {
@@ -40,7 +44,9 @@ func Start(ctx context.Context, fn func(context.Context) error) error {
 			}()
 			server = api.NewServer(gw)
 
-			err = fn(dagger.WithInMemoryAPIClient(ctx, server))
+			ctx = dagger.WithInMemoryAPIClient(ctx, server)
+			ctx = withGatewayClient(ctx, gw)
+			err = fn(ctx)
 			if err != nil {
 				return nil, err
 			}
@@ -60,4 +66,78 @@ func Start(ctx context.Context, fn func(context.Context) error) error {
 		return err
 	}
 	return nil
+}
+
+type gatewayClientKey struct{}
+
+func withGatewayClient(ctx context.Context, gw bkgw.Client) context.Context {
+	return context.WithValue(ctx, gatewayClientKey{}, gw)
+}
+
+func Shell(ctx context.Context, inputFS string) error {
+	gw := ctx.Value(gatewayClientKey{}).(bkgw.Client)
+	baseDef, err := llb.Image("alpine:3.15").Marshal(ctx)
+	if err != nil {
+		return err
+	}
+	baseRes, err := gw.Solve(ctx, bkgw.SolveRequest{
+		Definition: baseDef.ToPB(),
+	})
+	if err != nil {
+		return err
+	}
+	baseRef, err := baseRes.SingleRef()
+	if err != nil {
+		return err
+	}
+
+	var fs api.FS
+	if err := json.Unmarshal([]byte(inputFS), &fs); err != nil {
+		return err
+	}
+	fsRes, err := gw.Solve(ctx, bkgw.SolveRequest{
+		Definition: fs.PB,
+	})
+	if err != nil {
+		return err
+	}
+	fsRef, err := fsRes.SingleRef()
+	if err != nil {
+		return err
+	}
+
+	ctr, err := gw.NewContainer(ctx, bkgw.NewContainerRequest{
+		Mounts: []bkgw.Mount{
+			{
+				Dest:      "/",
+				Ref:       baseRef,
+				MountType: pb.MountType_BIND,
+			},
+			{
+				Dest:      "/output",
+				Ref:       fsRef,
+				MountType: pb.MountType_BIND,
+			},
+		},
+	})
+	if err != nil {
+		return err
+	}
+	proc, err := ctr.Start(ctx, bkgw.StartRequest{
+		Args:   []string{"/bin/sh"},
+		Cwd:    "/output",
+		Tty:    true,
+		Stdin:  os.Stdin,
+		Stdout: os.Stdout,
+		Stderr: os.Stderr,
+	})
+	if err != nil {
+		return err
+	}
+	termState, err := terminal.MakeRaw(int(os.Stdin.Fd()))
+	if err != nil {
+		return err
+	}
+	defer terminal.Restore(int(os.Stdin.Fd()), termState)
+	return proc.Wait()
 }
