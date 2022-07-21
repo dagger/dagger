a15baaef3b3b9300e6fe2feafbc29c84adfda5c5
Erik Sipsma <erik@sipsma.dev>
2022-07-20 18:20:16 -0700

    include full graphql query in inputs
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- examples/graphql_ts/server.ts
+++ examples/graphql_ts/server.ts
@@ -24,13 +24,13 @@ export class DaggerServer extends ApolloServerBase {
     return super.graphQLServerOptions();
   }
 
-  private async query(input: string): Promise<string> {
+  private async query(input: Record<string, any>): Promise<string> {
     const { graphqlResponse, responseInit } = await runHttpQuery(
       [],
       {
         method: "POST",
         options: () => this.createGraphQLServerOptions(),
-        query: { query: input },
+        query: input,
         request: new Request("/graphql", {
           headers: new Headers(),
           method: "POST",
@@ -44,9 +44,9 @@ export class DaggerServer extends ApolloServerBase {
   public run() {
     this.start();
 
-    const inputs = fs.readFileSync("/inputs/dagger.graphql", "utf8");
+    const inputs = JSON.parse(fs.readFileSync("/inputs/dagger.json", "utf8"));
     this.query(inputs).then((resp) =>
-      fs.writeFileSync("/outputs/dagger.json", JSON.stringify(JSON.parse(resp)))
+      fs.writeFileSync("/outputs/dagger.json", resp)
     );
   }
 }
