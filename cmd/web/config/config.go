3201168273c6bb81132ce2e4c4837d71ef413c84
Andrea Luzzardi <aluzzardi@gmail.com>
2022-07-25 13:29:21 -0700

    cmd/web: cleanup config loading
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- cmd/web/config/config.go
+++ cmd/web/config/config.go
@@ -1,11 +1,14 @@
 package config
 
 import (
+	"context"
 	"fmt"
 	"os"
 	"path"
 	"path/filepath"
 
+	"github.com/Khan/genqlient/graphql"
+	"github.com/dagger/cloak/sdk/go/dagger"
 	"gopkg.in/yaml.v2"
 )
 
@@ -44,4 +47,126 @@ func ParseFile(f string) (*Config, error) {
 	fmt.Fprintf(os.Stderr, "Loading:\n%s\n", string(loaded))
 
 	return &cfg, nil
+}
+
+func (c *Config) LocalDirs() map[string]string {
+	localDirs := make(map[string]string)
+	for _, action := range c.Actions {
+		if action.Local != "" {
+			localDirs[action.Local] = action.Local
+		}
+	}
+	return localDirs
+}
+
+func (c *Config) Import(ctx context.Context, localDirs map[string]dagger.FS) error {
+	for name, action := range c.Actions {
+		switch {
+		case action.Local != "":
+			err := importLocal(ctx, name, localDirs[action.Local], action.Dockerfile)
+			if err != nil {
+				return fmt.Errorf("error importing %s: %w", name, err)
+			}
+		case action.Image != "":
+			err := importImage(ctx, name, action.Image)
+			if err != nil {
+				return fmt.Errorf("error importing %s: %w", name, err)
+			}
+		}
+	}
+
+	return nil
+}
+
+func importLocal(ctx context.Context, name string, cwd dagger.FS, dockerfile string) error {
+	cl, err := dagger.Client(ctx)
+	if err != nil {
+		return err
+	}
+	data := struct {
+		Core struct {
+			Dockerfile dagger.FS
+		}
+	}{}
+	resp := &graphql.Response{Data: &data}
+	err = cl.MakeRequest(ctx,
+		&graphql.Request{
+			Query: `
+			query Dockerfile($context: FS!, $dockerfile: String!) {
+				core {
+					dockerfile(
+						context: $context,
+						dockerfileName: $dockerfile,
+					)
+				}
+			}`,
+			Variables: map[string]any{
+				"context":    cwd,
+				"dockerfile": dockerfile,
+			},
+		},
+		resp,
+	)
+	if err != nil {
+		return err
+	}
+	return importFS(ctx, name, data.Core.Dockerfile)
+}
+
+func importImage(ctx context.Context, name string, ref string) error {
+	cl, err := dagger.Client(ctx)
+	if err != nil {
+		return err
+	}
+	data := struct {
+		Core struct {
+			Image struct {
+				FS dagger.FS
+			}
+		}
+	}{}
+	resp := &graphql.Response{Data: &data}
+	err = cl.MakeRequest(ctx,
+		&graphql.Request{
+			Query: `
+			query Image($ref: String!) {
+				core {
+					image(ref: $ref) {
+						fs
+					}
+				}
+			}`,
+			Variables: map[string]any{
+				"ref": ref,
+			},
+		},
+		resp,
+	)
+	if err != nil {
+		return err
+	}
+	return importFS(ctx, name, data.Core.Image.FS)
+}
+
+func importFS(ctx context.Context, name string, fs dagger.FS) error {
+	cl, err := dagger.Client(ctx)
+	if err != nil {
+		return err
+	}
+
+	return cl.MakeRequest(ctx,
+		&graphql.Request{
+			Query: `
+			mutation Import($name: String!, $fs: FS!) {
+				import(name: $name, fs: $fs) {
+						name
+				}
+			}`,
+			Variables: map[string]any{
+				"name": name,
+				"fs":   fs,
+			},
+		},
+		&graphql.Response{},
+	)
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/Khan/genqlient/graphql"
 	"github.com/dagger/cloak/cmd/web/config"
 	"github.com/dagger/cloak/engine"
 	"github.com/dagger/cloak/sdk/go/dagger"
@@ -23,23 +22,14 @@ func main() {
 	}
 
 	startOpts := &engine.StartOpts{
-		LocalDirs: make(map[string]string),
-	}
-	for _, action := range cfg.Actions {
-		if action.Local != "" {
-			startOpts.LocalDirs[action.Local] = action.Local
-		}
+		LocalDirs: cfg.LocalDirs(),
+		Secrets:   make(map[string]string),
 	}
 
 	err = engine.Start(context.Background(), startOpts,
 		func(ctx context.Context, localDirs map[string]dagger.FS, secrets map[string]string) (*dagger.FS, error) {
-			for name, action := range cfg.Actions {
-				switch {
-				case action.Local != "":
-					importLocal(ctx, localDirs[action.Local], name, action.Dockerfile)
-				case action.Image != "":
-					importImage(ctx, name, action.Image)
-				}
+			if err := cfg.Import(ctx, localDirs); err != nil {
+				return nil, err
 			}
 			return nil, engine.ListenAndServe(ctx, 8080)
 		})
@@ -47,111 +37,4 @@ func main() {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
 	}
-}
-
-func importLocal(ctx context.Context, cwd dagger.FS, name string, dockerfile string) {
-	cl, err := dagger.Client(ctx)
-	if err != nil {
-		panic(err)
-	}
-	data := struct {
-		Core struct {
-			Dockerfile dagger.FS
-		}
-	}{}
-	resp := &graphql.Response{Data: &data}
-	err = cl.MakeRequest(ctx,
-		&graphql.Request{
-			Query: `
-			query Dockerfile($context: FS!, $dockerfile: String!) {
-				core{
-					dockerfile(
-						context: $context,
-						dockerfileName: $dockerfile,
-					)
-				}
-			}`,
-			Variables: map[string]any{
-				"context":    cwd,
-				"dockerfile": dockerfile,
-			},
-		},
-		resp,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	err = cl.MakeRequest(ctx,
-		&graphql.Request{
-			Query: `
-			mutation Import($name: String!, $fs: FS!) {
-				import(name: $name, fs: $fs) {
-						name
-				}
-			}`,
-			Variables: map[string]any{
-				"name": name,
-				"fs":   data.Core.Dockerfile,
-			},
-		},
-		&graphql.Response{},
-	)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func importImage(ctx context.Context, name string, ref string) {
-	cl, err := dagger.Client(ctx)
-	if err != nil {
-		panic(err)
-	}
-	data := struct {
-		Core struct {
-			Image struct {
-				FS dagger.FS
-			}
-		}
-	}{}
-	resp := &graphql.Response{Data: &data}
-	err = cl.MakeRequest(ctx,
-		&graphql.Request{
-			Query: `
-			query Image($ref: String!) {
-				core{
-					image(ref: $ref) {
-						fs
-					}
-				}
-			}`,
-			Variables: map[string]any{
-				"ref": ref,
-			},
-		},
-		resp,
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	err = cl.MakeRequest(ctx,
-		&graphql.Request{
-			Query: `
-			mutation Import($name: String!, $fs: FS!) {
-				import(name: $name, fs: $fs) {
-						name
-				}
-			}`,
-			Variables: map[string]any{
-				"name": name,
-				"fs":   data.Core.Image.FS,
-			},
-		},
-		&graphql.Response{},
-	)
-	if err != nil {
-		panic(err)
-	}
-
 }
