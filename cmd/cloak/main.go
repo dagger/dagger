170a8420f40f93dc6f7673367c967da4266dce24
Erik Sipsma <erik@sipsma.dev>
2022-07-25 13:53:33 -0700

    use new dagger.yaml in cloak cli
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- cmd/cloak/main.go
+++ cmd/cloak/main.go
@@ -8,26 +8,33 @@ import (
 	"os"
 
 	"github.com/Khan/genqlient/graphql"
-	"github.com/dagger/cloak/cmd/cloak/gen/core"
-	"golang.org/x/sync/errgroup"
 
-	// "github.com/dagger/cloak/cmd/cloak/gen/todoapp"
+	"github.com/dagger/cloak/cmd/web/config"
 	"github.com/dagger/cloak/engine"
 	"github.com/dagger/cloak/sdk/go/dagger"
 )
 
 func main() {
-	startOpts := &engine.StartOpts{
-		// TODO: read these from cli flags
-		LocalDirs: map[string]string{
-			".":   ".",
-			"src": "./examples/todoapp/app",
-		},
+	cfg, err := config.ParseFile("./dagger.yaml")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
 
-	err := engine.Start(context.Background(), startOpts,
+	localDirs := cfg.LocalDirs()
+	// TODO: read this from cli flags
+	localDirs["src"] = "../../examples/todoapp/app"
+
+	startOpts := &engine.StartOpts{
+		LocalDirs: localDirs,
+	}
+
+	err = engine.Start(context.Background(), startOpts,
 		func(ctx context.Context, localDirs map[string]dagger.FS, secrets map[string]string) (*dagger.FS, error) {
-			importAll(ctx, localDirs)
+			if err := cfg.Import(ctx, localDirs); err != nil {
+				return nil, err
+			}
+
 			inBytes, err := io.ReadAll(os.Stdin)
 			if err != nil {
 				return nil, err
@@ -63,51 +70,5 @@ func main() {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
-	}
-}
-
-func importAll(ctx context.Context, localDirs map[string]dagger.FS) {
-	var eg errgroup.Group
-	eg.Go(func() error {
-		importLocal(ctx, localDirs["."], "alpine", "Dockerfile.alpine")
-		return nil
-	})
-	eg.Go(func() error {
-		importLocal(ctx, localDirs["."], "netlify", "Dockerfile.netlify")
-		return nil
-	})
-	eg.Go(func() error {
-		importLocal(ctx, localDirs["."], "yarn", "Dockerfile.yarn")
-		return nil
-	})
-	eg.Go(func() error {
-		importLocal(ctx, localDirs["."], "todoapp", "Dockerfile.todoapp")
-		return nil
-	})
-	err := eg.Wait()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func importLocal(ctx context.Context, cwd dagger.FS, pkgName string, dockerfile string) {
-	output, err := core.Dockerfile(ctx, cwd, dockerfile)
-	if err != nil {
-		panic(err)
-	}
-	_, err = core.Import(ctx, pkgName, output.Core.Dockerfile)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func importImage(ctx context.Context, pkgName string, ref string) {
-	output, err := core.Image(ctx, ref)
-	if err != nil {
-		panic(err)
-	}
-	_, err = core.Import(ctx, pkgName, output.Core.Image.Fs)
-	if err != nil {
-		panic(err)
 	}
 }
