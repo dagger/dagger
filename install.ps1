5dc1d007187dc972300ccf1628a267b2c314b59b
Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>
2022-08-05 11:55:34 -0400

    feat: improve windows install script, added optional parameters
    Signed-off-by: Brittan DeYoung <32572259+brittandeyoung@users.noreply.github.com>

--- install.ps1
+++ install.ps1
@@ -1,50 +1,58 @@
-# param (
-#     [Parameter(Mandatory)] $PersonalToken  
-# )
+#Requires -Version 7.0
+
+param (
+    # [Parameter(Mandatory)] $PersonalToken  
+    [Parameter(Mandatory = $false)] [System.Management.Automation.SemanticVersion]$DaggerVersion,
+    [Parameter(Mandatory = $false)] [System.Boolean]$InteractiveInstall = $false,
+    [Parameter(Mandatory = $false)] [string]$InstallPath = $env:HOMEPATH + '\dagger'
+)
 Clear-Host
 @"
 
 ---------------------------------------------------------------------------------
 Author: Alessandro Festa
+Co Author: Brittan DeYoung
 Dagger Installation Utility  for Windows users
 ---------------------------------------------------------------------------------
 
 "@
 
 # Since we are already authenticated we may directly download latest version.
-$name="dagger"
-$base="https://dagger-io.s3.amazonaws.com"
+$name = "dagger"
+$base = "https://dagger-io.s3.amazonaws.com"
 function http_download {
-    $version=Get_Version
-    $version=$version -replace '[""]'
-    $version=$version -replace '\n'
-    $fileName="dagger_v" + $version + "_windows_amd64"
+    $version = Get_Version
+    $version = $version -replace '[""]'
+    $version = $version -replace '\n'
+    $fileName = "dagger_v" + $version + "_windows_amd64"
     Clear-Host
     $url = $base + "/" + $name + "/releases/" + $version + "/" + $fileName + ".zip"
     write-host $url
-    Pause
+    if ($InteractiveInstall) {
+        Pause
+    }
     
 
     Invoke-WebRequest -Uri $url -OutFile $env:temp/$fileName.zip -ErrorAction Stop
-    Expand-Archive -Path $env:temp/$fileName.zip -DestinationPath $env:HOMEPATH/dagger -Force -ErrorVariable ProcessError;
-    If ($ProcessError)
-    {
-    Clear-Host
-@"
+    Expand-Archive -Path $env:temp/$fileName.zip -DestinationPath $InstallPath -Force -ErrorVariable ProcessError;
+    If ($ProcessError) {
+        Clear-Host
+        @"
 Whoops apparently we had an issue in unzipping the file, please check
 you have the right permission to do so and try to unzip manually the file.
 Currently we saved Dagger at your temp folder.
 "@
-exit
-    } else {
+        exit
+    }
+    else {
         Clear-Host
     
-@"
+        @"
 
 Thank You for downloading Dagger!
 
 -----------------------------------------------------
-Dagger has been saved at <YOUR HOME FOLDER>/dagger/
+Dagger has been saved in $InstallPath
 Please add dagger.exe to your PATH in order to use it
 ----------------------------------------------------
 
@@ -54,11 +62,14 @@ Please add dagger.exe to your PATH in order to use it
 }
 
 function Get_Version { 
-    $response = Invoke-RestMethod 'http://releases.dagger.io/dagger/latest_version' -Method 'GET'  -Body $body -ErrorAction SilentlyContinue -ErrorVariable DownloadError
-    If ($DownloadError)
-    {
-    Clear-Host
-@"
+    if ($DaggerVersion) {
+        $version = $DaggerVersion
+    }
+    else {
+        $response = Invoke-RestMethod 'http://releases.dagger.io/dagger/latest_version' -Method 'GET'  -Body $body -ErrorAction SilentlyContinue -ErrorVariable DownloadError
+        If ($DownloadError) {
+            Clear-Host
+            @"
 
 ---------------------------------------------------------------------------
 Houston we have a problem!
@@ -68,9 +79,11 @@ run the script and if it still fail please open an issue on the Dagger repo.
 ----------------------------------------------------------------------------
 
 "@
-exit
+            exit
+        }
+        $version = $response
     }
-    return $response
+    return $version
     
 }
 
