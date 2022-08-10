932c34563d2e59b1738a22f725401f60276b3705
Solomon Hykes <solomon@dagger.io>
2022-08-10 12:09:48 -0700

    Use the term "extension" in cloak.yaml and README
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- cmd/cloak/config/config.go
+++ cmd/cloak/config/config.go
@@ -14,12 +14,12 @@ import (
 )
 
 type Config struct {
-	Path    string             `yaml:"-,omitempty"`
-	Actions map[string]*Action `yaml:"actions,omitempty"`
-	Context string             `yaml:"context,omitempty"`
+	Path       string                `yaml:"-,omitempty"`
+	Extensions map[string]*Extension `yaml:"extensions,omitempty"`
+	Context    string                `yaml:"context,omitempty"`
 }
 
-type Action struct {
+type Extension struct {
 	Local      string `yaml:"local,omitempty"`
 	Image      string `yaml:"image,omitempty"`
 	Context    string `yaml:"context,omitempty"`
@@ -27,11 +27,11 @@ type Action struct {
 	operations string
 }
 
-func (a *Action) GetSchema() string {
+func (a *Extension) GetSchema() string {
 	return a.schema
 }
 
-func (a *Action) GetOperations() string {
+func (a *Extension) GetOperations() string {
 	return a.operations
 }
 
@@ -45,21 +45,21 @@ func ParseFile(f string) (*Config, error) {
 	if err := yaml.Unmarshal(data, &cfg); err != nil {
 		return nil, err
 	}
-	if cfg.Actions == nil {
-		cfg.Actions = make(map[string]*Action)
+	if cfg.Extensions == nil {
+		cfg.Extensions = make(map[string]*Extension)
 	}
 
-	for _, action := range cfg.Actions {
-		if action.Local != "" {
-			if action.Context == "" {
-				action.Context = cfg.Context
+	for _, ext := range cfg.Extensions {
+		if ext.Local != "" {
+			if ext.Context == "" {
+				ext.Context = cfg.Context
 			}
-			action.Context = filepath.Join(filepath.Dir(f), action.Context)
-			action.Local = filepath.Join(cfg.Context, action.Local)
+			ext.Context = filepath.Join(filepath.Dir(f), ext.Context)
+			ext.Local = filepath.Join(cfg.Context, ext.Local)
 		}
 	}
 	// implicitly include core in every import
-	cfg.Actions["core"] = &Action{}
+	cfg.Extensions["core"] = &Extension{}
 
 	loaded, err := yaml.Marshal(&cfg)
 	if err != nil {
@@ -72,20 +72,20 @@ func ParseFile(f string) (*Config, error) {
 
 func (c *Config) LocalDirs() map[string]string {
 	localDirs := make(map[string]string)
-	for _, action := range c.Actions {
-		if action.Local == "" {
+	for _, ext := range c.Extensions {
+		if ext.Local == "" {
 			continue
 		}
-		localDirs[action.Context] = action.Context
+		localDirs[ext.Context] = ext.Context
 	}
 	return localDirs
 }
 
 func (c *Config) LoadExtensions(ctx context.Context, localDirs map[string]dagger.FSID) error {
 	var eg errgroup.Group
-	for name, action := range c.Actions {
+	for name, ext := range c.Extensions {
 		name := name
-		action := action
+		ext := ext
 		eg.Go(func() error {
 			switch {
 			case name == "core":
@@ -93,16 +93,16 @@ func (c *Config) LoadExtensions(ctx context.Context, localDirs map[string]dagger
 				if err != nil {
 					return fmt.Errorf("error importing %s: %w", name, err)
 				}
-				action.schema = schema
-				action.operations = operations
-			case action.Local != "":
-				dockerfile := path.Join(action.Local, "Dockerfile")
-				schema, operations, err := importLocal(ctx, name, localDirs[action.Context], dockerfile)
+				ext.schema = schema
+				ext.operations = operations
+			case ext.Local != "":
+				dockerfile := path.Join(ext.Local, "Dockerfile")
+				schema, operations, err := importLocal(ctx, name, localDirs[ext.Context], dockerfile)
 				if err != nil {
 					return fmt.Errorf("error importing %s: %w", name, err)
 				}
-				action.schema = schema
-				action.operations = operations
+				ext.schema = schema
+				ext.operations = operations
 			}
 			return nil
 		})
