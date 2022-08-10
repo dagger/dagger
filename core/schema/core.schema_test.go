c5eca6138897b3d9a8ee1cb914102b69dae7b93f
Andrea Luzzardi <aluzzardi@gmail.com>
2022-08-10 15:51:36 -0700

    core test: use testutil
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- core/schema/core.schema_test.go
+++ core/schema/core.schema_test.go
@@ -1,79 +1,50 @@
 package core
 
 import (
-	"context"
 	"testing"
 
-	"github.com/Khan/genqlient/graphql"
-	"github.com/dagger/cloak/engine"
-	"github.com/dagger/cloak/sdk/go/dagger"
+	"github.com/dagger/cloak/core/schema/testutil"
 	"github.com/stretchr/testify/require"
 )
 
 func TestCoreImage(t *testing.T) {
-	require.NoError(t, engine.Start(context.Background(), nil, func(ctx context.Context) error {
-		cl, err := dagger.Client(ctx)
-		require.NoError(t, err)
+	res := struct {
+		Core struct {
+			Image struct {
+				File string
+			}
+		}
+	}{}
 
-		res := struct {
-			Core struct {
-				Image struct {
-					File string
+	testutil.Query(t,
+		`{
+			core {
+				image(ref: "alpine:3.16.2") {
+					file(path: "/etc/alpine-release")
 				}
 			}
-		}{}
-		err = cl.MakeRequest(ctx,
-			&graphql.Request{
-				Query: `
-					{
-						core {
-							image(ref: "alpine:3.16.2") {
-								file(path: "/etc/alpine-release")
-							}
-						}
-					}
-				`,
-			},
-			&graphql.Response{Data: &res},
-		)
-		require.NoError(t, err)
-		require.NotEmpty(t, res.Core.Image.File)
-		require.Equal(t, res.Core.Image.File, "3.16.2\n")
+		}`, nil, &res)
 
-		return nil
-	}))
+	require.Equal(t, res.Core.Image.File, "3.16.2\n")
 }
 
 func TestCoreGit(t *testing.T) {
-	require.NoError(t, engine.Start(context.Background(), nil, func(ctx context.Context) error {
-		cl, err := dagger.Client(ctx)
-		require.NoError(t, err)
+	res := struct {
+		Core struct {
+			Git struct {
+				File string
+			}
+		}
+	}{}
 
-		res := struct {
-			Core struct {
-				Git struct {
-					File string
+	testutil.Query(t,
+		`{
+			core {
+				git(remote: "github.com/dagger/dagger") {
+					file(path: "README.md")
 				}
 			}
-		}{}
-		err = cl.MakeRequest(ctx,
-			&graphql.Request{
-				Query: `
-					{
-						core {
-							git(remote: "github.com/dagger/dagger") {
-								file(path: "README.md")
-							}
-						}
-					}
-				`,
-			},
-			&graphql.Response{Data: &res},
-		)
-		require.NoError(t, err)
-		require.NotEmpty(t, res.Core.Git.File)
-		require.Contains(t, res.Core.Git.File, "dagger")
+		}`, nil, &res)
 
-		return nil
-	}))
+	require.Contains(t, res.Core.Git.File, "dagger")
 }
