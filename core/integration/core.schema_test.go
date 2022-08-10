2634c5f03dcb207ae386e3b3ddf1ec1394b7bcce
Andrea Luzzardi <aluzzardi@gmail.com>
2022-08-10 16:33:38 -0700

    core: add basic integration tests
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ core/integration/core.schema_test.go
@@ -0,0 +1,50 @@
+package core
+
+import (
+	"testing"
+
+	"github.com/dagger/cloak/core/integration/testutil"
+	"github.com/stretchr/testify/require"
+)
+
+func TestCoreImage(t *testing.T) {
+	res := struct {
+		Core struct {
+			Image struct {
+				File string
+			}
+		}
+	}{}
+
+	testutil.Query(t,
+		`{
+			core {
+				image(ref: "alpine:3.16.2") {
+					file(path: "/etc/alpine-release")
+				}
+			}
+		}`, nil, &res)
+
+	require.Equal(t, res.Core.Image.File, "3.16.2\n")
+}
+
+func TestCoreGit(t *testing.T) {
+	res := struct {
+		Core struct {
+			Git struct {
+				File string
+			}
+		}
+	}{}
+
+	testutil.Query(t,
+		`{
+			core {
+				git(remote: "github.com/dagger/dagger") {
+					file(path: "README.md")
+				}
+			}
+		}`, nil, &res)
+
+	require.Contains(t, res.Core.Git.File, "dagger")
+}
