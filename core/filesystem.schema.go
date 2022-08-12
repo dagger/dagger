394195fbdfa764b04d0c800155b49f59b089cc47
Andrea Luzzardi <aluzzardi@gmail.com>
2022-08-12 16:02:36 -0700

    core: improve docstrings
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- core/filesystem.schema.go
+++ core/filesystem.schema.go
@@ -48,10 +48,12 @@ func (s *filesystemSchema) Schema() string {
 
 	"""
 	A reference to a filesystem tree.
+
 	For example:
 	 - The root filesystem of a container
 	 - A source code repository
 	 - A directory containing binary artifacts
+
 	Rule of thumb: if it fits in a tar archive, it fits in a Filesystem.
 	"""
 	type Filesystem {
