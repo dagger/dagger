e4c64eb17b935ccecb244aec08269908e2aa70cd
Andrea Luzzardi <aluzzardi@gmail.com>
2022-09-06 20:21:24 +0200

    core: filesystem: add copy tests
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- core/integration/core.schema_test.go
+++ core/integration/core.schema_test.go
@@ -58,3 +58,57 @@ func TestCoreGit(t *testing.T) {
 	require.NoError(t, err)
 	require.Contains(t, res.Core.Git.File, "dagger")
 }
+
+func TestFilesystemCopy(t *testing.T) {
+	t.Parallel()
+
+	alpine := struct {
+		Core struct {
+			Image struct {
+				ID string
+			}
+		}
+	}{}
+
+	err := testutil.Query(
+		`{
+			core {
+				image(ref: "alpine:3.16.2") {
+					id
+				}
+			}
+		}`, &alpine, nil)
+	require.NoError(t, err)
+	require.NotEmpty(t, alpine.Core.Image.ID)
+
+	res := struct {
+		Core struct {
+			Filesystem struct {
+				Copy struct {
+					File string
+				}
+			}
+		}
+	}{}
+
+	testutil.Query(
+		`query ($from: FSID!) {
+			core {
+				filesystem(id: "scratch") {
+					copy(
+						from: $from
+						srcPath: "/etc/alpine-release"
+						destPath: "/test"
+					) {
+						file(path: "/test")
+					}
+				}
+			}
+		}`, &res, &testutil.QueryOptions{
+			Variables: map[string]any{
+				"from": alpine.Core.Image.ID,
+			},
+		})
+	require.NoError(t, err)
+	require.Equal(t, "3.16.2\n", res.Core.Filesystem.Copy.File)
+}
