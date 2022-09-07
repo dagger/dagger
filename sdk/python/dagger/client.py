fd8d81b83c377649fa19c27435ff38529dc554e8
Sam Alba <samalba@users.noreply.github.com>
2022-09-06 20:37:10 -0700

    sdk/python: basic support for engine and client
    Signed-off-by: Sam Alba <samalba@users.noreply.github.com>

---
+++ sdk/python/dagger/client.py
@@ -0,0 +1,19 @@
+from gql.transport import requests
+from gql import Client as gqlClient, gql
+
+
+class Client:
+
+    def __init__(self, host: str, port: int):
+        self._client = None
+        transport = requests.RequestsHTTPTransport(
+            url="http://{}:{}/".format(host, port),
+            timeout=30,
+            retries=10,
+        )
+        self._client = gqlClient(transport=transport,
+                                 fetch_schema_from_transport=True)
+
+    def do(self, query: str):
+        result = self._client.execute(gql(query))
+        print(result)
