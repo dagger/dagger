324aa0032f34425e8a965226a62b090c24524326
Jonnatan Jossemar Cordero <jossemargt@gmail.com>
2022-09-08 22:18:53 -0600

    docs: Disable medium-zoom on non-illustrative imgs
    Signed-off-by: Jonnatan Jossemar Cordero <jossemargt@gmail.com>

--- website/src/components/molecules/tutorialCard.js
+++ website/src/components/molecules/tutorialCard.js
@@ -3,10 +3,10 @@ import styles from "../../css/molecules/tutorialCard.module.scss";
 import Link from "@docusaurus/Link";
 import Button from "../atoms/button";
 
-const TutorialCard = ({img, title, description, buttonLabel, url}) => {
+const TutorialCard = ({ img, title, description, buttonLabel, url }) => {
   return (
     <div className={styles.tutorialCard}>
-      <img src={img}></img>
+      <img src={img} className="not-zoom"></img>
       <div>
         <h4>{title}</h4>
         <p>{description}</p>
