983cd6eb04472362f42d30c07ffd621cd25c8c85
Erik Sipsma <erik@sipsma.dev>
2022-09-09 16:30:19 -0700

    Add a test for image export.
    This test is the epitome of hacky+brittle as it:
    1. Relies on the fact that all containers currently run in the same
       network namespace.
    2. Relies on localhost port 5000 not being occupied inside the buildkitd
       container.
    
    However, the more robust options are either pushing to an actual
    registry (which is a lot of infra management to deal w/ for a trivial
    initial test) or fleshing out the whole concept of being able to run
    services and expose sockets (which is a ton of work). So for now I think
    it's better to have a hacky test than no test at all. If it turns out to
    be flaky we can disable it.
    
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- core/integration/core.schema_test.go
+++ core/integration/core.schema_test.go
@@ -1,8 +1,11 @@
 package core
 
 import (
+	"context"
 	"testing"
 
+	"github.com/Khan/genqlient/graphql"
+	"github.com/dagger/cloak/engine"
 	"github.com/dagger/cloak/internal/testutil"
 	"github.com/stretchr/testify/require"
 )
@@ -51,12 +54,12 @@ func TestCoreGit(t *testing.T) {
 		`{
 			core {
 				git(remote: "github.com/dagger/dagger") {
-					file(path: "README.md")
+					file(path: "README.md", lines: 1)
 				}
 			}
 		}`, &res, nil)
 	require.NoError(t, err)
-	require.Contains(t, res.Core.Git.File, "dagger")
+	require.Contains(t, res.Core.Git.File, "Dagger")
 }
 
 func TestFilesystemCopy(t *testing.T) {
@@ -164,4 +167,96 @@ func TestCoreExec(t *testing.T) {
 		}})
 	require.NoError(t, err)
 	require.Equal(t, "hi\n", execRes.Core.Image.Exec.Mount.File)
+}
+
+func TestCoreImageExport(t *testing.T) {
+	// FIXME:(sipsma) this test is a bit hacky+brittle, but unless we push to a real registry
+	// or flesh out the idea of local services, it's probably the best we can do for now.
+	err := engine.Start(context.Background(), nil, func(ctx engine.Context) error {
+		go func() {
+			err := ctx.Client.MakeRequest(ctx,
+				&graphql.Request{
+					Query: `query RunRegistry {
+						core {
+							image(ref: "registry:2") {
+								exec(input: {
+									args: ["/entrypoint.sh", "/etc/docker/registry/config.yml"]
+								}) {
+									stdout
+									stderr
+								}
+							}
+						}
+					}`,
+				},
+				&graphql.Response{Data: new(map[string]any)},
+			)
+			if err != nil {
+				t.Logf("error running registry: %v", err)
+			}
+		}()
+
+		err := ctx.Client.MakeRequest(ctx,
+			&graphql.Request{
+				Query: `query WaitForRegistry {
+					core {
+						image(ref: "alpine:3.16.2") {
+							exec(input: {
+								args: ["sh", "-c", "for i in $(seq 1 60); do nc -zv 127.0.0.1 5000 && exit 0; sleep 1; done; exit 1"]
+							}) {
+								stdout
+							}
+						}
+					}
+				}`,
+			},
+			&graphql.Response{Data: new(map[string]any)},
+		)
+		require.NoError(t, err)
+
+		testRef := "127.0.0.1:5000/testimagepush:latest"
+		err = ctx.Client.MakeRequest(ctx,
+			&graphql.Request{
+				Query: `query TestImagePush($ref: String!) {
+					core {
+						image(ref: "alpine:3.16.2") {
+							pushImage(ref: $ref)
+						}
+					}
+				}`,
+				Variables: map[string]any{
+					"ref": testRef,
+				},
+			},
+			&graphql.Response{Data: new(map[string]any)},
+		)
+		require.NoError(t, err)
+
+		res := struct {
+			Core struct {
+				Image struct {
+					File string
+				}
+			}
+		}{}
+		err = ctx.Client.MakeRequest(ctx,
+			&graphql.Request{
+				Query: `query TestImagePull($ref: String!) {
+					core {
+						image(ref: $ref) {
+							file(path: "/etc/alpine-release")
+						}
+					}
+				}`,
+				Variables: map[string]any{
+					"ref": testRef,
+				},
+			},
+			&graphql.Response{Data: &res},
+		)
+		require.NoError(t, err)
+		require.Equal(t, res.Core.Image.File, "3.16.2\n")
+		return nil
+	})
+	require.NoError(t, err)
 }
