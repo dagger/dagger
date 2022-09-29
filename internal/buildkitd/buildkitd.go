ab19a0ff466a5e5a6fc382cc68abeca3407aa568
Sam Alba <samalba@users.noreply.github.com>
2022-09-29 15:03:07 -0700

    simplifies logic in buildkitd starting checks
    Signed-off-by: Sam Alba <samalba@users.noreply.github.com>

--- internal/buildkitd/buildkitd.go
+++ internal/buildkitd/buildkitd.go
@@ -148,23 +148,24 @@ func checkBuildkit(ctx context.Context, version string) error {
 		if err := installBuildkit(ctx, version); err != nil {
 			return err
 		}
-	} else {
-		if config.Version != version {
-			fmt.Println("Buildkitd container is out of date, updating it...")
+		return nil
+	}
 
-			if err := removeBuildkit(ctx); err != nil {
-				return err
-			}
-			if err := installBuildkit(ctx, version); err != nil {
-				return err
-			}
+	if config.Version != version {
+		fmt.Println("Buildkitd container is out of date, updating it...")
+
+		if err := removeBuildkit(ctx); err != nil {
+			return err
 		}
-		if !config.IsActive {
-			fmt.Println("Buildkitd container is not running, starting it...")
+		if err := installBuildkit(ctx, version); err != nil {
+			return err
+		}
+	}
+	if !config.IsActive {
+		fmt.Println("Buildkitd container is not running, starting it...")
 
-			if err := startBuildkit(ctx); err != nil {
-				return err
-			}
+		if err := startBuildkit(ctx); err != nil {
+			return err
 		}
 	}
 
