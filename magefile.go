3ddd5220a92b873cb501b46a5381965d5620828a
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2022-10-14 14:57:35 +0200

    build: build dagger with mage
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- magefile.go
+++ magefile.go
@@ -55,3 +55,43 @@ func (Lint) Codegen(ctx context.Context) error {
 
 	return nil
 }
+
+type Build mg.Namespace
+
+// Dagger will build the dagger binary
+func (Build) Dagger(ctx context.Context) error {
+	return engine.Start(ctx, nil, func(ctx engine.Context) error {
+		core := api.New(ctx.Client)
+
+		builder := core.Container().From("golang:1.18.2-alpine")
+
+		src, err := core.Host().Workdir().Read().ID(ctx)
+		if err != nil {
+			return err
+		}
+
+		builder = builder.WithMountedDirectory(".", src)
+
+		builder = builder.Exec(api.ContainerExecOpts{
+			Args: []string{"mkdir", "./build"},
+		})
+
+		builder = builder.Exec(api.ContainerExecOpts{
+			Args: []string{"go", "build", "-o", "./build/dagger", "./cmd/dagger"},
+		})
+
+		daggerBuildDir, err := builder.Directory("./build").ID(ctx)
+		if err != nil {
+			return err
+		}
+
+		ok, err := core.Host().Workdir().Write(ctx, daggerBuildDir, api.HostDirectoryWriteOpts{Path: "."})
+		if err != nil {
+			return err
+		}
+		if !ok {
+			return errors.New("HostDirectoryWrite not ok")
+		}
+		return nil
+	})
+}
