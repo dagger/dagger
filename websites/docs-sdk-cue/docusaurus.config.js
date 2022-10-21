be859fcfc061aeacb8228ea97427865e09c709c5
Gerhard Lazu <gerhard@dagger.io>
2022-10-21 15:44:14 +0100

    Revert "docs: Fix sdk-cue baseUrl for Netlify"
    This reverts commit ea4c2957ec35bcd7f62d6f1b9c0d5f1235de42dd.
    
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>

--- websites/docs-sdk-cue/docusaurus.config.js
+++ websites/docs-sdk-cue/docusaurus.config.js
@@ -7,7 +7,7 @@ module.exports = {
   title: "Dagger",
   tagline: "Dagger is a programmable deployment system",
   url: "https://docs.dagger.io",
-  baseUrl: "/",
+  baseUrl: "/sdk/cue/",
   onBrokenMarkdownLinks: "throw",
   onBrokenLinks: "ignore",
   favicon: "img/favicon.png",
