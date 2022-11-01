c52ff00a7327ad30841b8836a182264f202761f4
Vikram Vaswani <vikram@dagger.io>
2022-11-01 21:15:04 +0530

    docs: Add guide for new host API
    When applied, this commit adds a guide for the new host API.
    
    It  also adds a sidebar node named "Guides" to the navigation tree and a guides index page.
    
    Closes #3593
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current/sdk/go/snippets/work-with-host-filesystem/list-dir-exclude/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	dir := os.TempDir()
+	os.WriteFile(filepath.Join(dir, "foo.txt"), []byte("1"), 0600)
+	os.WriteFile(filepath.Join(dir, "bar.txt"), []byte("2"), 0600)
+	os.WriteFile(filepath.Join(dir, "baz.rar"), []byte("3"), 0600)
+
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx, dagger.WithWorkdir(dir))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer client.Close()
+
+	// highlight-start
+	entries, err := client.Host().Workdir(dagger.HostWorkdirOpts{
+		Exclude: []string{"*.txt"},
+	}).Entries(ctx)
+	// highlight-end
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(entries)
+}
---
+++ docs/current/sdk/go/snippets/work-with-host-filesystem/mount-dir/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"dagger.io/dagger"
+)
+
+func main() {
+	ctx := context.Background()
+
+	client, err := dagger.Connect(ctx, dagger.WithWorkdir("."))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer client.Close()
+
+	// highlight-start
+	contents, err := client.Container().
+		From("alpine:latest").
+		WithMountedDirectory("/host", client.Host().Workdir()).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"ls", "/host"},
+		}).Stdout().Contents(ctx)
+	// highlight-end
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	fmt.Println(contents)
+}
