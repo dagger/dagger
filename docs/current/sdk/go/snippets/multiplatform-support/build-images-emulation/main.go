8828d5e959d843baf7006552871a5e1e70b99cb8
Erik Sipsma <erik@sipsma.dev>
2022-11-02 09:11:24 -0700

    Fixup multiplatform snippet code.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- docs/current/sdk/go/snippets/multiplatform-support/build-images-emulation/main.go
+++ docs/current/sdk/go/snippets/multiplatform-support/build-images-emulation/main.go
@@ -1,83 +1,84 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "os"
+	"context"
+	"fmt"
+	"os"
 
-  "dagger.io/dagger"
+	"dagger.io/dagger"
 )
 
 // the platforms to build for and push in a multi-platform image
 var platforms = []dagger.Platform{
-  "linux/amd64", // a.k.a. x86_64
-  "linux/arm64", // a.k.a. aarch64
-  "linux/s390x", // a.k.a. IBM S/390
+	"linux/amd64", // a.k.a. x86_64
+	"linux/arm64", // a.k.a. aarch64
+	"linux/s390x", // a.k.a. IBM S/390
 }
 
 // the container registry for the multi-platform image
 const imageRepo = "localhost/testrepo:latest"
 
 func main() {
-  ctx := context.Background()
-  client, err := dagger.Connect(ctx)
-  if err != nil {
-    return err
-  }
-  defer client.Close()
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
 
-  // the git repository containing code for the binary to be built
-  gitRepo := client.Git("https://github.com/dagger/dagger.git").
-    Branch("027d0c0").
-    Tree()
+	// the git repository containing code for the binary to be built
+	gitRepo := client.Git("https://github.com/dagger/dagger.git").
+		Branch("086862926433e19e1f24cd709e6165c36bdb2633").
+		Tree()
 
-  var platformVariants []*dagger.Container
-  for _, platform := range platforms {
-    // pull the golang image for this platform
-    ctr := client.Container(dagger.ContainerOpts{Platform: platform})
-    ctr = ctr.From("golang:1.19-alpine")
+	var platformVariants []*dagger.Container
+	for _, platform := range platforms {
+		// pull the golang image for this platform
+		ctr := client.Container(dagger.ContainerOpts{Platform: platform})
+		ctr = ctr.From("golang:1.19-alpine")
 
-    // mount in source code
-    ctr = ctr.WithMountedDirectory("/src", gitRepo)
+		// mount in source code
+		ctr = ctr.WithMountedDirectory("/src", gitRepo)
 
-    // mount in an empty dir where the built binary will live
-    ctr = ctr.WithMountedDirectory("/output", client.Directory())
+		// mount in an empty dir where the built binary will live
+		ctr = ctr.WithMountedDirectory("/output", client.Directory())
 
-    // ensure the binary will be statically linked and thus executable
-    // in the final image
-    ctr = ctr.WithEnvVariable("CGO_ENABLED", "0")
+		// ensure the binary will be statically linked and thus executable
+		// in the final image
+		ctr = ctr.WithEnvVariable("CGO_ENABLED", "0")
 
-    // build the binary and put the result at the mounted output
-    // directory
-    ctr = ctr.Exec(dagger.ContainerExecOpts{
-      Args: []string{
-        "go", "build",
-        "-o", "/output/cloak",
-        "/src/cmd/cloak",
-      },
-    })
+		// build the binary and put the result at the mounted output
+		// directory
+		ctr = ctr.WithWorkdir("/src")
+		ctr = ctr.Exec(dagger.ContainerExecOpts{
+			Args: []string{
+				"go", "build",
+				"-o", "/output/cloak",
+				"/src/cmd/cloak",
+			},
+		})
 
-    // select the output directory
-    outputDir := ctr.Directory("/output")
+		// select the output directory
+		outputDir := ctr.Directory("/output")
 
-    // wrap the output directory in a new empty container marked
-    // with the same platform
-    binaryCtr := client.
-      Container(dagger.ContainerOpts{Platform: platform}).
-      WithFS(outputDir)
-    platformVariants = append(platformVariants, binaryCtr)
-  }
+		// wrap the output directory in a new empty container marked
+		// with the same platform
+		binaryCtr := client.
+			Container(dagger.ContainerOpts{Platform: platform}).
+			WithFS(outputDir)
+		platformVariants = append(platformVariants, binaryCtr)
+	}
 
-  // publishing the final image uses the same API as single-platform
-  // images, but now additionally specify the `PlatformVariants`
-  // option with the containers built before.
-  imageDigest, err := client.
-    Container().
-    Publish(ctx, imageRepo, dagger.ContainerPublishOpts{
-      PlatformVariants: platformVariants,
-    })
-  if err != nil {
-    panic(err)
-  }
-  fmt.Println("Pushed multi-platform image w/ digest: ", imageDigest)
+	// publishing the final image uses the same API as single-platform
+	// images, but now additionally specify the `PlatformVariants`
+	// option with the containers built before.
+	imageDigest, err := client.
+		Container().
+		Publish(ctx, imageRepo, dagger.ContainerPublishOpts{
+			PlatformVariants: platformVariants,
+		})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Pushed multi-platform image w/ digest: ", imageDigest)
 }
--- docs/current/sdk/go/snippets/multiplatform-support/non-linux-support/main.go
+++ docs/current/sdk/go/snippets/multiplatform-support/non-linux-support/main.go
@@ -1,40 +1,40 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "os"
+	"context"
+	"fmt"
+	"os"
 
-  "dagger.io/dagger"
+	"dagger.io/dagger"
 )
 
 func main() {
-  ctx := context.Background()
-  client, err := dagger.Connect(ctx)
-  if err != nil {
-    return err
-  }
-  defer client.Close()
+	ctx := context.Background()
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
 
-  // pull a Windows base image
-  ctr := client.
-    Container(dagger.ContainerOpts{Platform: "windows/amd64"}).
-    From("mcr.microsoft.com/windows/nanoserver:ltsc2022")
+	// pull a Windows base image
+	ctr := client.
+		Container(dagger.ContainerOpts{Platform: "windows/amd64"}).
+		From("mcr.microsoft.com/windows/nanoserver:ltsc2022")
 
-  // listing files works, no error should be returned
-  entries, err := ctr.FS().Entries(ctx)
-  if err != nil {
-    panic(err) // shouldn't happen
-  }
-  for _, entry := range entries {
-    fmt.Println(entry)
-  }
+	// listing files works, no error should be returned
+	entries, err := ctr.FS().Entries(ctx)
+	if err != nil {
+		panic(err) // shouldn't happen
+	}
+	for _, entry := range entries {
+		fmt.Println(entry)
+	}
 
-  // however, executing a command will fail
-  output, err := ctr.Exec(dagger.ContainerExecOpts{
-    Args: []string{"cmd.exe"},
-  }).Stdout().Contents(ctx)
-  if err != nil {
-    panic(err) // should happen
-  }
+	// however, executing a command will fail
+	_, err = ctr.Exec(dagger.ContainerExecOpts{
+		Args: []string{"cmd.exe"},
+	}).Stdout().Contents(ctx)
+	if err != nil {
+		panic(err) // should happen
+	}
 }
