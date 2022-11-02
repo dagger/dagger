1573ce8a2326d11a529fe1d8920cb45bad6184d2
Andrea Luzzardi <aluzzardi@gmail.com>
2022-11-01 23:22:49 -0700

    docs: go: update snippets for new SDK
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

--- docs/current/sdk/go/snippets/step5a/main.go
+++ docs/current/sdk/go/snippets/step5a/main.go
@@ -1,94 +1,82 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "os"
-  "path/filepath"
+	"context"
+	"fmt"
+	"os"
+	"path/filepath"
 
-  "dagger.io/dagger"
+	"dagger.io/dagger"
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Must pass in a Git repository to build")
-    os.Exit(1)
-  }
-  repo := os.Args[1]
-  if err := build(repo); err != nil {
-    fmt.Println(err)
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("Must pass in a Git repository to build")
+		os.Exit(1)
+	}
+	repo := os.Args[1]
+	if err := build(context.Background(), repo); err != nil {
+		fmt.Println(err)
+	}
 }
 
-func build(repoUrl string) error {
-  fmt.Printf("Building %s\n", repoUrl)
+func build(ctx context.Context, repoURL string) error {
+	fmt.Printf("Building %s\n", repoURL)
 
-  // highlight-start
-  // define build matrix
-  oses := []string{"linux", "darwin"}
-  arches := []string{"amd64", "arm64"}
-  // highlight-end
+	// highlight-start
+	// define build matrix
+	oses := []string{"linux", "darwin"}
+	arches := []string{"amd64", "arm64"}
+	// highlight-end
 
-  ctx := context.Background()
+	// initialize Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
+	if err != nil {
+		return err
+	}
+	defer client.Close()
 
-  // initialize Dagger client
-  client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stdout))
-  if err != nil {
-    return err
-  }
-  defer client.Close()
+	// clone repository with Dagger
+	repo := client.Git(repoURL)
+	src := repo.Branch("main").Tree()
 
-  // clone repository with Dagger
-  repo := client.Git(repoUrl)
-  src, err := repo.Branch("main").Tree().ID(ctx)
-  if err != nil {
-    return err
-  }
+	// get `golang` image
+	golang := client.Container().From("golang:latest")
 
-  // get `golang` image
-  golang := client.Container().From("golang:latest")
+	// mount cloned repository into `golang` image
+	golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
 
-  // mount cloned repository into `golang` image
-  golang = golang.WithMountedDirectory("/src", src).WithWorkdir("/src")
+	// highlight-start
+	for _, goos := range oses {
+		for _, goarch := range arches {
+			// create a directory for each os and arch
+			path := fmt.Sprintf("build/%s/%s/", goos, goarch)
+			outpath := filepath.Join(".", path)
+			err = os.MkdirAll(outpath, os.ModePerm)
+			if err != nil {
+				return err
+			}
 
-  // get reference to current working directory on the host
-  workdir := client.Host().Workdir()
+			// set GOARCH and GOOS in the build environment
+			build := golang.WithEnvVariable("GOOS", goos)
+			build = build.WithEnvVariable("GOARCH", goarch)
 
-  // highlight-start
-  for _, goos := range oses {
-    for _, goarch := range arches {
-      // create a directory for each os and arch
-      path := fmt.Sprintf("build/%s/%s/", goos, goarch)
-      outpath := filepath.Join(".", path)
-      err = os.MkdirAll(outpath, os.ModePerm)
-      if err != nil {
-        return err
-      }
+			// build application
+			build = build.Exec(dagger.ContainerExecOpts{
+				Args: []string{"go", "build", "-o", path},
+			})
 
-      // set GOARCH and GOOS in the build environment
-      build := golang.WithEnvVariable("GOOS", goos)
-      build = build.WithEnvVariable("GOARCH", goarch)
+			// get reference to build output directory in container
+			output := build.Directory(path)
 
-      // build application
-      build = build.Exec(dagger.ContainerExecOpts{
-        Args: []string{"go", "build", "-o", path},
-      })
+			// write contents of container build/ directory to the host
+			_, err = output.Export(ctx, path)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	// highlight-end
 
-      // get reference to build output directory in container
-      output, err := build.Directory(path).ID(ctx)
-      if err != nil {
-        return err
-      }
-
-      // write contents of container build/ directory
-      // to the host working directory
-      _, err = workdir.Write(ctx, output, dagger.HostDirectoryWriteOpts{Path: path})
-      if err != nil {
-        return err
-      }
-    }
-  }
-  // highlight-end
-
-  return nil
+	return nil
 }
