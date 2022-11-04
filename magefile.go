cb54e90cf1786ba060b2b4acfe5c8a9ea4f943bb
Andrea Luzzardi <aluzzardi@gmail.com>
2022-11-03 19:25:35 -0700

    ci: multi-language support
    - Add a common interface to build/test/generate SDK code
    - Re-organize mage code to be under `internal/mage`
    - Rename `build` to `engine:build` and `lint:markdown` to `docs:lint`
    
    Signed-off-by: Andrea Luzzardi <aluzzardi@gmail.com>

---
+++ magefile.go
@@ -0,0 +1,12 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	//mage:import
+	_ "github.com/dagger/dagger/internal/mage"
+
+	//mage:import sdk
+	_ "github.com/dagger/dagger/internal/mage/sdk"
+)
