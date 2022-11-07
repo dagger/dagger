6f68caf5a6b8a6691c12ccb3816c46a121383bca
Helder Correia <174525+helderco@users.noreply.github.com>
2022-11-07 15:45:42 -0100

    sdk: python: Remove wrapping result object (#3713)
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/tests/api/test_integration.py
+++ sdk/python/tests/api/test_integration.py
@@ -11,20 +11,12 @@ pytestmark = [
 ]
 
 
-async def test_python_command():
-    async with dagger.Connection() as client:
-        alpine = client.container().from_("python:3.10.8-alpine")
-
-        version = await alpine.exec(["python", "-V"]).stdout().contents()
-        assert version.value == "Python 3.10.8\n"
-
-
 async def test_container():
     async with dagger.Connection() as client:
         alpine = client.container().from_("alpine:3.16.2")
         version = await alpine.exec(["cat", "/etc/alpine-release"]).stdout().contents()
 
-        assert version.value == "3.16.2\n"
+        assert version == "3.16.2\n"
 
 
 async def test_git_repository():
@@ -32,18 +24,18 @@ async def test_git_repository():
         repo = client.git("https://github.com/dagger/dagger").tag("v0.3.0").tree()
         readme = await repo.file("README.md").contents()
 
-        assert readme.value.split("\n")[0] == "## What is Dagger?"
+        assert readme.split("\n")[0] == "## What is Dagger?"
 
 
 async def test_container_build():
     async with dagger.Connection() as client:
-        repo = await client.git("https://github.com/dagger/dagger").tag("v0.3.0").tree().id()
+        repo_id = await client.git("https://github.com/dagger/dagger").tag("v0.3.0").tree().id()
 
-        dagger_img = client.container().build(repo.value)
+        dagger_img = client.container().build(repo_id)
 
         out = await dagger_img.exec(["version"]).stdout().contents()
 
-        words = out.value.strip().split(" ")
+        words = out.strip().split(" ")
 
         assert words[0] == "dagger"
 
@@ -53,23 +45,23 @@ async def test_container_with_env_variable():
         container = client.container().from_("alpine:3.16.2").with_env_variable("FOO", "bar")
         out = await container.exec(["sh", "-c", "echo -n $FOO"]).stdout().contents()
 
-        assert out.value == "bar"
+        assert out == "bar"
 
 
 async def test_container_with_mounted_directory():
     async with dagger.Connection() as client:
-        dir = await (
+        dir_id = await (
             client.directory()
             .with_new_file("hello.txt", "Hello, world!")
             .with_new_file("goodbye.txt", "Goodbye, world!")
             .id()
         )
 
-        container = client.container().from_("alpine:3.16.2").with_mounted_directory("/mnt", dir.value)
+        container = client.container().from_("alpine:3.16.2").with_mounted_directory("/mnt", dir_id)
 
         out = await container.exec(["ls", "/mnt"]).stdout().contents()
 
-        assert out.value == dedent(
+        assert out == dedent(
             """\
             goodbye.txt
             hello.txt
@@ -81,9 +73,9 @@ async def test_container_with_mounted_cache():
     async with dagger.Connection() as client:
         cache_key = f"example-{datetime.now().isoformat()}"
 
-        cache = await client.cache_volume(cache_key).id()
+        cache_id = await client.cache_volume(cache_key).id()
 
-        container = client.container().from_("alpine:3.16.2").with_mounted_cache("/cache", cache.value)
+        container = client.container().from_("alpine:3.16.2").with_mounted_cache("/cache", cache_id)
 
         for i in range(5):
             out = (
@@ -99,7 +91,7 @@ async def test_container_with_mounted_cache():
                 .contents()
             )
 
-        assert out.value == "0\n1\n2\n3\n4\n"
+        assert out == "0\n1\n2\n3\n4\n"
 
 
 async def test_directory():
@@ -112,12 +104,12 @@ async def test_directory():
 
         entries = await dir.entries()
 
-        assert entries.value == ["goodbye.txt", "hello.txt"]
+        assert entries == ["goodbye.txt", "hello.txt"]
 
 
 async def test_host_workdir():
     async with dagger.Connection(dagger.Config(workdir=".")) as client:
         readme = await client.host().workdir().file("README.md").contents()
-        lines = readme.value.strip().split("\n")
+        lines = readme.strip().split("\n")
 
         assert lines[0] == "# Dagger Python SDK"
