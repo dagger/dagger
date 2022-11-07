ace452ee0139d26ba0d8b06185ddb835d2c75aba
Andrea Luzzardi <al@dagger.io>
2022-11-07 15:02:48 -0800

    ci: publish support
    Signed-off-by: Andrea Luzzardi <al@dagger.io>

--- internal/mage/sdk/go.go
+++ internal/mage/sdk/go.go
@@ -2,8 +2,12 @@ package sdk
 
 import (
 	"context"
+	"encoding/base64"
+	"errors"
+	"fmt"
 	"os"
 	"path"
+	"strings"
 
 	"dagger.io/dagger"
 	"github.com/dagger/dagger/internal/mage/util"
@@ -81,4 +85,70 @@ func (t Go) Generate(ctx context.Context) error {
 		return err
 	}
 	return os.WriteFile(goGeneratedAPIPath, []byte(generated), 0600)
+}
+
+// Publish publishes the Go SDK
+func (t Go) Publish(ctx context.Context, tag string) error {
+	c, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	var (
+		targetTag  = strings.TrimPrefix(tag, "sdk/go/")
+		targetRepo = "https://github.com/aluzzardi/test.git"
+		pat        = os.Getenv("GITHUB_PAT")
+	)
+
+	if pat == "" {
+		return errors.New("export GITHUB_PAT=xxx before using this")
+	}
+
+	encodedPAT := base64.URLEncoding.EncodeToString([]byte("pat:" + pat))
+
+	git := util.GoBase(c).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"apk", "add", "-U", "--no-cache", "git"},
+		}).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"git", "config", "--global", "user.name", "dagger-ci"},
+		}).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"git", "config", "--global", "user.email", "hello@dagger.io"},
+		}).
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{"git", "config", "--global",
+				"http.https://github.com/.extraheader",
+				fmt.Sprintf("AUTHORIZATION: Basic %s", encodedPAT),
+			},
+		})
+
+	repository := git.Exec(dagger.ContainerExecOpts{
+		Args: []string{"git", "clone", "https://github.com/dagger/dagger.git", "/src/dagger"},
+	}).WithWorkdir("/src/dagger")
+
+	filtered := repository.
+		WithEnvVariable("FILTER_BRANCH_SQUELCH_WARNING", "1").
+		Exec(dagger.ContainerExecOpts{
+			Args: []string{
+				"git", "filter-branch", "-f", "--prune-empty",
+				"--subdirectory-filter", "sdk/go",
+				"--tree-filter", "if [ -f go.mod ]; then go mod edit -dropreplace github.com/dagger/dagger; fi",
+				"--", tag,
+			},
+		})
+
+	// Push
+	_, err = filtered.Exec(dagger.ContainerExecOpts{
+		Args: []string{
+			"git",
+			"push",
+			"-f",
+			targetRepo,
+			fmt.Sprintf("%s:%s", tag, targetTag),
+		},
+	}).ExitCode(ctx)
+
+	return err
 }
