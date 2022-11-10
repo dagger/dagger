ae35f1bc6188530aa049b79a341f5b629027017e
Helder Correia <174525+helderco@users.noreply.github.com>
2022-11-10 13:31:50 -0100

    Use pytest fixture and anyio
    - By using dagger.Config() instead of just os.environ we can test using the default value for DAGGER_HOST;
    - Temporary dirs should be created by a pytest fixture;
    - Use anyio instead of asyncio for compat with other backends like trio;
    
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/tests/connectors/test_docker.py
+++ sdk/python/tests/connectors/test_docker.py
@@ -1,8 +1,6 @@
-import asyncio
-import os
 from pathlib import Path
-import tempfile
 
+import anyio
 import pytest
 
 import dagger
@@ -13,28 +11,33 @@ pytestmark = [
 ]
 
 
-async def test_docker_image_provision():
-    # skip test if DAGGER_HOST is set and not prefixed with docker-image
-    dagger_host = os.environ.get("DAGGER_HOST")
-    if dagger_host and not dagger_host.startswith("docker-image:"):
-        pytest.skip("DAGGER_HOST is not docker-image")
+@pytest.mark.skipif(
+    dagger.Config().host.scheme != "docker-image",
+    reason="DAGGER_HOST is not docker-image",
+)
+async def test_docker_image_provision(tmp_path: Path):
+    cache_dir = tmp_path / "dagger"
+    cache_dir.mkdir()
 
-    with tempfile.TemporaryDirectory() as tmpdir:
-        cache_dir = Path(tmpdir) / "dagger"
-        cache_dir.mkdir(parents=True, exist_ok=True)
+    # make some garbage for the image provisioner to collect
+    (cache_dir / "dagger-engine-session-gcme").touch()
 
-        # make some garbage for the image provisioner to collect
-        (cache_dir / "dagger-engine-session-gcme").touch()
-        # clean up of existing containers is handled in engine-session binary
-        # and is already tested in go, skipping coverage here
+    # clean up of existing containers is handled in engine-session binary
+    # and is already tested in go, skipping coverage here
 
-        # run a bunch of provisions in parallel
-        async def connect_once():
-            async with dagger.Connection() as client:
-                assert await client.container().from_("alpine:3.16.2").id()
-        parallelism = 30
-        await asyncio.gather(*[connect_once() for _ in range(parallelism)])
+    # run a bunch of provisions concurrently
+    async def connect_once():
+        async with dagger.Connection() as client:
+            assert await client.container().from_("alpine:3.16.2").id()
 
-        # assert that there's only one engine-session binary in the cache
-        assert len(list(cache_dir.glob("*"))) == 1
-        assert list(cache_dir.glob("*"))[0].name.startswith("dagger-engine-session-")
+    concurrency = 30
+    async with anyio.create_task_group() as tg:
+        for _ in range(concurrency):
+            tg.start_soon(connect_once)
+
+    # assert that there's only one engine-session binary in the cache
+    files = cache_dir.iterdir()
+    bin = next(files)
+    assert bin.name.startswith("dagger-engine-session-")
+    with pytest.raises(StopIteration):
+        next(files)
