303c291e0e234f8853c935a28ea17c92f47eadad
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2022-11-15 12:40:02 +0100

    fix(codegen/nodejs): use updates from the provisioning part
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- codegen/generator/nodejs/templates/src/testdata/want-api-full.golden.ts
+++ codegen/generator/nodejs/templates/src/testdata/want-api-full.golden.ts
@@ -25,7 +25,7 @@ class BaseClient {
   constructor({queryTree, port}: ClientConfig = {}) {
     this._queryTree = queryTree || []
     this.port = port || 8080
-		this.client = new GraphQLClient(`http://localhost:${port}/query`);
+		this.client = new GraphQLClient(`http://127.0.0.1:${port}/query`);
   }
 
   get queryTree() {
