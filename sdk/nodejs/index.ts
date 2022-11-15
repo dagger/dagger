f9488548f62b3a73c6b6af62dc46af2d1bc48999
jffarge <jf@dagger.io>
2022-11-15 17:24:50 +0100

    export client as default
    Signed-off-by: jffarge <jf@dagger.io>

--- sdk/nodejs/index.ts
+++ sdk/nodejs/index.ts
@@ -1,7 +1,9 @@
-export * from "./api/client.gen.js";
+import Client from "./api/client.gen.js";
 
 export { gql } from "graphql-tag";
 export { GraphQLClient } from "graphql-request";
 
 export { connect, ConnectOpts } from './connect.js';
 export { getProvisioner } from './provisioning/index.js';
+
+export default Client
