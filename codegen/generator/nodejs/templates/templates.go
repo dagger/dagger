3a94ead2032012a0aba0c0472614f1acc8974fda
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2022-11-15 03:06:36 +0100

    refactor(codegen/nodejs): remove call_arg
    it was too simple a template
    
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- codegen/generator/nodejs/templates/templates.go
+++ codegen/generator/nodejs/templates/templates.go
@@ -13,7 +13,7 @@ var srcs embed.FS
 func New() *template.Template {
 	topLevelTemplate := "api"
 	templateDeps := []string{
-		topLevelTemplate, "header", "objects", "object", "object_comment", "method", "method_solve", "call_args", "call_arg", "field", "return_solve", "input_args", "return", "field_comment", "types", "type", "args", "arg",
+		topLevelTemplate, "header", "objects", "object", "object_comment", "method", "method_solve", "call_args", "field", "return_solve", "input_args", "return", "field_comment", "types", "type", "args", "arg",
 	}
 
 	fileNames := make([]string, 0, len(templateDeps))
