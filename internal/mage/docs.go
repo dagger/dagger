c85088a6682edbd4acb86deceef0d79faba59e0a
Andrea Luzzardi <al@dagger.io>
2022-11-15 19:33:24 -0800

    docs: lint nodejs code snippets
    Signed-off-by: Andrea Luzzardi <al@dagger.io>

--- internal/mage/docs.go
+++ internal/mage/docs.go
@@ -7,6 +7,7 @@ import (
 	"dagger.io/dagger"
 	"github.com/dagger/dagger/internal/mage/util"
 	"github.com/magefile/mage/mg"
+	"golang.org/x/sync/errgroup"
 )
 
 type Docs mg.Namespace
@@ -21,19 +22,47 @@ func (Docs) Lint(ctx context.Context) error {
 
 	workdir := util.Repository(c)
 
-	_, err = c.Container().
-		From("tmknom/markdownlint:0.31.1").
-		WithMountedDirectory("/src", workdir).
-		WithMountedFile("/src/.markdownlint.yaml", workdir.File(".markdownlint.yaml")).
-		WithWorkdir("/src").
-		Exec(dagger.ContainerExecOpts{
-			Args: []string{
-				"-c",
-				".markdownlint.yaml",
-				"--",
-				"./docs",
-				"README.md",
-			},
-		}).ExitCode(ctx)
-	return err
+	eg, gctx := errgroup.WithContext(ctx)
+
+	// Markdown
+	eg.Go(func() error {
+		_, err = c.Container().
+			From("tmknom/markdownlint:0.31.1").
+			WithMountedDirectory("/src", workdir).
+			WithMountedFile("/src/.markdownlint.yaml", workdir.File(".markdownlint.yaml")).
+			WithWorkdir("/src").
+			Exec(dagger.ContainerExecOpts{
+				Args: []string{
+					"-c",
+					".markdownlint.yaml",
+					"--",
+					"./docs",
+					"README.md",
+				},
+			}).ExitCode(gctx)
+		return err
+	})
+
+	// NodeJS
+	eg.Go(func() error {
+		nodeSnippets := c.Directory().
+			WithDirectory("/", workdir.Directory("docs/current/sdk/nodejs/snippets"))
+		_, err = c.Container().
+			From("node:16-alpine").
+			WithWorkdir("/src").
+			WithMountedDirectory("/src", nodeSnippets).
+			Exec(dagger.ContainerExecOpts{
+				Args: []string{"yarn", "install"},
+			}).
+			Exec(dagger.ContainerExecOpts{
+				Args: []string{"yarn", "lint"},
+			}).ExitCode(gctx)
+		return err
+	})
+
+	// FIXME: Python
+
+	// Go is already linted by engine:lint
+
+	return eg.Wait()
 }
