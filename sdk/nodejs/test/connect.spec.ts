4477f6a58462302589d03ff30606f96c16a9b6f7
Benjamin Strasser <55442329+benjaminstrasser@users.noreply.github.com>
2022-11-21 23:24:52 +0000

    Added basic implementation of nodejs SDK throwing an error + testcase
    Signed-off-by: Benjamin Strasser <55442329+benjaminstrasser@users.noreply.github.com>

--- sdk/nodejs/test/connect.spec.ts
+++ sdk/nodejs/test/connect.spec.ts
@@ -1,5 +1,5 @@
 import { connect } from "../connect.js"
-import assert from "assert"
+import assert, { AssertionError } from "assert"
 
 describe("NodeJS sdk", function () {
   it("Connect to local engine and execute a simple query to make sure it does not fail", async function () {
@@ -9,11 +9,28 @@ describe("NodeJS sdk", function () {
       const result = await client
         .container()
         .from("alpine")
-        .withExec(["apk", "add", "curl"])
-        .withExec(["curl", "https://dagger.io/"])
-        .stdout()
+        .exec(["apk", "add", "curl"])
+        .exec(["curl", "https://dagger.io/"])
+        .exitCode()
 
-      assert.ok(result.length > 10000)
+      assert.ok(result === 0)
     })
+  })
+
+  it("throws error", async function () {
+    this.timeout(60000)
+
+    try {
+      await connect(async (client) => {
+        await client.container().from("alpine").file("unknown_file").contents()
+
+        assert.fail("Should throw error before reaching this")
+      })
+    } catch (e) {
+      if (e instanceof AssertionError) {
+        throw e
+      }
+      assert(e instanceof Error)
+    }
   })
 })
