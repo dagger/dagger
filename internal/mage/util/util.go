155c4f5273da93eb3f4a3cc8f262421de5469b97
Gerhard Lazu <gerhard@dagger.io>
2022-11-28 16:57:59 +0000

    chore: Require DAGGER_ENGINE_IMAGE to be declared via env var
    We can test this locally or in a fork without worrying that this will
    publish to ghcr.io/dagger/engine
    
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>

--- internal/mage/util/util.go
+++ internal/mage/util/util.go
@@ -132,8 +132,8 @@ func HostDockerDir(c *dagger.Client) *dagger.Directory {
 
 func WithSetHostVar(ctx context.Context, h *dagger.Host, varName string) *dagger.HostVariable {
 	hv := h.EnvVariable(varName)
-	if val, err := hv.Secret().Plaintext(ctx); err != nil || val == "" {
-		fmt.Fprintf(os.Stderr, "env var %s is empty", varName)
+	if val, _ := hv.Secret().Plaintext(ctx); val == "" {
+		fmt.Fprintf(os.Stderr, "env var %s must be set", varName)
 		os.Exit(1)
 	}
 	return hv
