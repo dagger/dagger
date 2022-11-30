afe0db2a75532c8a30ee62f90bc3bc4e1b8200cb
Erik Sipsma <erik@sipsma.dev>
2022-11-30 08:13:51 -0800

    Update dagger run example w/ auth token.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- cmd/dagger/run.go
+++ cmd/dagger/run.go
@@ -18,10 +18,11 @@ var runCmd = &cobra.Command{
 	Use:                   "run [command]",
 	Aliases:               []string{"r"},
 	DisableFlagsInUseLine: true,
-	Long:                  "Runs the specified command in a Dagger session\n\nDAGGER_SESSION_URL will be convieniently injected automatically.",
+	Long:                  "Runs the specified command in a Dagger session\n\nDAGGER_SESSION_URL and DAGGER_SESSION_TOKEN will be convieniently injected automatically.",
 	Short:                 "Runs a command in a Dagger session",
 	Example: `
 dagger run -- sh -c 'curl \
+-u $DAGGER_SESSION_TOKEN \
 -H "content-type:application/json" \
 -d "{\"query\":\"{container{id}}\"}" $DAGGER_SESSION_URL'`,
 	Run:  Run,
