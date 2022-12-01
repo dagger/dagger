f8e329c5ab10f0d4d856d649ad605570b9dc9934
jffarge <jf@dagger.io>
2022-12-01 10:18:54 +0100

    sdk: nodejs: use let for const
    Signed-off-by: jffarge <jf@dagger.io>

--- docs/current/sdk/nodejs/snippets/get-started/step3/build.js
+++ docs/current/sdk/nodejs/snippets/get-started/step3/build.js
@@ -4,10 +4,10 @@ import { connect } from "@dagger.io/dagger"
 connect(async (client) => {
   // get Node image
   // get Node version
-  let node = client.container().from("node:16").withExec(["node", "-v"])
+  const node = client.container().from("node:16").withExec(["node", "-v"])
 
   // execute
-  let version = await node.stdout()
+  const version = await node.stdout()
 
   // print output
   console.log("Hello from Dagger and Node " + version)
