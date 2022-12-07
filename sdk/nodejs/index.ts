631b1185b90e86539c70b3ca75c91d93595ef99d
Tanguy ⧓ Herrmann <928722+dolanor@users.noreply.github.com>
2022-12-07 10:36:48 +0100

    doc(sdk/nodejs): export CallbackFct from connect.ts (#4091)
    otherwise, we have no idea what the callback should look like in the
    Reference documentation.
    
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- sdk/nodejs/index.ts
+++ sdk/nodejs/index.ts
@@ -3,7 +3,7 @@ import Client from "./api/client.gen.js"
 export { gql } from "graphql-tag"
 export { GraphQLClient } from "graphql-request"
 
-export { connect, ConnectOpts } from "./connect.js"
+export { connect, ConnectOpts, CallbackFct } from "./connect.js"
 export { getProvisioner } from "./provisioning/index.js"
 
 export default Client
