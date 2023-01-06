5de54a39afd56d7dd5aaf049b485557dbfba6dd1
Andrea Luzzardi <al@dagger.io>
2023-01-06 17:31:03 +0100

    WIP: Observability
    This is a test PR.
    
    Signed-off-by: Andrea Luzzardi <al@dagger.io>

---
+++ cmd/otel-collector/collector.go
@@ -0,0 +1,300 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strings"
+	"sync"
+	"time"
+
+	bkclient "github.com/moby/buildkit/client"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	otrace "go.opentelemetry.io/otel/trace"
+
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+)
+
+const (
+	tracer        = "dagger"
+	rootContextID = ""
+	rootSpanName  = "pipeline"
+)
+
+type OtelCollector struct {
+	tr otrace.Tracer
+
+	rootSpan        otrace.Span
+	vertexByID      map[string]*bkclient.Vertex
+	contextByVertex map[string]context.Context
+	contextByGroup  map[string]context.Context
+}
+
+func NewCollector() *OtelCollector {
+	return &OtelCollector{
+		vertexByID:      make(map[string]*bkclient.Vertex),
+		contextByVertex: make(map[string]context.Context),
+		contextByGroup:  make(map[string]context.Context),
+	}
+}
+
+func vertexGroup(v *bkclient.Vertex) []string {
+	group := []string{}
+	if groupID := v.ProgressGroup.GetId(); groupID != "" {
+		if err := json.Unmarshal([]byte(groupID), &group); err != nil {
+			panic(err)
+		}
+	}
+	return group
+}
+
+func (c *OtelCollector) Run(ctx context.Context, ch chan *bkclient.SolveStatus) error {
+	exp, err := newExporter()
+	if err != nil {
+		return err
+	}
+
+	tp := trace.NewTracerProvider(
+		trace.WithBatcher(exp),
+		trace.WithResource(newResource()),
+	)
+	defer func() {
+		_ = tp.Shutdown(context.Background())
+	}()
+	otel.SetTracerProvider(tp)
+
+	c.tr = otel.Tracer(tracer)
+
+	var rootCtx context.Context
+	wg := sync.WaitGroup{}
+	for ev := range ch {
+		for ev == nil {
+			break
+		}
+
+		for _, v := range ev.Vertexes {
+			// On very first event, start root span
+			if c.rootSpan == nil {
+				rootCtx, c.rootSpan = c.tr.Start(ctx,
+					rootSpanName,
+					otrace.WithTimestamp(*v.Started),
+				)
+				c.contextByGroup[rootContextID] = rootCtx
+			}
+
+			// Ignore vertex that haven't started yet
+			if v.Started == nil {
+				continue
+			}
+
+			id := v.Digest.String()
+			last := c.vertexByID[id]
+			if last == nil {
+				c.startSpan(v)
+				c.vertexByID[id] = v
+				last = v
+			}
+			vCtx := c.contextByVertex[id]
+			span := otrace.SpanFromContext(vCtx)
+			if v.Cached {
+				span.SetAttributes(attribute.Bool("cached", v.Cached))
+			}
+			if v.Completed != nil && last.Completed == nil {
+				// buildkit might decide later on this vertex was not completed.
+				// So we need to debounce -- wait 100ms, if we didn't receive any updates,
+				// then it's really completed.
+				v := v
+				wg.Add(1)
+				go func() {
+					defer wg.Done()
+					for {
+						time.Sleep(100 * time.Millisecond)
+						current := c.vertexByID[v.Digest.String()]
+						if v != current {
+							v = current
+							continue
+						}
+						span.End(otrace.WithTimestamp(*v.Completed))
+						break
+					}
+				}()
+			}
+			c.vertexByID[id] = v
+			fmt.Fprintf(os.Stderr, "%s => %s\n", span.SpanContext().TraceID(), span.SpanContext().SpanID())
+		}
+	}
+
+	wg.Wait()
+
+	c.endGroups()
+
+	return nil
+}
+
+func (c *OtelCollector) groupContext(v *bkclient.Vertex) context.Context {
+	group := vertexGroup(v)
+
+	ctx := c.contextByGroup[rootContextID]
+	for i := range group {
+		parent := strings.Join(group[0:i+1], "/")
+		parentCtx := c.contextByGroup[parent]
+		if parentCtx == nil {
+			parentCtx, _ = c.tr.Start(ctx, group[i], otrace.WithTimestamp(*v.Started))
+			c.contextByGroup[parent] = parentCtx
+		}
+		ctx = parentCtx
+	}
+
+	return ctx
+}
+
+func (c *OtelCollector) startSpan(v *bkclient.Vertex) {
+	id := v.Digest.String()
+	c.vertexByID[id] = v
+
+	// Register links for vertex inputs
+	links := []otrace.Link{}
+	for _, input := range v.Inputs {
+		inputCtx := c.contextByVertex[input.String()]
+		if inputCtx == nil {
+			fmt.Printf("input %s not found\n", input.String())
+			continue
+		}
+		inputLink := otrace.LinkFromContext(inputCtx)
+		links = append(links, inputLink)
+	}
+
+	groupCtx := c.groupContext(v)
+	vertexCtx, _ := c.tr.Start(
+		groupCtx,
+		v.Name,
+		otrace.WithTimestamp(*v.Started),
+		otrace.WithLinks(links...),
+	)
+	c.contextByVertex[id] = vertexCtx
+}
+
+func (c *OtelCollector) endGroups() {
+	// End all groups
+	for groupName, ctx := range c.contextByGroup {
+		group := strings.Split(groupName, "/")
+		if groupName == rootContextID {
+			group = []string{}
+		}
+
+		// Find the last completed vertex within the group and use that as group completion time
+		vertices := c.verticesForGroup(group)
+		fmt.Fprintf(os.Stderr, "group: %+v || vertices: %d\n", group, len(vertices))
+		var last *time.Time
+		for _, v := range vertices {
+			if last == nil || last.Before(*v.Completed) {
+				last = v.Completed
+			}
+		}
+
+		otrace.SpanFromContext(ctx).End(
+			otrace.WithTimestamp(*last),
+		)
+	}
+}
+
+func (c *OtelCollector) verticesForGroup(selector []string) []*bkclient.Vertex {
+	matches := []*bkclient.Vertex{}
+	for _, v := range c.vertexByID {
+		if matchGroup(v, selector) {
+			matches = append(matches, v)
+		}
+	}
+	return matches
+}
+
+func matchGroup(v *bkclient.Vertex, selector []string) bool {
+	group := vertexGroup(v)
+	if len(selector) > len(group) {
+		return false
+	}
+	for i, sel := range selector {
+		if group[i] != sel {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (c *OtelCollector) TraceID() string {
+	if c.rootSpan == nil {
+		return ""
+	}
+	return c.rootSpan.SpanContext().TraceID().String()
+}
+
+func (c *OtelCollector) Duration() time.Duration {
+	var (
+		first *time.Time
+		last  *time.Time
+	)
+	for _, v := range c.vertexByID {
+		if first == nil || first.After(*v.Started) {
+			first = v.Started
+		}
+		if last == nil || last.Before(*v.Completed) {
+			last = v.Completed
+		}
+	}
+
+	return last.Sub(*first)
+}
+
+func (c *OtelCollector) Breakdown() map[string]time.Duration {
+	breakdown := map[string]time.Duration{}
+	for groupName := range c.contextByGroup {
+		group := strings.Split(groupName, "/")
+		if groupName == rootContextID {
+			continue
+		}
+
+		vertices := c.verticesForGroup(group)
+		var (
+			first *time.Time
+			last  *time.Time
+		)
+		for _, v := range vertices {
+			if first == nil || first.After(*v.Started) {
+				first = v.Started
+			}
+			if last == nil || last.Before(*v.Completed) {
+				last = v.Completed
+			}
+		}
+
+		breakdown[groupName] = last.Sub(*first)
+	}
+
+	return breakdown
+}
+
+func newExporter() (trace.SpanExporter, error) {
+	return otlptracegrpc.New(context.Background())
+}
+
+func newResource() *resource.Resource {
+	r, err := resource.Merge(
+		resource.Default(),
+		resource.NewWithAttributes(
+			semconv.SchemaURL,
+			semconv.ServiceNameKey.String("dagger"),
+			semconv.ServiceVersionKey.String("v0.1.0"),
+			attribute.String("environment", "test"),
+		),
+	)
+	if err != nil {
+		panic(err)
+	}
+	return r
+}
---
+++ cmd/otel-collector/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+	"text/tabwriter"
+	"time"
+
+	bkclient "github.com/moby/buildkit/client"
+)
+
+const traceURL = "https://daggerboard.grafana.net/explore?orgId=1&left=%7B%22datasource%22:%22grafanacloud-traces%22,%22queries%22:%5B%7B%22refId%22:%22A%22,%22datasource%22:%7B%22type%22:%22tempo%22,%22uid%22:%22grafanacloud-traces%22%7D,%22queryType%22:%22traceId%22,%22query%22:%22{TRACE_ID}%22%7D%5D,%22range%22:%7B%22from%22:%22now-1h%22,%22to%22:%22now%22%7D%7D"
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Fprintf(os.Stderr, "Usage: %s <JOURNAL_FILE>\n", os.Args[0])
+		os.Exit(1)
+	}
+	ch := loadEvents(os.Args[1])
+	collector := NewCollector()
+	err := collector.Run(context.Background(), ch)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+
+	printSummary(os.Stdout, collector)
+}
+
+func printSummary(w io.Writer, collector *OtelCollector) {
+	duration := collector.Duration()
+	breakdown := collector.Breakdown()
+	groups := []string{}
+	for g := range breakdown {
+		groups = append(groups, g)
+	}
+	sort.Strings(groups)
+
+	fmt.Fprintf(w, "🚀 Dagger pipeline completed in **%s**\n\n", formatDuration(duration))
+	tw := tabwriter.NewWriter(w, 4, 4, 1, ' ', 0)
+	fmt.Fprintf(tw, "| **Pipeline** \t| **Duration** \t|\n")
+	fmt.Fprintf(tw, "| --- \t| --- \t|\n")
+	for _, g := range groups {
+		duration := breakdown[g]
+		fmt.Fprintf(tw, "| ✅ **%s** \t| %s \t|\n", strings.Join(strings.Split(g, "/"), " / "), formatDuration(duration))
+	}
+	tw.Flush()
+
+	url := strings.ReplaceAll(traceURL, "{TRACE_ID}", collector.TraceID())
+	fmt.Fprintf(w, "\n- 📈 [Explore traces](%s)\n", url)
+}
+
+func formatDuration(dur time.Duration) string {
+	prec := 1
+	sec := dur.Seconds()
+	if sec < 10 {
+		prec = 2
+	} else if sec < 100 {
+		prec = 1
+	}
+	return fmt.Sprintf("%.[2]*[1]fs", sec, prec)
+}
+
+func loadEvents(journal string) chan *bkclient.SolveStatus {
+	f, err := os.Open(journal)
+	if err != nil {
+		panic(err)
+	}
+
+	s := bufio.NewScanner(f)
+	s.Split(bufio.ScanLines)
+
+	ch := make(chan *bkclient.SolveStatus)
+	go func() {
+		defer close(ch)
+		for s.Scan() {
+			entry := struct {
+				Event *bkclient.SolveStatus
+				TS    int64
+			}{}
+
+			if err := json.Unmarshal(s.Bytes(), &entry); err != nil {
+				panic(err)
+			}
+
+			ch <- entry.Event
+		}
+	}()
+
+	return ch
+}
