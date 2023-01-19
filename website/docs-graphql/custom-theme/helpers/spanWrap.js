2fdbaa8b1b82cb7fab4f7b1d1089f7472ce0d616
Julián Cruciani <julian@dagger.io>
2023-01-19 12:29:15 -0300

    add type as id prefix
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- website/docs-graphql/custom-theme/helpers/spanWrap.js
+++ website/docs-graphql/custom-theme/helpers/spanWrap.js
@@ -4,7 +4,8 @@
 //   clazz (String)
 module.exports = function (value, options) {
     if (options?.hash?.isField) {
-        return `<a class="${options?.hash?.className || ''}" id="${options?.hash?.id || ""}" href="#${options?.hash?.id}">${value}</a>`
+        let id = `${options?.hash?.prefix}-${options?.hash?.id || ""}`
+        return `<a class="${options?.hash?.className || ''}" id="${id}" href="#${id}">${value}</a>`
     } else {
         return `<span class="${options?.hash?.className || ''}">${value}</span>` 
     }
