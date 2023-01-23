15ec87d0743c61323924620637ec5da497c7f2fd
jffarge <jf@dagger.io>
2023-01-23 17:19:40 +0100

    sdk: nodejs: Fix codegen tests
    Signed-off-by: jffarge <jf@dagger.io>

--- codegen/generator/nodejs/templates/src/testdata/objects_test_want.ts
+++ codegen/generator/nodejs/templates/src/testdata/objects_test_want.ts
@@ -17,6 +17,31 @@ export class CacheVolume extends BaseClient {
 
     return response
   }
+
+  /**
+  * Chain objects together
+  * @example
+  * ```ts
+  *	function AddAFewMounts(c) {
+  *			return c
+  *			.withMountedDirectory("/foo", new Client().host().directory("/Users/slumbering/forks/dagger"))
+  *			.withMountedDirectory("/bar", new Client().host().directory("/Users/slumbering/forks/dagger/sdk/nodejs"))
+  *	}
+  *
+  * connect(async (client) => {
+  *		const tree = await client
+  *			.container()
+  *			.from("alpine")
+  *			.withWorkdir("/foo")
+  *			.with(AddAFewMounts)
+  *			.withExec(["ls", "-lh"])
+  *			.stdout()
+  * })
+  *```
+  */
+  with(arg: (param: CacheVolume) => CacheVolume) {
+    return arg(this)
+  }
 }
 
 /**
@@ -74,5 +99,30 @@ export class Host extends BaseClient {
       host: this.clientHost,
       sessionToken: this.sessionToken,
     })
+  }
+
+  /**
+  * Chain objects together
+  * @example
+  * ```ts
+  *	function AddAFewMounts(c) {
+  *			return c
+  *			.withMountedDirectory("/foo", new Client().host().directory("/Users/slumbering/forks/dagger"))
+  *			.withMountedDirectory("/bar", new Client().host().directory("/Users/slumbering/forks/dagger/sdk/nodejs"))
+  *	}
+  *
+  * connect(async (client) => {
+  *		const tree = await client
+  *			.container()
+  *			.from("alpine")
+  *			.withWorkdir("/foo")
+  *			.with(AddAFewMounts)
+  *			.withExec(["ls", "-lh"])
+  *			.stdout()
+  * })
+  *```
+  */
+  with(arg: (param: Host) => Host) {
+    return arg(this)
   }
 }
