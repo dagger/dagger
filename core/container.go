e673f8c8b1d2f4e67b05e094b69cf96b65810c13
LokeshNSF <lokeshkumar.n@salesforce.com>
2023-01-26 21:47:41 +0530

    Merge container from env vars with existing env vars

--- core/container.go
+++ core/container.go
@@ -249,7 +249,13 @@ func (container *Container) From(ctx context.Context, gw bkgw.Client, addr strin
 		return nil, err
 	}
 
-	return ctr.UpdateImageConfig(ctx, func(specs.ImageConfig) specs.ImageConfig {
+	return ctr.UpdateImageConfig(ctx, func(config specs.ImageConfig) specs.ImageConfig {
+		// merge config.Env with imgSpec.Config.Env
+		newEnv := []string{}
+		if config.Env != nil {
+			newEnv = append(config.Env, imgSpec.Config.Env...)
+		}
+		imgSpec.Config.Env = newEnv
 		return imgSpec.Config
 	})
 }
