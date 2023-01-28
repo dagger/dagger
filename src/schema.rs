b63c44adedfa0c8ef4899198bea75a5744570a87
kjuulh <contact@kjuulh.io>
2023-01-28 13:36:19 +0100

    add todo for not dropping proc

--- src/schema.rs
+++ src/schema.rs
@@ -1,13 +1,11 @@
-use core::time;
-use std::thread::sleep;
-
 use graphql_introspection_query::introspection_response::IntrospectionResponse;
 
 use crate::{config::Config, engine::Engine, session::Session};
 
 pub fn get_schema() -> eyre::Result<IntrospectionResponse> {
-    //TODO: Implement cotext for proc
     let cfg = Config::new(None, None, None, None);
+
+    //TODO: Implement context for proc
     let (conn, proc) = Engine::new().start(&cfg)?;
     let session = Session::new();
     let req_builder = session.start(cfg, &conn)?;
