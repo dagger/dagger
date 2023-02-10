8282009fa0181a3596488e0fb2a6120a24baa1c9
Julián Cruciani <julian@dagger.io>
2023-02-10 11:46:53 -0300

    add responsiveness + use spinner in content embeds
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- website/src/components/molecules/quickstartDoc.js
+++ website/src/components/molecules/quickstartDoc.js
@@ -1,14 +1,10 @@
 import styles from "../../css/molecules/quickstartDoc.module.scss";
-import React, {useEffect, useState} from "react";
+import React from "react";
 import Tabs from "@theme/Tabs";
 import TabItem from "@theme/TabItem";
+import Embed from "../atoms/embed";
 
 const QuickstartDoc = ({children, embeds}) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    console.log(loading);
-  }, [loading]);
 
   return (
     <>
@@ -19,16 +15,7 @@ const QuickstartDoc = ({children, embeds}) => {
             {Object.keys(embeds).map((x, index) => {
               return (
                 <TabItem key={x} value={x}>
-                  {loading ? (
-                    <div className={styles.spinnerWrapper}>
-                      <div className={styles.spinner}></div>
-                    </div>
-                  ) : null}
-                  <iframe
-                    onLoad={() => setLoading(false)}
-                    style={{display: loading ? "hidden" : "inherit"}}
-                    loading={index === 0 ? "eager" : "lazy"}
-                    src={`https://play.dagger.cloud/embed/${embeds[x]}`}></iframe>
+                  <Embed id={embeds[x]} index={index}/>
                 </TabItem>
               );
             })}
