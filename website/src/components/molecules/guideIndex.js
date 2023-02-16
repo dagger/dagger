840272bd06e3d7160eed27f24438683bf3acd2bf
Julián Cruciani <julian@dagger.io>
2023-02-16 13:55:36 -0300

    add guide filtering by tags + set url query string
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- website/src/components/molecules/guideIndex.js
+++ website/src/components/molecules/guideIndex.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Link from "@docusaurus/Link";
 import styles from "@site/src/css/molecules/guideIndex.module.scss";
 import guidesJSON from "@site/static/guides.json";
@@ -6,21 +6,78 @@ import Tag from "../atoms/tag";
 
 export default function GuidesIndex() {
   const guides = guidesJSON.sort((a, b) => {
-    return b.timestamp - a.timestamp
-  })
+    return b.timestamp - a.timestamp;
+  });
+
+  const [filteredGuides, setFilteredGuides] = useState(guides);
+  const [selectedTags, setSelectedTags] = useState([]);
+
+  useEffect(() => {
+    if (selectedTags.length > 0) {
+      setFilteredGuides(
+        guides.filter((item) =>
+          selectedTags.every((tag) => item.frontMatter.tags.includes(tag)),
+        ),
+      );
+    } else {
+      setFilteredGuides(guides);
+    }
+  }, [selectedTags]);
+
+  useEffect(() => {
+    const tagURLParams = new URL(document.location).searchParams.getAll("tags");
+    tagURLParams && setSelectedTags([...tagURLParams]);
+  }, []);
+
+  const pushTag = (tag) => {
+    if (!selectedTags.some((x) => x === tag)) {
+      if ("URLSearchParams" in window) {
+        var searchParams = new URLSearchParams(window.location.search);
+        searchParams.append("tags", tag);
+        var newRelativePathQuery =
+          window.location.pathname + "?" + searchParams.toString();
+        history.pushState(null, "", newRelativePathQuery);
+      }
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const popTag = (tag) => {
+    if ("URLSearchParams" in window) {
+      var searchParams = new URLSearchParams(window.location.search);
+      const path = window.location.pathname;
+      let allSearchParams = searchParams.getAll("tags");
+      if (allSearchParams.length === 1) {
+        searchParams.delete("tags");
+        let newRelativePathQuery = path;
+        history.pushState(null, "", newRelativePathQuery);
+      } else {
+        searchParams.delete("tags")
+        allSearchParams.forEach(x => x != tag ? searchParams.append("tags", x) : null)
+        let newRelativePathQuery = path + "?" + searchParams.toString();
+        history.pushState(null, "", newRelativePathQuery);
+      }
+    }
+    setSelectedTags(selectedTags.filter((x) => x != tag));
+  };
 
   return (
     <div className={styles.guideIndex}>
+      <div className={styles.selectedTags}>
+        {selectedTags.map((x, i) => (
+          <Tag key={i} label={x} onCloseClick={() => popTag(x)} removable></Tag>
+        ))}
+      </div>
       <ul>
-        {guides.map((x, i) => (
+        {filteredGuides.map((x, i) => (
           <li key={i}>
             <GuideCard
               title={x.contentTitle}
-              description={x.contentTitle}
               url={x.frontMatter.slug}
               tags={x.frontMatter.tags}
               authors={x.frontMatter.authors}
               timestamp={x.timestamp}
+              pushTag={pushTag}
             />
           </li>
         ))}
@@ -29,14 +86,16 @@ export default function GuidesIndex() {
   );
 }
 
-function GuideCard({title, description, url, tags, authors, timestamp}) {
+function GuideCard({title, url, tags, authors, timestamp, pushTag}) {
   const handleAuthors = () => {
     let authorsString = "";
     authors.forEach((x) => (authorsString += `, ${x}`));
     return `By ${authorsString.slice(1)}`;
   };
-  const dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
-  const date = new Date(timestamp).toLocaleDateString('en-US', dateOptions)
+
+  const dateOptions = {year: "numeric", month: "long", day: "numeric"};
+  const date = new Date(timestamp).toLocaleDateString("en-US", dateOptions);
+
   return (
     <div className={styles.guideCard}>
       <div className={styles.info}>
@@ -45,10 +104,12 @@ function GuideCard({title, description, url, tags, authors, timestamp}) {
         </Link>
         {date && <time>{date}</time>}
         {authors && <h4 className={styles.author}>{handleAuthors()}</h4>}
-        {/* <p>{description}</p> */}
       </div>
       <div className={styles.tags}>
-        {tags && tags.map((x, i) => <Tag key={i} label={x} />)}
+        {tags &&
+          tags.map((x, i) => (
+            <Tag onTagClick={() => pushTag(x)} key={i} label={x} />
+          ))}
       </div>
     </div>
   );
