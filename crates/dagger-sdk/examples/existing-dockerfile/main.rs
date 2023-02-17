0cbd1790b0b4030c68f0a0dd619325da26f14f60
kjuulh <contact@kjuulh.io>
2023-02-17 17:50:45 +0100

    add with dockerfile

---
+++ crates/dagger-sdk/examples/existing-dockerfile/main.rs
@@ -0,0 +1,24 @@
+use dagger_sdk::gen::HostDirectoryOpts;
+use rand::Rng;
+
+fn main() -> eyre::Result<()> {
+    let mut rng = rand::thread_rng();
+
+    let client = dagger_sdk::client::connect()?;
+
+    let context_dir = client
+        .host()
+        .directory("./examples/existing-dockerfile/app".into(), None);
+
+    let ref_ = client
+        .container(None)
+        .build(context_dir.id(), None)
+        .publish(
+            format!("ttl.sh/hello-dagger-rs-{}:1h", rng.gen::<u64>()),
+            None,
+        );
+
+    println!("published image to: {}", ref_);
+
+    Ok(())
+}
