b947b3fde759548b78062fdb4172045614b93abe
Alex Suraci <alex@dagger.io>
2023-02-22 12:01:29 -0500

    remove TODOs
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- internal/mage/engine.go
+++ internal/mage/engine.go
@@ -210,9 +210,6 @@ func (t Engine) test(ctx context.Context, race bool) error {
 		WithEnvVariable("CGO_ENABLED", cgoEnabledEnv).
 		WithMountedDirectory("/root/.docker", util.HostDockerDir(c)).
 		WithExec(args).
-		// TODO(vito): troubleshooting scheduler errors in CI, remove before
-		// merging
-		WithExec([]string{"go", "test", "-c", "./core/integration", "-run", "ContainerWithServiceFile/copying", "-count=100"}).
 		Stdout(ctx)
 	if err != nil {
 		return err
@@ -376,9 +373,6 @@ func devEngineContainer(c *dagger.Client, arches []string) []*dagger.Container {
 				Contents:    engineEntrypoint,
 				Permissions: 755,
 			}).
-			// TODO(vito): troubleshooting scheduler errors in CI, remove before
-			// merging
-			WithEnvVariable("BUILDKIT_SCHEDULER_DEBUG", "1").
 			WithEntrypoint([]string{"dagger-entrypoint.sh"}),
 		)
 	}
