fb86fbb73db2cc866ee1e98f4c72329e15eeb2e3
Vikram Vaswani <vikram@dagger.io>
2023-03-07 00:50:20 +0530

    docs: Linter fix
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/guides/snippets/use-services/test-service-lifecycle-2/main.go
+++ docs/current/guides/snippets/use-services/test-service-lifecycle-2/main.go
@@ -1,55 +1,55 @@
 package main
 
 import (
-  "context"
-  "fmt"
-  "os"
+	"context"
+	"fmt"
+	"os"
 
-  "dagger.io/dagger"
+	"dagger.io/dagger"
 )
 
 func main() {
-  ctx := context.Background()
+	ctx := context.Background()
 
-  // create Dagger client
-  client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	// create Dagger client
+	client, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
 
-  if err != nil {
-    panic(err)
-  }
-  defer client.Close()
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
 
-  // create Redis service container
-  redisSrv := client.Container().
-    From("redis").
-    WithExposedPort(6379).
-    WithExec(nil)
+	// create Redis service container
+	redisSrv := client.Container().
+		From("redis").
+		WithExposedPort(6379).
+		WithExec(nil)
 
-  // create Redis client container
-  redisCLI := client.Container().
-    From("redis").
-    WithServiceBinding("redis-srv", redisSrv).
-    WithEntrypoint([]string{"redis-cli", "-h", "redis-srv"})
+	// create Redis client container
+	redisCLI := client.Container().
+		From("redis").
+		WithServiceBinding("redis-srv", redisSrv).
+		WithEntrypoint([]string{"redis-cli", "-h", "redis-srv"})
 
-  // set value
-  setter, err1 := redisCLI.
-    WithExec([]string{"set", "foo", "abc"}).
-    Stdout(ctx)
+	// set value
+	setter, err1 := redisCLI.
+		WithExec([]string{"set", "foo", "abc"}).
+		Stdout(ctx)
 
-  if err1 != nil {
-    panic(err1)
-  }
+	if err1 != nil {
+		panic(err1)
+	}
 
-  fmt.Println(setter)
+	fmt.Println(setter)
 
-  // get value
-  getter, err2 := redisCLI.
-    WithExec([]string{"get", "foo"}).
-    Stdout(ctx)
+	// get value
+	getter, err2 := redisCLI.
+		WithExec([]string{"get", "foo"}).
+		Stdout(ctx)
 
-  if err2 != nil {
-    panic(err2)
-  }
+	if err2 != nil {
+		panic(err2)
+	}
 
-  fmt.Println(getter)
+	fmt.Println(getter)
 }
