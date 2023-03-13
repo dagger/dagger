da4c10dd7b4ee6cec24dd5cb30c7126d1b195bdb
Julián Cruciani <julian@dagger.io>
2023-03-13 15:17:27 -0300

    make embed client-side rendered
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- website/src/components/atoms/embed.js
+++ website/src/components/atoms/embed.js
@@ -1,23 +1,32 @@
 import styles from "../../css/atoms/embed.module.scss";
 import React, {useState} from "react";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 const Embed = ({id, index}) => {
   const [loading, setLoading] = useState(true);
 
   return (
-    <div className={styles.embedWrapper} id="embedWrapper">
-      {loading && (
-        <div className={styles.spinnerWrapper}>
-          <div className={styles.spinner}></div>
+    <BrowserOnly>
+      {() => (
+        <div className={styles.embedWrapper} id="embedWrapper">
+          {loading && (
+            <div className={styles.spinnerWrapper}>
+              <div className={styles.spinner}></div>
+            </div>
+          )}
+          <iframe
+            className={styles.embed}
+            onLoad={() => setLoading(false)}
+            style={{display: loading ? "hidden" : "inherit"}}
+            loading={index === 0 || !index ? "eager" : "lazy"}
+            src={`${
+              window.location.origin.includes("localhost")
+                ? "https://play.dagger.cloud"
+                : window.location.origin
+            }/embed/${id}`}></iframe>
         </div>
       )}
-      <iframe
-        className={styles.embed}
-        onLoad={() => setLoading(false)}
-        style={{display: loading ? "hidden" : "inherit"}}
-        loading={(index === 0 || !index) ? "eager" : "lazy"}
-        src={`${window.location.origin}/embed/${id}`}></iframe>
-    </div>
+    </BrowserOnly>
   );
 };
 
