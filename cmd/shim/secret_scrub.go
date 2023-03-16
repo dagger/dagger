441b30f3264b5d3824c413f6ad61407afaefe39b
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2023-03-16 11:04:05 +0100

    feat: scrub secrets with whitespace (WIP)
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- cmd/shim/secret_scrub.go
+++ cmd/shim/secret_scrub.go
@@ -7,6 +7,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"unicode/utf8"
 
 	"github.com/dagger/dagger/core"
 )
@@ -14,9 +15,11 @@ import (
 // SecretScrubWriter is a writer that will scrub secretValues before writing to the underlying writer.
 // It is safe to write to it concurrently.
 type SecretScrubWriter struct {
-	mu           sync.Mutex
-	w            io.Writer
+	mu sync.Mutex
+	w  io.Writer
+
 	secretValues []string
+	dangling     []byte
 }
 
 // Write scrubs secret values from b and replace them with `***`.
@@ -25,6 +28,7 @@ func (w *SecretScrubWriter) Write(b []byte) (int, error) {
 	defer w.mu.Unlock()
 
 	s := string(b)
+
 	for _, secret := range w.secretValues {
 		if secret == "" {
 			continue
@@ -39,6 +43,72 @@ func (w *SecretScrubWriter) Write(b []byte) (int, error) {
 	}
 
 	return len(b), err
+}
+
+func (w *SecretScrubWriter) WriteNew(b []byte) (int, error) {
+	// FIXME: TDD the implementation
+	return 0, nil
+}
+
+func (w *SecretScrubWriter) WriteConcourseWIP(b []byte) (int, error) {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	var buf []byte
+
+	if b != nil {
+		buf = w.cacheDangling(b)
+		if buf == nil {
+			return len(b), nil
+		}
+	} else {
+		if w.dangling == nil || len(w.dangling) == 0 {
+			return 0, nil
+		}
+		buf = w.dangling
+	}
+
+	text := string(buf)
+	if b != nil {
+		i := strings.LastIndex(text, "\n")
+		if i >= 0 && i < len(text) {
+			// Cache content after the last new-line, and proceed contents
+			// before the last new-line.
+			w.dangling = []byte(text[i+1:])
+			text = text[:i+1]
+		} else {
+			// No new-line found, then cache the log.
+			w.dangling = buf
+			return len(b), nil
+		}
+	}
+
+	for _, secret := range w.secretValues {
+		if secret == "" {
+			continue
+		}
+		// FIXME: I think we can do better
+		text = strings.ReplaceAll(text, secret, "***")
+	}
+
+	_, err := w.w.Write([]byte(text))
+	if err != nil {
+		return -1, err
+	}
+
+	return len(b), err
+}
+
+func (w *SecretScrubWriter) cacheDangling(b []byte) []byte {
+	text := append(w.dangling, b...)
+
+	checkEncoding, _ := utf8.DecodeLastRune(text)
+	if checkEncoding == utf8.RuneError {
+		w.dangling = text
+		return nil
+	}
+
+	w.dangling = nil
+	return text
 }
 
 // NewSecretScrubWriter replaces known secrets by "***".
