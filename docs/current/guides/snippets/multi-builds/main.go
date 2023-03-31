ebfa20751b27c19e71e16f07dbc7aa343805ec0f
Vikram Vaswani <vikram@dagger.io>
2023-03-31 19:03:37 +0530

    Lint fix
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/guides/snippets/multi-builds/main.go
+++ docs/current/guides/snippets/multi-builds/main.go
@@ -2,63 +2,63 @@
 package main
 
 import (
-    "context"
-    "fmt"
-    "os"
+	"context"
+	"fmt"
+	"os"
 
-    "dagger.io/dagger"
+	"dagger.io/dagger"
 )
 
 func main() {
-    println("Building with Dagger")
+	println("Building with Dagger")
 
-    // define build matrix
-    geese := []string{"linux", "darwin"}
-    goarches := []string{"amd64", "arm64"}
+	// define build matrix
+	geese := []string{"linux", "darwin"}
+	goarches := []string{"amd64", "arm64"}
 
-    ctx := context.Background()
-    // initialize dagger client
-    c, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
-    if err != nil {
-        panic(err)
-    }
+	ctx := context.Background()
+	// initialize dagger client
+	c, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		panic(err)
+	}
 
-    // get reference to the local project
-    src := c.Host().Directory(".")
+	// get reference to the local project
+	src := c.Host().Directory(".")
 
-    // create empty directory to put build outputs
-    outputs := c.Directory()
+	// create empty directory to put build outputs
+	outputs := c.Directory()
 
-    golang := c.Container().
-        // get golang image
-        From("golang:latest").
-        // mount source code into golang image
-        WithMountedDirectory("/src", src).
-        WithWorkdir("/src")
+	golang := c.Container().
+		// get golang image
+		From("golang:latest").
+		// mount source code into golang image
+		WithMountedDirectory("/src", src).
+		WithWorkdir("/src")
 
-    for _, goos := range geese {
-        for _, goarch := range goarches {
-            // create a directory for each OS and architecture
-            path := fmt.Sprintf("build/%s/%s/", goos, goarch)
+	for _, goos := range geese {
+		for _, goarch := range goarches {
+			// create a directory for each OS and architecture
+			path := fmt.Sprintf("build/%s/%s/", goos, goarch)
 
-            build := golang.
-                // set GOARCH and GOOS in the build environment
-                WithEnvVariable("GOOS", goos).
-                WithEnvVariable("GOARCH", goarch).
-                WithExec([]string{"go", "build", "-o", path})
+			build := golang.
+				// set GOARCH and GOOS in the build environment
+				WithEnvVariable("GOOS", goos).
+				WithEnvVariable("GOARCH", goarch).
+				WithExec([]string{"go", "build", "-o", path})
 
-            // add build to outputs
-            outputs = outputs.WithDirectory(path, build.Directory(path))
-        }
-    }
+			// add build to outputs
+			outputs = outputs.WithDirectory(path, build.Directory(path))
+		}
+	}
 
-    // write build artifacts to host
-    ok, err := outputs.Export(ctx, ".")
-    if err != nil {
-        panic(err)
-    }
+	// write build artifacts to host
+	ok, err := outputs.Export(ctx, ".")
+	if err != nil {
+		panic(err)
+	}
 
-    if !ok {
-        panic("did not export files")
-    }
+	if !ok {
+		panic("did not export files")
+	}
 }
