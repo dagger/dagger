cb0ab536c09d9870c4ef7fa9b8daedd6fa2743cc
grouville <guillaume@dagger.io>
2023-04-06 16:37:09 +0200

    chore: fix linting issue Python SDK + other failing tests
    - fp.register() line was too. Split to avoid triggering RUFF linting issue
    - Fix other failing tests: test_stderr and test_cli_exec_errors
    
    Signed-off-by: grouville <guillaume@dagger.io>

--- sdk/python/tests/engine/test_cli.py
+++ sdk/python/tests/engine/test_cli.py
@@ -11,7 +11,8 @@ from dagger.exceptions import ProvisionError
 
 def test_getting_connect_params(fp: FakeProcess):
     fp.register(
-        ["dagger", "session", "--label" ,"sdk:python","--label", "sdk_version:0.0.0", "--label", "sdk_async:true"],
+        ["dagger", "session", "--label" ,"sdk:python","--label",
+         "sdk_version:0.0.0", "--label", "sdk_async:true"],
         stdout=['{"port":50004,"session_token":"abc"}', ""],
     )
     with cli.CLISession(dagger.Config(), "dagger") as conn:
@@ -41,7 +42,8 @@ def test_getting_connect_params(fp: FakeProcess):
 )
 def test_cli_exec_errors(config_args: dict, call_kwargs: dict, fp: FakeProcess):
     fp.register(
-        ["dagger", "session"],
+       ["dagger", "session", "--label" ,"sdk:python","--label",
+         "sdk_version:0.0.0", "--label", "sdk_async:true"],
         **call_kwargs,
     )
     with pytest.raises(
@@ -56,7 +58,8 @@ def test_cli_exec_errors(config_args: dict, call_kwargs: dict, fp: FakeProcess):
 
 def test_stderr(fp: FakeProcess):
     fp.register(
-        ["dagger", "session"],
+        ["dagger", "session", "--label" ,"sdk:python","--label",
+         "sdk_version:0.0.0", "--label", "sdk_async:true"],
         stderr=["Error: buildkit failed to respond", ""],
         returncode=1,
     )
