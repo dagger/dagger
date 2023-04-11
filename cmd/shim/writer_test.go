b1c0e942a8ac197f1ff8abe19ad77325e54501f4
Tanguy ⧓ Herrmann <tanguy@dagger.io>
2023-04-11 12:50:24 +0200

    add linebreak writer
    Signed-off-by: Tanguy ⧓ Herrmann <tanguy@dagger.io>

--- cmd/shim/writer_test.go
+++ cmd/shim/writer_test.go
@@ -44,3 +44,43 @@ func TestUTF8DanglingWriter(t *testing.T) {
 		w.w.(*bytes.Buffer).Reset()
 	}
 }
+
+func TestLineBreakWriter(t *testing.T) {
+	cases := []struct {
+		s     string
+		split int
+	}{
+		{"Forêt\nArbre\nLol\n", 4},
+		{"Ah !\n Ça ira,\n ça ira !\n", 7},
+		{"🇫🇷\n🇺🇾\n🇦🇷\n🇺🇸\n🇵🇹\n🇮🇳\n🇨🇦\n🇬🇧\n", 2},
+	}
+	buf := bytes.Buffer{}
+	w := NewLineBreakWriter(&buf)
+
+	for _, c := range cases {
+		prev := 0
+		next := c.split
+		for {
+			if next > len(c.s) {
+				next = len(c.s)
+			}
+
+			t.Logf("next: %d, buf: %q", next, w.buffer)
+			s := c.s[prev:next]
+
+			n, err := w.Write([]byte(s))
+			require.NoError(t, err)
+			require.Equal(t, len(s), n)
+			t.Log("s:", s)
+
+			if next == len(c.s) {
+				break
+			}
+
+			prev = next
+			next += c.split
+		}
+		require.Equal(t, c.s, buf.String())
+		buf.Reset()
+	}
+}
