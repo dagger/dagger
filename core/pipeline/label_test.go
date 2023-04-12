c3393d1118f38d1f2d93d29e5a972633064c1771
Alex Suraci <alex@dagger.io>
2023-04-11 20:03:38 -0400

    collect github.com/pr.branch label
    Also: avoid putting a useless 'HEAD' value in dagger.io/git.branch when
    running on a detached HEAD.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/pipeline/label_test.go
+++ core/pipeline/label_test.go
@@ -2,12 +2,133 @@ package pipeline_test
 
 import (
 	"os"
+	"os/exec"
 	"strings"
 	"testing"
 
 	"github.com/dagger/dagger/core/pipeline"
 	"github.com/stretchr/testify/require"
 )
+
+func run(t *testing.T, exe string, args ...string) string {
+	t.Helper()
+	cmd := exec.Command(exe, args...)
+	cmd.Stderr = os.Stderr
+	out, err := cmd.Output()
+	require.NoError(t, err)
+	return strings.TrimSpace(string(out))
+}
+
+func setupRepo(t *testing.T) string {
+	repo := t.TempDir()
+	run(t, "git", "-C", repo, "init")
+	run(t, "git", "-C", repo, "config", "--local", "--add", "user.name", "Test User")
+	run(t, "git", "-C", repo, "config", "--local", "--add", "user.email", "test@example.com")
+	run(t, "git", "-C", repo, "remote", "add", "origin", "https://example.com")
+	run(t, "git", "-C", repo, "checkout", "-b", "main")
+	run(t, "git", "-C", repo, "commit", "--allow-empty", "-m", "init")
+	return repo
+}
+
+func TestLoadGitLabels(t *testing.T) {
+	normalRepo := setupRepo(t)
+	repoHead := run(t, "git", "-C", normalRepo, "rev-parse", "HEAD")
+
+	detachedRepo := setupRepo(t)
+	run(t, "git", "-C", detachedRepo, "commit", "--allow-empty", "-m", "second")
+	run(t, "git", "-C", detachedRepo, "commit", "--allow-empty", "-m", "third")
+	run(t, "git", "-C", detachedRepo, "checkout", "HEAD~2")
+	run(t, "git", "-C", detachedRepo, "merge", "main")
+	detachedHead := run(t, "git", "-C", detachedRepo, "rev-parse", "HEAD")
+
+	type Example struct {
+		Name   string
+		Repo   string
+		Labels []pipeline.Label
+	}
+
+	for _, example := range []Example{
+		{
+			Name: "normal branch state",
+			Repo: normalRepo,
+			Labels: []pipeline.Label{
+				{
+					Name:  "dagger.io/git.remote",
+					Value: "example.com",
+				},
+				{
+					Name:  "dagger.io/git.branch",
+					Value: "main",
+				},
+				{
+					Name:  "dagger.io/git.ref",
+					Value: repoHead,
+				},
+				{
+					Name:  "dagger.io/git.author.name",
+					Value: "Test User",
+				},
+				{
+					Name:  "dagger.io/git.author.email",
+					Value: "test@example.com",
+				},
+				{
+					Name:  "dagger.io/git.committer.name",
+					Value: "Test User",
+				},
+				{
+					Name:  "dagger.io/git.committer.email",
+					Value: "test@example.com",
+				},
+				{
+					Name:  "dagger.io/git.title",
+					Value: "init",
+				},
+			},
+		},
+		{
+			Name: "detached HEAD state",
+			Repo: detachedRepo,
+			Labels: []pipeline.Label{
+				{
+					Name:  "dagger.io/git.remote",
+					Value: "example.com",
+				},
+				{
+					Name:  "dagger.io/git.ref",
+					Value: detachedHead,
+				},
+				{
+					Name:  "dagger.io/git.author.name",
+					Value: "Test User",
+				},
+				{
+					Name:  "dagger.io/git.author.email",
+					Value: "test@example.com",
+				},
+				{
+					Name:  "dagger.io/git.committer.name",
+					Value: "Test User",
+				},
+				{
+					Name:  "dagger.io/git.committer.email",
+					Value: "test@example.com",
+				},
+				{
+					Name:  "dagger.io/git.title",
+					Value: "third",
+				},
+			},
+		},
+	} {
+		example := example
+		t.Run(example.Name, func(t *testing.T) {
+			labels, err := pipeline.LoadGitLabels(example.Repo)
+			require.NoError(t, err)
+			require.ElementsMatch(t, example.Labels, labels)
+		})
+	}
+}
 
 func TestLoadGitHubLabels(t *testing.T) {
 	type Example struct {
@@ -104,6 +225,10 @@ func TestLoadGitHubLabels(t *testing.T) {
 				{
 					Name:  "github.com/pr.url",
 					Value: "https://github.com/dagger/testdata/pull/2018",
+				},
+				{
+					Name:  "github.com/pr.branch",
+					Value: "dump-env",
 				},
 				{
 					Name:  "github.com/pr.head",
