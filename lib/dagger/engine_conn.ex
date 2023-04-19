2d50ad1ab84f58c2bbf0e61b406673e2d7d0ac39
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-04-19 13:47:32 +0700

    Handle error when fetching Dagger session

--- lib/dagger/engine_conn.ex
+++ lib/dagger/engine_conn.ex
@@ -6,24 +6,34 @@ defmodule Dagger.EngineConn do
   @doc """
   Get Dagger engine connection.
   """
+  # TODO: handle error.
   # TODO: plan b: construct conn by using local cli.
   # TODO: plan c: construct conn by downloading cli.
   def get() do
     from_session_env()
   end
 
+  @doc """
+  Getting Dagger connection from environment variables.
+  """
   def from_session_env() do
-    port = System.get_env("DAGGER_SESSION_PORT")
-    token = System.get_env("DAGGER_SESSION_TOKEN")
-
-    {:ok,
-     %__MODULE__{
-       port: port,
-       token: token
-     }}
+    with {:ok, port} <- System.fetch_env("DAGGER_SESSION_PORT"),
+         {:ok, token} <- System.fetch_env("DAGGER_SESSION_TOKEN") do
+      {:ok,
+       %__MODULE__{
+         port: port,
+         token: token
+       }}
+    end
   end
 
+  @doc """
+  Constructing host connection.
+  """
   def host(%__MODULE__{port: port}), do: "127.0.0.1:#{port}"
 
+  @doc """
+  Get the token.
+  """
   def token(%__MODULE__{token: token}), do: token
 end
