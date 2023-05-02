bd68323ef9aa789d18ab76aaeac47f5a0eca4ba6
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-05-02 22:23:40 +0700

    Experiment with_session/2 in mix ci.test

--- lib/mix/tasks/ci.test.ex
+++ lib/mix/tasks/ci.test.ex
@@ -19,32 +19,30 @@ defmodule Mix.Tasks.Ci.Test do
 
     elixir_image = "hexpm/elixir:1.14.4-erlang-25.3-debian-buster-20230227-slim"
 
-    client = Dagger.connect!()
+    with_session(fn client ->
+      repo =
+        client
+        |> Dagger.Query.host()
+        |> Dagger.Host.directory(".", exclude: @ignore)
 
-    repo =
-      client
-      |> Dagger.Query.host()
-      |> Dagger.Host.directory(".", exclude: @ignore)
+      base_image =
+        client
+        |> Dagger.Query.pipeline("Prepare")
+        |> Dagger.Query.container()
+        |> Dagger.Container.from(elixir_image)
+        |> Dagger.Container.with_mounted_directory("/dagger_ex", repo)
+        |> Dagger.Container.with_workdir("/dagger_ex")
+        |> Dagger.Container.with_exec(["mix", "local.rebar", "--force"])
+        |> Dagger.Container.with_exec(["mix", "local.hex", "--force"])
+        |> Dagger.Container.with_env_variable("MIX_ENV", "test")
+        |> Dagger.Container.with_exec(["mix", "deps.get"])
 
-    base_image =
-      client
-      |> Dagger.Query.pipeline("Prepare")
-      |> Dagger.Query.container()
-      |> Dagger.Container.from(elixir_image)
-      |> Dagger.Container.with_mounted_directory("/dagger_ex", repo)
-      |> Dagger.Container.with_workdir("/dagger_ex")
-      |> Dagger.Container.with_exec(["mix", "local.rebar", "--force"])
-      |> Dagger.Container.with_exec(["mix", "local.hex", "--force"])
-      |> Dagger.Container.with_env_variable("MIX_ENV", "test")
-      |> Dagger.Container.with_exec(["mix", "deps.get"])
-
-    [
-      Task.async(__MODULE__, :test, [base_image]),
-      Task.async(__MODULE__, :check_format, [base_image])
-    ]
-    |> Task.await_many(:timer.seconds(180))
-
-    Dagger.disconnect(client)
+      [
+        Task.async(__MODULE__, :test, [base_image]),
+        Task.async(__MODULE__, :check_format, [base_image])
+      ]
+      |> Task.await_many(:timer.seconds(180))
+    end)
   end
 
   def test(base_image) do
@@ -61,5 +59,13 @@ defmodule Mix.Tasks.Ci.Test do
     |> Dagger.Container.with_exec(["mix", "format", "--check-formatted"])
     |> Dagger.Container.stdout()
     |> IO.puts()
+  end
+
+  def with_session(fun, opts \\ []) when is_function(fun, 1) do
+    with {:ok, client} <- Dagger.connect(opts) do
+      result = fun.(client)
+      Dagger.disconnect(client)
+      result
+    end
   end
 end
