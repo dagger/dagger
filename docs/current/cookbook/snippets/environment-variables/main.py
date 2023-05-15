0864d4c15197777b08f101f030f5ad4b6df32638
grouville <guillaume@dagger.io>
2023-05-15 17:34:09 +0200

    docs: withEnvVariables examples
    This PR introduces a new utility function `env_variables` that can be used to set multiple environment variables.
    It relies on the `with()` helper that users can rely on to implement their own API wrapper
    
    Signed-off-by: grouville <guillaume@dagger.io>

---
+++ docs/current/cookbook/snippets/environment-variables/main.py
@@ -0,0 +1,35 @@
+import sys
+
+import anyio
+import dagger
+
+async def main():
+    config = dagger.Config(log_output=sys.stderr)
+
+    # create Dagger client
+    async with dagger.Connection(config) as client:
+        # setup container and
+        # define environment variables
+        ctr = (
+            client
+            .container()
+            .from_("alpine")
+            .with_(env_variables({
+                "ENV_VAR_1": "VALUE 1",
+                "ENV_VAR_2": "VALUE 2",
+                "ENV_VAR_3": "VALUE 3",
+            }))
+            .with_exec(["env"])
+        )
+
+        # print environment variables
+        print(await ctr.stdout())
+
+anyio.run(main)
+
+def env_variables(envs: dict[str, str]):
+    def env_variables_inner(ctr: dagger.Container):
+        for key, value in envs.items():
+            ctr = ctr.with_env_variable(key, value)
+        return ctr
+    return env_variables_inner
