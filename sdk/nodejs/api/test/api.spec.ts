0c1d7fc40f99c43a1e0b33107d8513417c928706
Jffarge <jf@dagger.io>
2023-05-17 20:38:43 +0200

    NodeJS SDK - Export Container type (#5141)
    * sdk: nodejs: Export Container type
    
    Signed-off-by: jffarge <jf@dagger.io>
    
    * Export every exportable type from client.gen
    
    Signed-off-by: jffarge <jf@dagger.io>
    
    * sdk: nodejs: Fix import Client for tests
    
    Signed-off-by: jffarge <jf@dagger.io>
    
    * fix imports in API test and index.ts
    
    Signed-off-by: Julián Cruciani <julian@dagger.io>
    
    ---------
    
    Signed-off-by: jffarge <jf@dagger.io>
    Signed-off-by: Julián Cruciani <julian@dagger.io>
    Co-authored-by: Julián Cruciani <julian@dagger.io>

--- sdk/nodejs/api/test/api.spec.ts
+++ sdk/nodejs/api/test/api.spec.ts
@@ -3,8 +3,12 @@ import { randomUUID } from "crypto"
 import fs from "fs"
 
 import { TooManyNestedObjectsError } from "../../common/errors/index.js"
-import Client, { connect } from "../../index.js"
-import { ClientContainerOpts, Container, Directory } from "../client.gen.js"
+import Client, {
+  connect,
+  ClientContainerOpts,
+  Container,
+  Directory,
+} from "../../index.js"
 import { queryFlatten, buildQuery } from "../utils.js"
 
 const querySanitizer = (query: string) => query.replace(/\s+/g, " ")
--- sdk/nodejs/index.ts
+++ sdk/nodejs/index.ts
@@ -1,8 +1,5 @@
-import Client from "./api/client.gen.js"
-
+export * from "./api/client.gen.js"
+export { default } from "./api/client.gen.js"
 export { gql } from "graphql-tag"
 export { GraphQLClient } from "graphql-request"
-
 export { connect, ConnectOpts, CallbackFct } from "./connect.js"
-
-export default Client
