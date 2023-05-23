8dc46d47965722ef7759758782d80dc7e72d1fc9
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2023-05-23 16:01:07 +0530

    Update docs/current/cookbook/snippets/cache-invalidation/index.mjs
    Co-authored-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>

--- docs/current/cookbook/snippets/cache-invalidation/index.mjs
+++ docs/current/cookbook/snippets/cache-invalidation/index.mjs
@@ -3,9 +3,9 @@ import { connect } from "@dagger.io/dagger"
 // create Dagger client
 connect(async (client) => {
 
-	// invalidate cache to force execution
-	// of second withExec() operation
-	const output = await client.pipeline("test").
+  // invalidate cache to force execution
+  // of second withExec() operation
+  const output = await client.pipeline("test").
     container().
     from("alpine").
     withExec(["apk", "add", "curl"]).
