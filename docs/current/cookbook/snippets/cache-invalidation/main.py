7f020b797287e4d7501613bef03e3e4d6d35a880
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2023-05-25 02:20:48 +0530

    docs: Improved cache miss recipe for Python (#5189)
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/cookbook/snippets/cache-invalidation/main.py
+++ docs/current/cookbook/snippets/cache-invalidation/main.py
@@ -1,12 +1,12 @@
 import sys
 import anyio
-import datetime
+from datetime import datetime
 import dagger
 
 async def main():
 
     # create Dagger client
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr, workdir=".")) as client:
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
 
         # invalidate cache to force execution
         # of second with_exec() operation
@@ -15,7 +15,7 @@ async def main():
             container().
             from_("alpine").
             with_exec(["apk", "add", "curl"]).
-            with_env_variable("CACHEBUSTER", str(datetime.datetime.utcnow().timestamp())).
+            with_env_variable("CACHEBUSTER", str(datetime.now())).
             with_exec(["apk", "add", "zip"]).
             stdout()
         )
