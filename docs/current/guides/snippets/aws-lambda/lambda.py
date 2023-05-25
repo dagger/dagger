7759103cfb875fb3488ec64255d904b9b38582f7
Vikram Vaswani <vikram@dagger.io>
2023-05-26 01:55:13 +0530

    docs: Added AWS Lambda with zip files guide
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current/guides/snippets/aws-lambda/lambda.py
@@ -0,0 +1,10 @@
+import requests
+import os
+
+def handler(event, context):
+
+    token = os.environ.get('GITHUB_API_TOKEN')
+    url = 'https://api.github.com/repos/dagger/dagger/issues'
+    headers = {'Accept': 'application/vnd.github+json', 'Authorization': f'Bearer {token}'}
+    r = requests.get(url, headers=headers)
+    return r.json()
