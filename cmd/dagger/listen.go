88b586a76ac91acd1e12366f01f6c2b9389f55aa
Alex Suraci <alex@dagger.io>
2023-05-30 12:23:26 -0400

    fix up dagger listen TUI integration
    this was working fine as far as I can tell, it just looked janky because
    it was still logging directly to stderr, clobbering the TUI.
    
    the TUI can be disabled with -s/--silent.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- cmd/dagger/listen.go
+++ cmd/dagger/listen.go
@@ -3,12 +3,15 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
+	"net"
 	"net/http"
 	"os"
 
 	"github.com/dagger/dagger/engine"
 	"github.com/dagger/dagger/router"
 	"github.com/spf13/cobra"
+	"github.com/vito/progrock"
 )
 
 var (
@@ -32,8 +35,33 @@ func init() {
 func Listen(cmd *cobra.Command, args []string) {
 	ctx := context.Background()
 	if err := withEngineAndTUI(ctx, engine.Config{}, func(ctx context.Context, r *router.Router) error {
-		fmt.Fprintf(os.Stderr, "==> server listening on http://%s/query\n", listenAddress)
-		return http.ListenAndServe(listenAddress, r) //nolint:gosec
+		rec := progrock.RecorderFromContext(ctx)
+
+		var stderr io.Writer
+		if silent {
+			stderr = os.Stderr
+		} else {
+			vtx := rec.Vertex("listen", "listen")
+			stderr = vtx.Stderr()
+		}
+
+		sessionL, err := net.Listen("tcp", listenAddress)
+		if err != nil {
+			return fmt.Errorf("session listen: %w", err)
+		}
+		defer sessionL.Close()
+
+		srv := &http.Server{Handler: r}
+
+		go func() {
+			<-ctx.Done()
+			fmt.Fprintln(stderr, "==> server shutting down")
+			srv.Shutdown(context.Background())
+		}()
+
+		fmt.Fprintf(stderr, "==> server listening on http://%s/query\n", listenAddress)
+
+		return srv.Serve(sessionL) // nolint:gosec
 	}); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
