aa70c2d9bf2bec54234d74acc9c6e6c46bcc27c0
Helder Correia <174525+helderco@users.noreply.github.com>
2023-05-30 17:40:35 +0000

    Python quick fixes
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- docs/current/guides/snippets/azure-pipelines-container-instances/main.py
+++ docs/current/guides/snippets/azure-pipelines-container-instances/main.py
@@ -7,23 +7,24 @@ from azure.identity import DefaultAzureCredential
 from azure.mgmt.containerinstance import ContainerInstanceManagementClient
 
 # configure container group, name and location
-container_name = "my-app";
-container_group_name = "my-app";
-container_group_location = "eastus";
-resource_group_name = "my-group";
+CONTAINER_NAME = "my-app"
+CONTAINER_GROUP_NAME = "my-app"
+CONTAINER_GROUP_LOCATION = "eastus"
+RESOURCE_GROUP_NAME = "my-group"
+
 
 async def main():
 
     # check for required variables in host environment
     for var in ["DOCKERHUB_USERNAME", "DOCKERHUB_PASSWORD", "AZURE_SUBSCRIPTION_ID", "AZURE_TENANT_ID", "AZURE_CLIENT_ID", "AZURE_CLIENT_SECRET"]:
         if var not in os.environ:
-            raise EnvironmentError('"%s" environment variable must be set' % var)
+            raise EnvironmentError(f'"{var}" environment variable must be set')
 
     # initialize Dagger client
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as dagger_client:
 
         # set registry password as Dagger secret
-        secret = dagger_client.set_secret("password", os.environ.get("DOCKERHUB_PASSWORD"));
+        secret = dagger_client.set_secret("password", os.environ["DOCKERHUB_PASSWORD"])
 
         # get reference to the project directory
         source = (
@@ -53,7 +54,7 @@ async def main():
         )
 
         # publish image
-        docker_hub_username = os.environ.get("DOCKERHUB_USERNAME")
+        docker_hub_username = os.environ["DOCKERHUB_USERNAME"]
         addr = await (
           c
           .with_registry_auth("docker.io", docker_hub_username, secret)
@@ -65,14 +66,15 @@ async def main():
 
         # initialize Azure client
         azure_client = ContainerInstanceManagementClient(
-          credential=DefaultAzureCredential(), subscription_id=os.environ.get("AZURE_SUBSCRIPTION_ID"),
+          credential=DefaultAzureCredential(),
+          subscription_id=os.environ["AZURE_SUBSCRIPTION_ID"],
         )
 
         # define deployment request
         container_group = {
           "containers": [
             {
-              "name": container_name,
+              "name": CONTAINER_NAME,
               "image": addr,
               "ports": [{ "port": "3000" }],
               "resources": { "requests": { "cpu": "1", "memoryInGB": "1.5" } },
@@ -80,18 +82,19 @@ async def main():
           ],
           "ipAddress": { "type": "Public", "ports": [{ "port": "3000", "protocol": "TCP" }] },
           "osType": "Linux",
-          "location": container_group_location,
+          "location": CONTAINER_GROUP_LOCATION,
           "restartPolicy": "OnFailure",
-        };
+        }
 
         # send request and wait until done
         result = azure_client.container_groups.begin_create_or_update(
-            resource_group_name,
-            container_group_name,
-            container_group
+            RESOURCE_GROUP_NAME,
+            CONTAINER_GROUP_NAME,
+            container_group,
         ).result()
 
     # print ref
     print(f"Deployment for image {addr} now available at http://{result.ip_address.ip}:{result.ip_address.ports[0].port}")
+
 
 anyio.run(main)
