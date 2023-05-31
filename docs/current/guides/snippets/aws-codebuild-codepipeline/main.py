93d90a1573ce3fbb75e61aedec70cd96503056c2
Helder Correia <174525+helderco@users.noreply.github.com>
2023-05-31 12:23:45 +0000

    Python formatting
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- docs/current/guides/snippets/aws-codebuild-codepipeline/main.py
+++ docs/current/guides/snippets/aws-codebuild-codepipeline/main.py
@@ -1,28 +1,24 @@
-import sys
 import os
+import sys
 
 import anyio
 import dagger
 
-async def main():
 
+async def main():
     # check for required variables in host environment
     for var in ["REGISTRY_ADDRESS", "REGISTRY_USERNAME", "REGISTRY_PASSWORD"]:
         if var not in os.environ:
-            raise EnvironmentError('"%s" environment variable must be set' % var)
+            msg = f'"{var}" environment variable must be set'
+            raise OSError(msg)
 
     # initialize Dagger client
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-
         # set registry password as Dagger secret
-        secret = client.set_secret("password", os.environ.get("REGISTRY_PASSWORD"));
+        secret = client.set_secret("password", os.environ["REGISTRY_PASSWORD"])
 
         # get reference to the project directory
-        source = (
-            client
-            .host()
-            .directory(".", exclude=["node_modules", "ci"])
-        )
+        source = client.host().directory(".", exclude=["node_modules", "ci"])
 
         # use a node:18-slim container
         node = (
@@ -48,12 +44,14 @@ async def main():
         # publish image to registry
         # at registry path [registry-username]/myapp
         # print image address
-        username = os.environ.get("REGISTRY_USERNAME")
+        username = os.environ["REGISTRY_USERNAME"]
         address = await (
             app
-            .with_registry_auth(os.environ.get("REGISTRY_ADDRESS"), username, secret)
+            .with_registry_auth(os.environ["REGISTRY_ADDRESS"], username, secret)
             .publish(f"{username}/myapp")
         )
-        print(f"Published image to: {address}")
+
+    print(f"Published image to: {address}")
+
 
 anyio.run(main)
