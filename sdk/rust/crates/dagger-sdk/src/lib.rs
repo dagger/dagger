f7bda48e112e6dcfad9efc47dd500fb4e6d9c187
Kasper Juul Hermansen <contact@kjuulh.io>
2023-06-01 20:27:58 +0200

    feat(rust): no-gen didn't work as intended (#5198)
    Signed-off-by: kjuulh <contact@kjuulh.io>

--- sdk/rust/crates/dagger-sdk/src/lib.rs
+++ sdk/rust/crates/dagger-sdk/src/lib.rs
@@ -1,28 +1,16 @@
-#[cfg(not(feature = "no-gen"))]
-mod client;
-
-#[cfg(feature = "gen")]
-mod client;
-
 pub mod core;
 pub mod errors;
-
-#[cfg(not(feature = "no-gen"))]
-mod gen;
-
-#[cfg(feature = "gen")]
-mod gen;
 
 pub mod logging;
 mod querybuilder;
 
 pub use crate::core::config::Config;
 
-#[cfg(not(feature = "no-gen"))]
-pub use client::*;
+#[cfg(feature = "gen")]
+mod client;
 
-#[cfg(not(feature = "no-gen"))]
-pub use gen::*;
+#[cfg(feature = "gen")]
+mod gen;
 
 #[cfg(feature = "gen")]
 pub use client::*;
