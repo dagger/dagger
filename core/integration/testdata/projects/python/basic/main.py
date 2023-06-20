91e8d0b6dd9a16d86174e86f43f21fd6bfa056f9
Helder Correia <174525+helderco@users.noreply.github.com>
2023-06-20 11:18:08 +0000

    Reuse go's integration tests
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

---
+++ core/integration/testdata/projects/python/basic/main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+import dagger
+from dagger.server import command
+
+
+@command
+def test_file(client: dagger.Client, prefix: str) -> dagger.File:
+    name = f"{prefix}foo.txt"
+    return client.directory().with_new_file(name, "foo\n").file(name)
+
+
+@command
+def test_dir(client: dagger.Client, prefix: str) -> dagger.Directory:
+    return (
+        client.directory()
+        .with_new_directory(f"{prefix}subdir")
+        .with_new_file(f"{prefix}subdir/subbar1.txt", "subbar1\n")
+        .with_new_file(f"{prefix}subdir/subbar2.txt", "subbar2\n")
+        .with_new_file(f"{prefix}bar1.txt", "bar1\n")
+        .with_new_file(f"{prefix}bar2.txt", "bar2\n")
+    )
+
+
+@command
+def test_imported_project_dir() -> str:
+    return "\n".join(str(p.absolute()) for p in Path().glob("**/*"))
