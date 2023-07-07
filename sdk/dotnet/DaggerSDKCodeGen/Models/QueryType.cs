1b0d936cec11ad9c6b7a063736f99d27d7bfdca5
Nabeel Sulieman <nabsul@users.noreply.github.com>
2023-07-07 15:51:12 -0700

    Start working on a code generation tool
    Fix parse errors
    
    Check in some intermediate stuff
    
    Re-order fields, add readme, etc.
    
    Document source of introspection query
    
    Signed-off-by: Nabeel Sulieman <nabsul@users.noreply.github.com>

---
+++ sdk/dotnet/DaggerSDKCodeGen/Models/QueryType.cs
@@ -0,0 +1,13 @@
+﻿namespace DaggerSDKCodeGen.Models;
+
+public class QueryType
+{
+    public string? Description { get; set; }
+    public EnumType[]? EnumValues { get; set; }
+    public QueryField[]? Fields { get; set; }
+    public InputField[]? InputFields { get; set; }
+    public string[]? Interfaces { get; set; }
+    public string? Kind { get; set; }
+    public string? Name { get; set; }
+    public string[]? PossibleTypes { get; set; }
+}
