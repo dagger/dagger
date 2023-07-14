c89c3075f7ffdcd6dbd097ad82f776bc74135eb1
Erik Sipsma <erik@sipsma.dev>
2023-07-14 08:34:58 -0700

    Add test for local dir import cache reuse.
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- core/integration/cache_test.go
+++ core/integration/cache_test.go
@@ -1,8 +1,12 @@
 package core
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"testing"
 
+	"dagger.io/dagger"
 	"github.com/dagger/dagger/core"
 	"github.com/dagger/dagger/internal/testutil"
 	"github.com/stretchr/testify/require"
@@ -64,4 +68,35 @@ func TestCacheVolume(t *testing.T) {
 
 		require.NotEqual(t, idOrig, idDiff)
 	})
+}
+
+func TestLocalImportCacheReuse(t *testing.T) {
+	t.Parallel()
+
+	hostDirPath := t.TempDir()
+	err := os.WriteFile(filepath.Join(hostDirPath, "foo"), []byte("bar"), 0o644)
+	require.NoError(t, err)
+
+	runExec := func(ctx context.Context, t *testing.T, c *dagger.Client) string {
+		out, err := c.Container().From("alpine:3.16.2").
+			WithDirectory("/fromhost", c.Host().Directory(hostDirPath)).
+			WithExec([]string{"sh", "-c", "head -c 128 /dev/random | sha256sum"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		return out
+	}
+
+	c1, ctx1 := connect(t)
+	defer c1.Close()
+	ctx1, cancel1 := context.WithCancel(ctx1)
+	defer cancel1()
+	out1 := runExec(ctx1, t, c1)
+
+	c2, ctx2 := connect(t)
+	defer c2.Close()
+	ctx2, cancel2 := context.WithCancel(ctx2)
+	defer cancel2()
+	out2 := runExec(ctx2, t, c2)
+
+	require.Equal(t, out1, out2)
 }
