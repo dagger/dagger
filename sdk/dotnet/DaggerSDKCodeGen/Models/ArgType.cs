fb25a527f95ff205f14465ff4fa5869232894f26
Nabeel Sulieman <nabsul@outlook.com>
2023-07-14 09:14:21 -0700

    Start working on a code generation tool (#5422)
    Fix parse errors
    
    Check in some intermediate stuff
    
    Re-order fields, add readme, etc.
    
    Document source of introspection query
    
    Signed-off-by: Nabeel Sulieman <nabsul@users.noreply.github.com>

---
+++ sdk/dotnet/DaggerSDKCodeGen/Models/ArgType.cs
@@ -0,0 +1,8 @@
+﻿namespace DaggerSDKCodeGen.Models;
+
+public class ArgType
+{
+    public string? Kind { get; set; }
+    public string? Name { get; set; }
+    public TypeDef? OfType { get; set; }
+}
---
+++ sdk/dotnet/DaggerSDKCodeGen/Models/EnumType.cs
@@ -0,0 +1,9 @@
+namespace DaggerSDKCodeGen.Models;
+
+public class EnumType
+{
+    public string? DeprecationReason { get; set; }
+    public string? Description { get; set; }
+    public bool IsDeprecated { get; set; }
+    public string? Name { get; set; }
+}
