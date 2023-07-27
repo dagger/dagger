48dabed44e64f49718b67c3d05586202d504f586
Helder Correia <174525+helderco@users.noreply.github.com>
2023-07-27 18:31:08 +0000

    Remove uneeded test
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- core/integration/services_test.go
+++ core/integration/services_test.go
@@ -1163,45 +1163,6 @@ func TestFileServiceTimestamp(t *testing.T) {
 	require.Contains(t, stdout, "1991-06-03")
 }
 
-func TestFileServiceSecret(t *testing.T) {
-	t.Parallel()
-
-	checkNotDisabled(t, engine.ServicesDNSEnvName)
-
-	c, ctx := connect(t)
-	defer c.Close()
-
-	content := identity.NewID()
-
-	httpSrv, httpURL := httpService(ctx, t, c, content)
-
-    // TODO: This is unsafe as it gets cached.
-	plaintext, err := c.HTTP(httpURL, dagger.HTTPOpts{
-		ExperimentalServiceHost: httpSrv,
-	}).Contents(ctx)
-    require.NoError(t, err)
-
-    secret := c.SetSecret("sekrit", plaintext)
-
-	t.Run("secret env", func(t *testing.T) {
-		_, err := c.Container().
-			From(alpineImage).
-			WithSecretVariable("SEKRIT", secret).
-			WithExec([]string{"sh", "-c", fmt.Sprintf(`test "$SEKRIT" = "%s"`, content)}).
-			Sync(ctx)
-		require.NoError(t, err)
-	})
-
-	t.Run("secret mount", func(t *testing.T) {
-		_, err := c.Container().
-			From(alpineImage).
-			WithMountedSecret("/sekrit", secret).
-			WithExec([]string{"sh", "-c", fmt.Sprintf(`test "$(cat /sekrit)" = "%s"`, content)}).
-			Sync(ctx)
-		require.NoError(t, err)
-	})
-}
-
 func httpService(ctx context.Context, t *testing.T, c *dagger.Client, content string) (*dagger.Container, string) {
 	t.Helper()
 
