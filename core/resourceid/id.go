45fc541290ae0d3609a5c6d3f82718f805e222f7
Alex Suraci <alex@dagger.io>
2023-08-11 17:43:03 -0400

    resourceids: include ID type in ID format
    this way we can detect the type of an ID'd return value from an
    environment
    
    should also be helpful for debugging and sanity checks; now you'll get
    an error if you try to decode a FooID into a Bar.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/resourceid/id.go
+++ core/resourceid/id.go
@@ -4,6 +4,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"strings"
 )
 
 // Encode JSON marshals and base64-encodes an arbitrary payload.
@@ -13,21 +14,30 @@ func Encode[T ~string](payload any) (T, error) {
 		return "", err
 	}
 
-	b64Bytes := make([]byte, base64.StdEncoding.EncodedLen(len(jsonBytes)))
-	base64.StdEncoding.Encode(b64Bytes, jsonBytes)
+	idEnc := base64.StdEncoding.EncodeToString(jsonBytes)
 
-	return T(b64Bytes), nil
+	var t T
+	t = T(fmt.Sprintf("%T:%s", t, idEnc))
+
+	return t, nil
 }
 
 // Decode base64-decodes and JSON unmarshals an ID into an arbitrary payload.
 func Decode[T ~string](payload any, id T) error {
-	jsonBytes := make([]byte, base64.StdEncoding.DecodedLen(len(id)))
-	n, err := base64.StdEncoding.Decode(jsonBytes, []byte(id))
+	actualType, idEnc, ok := strings.Cut(string(id), ":")
+	if !ok {
+		return fmt.Errorf("malformed ID: %v", id)
+	}
+
+	desiredType := fmt.Sprintf("%T", id)
+	if actualType != desiredType {
+		return fmt.Errorf("ID type mismatch: %v != %v", actualType, desiredType)
+	}
+
+	jsonBytes, err := base64.StdEncoding.DecodeString(idEnc)
 	if err != nil {
 		return fmt.Errorf("failed to decode %T bytes: %v: %w", payload, id, err)
 	}
-
-	jsonBytes = jsonBytes[:n]
 
 	return json.Unmarshal(jsonBytes, payload)
 }
