59e54cbcbafb9e2fffc0de386cfd2f9ad4b2a818
Erik Sipsma <erik@dagger.io>
2023-08-23 08:47:33 -0700

    engine: update go, runc, cni and qemu deps in engine image
    Signed-off-by: Erik Sipsma <erik@dagger.io>

--- internal/mage/util/engine.go
+++ internal/mage/util/engine.go
@@ -16,10 +16,11 @@ import (
 const (
 	engineBinName = "dagger-engine"
 	shimBinName   = "dagger-shim"
+	golangVersion = "1.20.6"
 	alpineVersion = "3.18"
-	runcVersion   = "v1.1.5"
-	cniVersion    = "v1.2.0"
-	qemuBinImage  = "tonistiigi/binfmt:buildkit-v7.1.0-30@sha256:45dd57b4ba2f24e2354f71f1e4e51f073cb7a28fd848ce6f5f2a7701142a6bf0"
+	runcVersion   = "v1.1.9"
+	cniVersion    = "v1.3.0"
+	qemuBinImage  = "tonistiigi/binfmt@sha256:e06789462ac7e2e096b53bfd9e607412426850227afeb1d0f5dfa48a731e0ba5"
 
 	engineTomlPath = "/etc/dagger/engine.toml"
 	// NOTE: this needs to be consistent with DefaultStateDir in internal/engine/docker.go
