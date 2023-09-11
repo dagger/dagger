5ef627b346ff52b1534f420fa395eb23e41e33fc
Alex Suraci <alex@dagger.io>
2023-09-11 16:05:52 -0400

    idproto: canonicalize recursively
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/idproto/id.go
+++ core/idproto/id.go
@@ -5,6 +5,7 @@ import (
 	"google.golang.org/protobuf/proto"
 )
 
+// Tainted returns true if the ID contains any tainted selectors.
 func (id *ID) Tainted() bool {
 	for _, sel := range id.Constructor {
 		if sel.Tainted {
@@ -14,11 +15,12 @@ func (id *ID) Tainted() bool {
 	return false
 }
 
+// Canonical returns the ID with any contained IDs canonicalized.
 func (id *ID) Canonical() *ID {
 	noMeta := []*Selector{}
 	for _, sel := range id.Constructor {
 		if !sel.Meta {
-			noMeta = append(noMeta, sel)
+			noMeta = append(noMeta, sel.Canonical())
 		}
 	}
 	return &ID{
@@ -27,10 +29,51 @@ func (id *ID) Canonical() *ID {
 	}
 }
 
+// Digest returns the digest of the encoded ID. It does NOT canonicalize the ID
+// first.
 func (id *ID) Digest() (digest.Digest, error) {
 	bytes, err := proto.Marshal(id)
 	if err != nil {
 		return "", err
 	}
 	return digest.FromBytes(bytes), nil
+}
+
+// Canonical returns the selector with any contained IDs canonicalized.
+func (sel *Selector) Canonical() *Selector {
+	cp := proto.Clone(sel).(*Selector)
+	for i := range cp.Args {
+		cp.Args[i] = cp.Args[i].Canonical()
+	}
+	return sel
+}
+
+// Canonical returns the literal with any contained IDs canonicalized.
+func (lit *Literal) Canonical() *Literal {
+	switch v := lit.Value.(type) {
+	case *Literal_Id:
+		return &Literal{Value: &Literal_Id{Id: v.Id.Canonical()}}
+	case *Literal_List:
+		list := make([]*Literal, len(v.List.Values))
+		for i, val := range v.List.Values {
+			list[i] = val.Canonical()
+		}
+		return &Literal{Value: &Literal_List{List: &List{Values: list}}}
+	case *Literal_Object:
+		args := make([]*Argument, len(v.Object.Values))
+		for i, arg := range v.Object.Values {
+			args[i] = arg.Canonical()
+		}
+		return &Literal{Value: &Literal_Object{Object: &Object{Values: args}}}
+	default:
+		return lit
+	}
+}
+
+// Canonical returns the argument with any contained IDs canonicalized.
+func (arg *Argument) Canonical() *Argument {
+	return &Argument{
+		Name:  arg.Name,
+		Value: arg.GetValue().Canonical(),
+	}
 }
