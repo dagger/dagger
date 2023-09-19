ffa59610d0093e9f6947d59083c4ce270c963595
Vikram Vaswani <vikram@dagger.io>
2023-09-19 22:33:03 +0530

    Lint fixes
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/cloud/snippets/get-started/step4/index.mjs
+++ docs/current/cloud/snippets/get-started/step4/index.mjs
@@ -1,30 +1,36 @@
-import { connect } from "@dagger.io/dagger"
+import { connect } from "@dagger.io/dagger";
 
-connect(async (client) => {
+connect(
+  async (client) => {
+    const nodeCache = client.cacheVolume("node");
 
-  const nodeCache = client.cacheVolume("node")
+    const source = client
+      .container()
+      .from("node:16-slim")
+      .withDirectory(
+        "/src",
+        client.host().directory(".", { exclude: ["node_modules/", "ci/"] }),
+      )
+      .withMountedCache("/src/node_modules", nodeCache);
 
-  const source = client.container()
-    .from("node:16-slim")
-    .withDirectory('/src', client.host().directory('.'), { exclude: ["node_modules/", "ci/"] })
-    .withMountedCache("/src/node_modules", nodeCache)
+    const runner = source.withWorkdir("/src").withExec(["npm", "install"]);
 
-  const runner = source
-    .withWorkdir("/src")
-    .withExec(["npm", "install"])
+    const test = runner.withExec(["npm", "test", "--", "--watchAll=false"]);
 
-  const test = runner
-    .withExec(["npm", "test", "--", "--watchAll=false"])
+    await test
+      .withExec(["npm", "run", "build"])
+      .directory("./build")
+      .export("./build");
 
-  await test
-    .withExec(["npm", "run", "build"])
-    .directory("./build")
-    .export("./build")
-
-  const imageRef = await client.container()
-    .from("nginx:1.23-alpine")
-    .withDirectory('/usr/share/nginx/html', client.host().directory('./build'))
-    .publish('ttl.sh/hello-dagger-' + Math.floor(Math.random() * 10000000))
-  console.log(`Published image to: ${imageRef}`)
-
-}, { LogOutput: process.stdout })
+    const imageRef = await client
+      .container()
+      .from("nginx:1.23-alpine")
+      .withDirectory(
+        "/usr/share/nginx/html",
+        client.host().directory("./build"),
+      )
+      .publish("ttl.sh/hello-dagger-" + Math.floor(Math.random() * 10000000));
+    console.log(`Published image to: ${imageRef}`);
+  },
+  { LogOutput: process.stdout },
+);
