f26cb7f7839ac126972b4253ff45e08e52977701
Joel Longtine <joel@dagger.io>
2023-09-20 15:57:56 -0700

    Dagger Engine Helm chart (#5505)
    * Dagger DaemonSet helm chart
    
    Signed-off-by: Joel Longtine <joel@longtine.io>
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Encode secret value
    
    Signed-off-by: Joel Longtine <joel@longtine.io>
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Add engine config
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Remove custom config
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * - Add resources limits + requests
    - Add readinessProbeSettings
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Update name of helm chart
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Add Dagger pipeline for helm publish
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Publish workflow
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Disable magicache by default
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Reduce complexity of helm chart
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Addressing some comments
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Make terminationGracePeriodSeconds configurable
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Add tests
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    * Bump to latest released version
    
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>
    
    * Comment tolerations & affinity
    
    Otherwise this will not be scheduled by default.
    
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>
    
    * Update when to run helm:publish
    
    Signed-off-by: Joel Longtine <joel@dagger.io>
    
    ---------
    
    Signed-off-by: Joel Longtine <joel@longtine.io>
    Signed-off-by: Joel Longtine <joel@dagger.io>
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>
    Co-authored-by: Gerhard Lazu <gerhard@dagger.io>

---
+++ internal/mage/helm.go
@@ -0,0 +1,94 @@
+package mage
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"dagger.io/dagger"
+	"github.com/magefile/mage/mg"
+)
+
+type Helm mg.Namespace
+
+func (Helm) Test(ctx context.Context) error {
+	c, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	c = c.Pipeline("helm").Pipeline("test")
+	helmChart := c.Host().Directory("helm/dagger")
+	helm := c.Container().From("alpine/helm:3.12.3").
+		WithDirectory("/tmp/dagger-helm", helmChart).
+		WithWorkdir("/tmp/dagger-helm")
+
+	_, err = helm.WithExec([]string{"lint"}).Sync(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = helm.WithExec([]string{"lint", "--debug", "--namespace", "dagger", "--set", "magicache.token=hello-world", "--set", "magicache.enabled=true"}).Sync(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	_, err = helm.WithExec([]string{"template", ".", "--debug", "--namespace", "dagger", "--set", "magicache.token=hello-world", "--set", "magicache.enabled=true"}).Sync(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (Helm) Publish(ctx context.Context) error {
+	c, err := dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	c = c.Pipeline("helm").Pipeline("publish")
+
+	helmChart := c.Host().Directory("helm/dagger")
+
+	env, ok := os.LookupEnv("GITHUB_TOKEN")
+	if !ok {
+		return fmt.Errorf("GITHUB_TOKEN is not set")
+	}
+
+	secret := c.SetSecret("GITHUB_TOKEN", env)
+
+	helm := c.Container().From("alpine/helm:3.12.3").
+		WithDirectory("/tmp/dagger-helm", helmChart).
+		WithWorkdir("/tmp/dagger-helm")
+
+	version, err := helm.WithExec([]string{"sh", "-c", "grep ^version Chart.yaml | cut -f 2 -d ' '"}, dagger.ContainerWithExecOpts{SkipEntrypoint: true}).Stdout(ctx)
+
+	version = strings.TrimSpace(version)
+
+	if err != nil {
+		return err
+	}
+
+	loggedIn := helm.
+		WithSecretVariable("GITHUB_TOKEN", secret).
+		WithExec([]string{"sh", "-c", "helm registry login -u dagger ghcr.io/dagger --password $GITHUB_TOKEN"}, dagger.ContainerWithExecOpts{SkipEntrypoint: true})
+
+	packaged := loggedIn.WithExec([]string{"package", "."})
+
+	_, err = packaged.WithExec([]string{"push", "dagger-helm-" + version + ".tgz", "oci://ghcr.io/dagger"}).Sync(ctx)
+
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("PUBLISHED HELM VERSION:", version)
+
+	return nil
+}
