350913e379e9bc95f42fdd9c28b711aea5d2f168
Alex Suraci <alex@dagger.io>
2023-09-25 18:54:35 -0400

    fix loading not-generated package
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- codegen/generator/go/generator.go
+++ codegen/generator/go/generator.go
@@ -80,7 +80,7 @@ func (g *GoGenerator) Generate(ctx context.Context, schema *introspection.Schema
 
 		// main.go is actually an input to codegen, so this requires another pass
 		partial = true
-	} else if g.Config.ModuleSourceDir != "" {
+	} else if !partial && g.Config.ModuleSourceDir != "" {
 		pkg, fset, err = loadPackage(ctx, g.Config.ModuleSourceDir)
 		if err != nil {
 			return nil, fmt.Errorf("load package: %w", err)
