1166109fdef88353126adeb324698195a62a095c
Vasek - Tom C <tom@epitech.eu>
2023-09-28 19:29:53 +0200

    style: fix linter
    Signed-off-by: Vasek - Tom C <tom@epitech.eu>

--- docs/current/guides/snippets/concurrency/main.py
+++ docs/current/guides/snippets/concurrency/main.py
@@ -12,18 +12,16 @@ async def long_time_task(c: dagger.Client):
 
     :param c: dagger client.
     """
-    await c.container() \
-        .from_("alpine") \
-        .with_exec(["sleep", str(secrets.randbelow(10))]) \
-        .with_exec(["echo", "task done"]) \
-        .sync()
+    await c.container().from_("alpine").with_exec(
+        ["sleep", str(secrets.randbelow(10))]
+    ).with_exec(["echo", "task done"]).sync()
 
 
 async def main():
     """Execute multiple tasks in concurrency."""
-    async with \
-            dagger.Connection(dagger.Config(log_output=sys.stderr)) as client, \
-            anyio.create_task_group() as tg:
+    async with dagger.Connection(
+        dagger.Config(log_output=sys.stderr)
+    ) as client, anyio.create_task_group() as tg:
         tg.start_soon(long_time_task, client)
         tg.start_soon(long_time_task, client)
         tg.start_soon(long_time_task, client)
