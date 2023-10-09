ac8faa0c930f6af485248a9c229bf0c76e4fcc6a
Justin Chadwell <me@jedevc.com>
2023-10-09 11:27:29 +0100

    fix: consistent errors for non-existent module directory
    Previously, when a module was not provided for the dagger query command
    using the `-m` flag, the error message did not indicate that the module
    did not exist even when it was requested by the user.
    
    This patch reworks the logic - dagger query will now always error if the
    module provided by `-m` did not exist, but will keep the desired
    behaviour that it should not error if the current directory is not a
    module.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- cmd/dagger/query.go
+++ cmd/dagger/query.go
@@ -43,7 +43,7 @@ EOF
 `,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		focus = queryFocus
-		return loadModCmdWrapper(Query, "", true)(cmd, args)
+		return loadModCmdWrapper(Query, "")(cmd, args)
 	},
 	Args: cobra.MaximumNArgs(1), // operation can be specified
 }
