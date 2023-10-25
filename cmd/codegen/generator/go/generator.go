5500de2b5e38fe0149dd0fcd33d896fdc7c26691
Erik Sipsma <erik@dagger.io>
2023-10-25 11:18:10 -0700

    zenith: remove .Sync from module default main.go in go sdk
    Calling sync isn't required there and just creates confusion, so it's
    better to remove it.
    
    Signed-off-by: Erik Sipsma <erik@dagger.io>

--- cmd/codegen/generator/go/generator.go
+++ cmd/codegen/generator/go/generator.go
@@ -345,7 +345,7 @@ import (
 type %s struct {}
 
 func (m *%s) MyFunction(ctx context.Context, stringArg string) (*Container, error) {
-	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg}).Sync(ctx)
+	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg}), nil
 }
 `, moduleStructName, moduleStructName)
 }
