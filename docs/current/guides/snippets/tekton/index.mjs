53a899e62fe802d77d6d3bb62b26a7a45f53e3c8
Vikram Vaswani <vikram@dagger.io>
2023-10-26 21:56:16 +0530

    Lint fix
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/guides/snippets/tekton/index.mjs
+++ docs/current/guides/snippets/tekton/index.mjs
@@ -11,8 +11,8 @@ connect(
       .withDirectory(
         "/src",
         client
-        .host()
-        .directory(".", { exclude: ["node_modules/", "ci/", "build/"] })
+          .host()
+          .directory(".", { exclude: ["node_modules/", "ci/", "build/"] })
       )
 
     // set the working directory in the container
