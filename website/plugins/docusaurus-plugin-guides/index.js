a313b191bd12445c45d28fc7fd328bd663aeb29c
Vikram Vaswani <vikram@dagger.io>
2023-11-03 01:32:54 +0530

    Revert "zenith guides filtering"
    This reverts commit 9700ff7bda9bad80280dd8d79a99ed4c56a09db3.
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- website/plugins/docusaurus-plugin-guides/index.js
+++ website/plugins/docusaurus-plugin-guides/index.js
@@ -3,96 +3,45 @@ const fs = require("fs");
 const utils = require("@docusaurus/utils");
 
 module.exports = async function guidesPlugin(context, options) {
-  const currentGuidesPath = path.resolve(options.currentGuidesPath);
-  const zenithGuidesPath = path.resolve(options.zenithGuidesPath);
   const guidesJSONPath = "./static/guides.json";
   return {
     name: "docusaurus-plugin-guides",
     async loadContent() {
-      const currentGuidesFolderPath = path.resolve(currentGuidesPath);
-      const zenithGuidesFolderPath = path.resolve(zenithGuidesPath);
+      const guidesFolderPath = path.resolve("../docs/current/guides");
+      const guides = fs.readdirSync(guidesFolderPath).flatMap((x) => {
+        const guidePath = `${guidesFolderPath}/${x}`;
+        const isFile = fs.lstatSync(guidePath).isFile();
+        let content = "";
+        if (isFile) {
+          content = fs.readFileSync(guidePath).toString();
+        } else {
+          return [];
+        }
 
-      const currentGuides = fs
-        .readdirSync(currentGuidesFolderPath)
-        .flatMap((x) => {
-          const currentGuidePath = `${currentGuidesFolderPath}/${x}`;
-          const isFile = fs.lstatSync(currentGuidePath).isFile();
+        const parsedMarkdown = utils.parseMarkdownString(content);
+        const timestamp = Date.parse(parsedMarkdown.frontMatter.date);
 
-          let content = "";
+        if (isNaN(timestamp)) {
+          throw new Error(
+            `Given date in ${x} is not ISO 8601 compatible. Please, set the date in this guide to MM-DD-YY format.`,
+          );
+        }
 
-          if (isFile) {
-            content = fs.readFileSync(currentGuidePath).toString();
-          } else {
-            return [];
-          }
-
-          const parsedMarkdown = utils.parseMarkdownString(content);
-          const timestamp = Date.parse(parsedMarkdown.frontMatter.date);
-
-          if (isNaN(timestamp)) {
-            throw new Error(
-              `Given date in ${x} is not ISO 8601 compatible. Please, set the date in this guide to MM-DD-YY format.`,
-            );
-          }
-
-          return [
-            {
-              path: currentGuidePath,
-              ...parsedMarkdown,
-              timestamp,
-            },
-          ];
-        });
-
-      const zenithGuides = fs
-        .readdirSync(zenithGuidesFolderPath)
-        .flatMap((x) => {
-          const zenithGuidePath = `${zenithGuidesFolderPath}/${x}`;
-          const isFile = fs.lstatSync(zenithGuidePath).isFile();
-
-          let content = "";
-
-          if (isFile) {
-            content = fs.readFileSync(zenithGuidePath).toString();
-          } else {
-            return [];
-          }
-
-          const parsedMarkdown = utils.parseMarkdownString(content);
-          const timestamp = Date.parse(parsedMarkdown.frontMatter.date);
-
-          if (isNaN(timestamp)) {
-            throw new Error(
-              `Given date in ${x} is not ISO 8601 compatible. Please, set the date in this guide to MM-DD-YY format.`,
-            );
-          }
-
-          return [
-            {
-              path: zenithGuidePath,
-              ...parsedMarkdown,
-              timestamp,
-            },
-          ];
-        });
-
-      currentGuides.sort((a, b) => {
-        return b.timestamp - a.timestamp;
+        return [
+          {
+            path: guidePath,
+            ...parsedMarkdown,
+            timestamp,
+          },
+        ];
       });
-
-      zenithGuides.sort((a, b) => {
+      guides.sort((a, b) => {
         return b.timestamp - a.timestamp;
-      });
-
+      })
       let allTags = new Set();
-      currentGuides.forEach((guide) =>
-        guide.frontMatter.tags.forEach((tag) => allTags.add(tag)),
-      );
-      allTags = [...allTags];
-      fs.writeFileSync(
-        guidesJSONPath,
-        JSON.stringify({currentGuides, zenithGuides, allTags}),
-      );
+      guides.forEach((guide) => guide.frontMatter.tags.forEach(tag => allTags.add(tag)));
+      allTags = [...allTags]
+      fs.writeFileSync(guidesJSONPath, JSON.stringify({guides, allTags}));
     },
   };
 };
