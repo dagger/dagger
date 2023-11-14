c8e4dd00e04b1ee81284c2673818833deeab0cdd
Helder Correia <174525+helderco@users.noreply.github.com>
2023-11-14 20:24:17 -0100

    fix: Python module codegen with different root (#6109)
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/runtime/main.go
+++ sdk/python/runtime/main.go
@@ -43,7 +43,7 @@ func (m *PythonSdk) Codegen(modSource *Directory, subPath string, introspectionJ
 	})
 
 	modified := ctr.Directory(ModSourceDirPath)
-	diff := modSource.Diff(modified)
+	diff := modSource.Diff(modified).Directory(subPath)
 
 	return dag.GeneratedCode(diff).
 		WithVCSIgnoredPaths([]string{
