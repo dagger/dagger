c7b11597bf9ad6b28093539fd0e2b63928616667
Vikram Vaswani <vikram@dagger.io>
2023-11-22 23:49:51 +0530

    Lint fixes
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current/cookbook/snippets/reuse-containers/index.mjs
+++ docs/current/cookbook/snippets/reuse-containers/index.mjs
@@ -4,19 +4,21 @@ connect(
   async (client) => {
     // build container in one pipeline
     const ctr = await client
-    .pipeline("Test")
-		.container()
-		.from("alpine")
-		.withExec(["apk", "add", "curl"])
-		.sync()
+      .pipeline("Test")
+      .container()
+      .from("alpine")
+      .withExec(["apk", "add", "curl"])
+      .sync()
 
     // get container ID
     const cid = await ctr.id()
 
     // use container in another pipeline via its ID
-    await client.container({id: cid})
-		.pipeline("Build")
-		.withExec(["curl", "https://dagger.io"]).sync()
+    await client
+      .container({id: cid})
+      .pipeline("Build")
+      .withExec(["curl", "https://dagger.io"])
+      .sync()
   },
   { LogOutput: process.stderr }
 )
