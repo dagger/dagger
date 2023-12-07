52ca09a17455b6bf81a3f66680108f1b4909175c
Justin Chadwell <me@jedevc.com>
2023-12-07 14:39:09 +0000

    Improve go sdk docs with most recent updates (#6225)
    * docs: fix zenith go example to remove json tags
    
    In the most recent go sdk version, we no longer need these little tags.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * docs: prefer new pragma syntax in go zenith sdk
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    ---------
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- docs/zenith/developer/go/snippets/quickstart/step3a/main.go
+++ docs/zenith/developer/go/snippets/quickstart/step3a/main.go
@@ -7,10 +7,12 @@ type Potato struct{}
 func (m *Potato) HelloWorld(
 	// the number of potatoes to process
 	count int,
-	// whether the potatoes are mashed (this is an optional parameter!)
-	mashed Optional[bool],
+
+	// whether the potatoes are mashed
+	// +optional
+	mashed bool,
 ) string {
-	if mashed.GetOr(false) {
+	if mashed {
 		return fmt.Sprintf("Hello world, I have mashed %d potatoes", count)
 	}
 	return fmt.Sprintf("Hello world, I have %d potatoes", count)
