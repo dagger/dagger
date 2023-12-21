6f6ef49b5b7b430641765dc14e6076e382f7fecd
Justin Chadwell <me@jedevc.com>
2023-12-21 13:43:31 +0000

    ci: attempt to build the publish env in TestPublish
    This should help ensure that we can correctly install nix in PR
    environments.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- internal/mage/cli.go
+++ internal/mage/cli.go
@@ -42,27 +42,10 @@ func (cl Cli) Publish(ctx context.Context, version string) error {
 		args = append(args, "--release-notes", fmt.Sprintf(".changes/%s.md", version))
 	}
 
-	ctr := c.Container().
-		From(fmt.Sprintf("ghcr.io/goreleaser/goreleaser-pro:%s", goReleaserVersion)).
-		WithEntrypoint([]string{}).
-		WithExec([]string{"apk", "add", "aws-cli"})
-
-	// install nix
-	ctr = ctr.
-		WithExec([]string{"apk", "add", "xz"}).
-		WithDirectory("/nix", c.Directory()).
-		WithNewFile("/etc/nix/nix.conf", dagger.ContainerWithNewFileOpts{
-			Contents: `build-users-group =`,
-		}).
-		WithExec([]string{"sh", "-c", "curl -L https://nixos.org/nix/install | sh -s -- --no-daemon"})
-	path, err := ctr.EnvVariable(ctx, "PATH")
+	ctr, err := publishEnv(ctx, c)
 	if err != nil {
 		return err
 	}
-	ctr = ctr.WithEnvVariable("PATH", path+":/nix/var/nix/profiles/default/bin")
-	// goreleaser requires nix-prefetch-url, so check we can run it
-	ctr = ctr.WithExec([]string{"sh", "-c", "nix-prefetch-url 2>&1 | grep 'error: you must specify a URL'"})
-
 	wd := c.Host().Directory(".")
 	_, err = ctr.
 		WithWorkdir("/app").
@@ -121,5 +104,40 @@ func (cl Cli) TestPublish(ctx context.Context) error {
 			})
 		}
 	}
+
+	eg.Go(func() error {
+		env, err := publishEnv(ctx, c)
+		if err != nil {
+			return err
+		}
+		_, err = env.Sync(ctx)
+		return err
+	})
+
 	return eg.Wait()
+}
+
+func publishEnv(ctx context.Context, c *dagger.Client) (*dagger.Container, error) {
+	ctr := c.Container().
+		From(fmt.Sprintf("ghcr.io/goreleaser/goreleaser-pro:%s", goReleaserVersion)).
+		WithEntrypoint([]string{}).
+		WithExec([]string{"apk", "add", "aws-cli"})
+
+	// install nix
+	ctr = ctr.
+		WithExec([]string{"apk", "add", "xz"}).
+		WithDirectory("/nix", c.Directory()).
+		WithNewFile("/etc/nix/nix.conf", dagger.ContainerWithNewFileOpts{
+			Contents: `build-users-group =`,
+		}).
+		WithExec([]string{"sh", "-c", "curl -L https://nixos.org/nix/install | sh -s -- --no-daemon"})
+	path, err := ctr.EnvVariable(ctx, "PATH")
+	if err != nil {
+		return nil, err
+	}
+	ctr = ctr.WithEnvVariable("PATH", path+":/nix/var/nix/profiles/default/bin")
+	// goreleaser requires nix-prefetch-url, so check we can run it
+	ctr = ctr.WithExec([]string{"sh", "-c", "nix-prefetch-url 2>&1 | grep 'error: you must specify a URL'"})
+
+	return ctr, nil
 }
