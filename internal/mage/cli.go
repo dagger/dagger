87805694f45b40e4087142ea94ff9faaa358130b
Justin Chadwell <me@jedevc.com>
2023-12-21 13:32:29 +0000

    fix: install nix using script
    The apk package for nix is only available in alpine:edge atm, and
    goreleaser does not use alpine:edge.
    
    So, we can install it using the same method we were previously using in
    github actions.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- internal/mage/cli.go
+++ internal/mage/cli.go
@@ -42,11 +42,29 @@ func (cl Cli) Publish(ctx context.Context, version string) error {
 		args = append(args, "--release-notes", fmt.Sprintf(".changes/%s.md", version))
 	}
 
-	wd := c.Host().Directory(".")
-	_, err = c.Container().
+	ctr := c.Container().
 		From(fmt.Sprintf("ghcr.io/goreleaser/goreleaser-pro:%s", goReleaserVersion)).
 		WithEntrypoint([]string{}).
-		WithExec([]string{"apk", "add", "aws-cli", "nix"}).
+		WithExec([]string{"apk", "add", "aws-cli"})
+
+	// install nix
+	ctr = ctr.
+		WithExec([]string{"apk", "add", "xz"}).
+		WithDirectory("/nix", c.Directory()).
+		WithNewFile("/etc/nix/nix.conf", dagger.ContainerWithNewFileOpts{
+			Contents: `build-users-group =`,
+		}).
+		WithExec([]string{"sh", "-c", "curl -L https://nixos.org/nix/install | sh -s -- --no-daemon"})
+	path, err := ctr.EnvVariable(ctx, "PATH")
+	if err != nil {
+		return err
+	}
+	ctr = ctr.WithEnvVariable("PATH", path+":/nix/var/nix/profiles/default/bin")
+	// goreleaser requires nix-prefetch-url, so check we can run it
+	ctr = ctr.WithExec([]string{"sh", "-c", "nix-prefetch-url 2>&1 | grep 'error: you must specify a URL'"})
+
+	wd := c.Host().Directory(".")
+	_, err = ctr.
 		WithWorkdir("/app").
 		WithMountedDirectory("/app", wd).
 		With(util.HostSecretVar(c, "GITHUB_TOKEN")).
