f11fcc425e9b25e261686dc5147a613eab179045
Vasek - Tom C <tom.chauveau@epitech.eu>
2023-12-21 12:20:35 +0100

    docs: Add quickstart guide for Typescript module (#6299)
    * docs: Add quickstart guide for Typescript module
    
    Follow Go and Python quickstart to create one for
    Typescript.
    Update generated doc with latest changes.
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>
    
    * fix: sidebar json
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>
    
    ---------
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

---
+++ docs/versioned_docs/version-zenith/developer/typescript/snippets/quickstart/trivy/index.ts
@@ -0,0 +1,29 @@
+import { dag, func, object, } from '@dagger.io/dagger';
+
+@object
+class Trivy {
+  @func
+  async scanImage(
+    imageRef: string,
+    severity: string = "UNKNOWN,LOW,MEDIUM,HIGH,CRITICAL",
+    exitCode = 0,
+    format = "table"
+  ): Promise<string> {
+    return dag
+      .container()
+      .from("aquasec/trivy:latest")
+      .withExec([
+        "image",
+        "--quiet",
+        "--severity",
+        severity,
+        "--exit-code",
+        `${exitCode}`,
+        "--format",
+        format,
+        imageRef,
+      ])
+      .stdout()
+  }
+}
+
