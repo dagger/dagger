b477d44ec592cec3f8b180ae61b118126461a339
Erik Sipsma <erik@dagger.io>
2023-12-28 10:14:10 -0800

    add sad path test for no dagger object embed
    Signed-off-by: Erik Sipsma <erik@dagger.io>

--- core/integration/module_iface_test.go
+++ core/integration/module_iface_test.go
@@ -3,10 +3,11 @@ package core
 import (
 	"testing"
 
+	"dagger.io/dagger"
 	"github.com/stretchr/testify/require"
 )
 
-func TestModuleGoIfaces(t *testing.T) {
+func TestModuleIfaceBasic(t *testing.T) {
 	t.Parallel()
 
 	c, ctx := connect(t)
@@ -18,4 +19,36 @@ func TestModuleGoIfaces(t *testing.T) {
 		With(daggerCall("test")).
 		Sync(ctx)
 	require.NoError(t, err)
+}
+
+func TestModuleIfaceGoSadPaths(t *testing.T) {
+	t.Parallel()
+
+	t.Run("no dagger object embed", func(t *testing.T) {
+		t.Parallel()
+		var logs safeBuffer
+		c, ctx := connect(t, dagger.WithLogOutput(&logs))
+
+		_, err := c.Container().From(golangImage).
+			WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
+			WithWorkdir("/work").
+			With(daggerExec("mod", "init", "--name=test", "--sdk=go")).
+			WithNewFile("main.go", dagger.ContainerWithNewFileOpts{
+				Contents: `package main
+type Test struct {}
+
+type BadIface interface {
+	Foo(ctx context.Context) (string, error)
+}
+
+func (m *Test) Fn() BadIface {
+	return nil
+}
+	`,
+			}).
+			With(daggerFunctions()).
+			Sync(ctx)
+		require.Error(t, err)
+		require.Regexp(t, `missing method .* from DaggerObject interface, which must be embedded in interfaces used in Functions and Objects`, logs.String())
+	})
 }
