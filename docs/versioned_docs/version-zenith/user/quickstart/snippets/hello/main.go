12e357612a9c528761e46eff63dffa454f46ec66
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-01-11 20:50:46 +0530

    Apply suggestions from code review
    Co-authored-by: Erik Sipsma <erik@sipsma.dev>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>

--- docs/versioned_docs/version-zenith/user/quickstart/snippets/hello/main.go
+++ docs/versioned_docs/version-zenith/user/quickstart/snippets/hello/main.go
@@ -7,10 +7,10 @@ import (
 type Mymodule struct{}
 
 // say hello
-func (m *Mymodule) Hello(ctx context.Context) string {
+func (m *Mymodule) Hello(ctx context.Context) (string, error) {
 	version, err := dag.Container().From("node:18-slim").WithExec([]string{"node", "-v"}).Stdout(ctx)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	return ("Hello from Dagger and Node " + version)
+	return ("Hello from Dagger and Node " + version), nil
 }
