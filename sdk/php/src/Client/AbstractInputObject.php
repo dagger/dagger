70ef3658f7910d5e46eebbec84e96ac52e2d97e9
zeroware <zeroware@users.noreply.github.com>
2024-01-16 14:28:44 +0100

    sdk(php): Implements handling of InputObjects in queries
    Add a unit test in `ClientTest` using `PipelineLabel` input object.
    
    Signed-off-by: Johan Schwartz <zeroware@users.noreply.github.com>

--- sdk/php/src/Client/AbstractInputObject.php
+++ sdk/php/src/Client/AbstractInputObject.php
@@ -2,10 +2,40 @@
 
 namespace Dagger\Client;
 
-abstract class AbstractInputObject
+use GraphQL\Util\StringLiteralFormatter;
+use Stringable;
+
+abstract class AbstractInputObject implements Stringable
 {
-    public function toArray(): array
+    public function __toString(): string
     {
-        return (array) $this;
+        $objectString = '{';
+        $first = true;
+
+        foreach ($this as $name => $value) {
+            if (empty($value)) {
+                continue;
+            }
+
+            // Append space at the beginning if it's not the first item on the list
+            if ($first) {
+                $first = false;
+            } else {
+                $objectString .= ', ';
+            }
+
+            // Convert argument values to graphql string literal equivalent
+            if (is_scalar($value)) {
+                // Convert scalar value to its literal in graphql
+                $value = StringLiteralFormatter::formatValueForRHS($value);
+            } elseif (is_array($value)) {
+                // Convert PHP array to its array representation in graphql arguments
+                $value = StringLiteralFormatter::formatArrayForGQLQuery($value);
+            }
+            $objectString .= $name.': '.$value;
+        }
+        $objectString .= '}';
+
+        return $objectString;
     }
 }
