3bd3ac6e4160baff15fb8f95326559b1be74bc89
Tom Chauveau <tom@epitech.eu>
2024-01-30 11:26:27 +0100

    fix: ts config updator path
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- sdk/typescript/runtime/bin/__tsconfig.updator.ts
+++ sdk/typescript/runtime/bin/__tsconfig.updator.ts
@@ -1,12 +1,6 @@
 import * as fs from "fs"
-import * as path from "path"
-import { fileURLToPath } from "url"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
-const moduleProjectDirectory = `${__dirname}/../../`
-const tsConfigPath = `${moduleProjectDirectory}/tsconfig.json`
+const tsConfigPath = `./tsconfig.json`
 
 const daggerPathAlias = "@dagger.io/dagger"
 const daggerPath = "./sdk"
