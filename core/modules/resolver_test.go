e6218a4298e1d40cde8e04f8c0310510bc100646
Kyle Penfound <kyle@dagger.io>
2024-01-31 12:47:14 -0500

    add a test case for refs v2 lite
    Signed-off-by: Kyle Penfound <kyle@dagger.io>

--- core/modules/resolver_test.go
+++ core/modules/resolver_test.go
@@ -7,17 +7,18 @@ import (
 )
 
 func TestMatchVersion(t *testing.T) {
-	vers := []string{"v1.0.0", "v1.0.1", "v2.0.0", "path/v1.0.1"}
+	vers := []string{"v1.0.0", "v1.0.1", "v2.0.0", "path/v1.0.1", "path/v2.0.1"}
 
-	match := "v1.0.1"
-
-	match1, err := matchVersion(vers, match, "")
+	match1, err := matchVersion(vers, "v1.0.1", "")
 	require.NoError(t, err)
 	require.Equal(t, "v1.0.1", match1)
 
-	match2, err := matchVersion(vers, match, "path")
+	match2, err := matchVersion(vers, "v1.0.1", "path")
 	require.NoError(t, err)
 	require.Equal(t, "path/v1.0.1", match2)
+
+	_, err = matchVersion(vers, "v2.0.1", "")
+	require.Error(t, err)
 }
 
 func TestIsSemver(t *testing.T) {
