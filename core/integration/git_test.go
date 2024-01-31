c59f3e1a39d268f2edfc94745ed7dfd5cb20b467
Justin Chadwell <me@jedevc.com>
2024-01-31 16:09:06 +0000

    fix: correctly select the right git tag
    This is a cherry-pick of moby/buildkit@6a8d2ca2bd965c35c5753f698daeb6805aa05db4
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- core/integration/git_test.go
+++ core/integration/git_test.go
@@ -91,10 +91,7 @@ func TestGit(t *testing.T) {
 	require.Contains(t, res.Git.Branch.Tree.File.Contents, "Dagger")
 
 	// v0.9.5
-	// FIXME: without moby/buildkit#4473, the wrong commit is selected:
-	//		  we *should* select refs/tags/v0.9.5, but we select
-	//		  refs/tags/sdk/elixir/v0.9.5
-	require.Equal(t, res.Git.Tag.Commit, "037520ed6c8a4ace2f1602fa5277d2cd1ed4ebd4")
+	require.Equal(t, res.Git.Tag.Commit, "9ea5ea7c848fef2a2c47cce0716d5fcb8d6bedeb")
 	require.Contains(t, res.Git.Tag.Tree.File.Contents, "Dagger")
 }
 
