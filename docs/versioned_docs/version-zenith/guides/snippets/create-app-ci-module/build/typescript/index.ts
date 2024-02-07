69526e47c0139900a582ac47798751245d83c2e3
Vikram Vaswani <vikram@dagger.io>
2024-02-08 01:43:34 +0530

    Added missing file
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/versioned_docs/version-zenith/guides/snippets/create-app-ci-module/build/typescript/index.ts
@@ -0,0 +1,41 @@
+import { dag, Container, Directory, object, func, field } from "@dagger.io/dagger"
+
+@object
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class MyModule {
+
+  source: Directory
+
+  // constructor
+  constructor (source: Directory) {
+    this.source = source
+  }
+
+  // create a production build
+  @func
+  build(): Directory {
+    return dag.node().withContainer(this.buildBaseImage())
+      .build()
+      .container()
+      .directory("./dist")
+  }
+
+  // run unit tests
+  @func
+  async test(): Promise<string> {
+    return await dag.node().withContainer(this.buildBaseImage())
+      .run(["run", "test:unit", "run"])
+      .stdout()
+  }
+
+  // build base image
+  buildBaseImage(): Container {
+    return dag.node()
+      .withVersion("21")
+      .withNpm()
+      .withSource(this.source)
+      .install([])
+      .container()
+  }
+
+}
