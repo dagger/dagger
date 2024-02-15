17a938b4a35fdb24f4f14320be9477ce8bc1cf63
Vikram Vaswani <vikram@dagger.io>
2024-02-15 15:42:47 +0530

    Updated text
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/python/main.py
+++ docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/python/main.py
@@ -1,6 +1,5 @@
 import dagger
 from dagger import dag, object_type, field, function
-from google.cloud import run_v2
 
 @object_type
 class MyModule:
@@ -20,43 +19,19 @@ class MyModule:
         )
 
     @function
-    async def publish(self, registry: str, credential: dagger.Secret) -> str:
+    async def publish(self, project: str, location: str, repository: str, credential: dagger.Secret) -> str:
         '''Publish an image'''
-        split = registry.split("/")
+        registry = f"{location}-docker.pkg.dev/{project}/{repository}"
         return await (
             self.build()
-            .with_registry_auth(split[0], "_json_key", credential)
+            .with_registry_auth(f"{location}-docker.pkg.dev", "_json_key", credential)
             .publish(registry)
         )
 
     @function
-    async def deploy(self, registry: str, service: str, credential: dagger.Secret) -> str:
+    async def deploy(self, project: str, registry_location: str, repository: str, service_location: str, service: str, credential: dagger.Secret) -> str:
         '''Deploy an image to Google Cloud Run'''
 
-        json = await credential.plaintext()
-        gcr_client = run_v2.ServicesAsyncClient(json)
+        addr = await self.publish(project, registry_location, repository, credential)
 
-        addr = self.publish(registry, credential)
-
-        gcr_request = run_v2.UpdateServiceRequest(
-            service=run_v2.Service(
-                name=service,
-                template=run_v2.RevisionTemplate(
-                    containers=[
-                        run_v2.Container(
-                            image=addr,
-                            ports=[
-                                run_v2.ContainerPort(
-                                    name="http1",
-                                    container_port=1323,
-                                ),
-                            ],
-                        ),
-                    ],
-                ),
-            )
-        )
-
-        gcr_operation = await gcr_client.update_service(request=gcr_request)
-
-        response = await gcr_operation.result()
+        return await dag.google_cloud_run().update_service(project, service_location, service, addr, 3000, credential)
--- docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/typescript/index.ts
+++ docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/typescript/index.ts
@@ -1,7 +1,6 @@
-import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
-import { ServicesClient } from "@google-cloud/run"
+import { dag, Container, Directory, Secret, object, func } from "@dagger.io/dagger"
 
-@object
+@object()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyModule {
 
@@ -12,67 +11,30 @@ class MyModule {
     this.source = source
   }
 
-  // build a container
-  @func
+  @func()
   build(): Container {
     return dag.container().from("node:21")
-    .withDirectory("/home/node", this.source)
-    .withWorkdir("/home/node")
-    .withExec(["npm", "install"])
-    .withEntrypoint(["npm", "start"])
+      .withDirectory("/src", this.source)
+      .withWorkdir("/src")
+      .withExec(["cp", "-R", ".", "/home/node"])
+      .withWorkdir("/home/node")
+      .withExec(["npm", "install"])
+      .withEntrypoint(["npm", "start"])
   }
 
-  // publish an image
-  // example: dagger call --source . publish --registry REGISTRY/myapp --credential env:GOOGLE_JSON
-  @func
-  async publish(registry: string, credential: Secret): Promise<string> {
-    const split = registry.split("/")
+  @func()
+  async publish(project: string, location: string, repository: string, credential: Secret): Promise<string> {
+    const registry = `${location}-docker.pkg.dev/${project}/${repository}`
     return await this.build()
-      .withRegistryAuth(split[0], "_json_key", credential)
+      .withRegistryAuth(`${location}-docker.pkg.dev`, "_json_key", credential)
       .publish(registry)
   }
 
-  // deploy an image to Google Cloud Run
-  // example: dagger call --source . publish --registry REGISTRY/myapp --service SERVICE --credential env:GOOGLE_JSON
-  @func
-  async deploy(service: string, registry: string, credential: Secret): Promise<string> {
+  @func()
+  async deploy(project: string, registryLocation: string, repository: string, serviceLocation: string, service: string, credential: Secret): Promise<string> {
 
-    // get JSON secret
-    const json = JSON.parse(await credential.plaintext())
-    const gcrClient = new ServicesClient({credentials: json})
+    const addr = await this.publish(project, registryLocation, repository, credential)
 
-    // publish image
-    const gcrContainerPublishResponse = await this.publish(registry, credential)
-
-    // define service request
-    const gcrServiceUpdateRequest = {
-      service: {
-        name: service,
-        template: {
-          containers: [
-            {
-              image: gcrContainerPublishResponse,
-              ports: [
-                {
-                  name: "http1",
-                  containerPort: 3000,
-                },
-              ],
-            },
-          ],
-        },
-      },
-    }
-
-    // update service
-    const [gcrServiceUpdateOperation] = await gcrClient.updateService(
-      gcrServiceUpdateRequest
-    )
-
-    // wait for service request completion
-    const [gcrServiceUpdateResponse] = await gcrServiceUpdateOperation.promise()
-
-    // return service URL
-    return gcrServiceUpdateResponse.uri
+    return dag.googleCloudRun().updateService(project, serviceLocation, service, addr, 3000, credential)
   }
 }
