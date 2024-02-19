a83a5c07cb87ec92a77df671848355916ef99bb7
Helder Correia <174525+helderco@users.noreply.github.com>
2024-02-19 00:12:16 -0100

    Remove wrapping on @typecheck decorator
    Beartype 0.17 introduced more configurability for violations, so we can remove the wrapper which was causing issues type checking correctly in PyCharm. The default for beartype is now good enough. Abandoned replacing "dagger.client.gen." with "dagger." since it's not worth it.
    
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/src/dagger/client/_guards.py
+++ sdk/python/src/dagger/client/_guards.py
@@ -1,13 +1,9 @@
-import contextlib
-import functools
-import inspect
 import typing
-from collections.abc import Callable, Coroutine, Sequence
-from typing import Annotated, Any, Concatenate, ParamSpec, TypeGuard, TypeVar, overload
+from collections.abc import Sequence
+from typing import Annotated, TypeGuard
 
-from beartype import beartype
+from beartype import BeartypeConf, BeartypeViolationVerbosity, beartype
 from beartype.door import TypeHint
-from beartype.roar import BeartypeCallHintViolation
 from beartype.vale import Is, IsInstance, IsSubclass
 
 from .base import Scalar, Type
@@ -42,100 +38,9 @@ def is_id_type_sequence(v: object) -> TypeGuard[IDTypeSeq]:
     return IDTypeSeqHint.is_bearable(v)
 
 
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
-_S = ParamSpec("_S")
-
-
-@overload
-def typecheck(
-    func: Callable[_P, Coroutine[Any, Any, _T]]
-) -> Callable[_P, Coroutine[Any, Any, _T]]:
-    ...
-
-
-@overload
-def typecheck(func: Callable[_P, _T]) -> Callable[_P, _T]:
-    ...
-
-
-@overload
-def typecheck(
-    func: Callable[Concatenate[_S, _P], Coroutine[Any, Any, _T]]
-) -> Callable[Concatenate[_S, _P], Coroutine[Any, Any, _T]]:
-    ...
-
-
-@overload
-def typecheck(
-    func: Callable[Concatenate[_S, _P], _T]
-) -> Callable[Concatenate[_S, _P], _T]:
-    ...
-
-
-def typecheck(
-    func: Callable[_P, _T | Coroutine[Any, Any, _T]]
-) -> Callable[_P, _T | Coroutine[Any, Any, _T]]:
-    ...
-
-    """
-    Runtime type checking.
-
-    Allows fast failure, before sending requests to the API,
-    and with greater detail over the specific method and
-    parameter with invalid type to help debug.
-
-    This includes catching typos or forgetting to await a
-    coroutine, but it's less forgiving in some instances.
-
-    For example, an `args: Sequence[str]` parameter set as
-    `args=["echo", 123]` was easily converting the int 123
-    to a string by the dynamic query builder. Now it'll fail.
-    """
-    # Using beartype for the hard work, just tune the traceback a bit.
-    # Hiding as **implementation detail** for now. The project is young
-    # but very active and with good plans on making it very modular/pluggable.
-
-    # Decorating here allows basic checks during definition time
-    # so it'll be catched early, during development.
-    bear = beartype(func)
-
-    @contextlib.contextmanager
-    def _handle_exception():
-        try:
-            yield
-        except BeartypeCallHintViolation as e:
-            # Tweak the error message a bit.
-            msg = str(e).replace("@beartyped ", "")
-
-            # Everything in `dagger.api.gen.` is exported under `dagger.`.
-            msg = msg.replace("dagger.client.gen.", "dagger.")
-
-            # No API methods accept a coroutine, add hint.
-            if "<coroutine object" in msg:
-                msg = f"{msg} Did you forget to await?"
-
-            # The following `raise` line will show in traceback, keep
-            # the noise down to minimum by instantiating outside of it.
-            err = TypeError(msg).with_traceback(None)
-            raise err from None
-
-    if inspect.iscoroutinefunction(bear):
-
-        @functools.wraps(func)
-        async def async_wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-            with _handle_exception():
-                return await bear(*args, **kwargs)
-
-        return async_wrapper
-
-    if inspect.isfunction(bear):
-
-        @functools.wraps(func)
-        def wrapper(*args: _P.args, **kwargs: _P.kwargs) -> _T:
-            with _handle_exception():
-                return bear(*args, **kwargs)
-
-        return wrapper
-
-    return bear
+typecheck = beartype(
+    conf=BeartypeConf(
+        violation_param_type=TypeError,
+        violation_verbosity=BeartypeViolationVerbosity.MINIMAL,
+    )
+)
