ca8821a12e9046044aef82e333359d0bb65edf7e
Tom Chauveau <tom@epitech.eu>
2024-02-19 23:56:38 +0100

    docs: add typescript docs
    Add Error handling doc
    Add conventions docs
    Start custom types doc
    Start chaining doc
    
    Fix some links in the sidebars
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/constructors/index.ts
+++ docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/constructors/index.ts
@@ -1,7 +1,6 @@
-import { dag, object, func, field } from "@dagger.io/dagger"
+import { object, func } from "@dagger.io/dagger"
 
 @object()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class HelloWorld {
 
   greeting: string
