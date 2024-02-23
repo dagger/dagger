04e46e88e6cab15f5f76fdf35bffa097e2cc1a66
Vikram Vaswani <vikram@dagger.io>
2024-02-23 20:23:53 +0530

    Removed TS linter comments, fixed Go doc link
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/arguments-return-values/arguments-directory/index.ts
+++ docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/arguments-return-values/arguments-directory/index.ts
@@ -1,7 +1,6 @@
 import { dag, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyModule {
 
   @func()
--- docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/arguments-return-values/return-values-container/index.ts
+++ docs/versioned_docs/version-zenith/developer-guide/typescript/snippets/arguments-return-values/return-values-container/index.ts
@@ -1,7 +1,6 @@
 import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 
 @object()
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyModule {
 
   @func()
