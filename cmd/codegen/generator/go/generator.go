8e1951c9623be3477f22e8cb438bcf9e6261e4ae
Jeremy Adams <jeremy@dagger.io>
2024-02-26 13:13:29 -0800

    Zenith: Update dagger init module for Go SDK (#6720)
    * Feat: Adds descriptions & comments for Go SDK init
    
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>
    
    * chore: avoid passing repeat args to fmt.Printf
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * feat: Update module comments, remove struct comment
    
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>
    
    ---------
    
    Signed-off-by: Jeremy Adams <jeremy@dagger.io>
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    Co-authored-by: Justin Chadwell <me@jedevc.com>

--- cmd/codegen/generator/go/generator.go
+++ cmd/codegen/generator/go/generator.go
@@ -343,21 +343,35 @@ func loadPackage(ctx context.Context, dir string) (*packages.Package, *token.Fil
 func (g *GoGenerator) baseModuleSource() string {
 	moduleStructName := strcase.ToCamel(g.Config.ModuleName)
 
-	return fmt.Sprintf(`package main
+	return fmt.Sprintf(`// A generated module for %[1]s functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+
+package main
 
 import (
 	"context"
 )
 
-type %s struct {}
+type %[1]s struct{}
 
-// example usage: "dagger call container-echo --string-arg yo stdout"
-func (m *%s) ContainerEcho(stringArg string) *Container {
+// Returns a container that echoes whatever string argument is provided
+func (m *%[1]s) ContainerEcho(stringArg string) *Container {
 	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
 }
 
-// example usage: "dagger call grep-dir --directory-arg . --pattern GrepDir"
-func (m *%s) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
+// Returns lines that match a pattern in the files of the provided Directory
+func (m *%[1]s) GrepDir(ctx context.Context, directoryArg *Directory, pattern string) (string, error) {
 	return dag.Container().
 		From("alpine:latest").
 		WithMountedDirectory("/mnt", directoryArg).
@@ -365,6 +379,5 @@ func (m *%s) GrepDir(ctx context.Context, directoryArg *Directory, pattern strin
 		WithExec([]string{"grep", "-R", pattern, "."}).
 		Stdout(ctx)
 }
-
-`, moduleStructName, moduleStructName, moduleStructName)
+`, moduleStructName)
 }
