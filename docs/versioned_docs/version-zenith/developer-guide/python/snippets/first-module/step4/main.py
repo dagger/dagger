6fc270a63a4c01c0abb95d9cba0e35b8498282f8
Vikram Vaswani <vikram@dagger.io>
2024-02-26 12:27:08 +0530

    Removed inline doc for Python first module
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/versioned_docs/version-zenith/developer-guide/python/snippets/first-module/step4/main.py
+++ docs/versioned_docs/version-zenith/developer-guide/python/snippets/first-module/step4/main.py
@@ -9,10 +9,9 @@ class PotatoMessage:
 
 @function
 def hello_world(
-    count: Annotated[int, Doc("The number of potatoes to process")],
-    mashed: Annotated[bool, Doc("Whether the potatoes are mashed")] = False,
+    count: int,
+    mashed: bool = False,
 ) -> PotatoMessage:
-    """Tell the world how many potatoes you have."""
     if mashed:
         message = f"Hello Daggernauts, I have mashed {count} potatoes"
     else:
