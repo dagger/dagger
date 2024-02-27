780b906e75c3e3722c66301d20a7400f8d90dc7f
Erik Sipsma <erik@dagger.io>
2024-02-27 10:01:15 -0800

    engine: fix stream goroutine leak
    Turns out that the goroutine used to call cancel when the grpc hijack
    conn's closeCh is closed is just not necessary at all and can be
    removed.
    
    That closeCh is called when .Close is explicitly called, but in that
    case there's no need to cancel our context; the context will be
    cancelled anyways when the method returns.
    
    Signed-off-by: Erik Sipsma <erik@dagger.io>

--- engine/server/buildkitcontroller.go
+++ engine/server/buildkitcontroller.go
@@ -172,13 +172,7 @@ func (e *BuildkitController) Session(stream controlapi.Control_SessionServer) (r
 		}
 	}()
 
-	conn, closeCh, hijackmd := grpchijack.Hijack(stream)
-	// TODO: this blocks if opts.RegisterClient and an error happens
-	// TODO: ? defer conn.Close()
-	go func() {
-		<-closeCh
-		cancel()
-	}()
+	conn, _, hijackmd := grpchijack.Hijack(stream)
 
 	if !opts.RegisterClient {
 		e.serverMu.RLock()
