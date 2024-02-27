65d07f0cf53e6a2f4b0a714aaf94cd4a83aa34d0
Tom Chauveau <tom@epitech.eu>
2024-02-27 10:57:51 +0100

    chore: fix missing files because ignored
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

---
+++ docs/current_docs/guides/snippets/create-app-ci-module/build/typescript/index.ts
@@ -0,0 +1,33 @@
+import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
+
+@object()
+class MyModule {
+
+  // create a production build
+  @func()
+  build(source: Directory): Directory {
+    return dag.node().withContainer(this.buildBaseImage(source))
+      .build()
+      .container()
+      .directory("./dist")
+  }
+
+  // run unit tests
+  @func()
+  async test(source: Directory): Promise<string> {
+    return await dag.node().withContainer(this.buildBaseImage(source))
+      .run(["run", "test:unit", "run"])
+      .stdout()
+  }
+
+  // build base image
+  buildBaseImage(source: Directory): Container {
+    return dag.node()
+      .withVersion("21")
+      .withNpm()
+      .withSource(source)
+      .install([])
+      .container()
+  }
+
+}
