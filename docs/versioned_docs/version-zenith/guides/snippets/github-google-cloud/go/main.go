785e84fad784d79e532d202f4f5c4b637cd419e6
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-02-27 20:01:05 +0530

    docs: Ports GitHub Actions guide to Zenith approach (#6638)
    * docs: Adds GitHub Actions guide
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Added content
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Updated content
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fixes
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Update docs/versioned_docs/version-zenith/guides/620942-github-google-cloud.mdx
    
    Co-authored-by: Helder Correia <174525+helderco@users.noreply.github.com>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    
    * Updated code
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Update main.go
    
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    
    * Updated text
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Updated text
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Update docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/go/main.go
    
    Co-authored-by: Helder Correia <174525+helderco@users.noreply.github.com>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    
    * Added feedback
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Removed constructors
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Updated local credential setup
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * remove extra line in deploy go code
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    ---------
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    Co-authored-by: Helder Correia <174525+helderco@users.noreply.github.com>
    Co-authored-by: Justin Chadwell <me@jedevc.com>

---
+++ docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/go/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type MyModule struct{}
+
+// build an image
+func (m *MyModule) Build(source *Directory) *Container {
+	return dag.Container().
+		From("node:21").
+		WithDirectory("/home/node", source).
+		WithWorkdir("/home/node").
+		WithExec([]string{"npm", "install"}).
+		WithEntrypoint([]string{"npm", "start"})
+}
+
+// publish an image
+// example: dagger call publish --source . --project PROJECT --location LOCATION --repository REPOSITORY/APPNAME --credential env:GOOGLE_JSON
+func (m *MyModule) Publish(ctx context.Context, source *Directory, project string, location string, repository string, credential *Secret) (string, error) {
+	registry := fmt.Sprintf("%s-docker.pkg.dev/%s/%s", location, project, repository)
+	return m.Build(source).
+		WithRegistryAuth(fmt.Sprintf("%s-docker.pkg.dev", location), "_json_key", credential).
+		Publish(ctx, registry)
+}
+
+// deploy an image to Google Cloud Run
+// example: dagger call deploy --source . --project PROJECT --registry-location LOCATION --repository REPOSITORY/APPNAME --service-location LOCATION --service SERVICE  --credential env:GOOGLE_JSON
+func (m *MyModule) Deploy(ctx context.Context, source *Directory, project, registryLocation, repository, serviceLocation, service string, credential *Secret) (string, error) {
+	// publish image
+	addr, err := m.Publish(ctx, source, project, registryLocation, repository, credential)
+	if err != nil {
+		return "", err
+	}
+
+	// update service with new image
+	return dag.GoogleCloudRun().UpdateService(ctx, project, serviceLocation, service, addr, 3000, credential)
+}
---
+++ docs/versioned_docs/version-zenith/guides/snippets/github-google-cloud/typescript/index.ts
@@ -0,0 +1,48 @@
+import { dag, Container, Directory, Secret, object, func } from "@dagger.io/dagger"
+
+@object()
+class MyModule {
+
+  /**
+   * Build an image
+   */
+  @func()
+  build(source: Directory): Container {
+    return dag.container().from("node:21")
+      .withDirectory("/home/node", source)
+      .withWorkdir("/home/node")
+      .withExec(["npm", "install"])
+      .withEntrypoint(["npm", "start"])
+  }
+
+  /**
+   * Publish an image
+   *
+   * example: dagger call publish --source . --project PROJECT
+   *   --location LOCATION --repository REPOSITORY/APPNAME
+   *   --credential env:GOOGLE_JSON
+   */
+  @func()
+  async publish(source: Directory, project: string, location: string, repository: string, credential: Secret): Promise<string> {
+    const registry = `${location}-docker.pkg.dev/${project}/${repository}`
+    return await this.build(source)
+      .withRegistryAuth(`${location}-docker.pkg.dev`, "_json_key", credential)
+      .publish(registry)
+  }
+
+  /**
+   * Deploy an image to Google Cloud Run
+   *
+   * example: dagger call deploy --source . --project PROJECT
+   *  --registry-location LOCATION --repository REPOSITORY/APPNAME
+   *  --service-location LOCATION --service SERVICE
+   *  --credential env:GOOGLE_JSON
+   */
+  @func()
+  async deploy(source: Directory, project: string, registryLocation: string, repository: string, serviceLocation: string, service: string, credential: Secret): Promise<string> {
+
+    const addr = await this.publish(source, project, registryLocation, repository, credential)
+
+    return dag.googleCloudRun().updateService(project, serviceLocation, service, addr, 3000, credential)
+  }
+}
