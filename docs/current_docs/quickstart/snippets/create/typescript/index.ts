c68bc835182f8ffa54022f4c7a3f4944b62e40d2
Vikram Vaswani <vikram@dagger.io>
2024-02-29 01:43:47 +0530

    Code fixes
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/quickstart/snippets/create/typescript/index.ts
+++ docs/current_docs/quickstart/snippets/create/typescript/index.ts
@@ -1,9 +1,9 @@
 import { dag, Directory, object, func } from "@dagger.io/dagger"
 
-@object
+@object()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 class MyModule {
-  @func
+  @func()
   async buildAndPublish(
     buildSrc: Directory,
     buildArgs: string[],
