543026b7ce8a166c7e91f35b76cadf4c1df78d95
Jenn Allen <jenn.allen@gmail.com>
2024-02-29 15:55:36 -0500

    Add kapa to docs
    Signed-off-by: Jenn Allen <jenn.allen@gmail.com>

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -4,48 +4,48 @@
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
 const path = require("path");
-import {themes as prismThemes} from 'prism-react-renderer';
-import remarkCodeImport from 'remark-code-import';
+import { themes as prismThemes } from "prism-react-renderer";
+import remarkCodeImport from "remark-code-import";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Dagger',
-  favicon: 'img/favicon.png',
+  title: "Dagger",
+  favicon: "img/favicon.png",
 
   // Set the production url of your site here
-  url: 'https://docs.dagger.io',
+  url: "https://docs.dagger.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   //organizationName: 'facebook', // Usually your GitHub org/user name.
   //projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'throw',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
   markdown: {
     mermaid: true,
   },
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
           breadcrumbs: false,
           path: "./current_docs",
-          routeBasePath: '/',
-          sidebarPath: './sidebars.js',
-          editUrl: 'https://github.com/dagger/dagger/edit/main/docs',
+          routeBasePath: "/",
+          sidebarPath: "./sidebars.js",
+          editUrl: "https://github.com/dagger/dagger/edit/main/docs",
           remarkPlugins: [
             [remarkCodeImport, { allowImportingFromOutside: true }],
           ],
@@ -57,59 +57,77 @@ const config = {
       },
     ],
   ],
+  scripts: [
+    {
+      src: "https://widget.kapa.ai/kapa-widget.bundle.js",
+      "data-website-id": "cd096dd4-a417-48fa-b9aa-1e7818babec5",
+      "data-project-name": "Dagger",
+      "data-project-color": "#121226",
+      "data-project-logo":
+        "https://i.ibb.co/m42W777/No-Margins-LOGO-Dagger-Color-On-Black-Badge.png",
+      async: true,
+    },
+  ],
   plugins: [
     "docusaurus-plugin-sass",
     "docusaurus-plugin-image-zoom",
-    [path.resolve(__dirname, "plugins/docusaurus-plugin-guides"), {
-      currentGuidesPath: "./current_docs/guides",
-    }],
+    [
+      path.resolve(__dirname, "plugins/docusaurus-plugin-guides"),
+      {
+        currentGuidesPath: "./current_docs/guides",
+      },
+    ],
     [
       "posthog-docusaurus",
       {
         appUrl: "https://dagger.io/analytics",
-        apiKey: "phc_rykA1oJnBnxTwavpgJKr4RAVXEgCkpyPVi21vQ7906d"
-      }
+        apiKey: "phc_rykA1oJnBnxTwavpgJKr4RAVXEgCkpyPVi21vQ7906d",
+      },
     ],
     [
       "docusaurus-plugin-typedoc",
       {
         id: "current-generation",
-        entryPoints: ['../sdk/typescript/connect.ts', '../sdk/typescript/api/client.gen.ts', '../sdk/typescript/common/errors/index.ts'],
-        tsconfig: '../sdk/typescript/tsconfig.json',
-        out: '../current_docs/reference/typescript/',
+        entryPoints: [
+          "../sdk/typescript/connect.ts",
+          "../sdk/typescript/api/client.gen.ts",
+          "../sdk/typescript/common/errors/index.ts",
+        ],
+        tsconfig: "../sdk/typescript/tsconfig.json",
+        out: "../current_docs/reference/typescript/",
         excludeProtected: true,
-        exclude: '../sdk/typescript/node_modules/**',
+        exclude: "../sdk/typescript/node_modules/**",
         skipErrorChecking: true,
         disableSources: true,
         sidebar: {
-          categoryLabel: 'TypeScript SDK Reference',
+          categoryLabel: "TypeScript SDK Reference",
         },
         frontmatter: {
-          displayed_sidebar: 'current',
-          sidebar_label: 'TypeScript SDK Reference',
-          title: "TypeScript SDK Reference"
+          displayed_sidebar: "current",
+          sidebar_label: "TypeScript SDK Reference",
+          title: "TypeScript SDK Reference",
         },
         hideMembersSymbol: true,
-        requiredToBeDocumented: ["Class"]
+        requiredToBeDocumented: ["Class"],
       },
     ],
   ],
-  themes: ['@docusaurus/theme-mermaid'],
+  themes: ["@docusaurus/theme-mermaid"],
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       sidebarCollapsed: false,
-      metadata: [{ name: 'og:image', content: '/img/favicon.png' }],
+      metadata: [{ name: "og:image", content: "/img/favicon.png" }],
       prism: {
         additionalLanguages: ["php", "rust", "elixir", "bash", "toml"],
         theme: prismThemes.dracula,
       },
       announcementBar: {
-        id: 'changed_docs',
+        id: "changed_docs",
         content:
           'We\'ve recently updated our documentation. For the previous documentation, visit <a target="_blank" rel="noopener noreferrer" href="https://archive.docs.dagger.io/0.9/">archive.docs.dagger.io/0.9/</a>.',
-        backgroundColor: '#3d66ff',
-        textColor: '#ffffff',
+        backgroundColor: "#3d66ff",
+        textColor: "#ffffff",
         isCloseable: false,
       },
       navbar: {
@@ -117,7 +135,7 @@ const config = {
           alt: "Dagger Logo",
           src: "img/dagger-logo-white.svg",
           height: "50px",
-          href: "https://dagger.io/"
+          href: "https://dagger.io/",
         },
         items: [
           {
@@ -153,15 +171,15 @@ const config = {
         defaultMode: "light",
       },
       zoom: {
-        selector: '.markdown img:not(.not-zoom)',
+        selector: ".markdown img:not(.not-zoom)",
         background: {
-          light: 'rgb(255, 255, 255)',
-          dark: 'rgb(50, 50, 50)'
+          light: "rgb(255, 255, 255)",
+          dark: "rgb(50, 50, 50)",
         },
         // medium-zoom configuration options
         // Refer to https://github.com/francoischalifour/medium-zoom#options
-        config: {}
-      }
+        config: {},
+      },
     }),
 };
 
