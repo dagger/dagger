dagger/dagger

commit 62ced562b1bdd69f35b5112d0d2736a2d2189c7a

fix: remove global Client property on generated structs (#6716)
* fix: remove global Client property on generated structs

Signed-off-by: Justin Chadwell <me@jedevc.com>

* fix: add GraphQLClient to module client

Signed-off-by: Justin Chadwell <me@jedevc.com>

---------

Signed-off-by: Justin Chadwell <me@jedevc.com>

cmd/codegen/generator/go/templates/src/_types/object.go.tmpl
@@ -2,7 +2,6 @@
 {{ .Description | Comment }}
 type {{ .Name | FormatName }} struct {
 	Query *querybuilder.Selection
-	Client graphql.Client
 
     {{ range $field := .Fields }}
         {{- if $field.TypeRef.IsScalar }}
@@ -83,13 +82,12 @@ type {{ $field | FieldOptionsStructName }} struct {
 	{{- end }}
 	{{- end }}
     {{ if $convertID }}
-	return r, q.Execute(ctx, r.Client)
+	return r, q.Execute(ctx)
 
 	{{- else if $field.TypeRef.IsObject }}
 	{{ $typeName := $field.TypeRef | FormatOutputType }}
 	return &{{ $field.TypeRef | FormatOutputType }} {
 		Query: q,
-		Client: r.Client,
 	}
 
 	{{- else if or $field.TypeRef.IsScalar $field.TypeRef.IsList }}
@@ -109,8 +107,7 @@ type {{ $field | FieldOptionsStructName }} struct {
         for i := range fields {
             val := {{ $field.TypeRef | FormatOutputType | FormatArrayToSingleType }}{{"{"}}{{ $field | GetArrayField | FormatArrayField }}{{"}"}}
             {{- if $eleType | IsIDableObject }}
-              val.Query = querybuilder.Query().Select("load{{$eleType | ObjectName}}FromID").Arg("id", fields[i].Id)
-              val.Client = r.Client
+              val.Query = q.Root().Select("load{{$eleType | ObjectName}}FromID").Arg("id", fields[i].Id)
             {{- end }}
             out = append(out, val)
         }
@@ -130,14 +127,14 @@ type {{ $field | FieldOptionsStructName }} struct {
 	{{- if ne $typeName "Client" }}
 	    {{- if and $field.TypeRef.IsList (IsListOfObject $field.TypeRef) }}
 
-	err := q.Execute(ctx, r.Client)
+	err := q.Execute(ctx)
 	if err != nil {
 	    return nil, err
 	}
 
 	return convert(response), nil
 	    {{- else }}
-	return response, q.Execute(ctx, r.Client)
+	return response, q.Execute(ctx)
 	    {{- end }}
 	{{- else }}
 	return response, q.Execute(ctx, r.gql)

cmd/codegen/generator/go/templates/module_interfaces.go
@@ -202,7 +202,6 @@ The struct definition for the concrete implementation of the interface. e.g.:
 
 	type customIfaceImpl struct {
 		Query  *querybuilder.Selection
-		Client graphql.Client
 		id     *CustomIfaceID
 		str    *string
 		int    *int
@@ -212,7 +211,6 @@ The struct definition for the concrete implementation of the interface. e.g.:
 func (spec *parsedIfaceType) concreteStructDefCode() *Statement {
 	return Type().Id(spec.concreteStructName()).StructFunc(func(g *Group) {
 		g.Id("Query").Op("*").Qual("querybuilder", "Selection")
-		g.Id("Client").Qual("graphql", "Client")
 		g.Id("id").Op("*").Id(spec.idTypeName())
 
 		for _, method := range spec.methods {
@@ -231,12 +229,11 @@ func (spec *parsedIfaceType) concreteStructDefCode() *Statement {
 /*
 The Load*FromID method attached to the top-level Client struct for this interface. e.g.:
 
-	func (r *Client) LoadCustomIfaceFromID(id CustomIfaceID) CustomIface {
+	func LoadCustomIfaceFromID(r *Client, id CustomIfaceID) CustomIface {
 		q := r.Query.Select("loadTestCustomIfaceFromID")
 		q = q.Arg("id", id)
 		return &customIfaceImpl{
 			Query:  q,
-			Client: r.Client,
 		}
 	}
 */
@@ -249,8 +246,7 @@ func (spec *parsedIfaceType) loadFromIDMethodCode() *Statement {
 			g.Id("q").Op(":=").Id("r").Dot("Query").Dot("Select").Call(Lit(loadFromIDGQLFieldName(spec)))
 			g.Id("q").Op("=").Id("q").Dot("Arg").Call(Lit("id"), Id("id"))
 			g.Return(Op("&").Id(spec.concreteStructName()).Values(Dict{
-				Id("Query"):  Id("q"),
-				Id("Client"): Id("r").Dot("Client"),
+				Id("Query"): Id("q"),
 			}))
 		})
 }
@@ -465,7 +461,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 				q := r.Query.Select("void")
 				var response Void
 				q = q.Bind(&response)
-				return q.Execute(ctx, r.Client)
+				return q.Execute(ctx)
 		*/
 
 		implTypeCode, err := spec.concreteMethodImplTypeCode(method.returnSpec)
@@ -475,7 +471,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 		s.Var().Id("response").Add(implTypeCode).Line()
 		s.Id("q").Op("=").Id("q").Dot("Bind").Call(Op("&").Id("response")).Line()
 		s.Return(
-			Id("q").Dot("Execute").Call(Id("ctx"), Id("r").Dot("Client")),
+			Id("q").Dot("Execute").Call(Id("ctx")),
 		)
 
 	case *parsedPrimitiveType:
@@ -485,7 +481,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 				q := r.Query.Select("str")
 				var response string
 				q = q.Bind(&response)
-				return response, q.Execute(ctx, r.Client)
+				return response, q.Execute(ctx)
 		*/
 
 		implTypeCode, err := spec.concreteMethodImplTypeCode(method.returnSpec)
@@ -496,7 +492,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 		s.Id("q").Op("=").Id("q").Dot("Bind").Call(Op("&").Id("response")).Line()
 		s.Return(
 			Id("response"),
-			Id("q").Dot("Execute").Call(Id("ctx"), Id("r").Dot("Client")),
+			Id("q").Dot("Execute").Call(Id("ctx")),
 		)
 
 	case *parsedIfaceTypeReference, *parsedObjectTypeReference:
@@ -504,7 +500,6 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 			Just object type with chained query (no error). e.g.:
 
 				return &customIfaceImpl{
-					Client: r.Client,
 					Query:  q,
 				}
 		*/
@@ -514,8 +509,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 			return nil, fmt.Errorf("failed to generate return type code: %w", err)
 		}
 		s.Return(Op("&").Add(implTypeCode).Values(Dict{
-			Id("Query"):  Id("q"),
-			Id("Client"): Id("r").Dot("Client"),
+			Id("Query"): Id("q"),
 		}))
 
 	case *parsedSliceType:
@@ -530,7 +524,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 						Id DirectoryID
 					}
 					q = q.Bind(&idResults)
-					err := q.Execute(ctx, r.Client)
+					err := q.Execute(ctx)
 					if err != nil {
 						return nil, err
 					}
@@ -539,8 +533,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 						id := idResult.Id
 
 						results = append(results, &Directory{
-							Query:  querybuilder.Query().Select("loadDirectoryFromID").Arg("id", id),
-							Client: r.Client,
+							Query:  q.Query.Root().Select("loadDirectoryFromID").Arg("id", id),
 						})
 					}
 					return results, nil
@@ -554,7 +547,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 			s.Var().Id("idResults").Index().Struct(Id("Id").Id(idScalarName)).Line()
 			s.Id("q").Op("=").Id("q").Dot("Bind").Call(Op("&").Id("idResults")).Line()
 
-			s.Id("err").Op(":=").Id("q").Dot("Execute").Call(Id("ctx"), Id("r").Dot("Client")).Line()
+			s.Id("err").Op(":=").Id("q").Dot("Execute").Call(Id("ctx")).Line()
 			s.If(Id("err").Op("!=").Nil()).Block(Return(Nil(), Id("err"))).Line()
 
 			underlyingReturnTypeCode, err := spec.concreteMethodSigTypeCode(returnType.underlying)
@@ -569,8 +562,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 			s.For(List(Id("_"), Id("idResult")).Op(":=").Range().Id("idResults")).BlockFunc(func(g *Group) {
 				g.Id("id").Op(":=").Id("idResult").Dot("Id").Line()
 				d := Dict{
-					Id("Query"):  Id("querybuilder").Dot("Query").Call().Dot("Select").Call(Lit(loadFromIDQueryName)).Dot("Arg").Call(Lit("id"), Id("id")),
-					Id("Client"): Id("r").Dot("Client"),
+					Id("Query"): Id("r").Dot("Query").Dot("Root").Call().Dot("Select").Call(Lit(loadFromIDQueryName)).Dot("Arg").Call(Lit("id"), Id("id")),
 					// FIXME: this is a nice optimization, but we can't enable
 					// it since the private "id" field can only be accessed on
 					// local structs
@@ -587,7 +579,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 
 					var response []string
 					q = q.Bind(&response)
-					return response, q.Execute(ctx, r.Client)
+					return response, q.Execute(ctx)
 			*/
 
 			implTypeCode, err := spec.concreteMethodImplTypeCode(method.returnSpec)
@@ -598,7 +590,7 @@ func (spec *parsedIfaceType) concreteMethodExecuteQueryCode(method *funcTypeSpec
 			s.Id("q").Op("=").Id("q").Dot("Bind").Call(Op("&").Id("response")).Line()
 			s.Return(
 				Id("response"),
-				Id("q").Dot("Execute").Call(Id("ctx"), Id("r").Dot("Client")),
+				Id("q").Dot("Execute").Call(Id("ctx")),
 			)
 
 		default:

cmd/codegen/generator/go/templates/src/_dagger.gen.go/defs.go.tmpl
@@ -115,8 +115,8 @@ func (e *ExecError) Unwrap() error {
 
 {{ if IsModuleCode }}
 type Client struct {
-	Client graphql.Client
 	Query *querybuilder.Selection
+	client graphql.Client
 }
 
 var dag *Client
@@ -124,13 +124,18 @@ var dag *Client
 func init() {
 	gqlClient, q := getClientParams()
 	dag = &Client{
-		Client: gqlClient,
-		Query: q,
+		Query: q.Client(gqlClient),
+		client: gqlClient,
 	}
 }
 
 func Connect() *Client {
 	return dag
+}
+
+// GraphQLClient returns the underlying graphql.Client
+func (c *Client) GraphQLClient() graphql.Client {
+	return c.client
 }
 
 func getClientParams() (graphql.Client, *querybuilder.Selection) {

cmd/dagger/functions.go
@@ -528,9 +528,9 @@ func (fc *FuncCommand) makeSubCmd(dag *dagger.Client, fn *modFunction) *cobra.Co
 
 			var response any
 
-			q := fc.q.Bind(&response)
+			q := fc.q.Bind(&response).Client(dag.GraphQLClient())
 
-			if err := q.Execute(ctx, dag.Client); err != nil {
+			if err := q.Execute(ctx); err != nil {
 				return fmt.Errorf("response from query: %w", err)
 			}
 

core/integration/module_test.go
@@ -2919,7 +2919,7 @@ type Test struct{}
 
 func (m *Test) FnA(ctx context.Context) (string, error) {
 	resp := &graphql.Response{}
-	err := dag.Client.MakeRequest(ctx, &graphql.Request{
+	err := dag.GraphQLClient().MakeRequest(ctx, &graphql.Request{
 		Query: "{test{fnB}}",
 	}, resp)
 	if err != nil {

sdk/go/client.go
@@ -17,8 +17,8 @@ import (
 type Client struct {
 	conn engineconn.EngineConn
 
-	Client graphql.Client
 	Query  *querybuilder.Selection
+	client graphql.Client
 }
 
 // ClientOpt holds a client option
@@ -75,8 +75,8 @@ func Connect(ctx context.Context, opts ...ClientOpt) (*Client, error) {
 	gql := errorWrappedClient{graphql.NewClient("http://"+conn.Host()+"/query", conn)}
 
 	c := &Client{
-		Client: gql,
-		Query:  querybuilder.Query(),
+		Query:  querybuilder.Query().Client(gql),
+		client: gql,
 		conn:   conn,
 	}
 
@@ -93,7 +93,7 @@ func Connect(ctx context.Context, opts ...ClientOpt) (*Client, error) {
 
 // GraphQLClient returns the underlying graphql.Client
 func (c *Client) GraphQLClient() graphql.Client {
-	return c.Client
+	return c.client
 }
 
 // Close the engine connection
@@ -112,7 +112,7 @@ func (c *Client) Do(ctx context.Context, req *Request, resp *Response) error {
 		r.Errors = resp.Errors
 		r.Extensions = resp.Extensions
 	}
-	return c.Client.MakeRequest(ctx, &graphql.Request{
+	return c.client.MakeRequest(ctx, &graphql.Request{
 		Query:     req.Query,
 		Variables: req.Variables,
 		OpName:    req.OpName,

sdk/go/querybuilder/querybuilder.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"runtime/debug"
 	"strings"
 	"sync"
 
@@ -22,6 +23,8 @@ type Selection struct {
 	bind  interface{}
 
 	prev *Selection
+
+	client graphql.Client
 }
 
 func (s *Selection) path() []*Selection {
@@ -33,11 +36,18 @@ func (s *Selection) path() []*Selection {
 	return selections
 }
 
+func (s *Selection) Root() *Selection {
+	return &Selection{
+		client: s.client,
+	}
+}
+
 func (s *Selection) SelectWithAlias(alias, name string) *Selection {
 	sel := &Selection{
-		name:  name,
-		prev:  s,
-		alias: alias,
+		name:   name,
+		prev:   s,
+		alias:  alias,
+		client: s.client,
 	}
 	return sel
 }
@@ -147,14 +157,25 @@ func (s *Selection) unpack(data interface{}) error {
 	return nil
 }
 
-func (s *Selection) Execute(ctx context.Context, c graphql.Client) error {
+func (s *Selection) Client(c graphql.Client) *Selection {
+	sel := *s
+	sel.client = c
+	return &sel
+}
+
+func (s *Selection) Execute(ctx context.Context) error {
+	if s.client == nil {
+		debug.PrintStack()
+		return fmt.Errorf("no client configured for selection")
+	}
+
 	query, err := s.Build(ctx)
 	if err != nil {
 		return err
 	}
 
 	var response any
-	err = c.MakeRequest(ctx,
+	err = s.client.MakeRequest(ctx,
 		&graphql.Request{
 			Query: query,
 		},

commit 468145491c16f04969a10302a895e1243f364cfc

fix: propagate graphql client to child clients
This regressed in 62ced562b1bdd69f35b5112d0d2736a2d2189c7a, and meant
that child clients were invalid, and couldn't be called with `Do()`, as
well as causing `GraphQLClient()` to return an invalid nil value.

This only previously worked because the GraphQL client and the dagger
client shared the same name `client`. When this property was removed,
the propagation stopped working. Now, we make this special case
explicit.

Signed-off-by: Justin Chadwell <me@jedevc.com>

cmd/codegen/generator/go/templates/src/_types/object.go.tmpl
@@ -88,6 +88,9 @@ type {{ $field | FieldOptionsStructName }} struct {
 	{{ $typeName := $field.TypeRef | FormatOutputType }}
 	return &{{ $field.TypeRef | FormatOutputType }} {
 		Query: q,
+		{{- if eq $typeName "Client" }}
+		client: r.client,
+		{{ end }}
 	}
 
 	{{- else if or $field.TypeRef.IsScalar $field.TypeRef.IsList }}

sdk/go/dagger.gen.go
@@ -5592,7 +5592,8 @@ func (r *Client) Pipeline(name string, opts ...PipelineOpts) *Client {
 	q = q.Arg("name", name)
 
 	return &Client{
-		Query: q,
+		Query:  q,
+		client: r.client,
 	}
 }
 

core/integration/pipeline_test.go
@@ -14,6 +14,27 @@ func TestPipeline(t *testing.T) {
 
 	cacheBuster := fmt.Sprintf("%d", time.Now().UTC().UnixNano())
 
+	t.Run("client pipeline", func(t *testing.T) {
+		t.Parallel()
+
+		var logs safeBuffer
+		c, ctx := connect(t, dagger.WithLogOutput(&logs))
+
+		_, err := c.
+			Pipeline("client pipeline").
+			Container().
+			From(alpineImage).
+			WithExec([]string{"echo", cacheBuster}).
+			Sync(ctx)
+
+		require.NoError(t, err)
+
+		require.NoError(t, c.Close()) // close + flush logs
+
+		t.Log(logs.String())
+		require.Contains(t, logs.String(), "client pipeline")
+	})
+
 	t.Run("container pipeline", func(t *testing.T) {
 		t.Parallel()
 
@@ -86,6 +107,14 @@ func TestPipeline(t *testing.T) {
 		require.Contains(t, logs.String(), "service "+hostname)
 		require.Regexp(t, `start python -m http.server.*DONE`, logs.String())
 	})
+}
+
+func TestPipelineGraphQLClient(t *testing.T) {
+	t.Parallel()
+
+	c, _ := connect(t)
+	require.NotNil(t, c.GraphQLClient())
+	require.NotNil(t, c.Pipeline("client pipeline").GraphQLClient())
 }
 
 func TestInternalVertexes(t *testing.T) {