d55e791204eef40360210f4e6760ee66cb4ffaad
Vikram Vaswani <vikram@dagger.io>
2024-03-12 12:25:49 +0530

    Updated code
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/developer-guide/go/snippets/services/argument/main.go
+++ docs/current_docs/developer-guide/go/snippets/services/argument/main.go
@@ -7,14 +7,14 @@ import (
 type MyModule struct{}
 
 // sends a query to a MariaDB service received as input and returns the response
-func (m *MyModule) UserList(ctx context.Context, svc *Service) string {
+func (m *MyModule) UserList(ctx context.Context, svc *Service) (string, error) {
 	out, err := dag.Container().
 		From("mariadb:10.11.2").
 		WithServiceBinding("db", svc).
 		WithExec([]string{"/bin/sh", "-c", "/usr/bin/mysql --user=root --password=secret --host=db -e 'SELECT Host, User FROM mysql.user'"}).
 		Stdout(ctx)
 	if err != nil {
-		return err
+		return "", err
 	}
-	return out
+	return out, nil
 }
