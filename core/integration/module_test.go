37c97394d52b91f07c8298f1eac8d805411a8981
Helder Correia <174525+helderco@users.noreply.github.com>
2024-03-14 23:15:36 -0100

    Fix tests
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- core/integration/module_test.go
+++ core/integration/module_test.go
@@ -1449,6 +1449,14 @@ class Test:
     foo: str = field(default="foo")
 `,
 				},
+				{
+					file: "pyproject.toml",
+					contents: `
+                        [project]
+                        name = "main"
+                        version = "0.0.0"
+                        `,
+				},
 			},
 		},
 		{
@@ -1781,7 +1789,7 @@ class Test {
 
 			c, ctx := connect(t)
 
-			modGen := modInit(ctx, t, c, tc.sdk, tc.source)
+			modGen := modInit(t, c, tc.sdk, tc.source)
 
 			q := heredoc.Doc(`
                 query {
@@ -2006,7 +2014,7 @@ class Test {
 			t.Parallel()
 			c, ctx := connect(t)
 
-			out, err := modInit(ctx, t, c, tc.sdk, tc.source).
+			out, err := modInit(t, c, tc.sdk, tc.source).
 				With(daggerQuery(`{test{repeater(msg:"echo!", times: 3){render}}}`)).
 				Stdout(ctx)
 
@@ -3833,7 +3841,7 @@ class Test {
 			t.Run(tc.sdk, func(t *testing.T) {
 				t.Parallel()
 				c, ctx := connect(t)
-				ctr := modInit(ctx, t, c, tc.sdk, tc.source)
+				ctr := modInit(t, c, tc.sdk, tc.source)
 
 				out, err := ctr.With(daggerCall("--foo=abc", "--baz=x,y,z", "--dir=.", "foo")).Stdout(ctx)
 				require.NoError(t, err)
@@ -4323,13 +4331,7 @@ def potato_%d() -> str:
 `, i, i)
 		}
 
-		modGen := c.Container().From(golangImage).
-			WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
-			WithWorkdir("/work").
-			WithNewFile("./src/main.py", dagger.ContainerWithNewFileOpts{
-				Contents: mainSrc,
-			}).
-			With(daggerExec("init", "--source=.", "--name=potatoSack", "--sdk=python"))
+		modGen := pythonModInit(t, c, mainSrc)
 
 		var eg errgroup.Group
 		for i := 0; i < funcCount; i++ {
@@ -4588,7 +4590,7 @@ func TestModuleReservedWords(t *testing.T) {
 					t.Parallel()
 					c, ctx := connect(t)
 
-					out, err := modInit(ctx, t, c, tc.sdk, tc.source).
+					out, err := modInit(t, c, tc.sdk, tc.source).
 						With(daggerQuery(`{test{fn(id:"YES!!!!")}}`)).
 						Stdout(ctx)
 					require.NoError(t, err)
@@ -5727,7 +5729,7 @@ func sdkCodegenFile(t *testing.T, sdk string) string {
 	}
 }
 
-func modInit(ctx context.Context, t *testing.T, c *dagger.Client, sdk, contents string) *dagger.Container {
+func modInit(t *testing.T, c *dagger.Client, sdk, contents string) *dagger.Container {
 	t.Helper()
 	modGen := c.Container().From(golangImage).
 		WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
