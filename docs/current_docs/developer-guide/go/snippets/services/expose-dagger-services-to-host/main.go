b0807a48605bf9aa47a11be8bb71c466c102d6e1
Vikram Vaswani <vikram@dagger.io>
2024-03-16 11:57:13 +0530

    Updated previous snippets
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/developer-guide/go/snippets/services/expose-dagger-services-to-host/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"io"
+	"net/http"
+
+	// FIXME
+	"dagger.io/dagger/dag"
+)
+
+type MyModule struct{}
+
+// starts and returns an HTTP service, then exposes it to the host
+func (m *MyModule) HttpService(ctx context.Context) (string, error) {
+	// create HTTP service container with exposed port 8080
+	httpSrv := dag.Container().
+		From("python").
+		WithWorkdir("/srv").
+		WithNewFile("index.html", ContainerWithNewFileOpts{
+			Contents: "Hello, world!",
+		}).
+		WithExec([]string{"python", "-m", "http.server", "8080"}).
+		WithExposedPort(8080).
+		AsService()
+
+	// expose HTTP service to host
+	// FIXME
+	tunnel, err := dag.Host().Tunnel(httpSrv).Start(ctx)
+	if err != nil {
+		return "", err
+	}
+	defer tunnel.Stop(ctx)
+
+	// get HTTP service address
+	srvAddr, err := tunnel.Endpoint(ctx)
+	if err != nil {
+		return "", err
+	}
+
+	// access HTTP service from host
+	res, err := http.Get("http://" + srvAddr)
+	if err != nil {
+		return "", err
+	}
+	defer res.Body.Close()
+
+	// print response
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	return string(body), nil
+}
