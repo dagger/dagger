b9b0574632ec276df05467e53babfe093a8d9b8e
Vikram Vaswani <vikram@dagger.io>
2024-04-04 23:51:05 +0530

    Ports Spring guide to use functions
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/guides/snippets/build-test-publish-java-spring/go/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+)
+
+type MyModule struct{}
+
+func (m *MyModule) Build(ctx context.Context, source *Directory) (string, error) {
+
+	mariadb := dag.Mariadb().Serve(dagger.MariadbServeOpts{Version: "10.11.2", DbName: "petclinic"})
+
+	dockerd := dag.Docker().Engine()
+
+	app := dag.Java().
+		WithJdk("17").
+		WithMaven("3.9.5").
+		WithProject(source.WithoutDirectory("dagger")).
+		Maven([]string{"-Dspring.profiles.active=mysql", "clean", "package"})
+
+	build := app.WithServiceBinding("db", mariadb).
+		WithServiceBinding("docker", dockerd).
+		WithEnvVariable("MYSQL_URL", "jdbc:mysql://db/petclinic").
+		WithEnvVariable("MYSQL_USER", "root").
+		WithEnvVariable("MYSQL_PASS", "")
+
+	deploy := dag.Container().
+		From("eclipse-temurin:17-alpine").
+		WithDirectory("/app", build.Directory("./target")).
+		WithEntrypoint([]string{"java", "-jar", "-Dspring.profiles.active=mysql", "/app/spring-petclinic-3.0.0-SNAPSHOT.jar"})
+
+	address, err := deploy.Publish(ctx, "ttl.sh/myapp")
+	if err != nil {
+		return "", err
+	}
+	return address, nil
+}
