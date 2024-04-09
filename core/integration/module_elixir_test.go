341eac035c30569f5c521e3792af6a95cca175e3
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-04-10 00:43:56 +0700

    test(sdk/elixir): use daggerCliBase
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- core/integration/module_elixir_test.go
+++ core/integration/module_elixir_test.go
@@ -15,9 +15,7 @@ func TestModuleElixirInit(t *testing.T) {
 
 		c, ctx := connect(t)
 
-		modGen := c.Container().From(golangImage).
-			WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
-			WithWorkdir("/work").
+		modGen := daggerCliBase(t, c).
 			With(daggerExec("init", "--name=bare", "--sdk=elixir"))
 
 		out, err := modGen.
@@ -33,10 +31,12 @@ func TestModuleElixirInit(t *testing.T) {
 		c, ctx := connect(t)
 
 		for _, name := range []string{"My-Module", "MyModule"} {
-			modGen := c.Container().From(golangImage).
-				WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
-				WithWorkdir("/work").
-				With(daggerExec("init", "--name="+name, "--sdk=elixir"))
+			modGen := daggerCliBase(t, c).
+				With(daggerExec("init", "-vv", "--name="+name, "--sdk=elixir"))
+
+			sourceEnts, err := modGen.Directory("dagger").Entries(ctx)
+			require.NoError(t, err)
+			require.Contains(t, sourceEnts, "my_module")
 
 			out, err := modGen.
 				With(daggerQuery(`{myModule{containerEcho(stringArg:"hello"){stdout}}}`)).
@@ -52,10 +52,8 @@ func TestModuleElixirInit(t *testing.T) {
 
 		c, ctx := connect(t)
 
-		modGen := c.Container().From(golangImage).
-			WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
-			WithWorkdir("/work").
-			With(daggerExec("init", "--name=bare", "--sdk=elixir", "--source=some/subdir"))
+		modGen := daggerCliBase(t, c).
+			With(daggerExec("init", "-vv", "--name=bare", "--sdk=elixir", "--source=some/subdir"))
 
 		out, err := modGen.
 			With(daggerQuery(`{bare{containerEcho(stringArg:"hello"){stdout}}}`)).
@@ -63,7 +61,7 @@ func TestModuleElixirInit(t *testing.T) {
 		require.NoError(t, err)
 		require.JSONEq(t, `{"bare":{"containerEcho":{"stdout":"hello\n"}}}`, out)
 
-		sourceSubdirEnts, err := modGen.Directory("/work/some/subdir").Entries(ctx)
+		sourceSubdirEnts, err := modGen.Directory("some/subdir").Entries(ctx)
 		require.NoError(t, err)
 		require.Contains(t, sourceSubdirEnts, "bare")
 
