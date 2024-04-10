0c2f38ed7e88bd7230936a61668bcf81ea94aad4
Alex Suraci <alex@dagger.io>
2024-04-10 17:56:53 -0400

    partially working spring/mariadb setup
    Docker still failing - can it be disabled instead? Seems distracting
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- docs/current_docs/guides/snippets/build-test-publish-java-spring/go/main.go
+++ docs/current_docs/guides/snippets/build-test-publish-java-spring/go/main.go
@@ -8,29 +8,33 @@ type MyModule struct{}
 
 func (m *MyModule) Build(ctx context.Context, source *Directory) (string, error) {
 
-	mariadb := dag.Container().
-		From("mariadb:10.11.2").
-		WithEnvVariable("MARIADB_USER", "petclinic").
-		WithEnvVariable("MARIADB_PASSWORD", "petclinic").
-		WithEnvVariable("MARIADB_DATABASE", "petclinic").
-		WithEnvVariable("MARIADB_ROOT_PASSWORD", "root").
-		WithExposedPort(3306).
-		AsService()
+	mariadb := dag.Mariadb().Serve(MariadbServeOpts{Version: "10.11.2", DbName: "petclinic"})
+
+	// TODO: this doesn't work because the docker CLI isn't installed. And
+	// installing it just gives us a broken podman CLI. Whee.
+	//
+	// Can we just disable the tests that use Docker?
+	// dockerd := dag.Docker().Engine()
 
 	app := dag.Java().
 		WithJdk("17").
 		WithMaven("3.9.5").
 		WithProject(source.WithoutDirectory("dagger")).
-		Maven([]string{"-X", "-Dspring.profiles.active=mysql", "clean", "package"})
-
-	build := app.WithServiceBinding("db", mariadb).
+		Container().
+		WithServiceBinding("db", mariadb).
 		WithEnvVariable("MYSQL_URL", "jdbc:mysql://db/petclinic").
-		WithEnvVariable("MYSQL_USER", "petclinic").
-		WithEnvVariable("MYSQL_PASS", "petclinic")
+		WithEnvVariable("MYSQL_USER", "root").
+		WithEnvVariable("MYSQL_PASS", "").
+		// TODO: this gets us kinda far, but we still don't have the 'docker' CLI installed
+		// WithServiceBinding("docker", dockerd).
+		// WithEnvVariable("DOCKER_HOST", "tcp://docker:2375").
+		// TODO: this is fool's gold, it just gives us a broken podman CLI
+		// WithExec([]string{"microdnf", "install", "docker"}).
+		WithExec([]string{"mvn", "-Dspring.profiles.active=mysql", "clean", "package"})
 
 	deploy := dag.Container().
 		From("eclipse-temurin:17-alpine").
-		WithDirectory("/app", build.Directory("./target")).
+		WithDirectory("/app", app.Directory("./target")).
 		WithEntrypoint([]string{"java", "-jar", "-Dspring.profiles.active=mysql", "/app/spring-petclinic-3.0.0-SNAPSHOT.jar"})
 
 	address, err := deploy.Publish(ctx, "ttl.sh/myapp")
