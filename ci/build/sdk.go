fb26e9e38ea9b3bdb99a2c20ecc1260e51303eef
Justin Chadwell <me@jedevc.com>
2024-04-22 12:17:52 +0100

    ci: avoid panic in sdk building
    Using functions that can error in `Container.With` is a bad idea. So,
    what we can do is to put the actual computation into an error group
    (which has the convenient little side-effect that they all get computed
    in parallel, hopefully improving build times).
    
    This lets us remove the panic, which was crashing the binary. Because of
    (I think) telemetry draining, this meant that the client job hung
    forever on this kind of failure.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- ci/build/sdk.go
+++ ci/build/sdk.go
@@ -12,114 +12,53 @@ import (
 	"github.com/dagger/dagger/ci/internal/dagger"
 )
 
-func (build *Builder) pythonSDKContent(ctx context.Context) dagger.WithContainerFunc {
-	return func(ctr *dagger.Container) *dagger.Container {
-		rootfs := dag.Directory().WithDirectory("/", build.source.Directory("sdk/python"), dagger.DirectoryWithDirectoryOpts{
-			Include: []string{
-				"pyproject.toml",
-				"src/**/*.py",
-				"src/**/*.typed",
-				"codegen/src/**/*.py",
-				"codegen/pyproject.toml",
-				"codegen/requirements.lock",
-				"runtime/",
-				"LICENSE",
-				"README.md",
-			},
+type sdkContent struct {
+	index   ocispecs.Index
+	sdkDir  *dagger.Directory
+	envName string
+}
+
+func (content *sdkContent) apply(ctr *dagger.Container) *dagger.Container {
+	manifest := content.index.Manifests[0]
+	manifestDgst := manifest.Digest.String()
+
+	return ctr.
+		WithEnvVariable(content.envName, manifestDgst).
+		WithDirectory(distconsts.EngineContainerBuiltinContentDir, content.sdkDir, dagger.ContainerWithDirectoryOpts{
+			Include: []string{"blobs/"},
+		})
+}
+
+type sdkContentF func(ctx context.Context) (*sdkContent, error)
+
+func (build *Builder) pythonSDKContent(ctx context.Context) (*sdkContent, error) {
+	rootfs := dag.Directory().WithDirectory("/", build.source.Directory("sdk/python"), dagger.DirectoryWithDirectoryOpts{
+		Include: []string{
+			"pyproject.toml",
+			"src/**/*.py",
+			"src/**/*.typed",
+			"codegen/src/**/*.py",
+			"codegen/pyproject.toml",
+			"codegen/requirements.lock",
+			"runtime/",
+			"LICENSE",
+			"README.md",
+		},
+	})
+
+	sdkCtrTarball := dag.Container().
+		WithRootfs(rootfs).
+		AsTarball(dagger.ContainerAsTarballOpts{
+			ForcedCompression: dagger.Uncompressed,
 		})
 
-		sdkCtrTarball := dag.Container().
-			WithRootfs(rootfs).
-			AsTarball(dagger.ContainerAsTarballOpts{
-				ForcedCompression: dagger.Uncompressed,
-			})
+	sdkDir := dag.Container().
+		From(consts.AlpineImage).
+		WithMountedDirectory("/out", dag.Directory()).
+		WithMountedFile("/sdk.tar", sdkCtrTarball).
+		WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
+		Directory("/out")
 
-		sdkDir := dag.Container().
-			From(consts.AlpineImage).
-			WithMountedDirectory("/out", dag.Directory()).
-			WithMountedFile("/sdk.tar", sdkCtrTarball).
-			WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
-			Directory("/out")
-
-		content, err := sdkContent(ctx, ctr, sdkDir, distconsts.PythonSDKManifestDigestEnvName)
-		if err != nil {
-			// FIXME: would be nice to not panic
-			panic(err)
-		}
-		return content
-	}
-}
-
-func (build *Builder) typescriptSDKContent(ctx context.Context) dagger.WithContainerFunc {
-	return func(ctr *dagger.Container) *dagger.Container {
-		rootfs := dag.Directory().WithDirectory("/", build.source.Directory("sdk/typescript"), dagger.DirectoryWithDirectoryOpts{
-			Include: []string{
-				"**/*.ts",
-				"LICENSE",
-				"README.md",
-				"runtime",
-				"package.json",
-				"dagger.json",
-			},
-			Exclude: []string{
-				"node_modules",
-				"dist",
-				"**/test",
-				"**/*.spec.ts",
-				"dev",
-			},
-		})
-		sdkCtrTarball := dag.Container().
-			WithRootfs(rootfs).
-			WithFile("/codegen", build.CodegenBinary()).
-			AsTarball(dagger.ContainerAsTarballOpts{
-				ForcedCompression: dagger.Uncompressed,
-			})
-
-		sdkDir := dag.Container().From("alpine:"+consts.AlpineVersion).
-			WithMountedDirectory("/out", dag.Directory()).
-			WithMountedFile("/sdk.tar", sdkCtrTarball).
-			WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
-			Directory("/out")
-
-		content, err := sdkContent(ctx, ctr, sdkDir, distconsts.TypescriptSDKManifestDigestEnvName)
-		if err != nil {
-			// FIXME: would be nice to not panic
-			panic(err)
-		}
-		return content
-	}
-}
-
-func (build *Builder) goSDKContent(ctx context.Context) dagger.WithContainerFunc {
-	return func(ctr *dagger.Container) *dagger.Container {
-		base := dag.Container(dagger.ContainerOpts{Platform: build.platform}).
-			From(fmt.Sprintf("golang:%s-alpine%s", consts.GolangVersion, consts.AlpineVersion))
-
-		sdkCtrTarball := base.
-			WithEnvVariable("GOTOOLCHAIN", "auto").
-			WithFile("/usr/local/bin/codegen", build.CodegenBinary()).
-			WithEntrypoint([]string{"/usr/local/bin/codegen"}).
-			AsTarball(dagger.ContainerAsTarballOpts{
-				ForcedCompression: dagger.Uncompressed,
-			})
-
-		sdkDir := base.
-			WithMountedDirectory("/out", dag.Directory()).
-			WithMountedFile("/sdk.tar", sdkCtrTarball).
-			WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
-			Directory("/out")
-
-		content, err := sdkContent(ctx, ctr, sdkDir, distconsts.GoSDKManifestDigestEnvName)
-		if err != nil {
-			// FIXME: would be nice to not panic
-			panic(err)
-		}
-		return content
-	}
-}
-
-func sdkContent(ctx context.Context, ctr *dagger.Container, sdkDir *dagger.Directory, envName string) (*dagger.Container, error) {
 	var index ocispecs.Index
 	indexContents, err := sdkDir.File("index.json").Contents(ctx)
 	if err != nil {
@@ -128,12 +67,91 @@ func sdkContent(ctx context.Context, ctr *dagger.Container, sdkDir *dagger.Direc
 	if err := json.Unmarshal([]byte(indexContents), &index); err != nil {
 		return nil, err
 	}
-	manifest := index.Manifests[0]
-	manifestDgst := manifest.Digest.String()
 
-	return ctr.
-		WithEnvVariable(envName, manifestDgst).
-		WithDirectory(distconsts.EngineContainerBuiltinContentDir, sdkDir, dagger.ContainerWithDirectoryOpts{
-			Include: []string{"blobs/"},
-		}), nil
+	return &sdkContent{
+		index:   index,
+		sdkDir:  sdkDir,
+		envName: distconsts.PythonSDKManifestDigestEnvName,
+	}, nil
+}
+
+func (build *Builder) typescriptSDKContent(ctx context.Context) (*sdkContent, error) {
+	rootfs := dag.Directory().WithDirectory("/", build.source.Directory("sdk/typescript"), dagger.DirectoryWithDirectoryOpts{
+		Include: []string{
+			"**/*.ts",
+			"LICENSE",
+			"README.md",
+			"runtime",
+			"package.json",
+			"dagger.json",
+		},
+		Exclude: []string{
+			"node_modules",
+			"dist",
+			"**/test",
+			"**/*.spec.ts",
+			"dev",
+		},
+	})
+	sdkCtrTarball := dag.Container().
+		WithRootfs(rootfs).
+		WithFile("/codegen", build.CodegenBinary()).
+		AsTarball(dagger.ContainerAsTarballOpts{
+			ForcedCompression: dagger.Uncompressed,
+		})
+
+	sdkDir := dag.Container().From("alpine:"+consts.AlpineVersion).
+		WithMountedDirectory("/out", dag.Directory()).
+		WithMountedFile("/sdk.tar", sdkCtrTarball).
+		WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
+		Directory("/out")
+
+	var index ocispecs.Index
+	indexContents, err := sdkDir.File("index.json").Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal([]byte(indexContents), &index); err != nil {
+		return nil, err
+	}
+
+	return &sdkContent{
+		index:   index,
+		sdkDir:  sdkDir,
+		envName: distconsts.TypescriptSDKManifestDigestEnvName,
+	}, nil
+}
+
+func (build *Builder) goSDKContent(ctx context.Context) (*sdkContent, error) {
+	base := dag.Container(dagger.ContainerOpts{Platform: build.platform}).
+		From(fmt.Sprintf("golang:%s-alpine%s", consts.GolangVersion, consts.AlpineVersion))
+
+	sdkCtrTarball := base.
+		WithEnvVariable("GOTOOLCHAIN", "auto").
+		WithFile("/usr/local/bin/codegen", build.CodegenBinary()).
+		WithEntrypoint([]string{"/usr/local/bin/codegen"}).
+		AsTarball(dagger.ContainerAsTarballOpts{
+			ForcedCompression: dagger.Uncompressed,
+		})
+
+	sdkDir := base.
+		WithMountedDirectory("/out", dag.Directory()).
+		WithMountedFile("/sdk.tar", sdkCtrTarball).
+		WithExec([]string{"tar", "xf", "/sdk.tar", "-C", "/out"}).
+		Directory("/out")
+
+	var index ocispecs.Index
+	indexContents, err := sdkDir.File("index.json").Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	if err := json.Unmarshal([]byte(indexContents), &index); err != nil {
+		return nil, err
+	}
+
+	return &sdkContent{
+		index:   index,
+		sdkDir:  sdkDir,
+		envName: distconsts.GoSDKManifestDigestEnvName,
+	}, nil
 }
