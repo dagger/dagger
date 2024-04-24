f7b4049177802df2a16a58cd1b989b181b85b791
Erik Sipsma <erik@sipsma.dev>
2024-04-24 14:18:13 -0700

    simplify sentinel logs to double check they are missing
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- cmd/engine/main.go
+++ cmd/engine/main.go
@@ -306,17 +306,13 @@ func main() { //nolint:gocyclo
 		// TODO:
 		// TODO:
 		// TODO:
-		lch := make(chan time.Time, 999999)
-		go func() {
-			for t := range time.Tick(200 * time.Millisecond) {
-				lch <- t
-			}
-		}()
 		go func() {
 			i := 0
-			for range lch {
-				bklog.G(ctx).Debugf("LOG SENTINEL: %d", i)
+			for {
+				t := time.Now().UnixNano()
+				fmt.Printf("LOG SENTINEL: %d %d\n", t, i)
 				i++
+				time.Sleep(200 * time.Millisecond)
 			}
 		}()
 
