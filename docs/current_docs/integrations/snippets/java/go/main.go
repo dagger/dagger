e9f9b921143ef31ef1c72e446c63b5f3440b5eb9
Vikram Vaswani <vikram@dagger.io>
2024-04-25 20:33:22 +0530

    Converted guide to integration page
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/integrations/snippets/java/go/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type MyModule struct{}
+
+func (m *MyModule) Build(ctx context.Context, source *Directory) *File {
+
+	return dag.Java().
+		WithJdk("17").
+		WithMaven("3.9.5").
+		WithProject(source.WithoutDirectory("dagger")).
+		Maven([]string{"package"}).
+		File("target/*.jar")
+}
+
+func (m *MyModule) Publish(ctx context.Context, source *Directory, version string, registryAddress string, registryUsername string, registryPassword *Secret, imageName string) (string, error) {
+
+	return dag.Container().
+		From("eclipse-temurin:17-alpine").
+		WithLabel("org.opencontainers.image.title", "Java with Dagger").
+		WithLabel("org.opencontainers.image.version", version).
+		WithFile("/app/myapp-0.0.1-SNAPSHOT.jar", m.Build(source)).
+		WithEntrypoint([]string{"java", "-jar", "/app/myapp-0.0.1-SNAPSHOT.jar"}).
+		WithRegistryAuth(registryAddress, registryUsername, registryPassword).
+		Publish(ctx, fmt.Sprintf("%s/%s/%s", registryAddress, registryUsername, imageName))
+}
