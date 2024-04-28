e919bcfd3360a14b668ce75012e28e7b5361212f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-04-28 23:27:50 +0700

    feat(sdk/elixir): use comment code from python
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- sdk/elixir/runtime/template.exs
+++ sdk/elixir/runtime/template.exs
@@ -60,7 +60,19 @@ defmodule Main do
   @module_ex """
   defmodule <%= @module %> do
     @moduledoc \"\"\"
-    Documentation for `<%= @module %>`.
+    A generated module for Main functions
+
+    This module has been generated via dagger init and serves as a reference to
+    basic module structure as you get started with Dagger.
+
+    Two functions have been pre-created. You can modify, delete, or add to them,
+    as needed. They demonstrate usage of arguments and return types using simple
+    echo and grep commands. The functions can be called from the dagger CLI or
+    from one of the SDKs.
+
+    The first line in this comment block is a short description line and the
+    rest is a long description with more detail on the module's purpose or usage,
+    if appropriate. All modules should have a short description.
     \"\"\"
 
     use Dagger.Mod, name: "<%= @module %>"
@@ -73,6 +85,9 @@ defmodule Main do
       ],
       return: Dagger.Container
     ]
+    @doc \"\"\"
+    Returns a container that echoes whatever string argument is provided.
+    \"\"\"
     def container_echo(self, args) do
       self.dag
       |> Dagger.Client.container()
@@ -87,6 +102,9 @@ defmodule Main do
       ],
       return: :string
     ]
+    @doc \"\"\"
+    Returns lines that match a pattern in the files of the provided Directory.
+    \"\"\"
     def grep_dir(self, %{directory_arg: directory, pattern: pattern}) do
       self.dag
       |> Dagger.Client.container()
