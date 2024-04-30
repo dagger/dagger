7227b07a06f9fb2d90bb0f0e82cd2cc9cbfe2d5f
Justin Chadwell <me@jedevc.com>
2024-04-30 10:57:37 +0100

    ci: simplify gpu support checks into switches
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- ci/build/builder.go
+++ ci/build/builder.go
@@ -203,21 +203,23 @@ func (build *Builder) Engine(ctx context.Context) (*dagger.Container, error) {
 		WithDirectory(distconsts.EngineDefaultStateDir, dag.Directory())
 
 	if build.gpuSupport {
-		if build.base != "ubuntu" && build.base != "wolfi" {
-			return nil, fmt.Errorf("gpu support requires %q base, not %q", "ubuntu or wolfi", build.base)
-		}
-		if build.platformSpec.Architecture != "amd64" {
+		switch build.platformSpec.Architecture {
+		case "amd64":
+		default:
 			return nil, fmt.Errorf("gpu support requires %q arch, not %q", "amd64", build.platformSpec.Architecture)
 		}
 
-		if build.base == "ubuntu" {
+		switch build.base {
+		case "ubuntu":
 			ctr = ctr.With(util.ShellCmd(`curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg`))
 			ctr = ctr.With(util.ShellCmd(`curl -s -L https://nvidia.github.io/libnvidia-container/experimental/"$(. /etc/os-release;echo $ID$VERSION_ID)"/libnvidia-container.list | sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | tee /etc/apt/sources.list.d/nvidia-container-toolkit.list`))
 			ctr = ctr.With(util.ShellCmd(`apt-get update && apt-get install -y nvidia-container-toolkit`))
-		} else if build.base == "wolfi" {
+		case "wolfi":
 			ctr = ctr.With(util.ShellCmd(`apk add chainguard-keys`))
 			ctr = ctr.With(util.ShellCmd(`echo "https://packages.cgr.dev/extras" >> /etc/apk/repositories`))
 			ctr = ctr.With(util.ShellCmd(`apk update && apk add nvidia-driver nvidia-tools`))
+		default:
+			return nil, fmt.Errorf("gpu support requires %q base, not %q", "ubuntu or wolfi", build.base)
 		}
 	}
 
