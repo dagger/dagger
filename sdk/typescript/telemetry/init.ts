32ca3e34a6dceb62a3a00a76860833dcc81df3ae
Helder Correia <174525+helderco@users.noreply.github.com>
2024-04-30 14:38:44 +0000

    Remove exported env variable names and fix initialization order/logic
    No need for `environment.ts`:
    - `otelEnabled` isn't necessary because only the global `OTEL_SDK_DISABLED` is used.
    - Env variables are already processed by the SDK so using that instead.
    - Removed `OTEL_SERVICE_NAME` default configuration because there's an easy way to set it in `NodeSDK` and that's preferred over something that can be overriden by users.
    - Replaced the explicit `_INSECURE` and `_ENDPOINT` env variables with a simple and generic logic.
    
    Refactored initialization:
    - Removed console.debug statements because they just print them into normal user output even under normal conditions. Should use a proper logger instead.
    - Added a `configure()` method to set `this._configured` properly, i.e., even if there's no `OTEL_` variables it shouldn't check for it twice.
    
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/typescript/telemetry/init.ts
+++ sdk/typescript/telemetry/init.ts
@@ -1,31 +1,15 @@
 import { NodeSDK } from "@opentelemetry/sdk-node"
-import { OtelEnv } from "./environment.js"
+import { getEnvWithoutDefaults } from "@opentelemetry/core"
 
 const SERVICE_NAME = "dagger-typescript-sdk"
 
+const env = getEnvWithoutDefaults()
+
 /*
- * Look for variables prefixed with OTEL to see if OTEL is enabled
+ * Look for variables prefixed with OTEL to see if OpenTelemetry is configured.
  */
 function otelConfigured(): boolean {
   return Object.keys(process.env).some((key) => key.startsWith("OTEL_"))
-}
-
-/**
- * Check if SDK is enabled, according to the current OTLP SDK node, it's only possible to disable
- * the whole SDK.
- *
- * See: https://github.com/open-telemetry/opentelemetry-js/blob/3ab4f765d8d696327b7d139ae6a45e7bd7edd924/experimental/packages/opentelemetry-sdk-node/README.md#L164
- */
-function otelEnabled(name?: string): boolean {
-  if (name) {
-    name = `OTEL_TYPESCRIPT_${name.toUpperCase()}_INSTRUMENTATION_DISABLED`
-  }
-
-  if (!name) {
-    name = OtelEnv.OTEL_SDK_DISABLED
-  }
-
-  return process.env[name] !== "true"
 }
 
 /**
@@ -34,43 +18,37 @@ function otelEnabled(name?: string): boolean {
 export class DaggerOtelConfigurator {
   private is_configured = false
 
-  private is_enabled = false
-
   private sdk?: NodeSDK
 
   /**
    * Initialize the Open Telemetry SDK if enabled or not already configured.
    */
   initialize() {
+    if (this.is_configured) {
+      return
+    }
+    this.configure()
+    this.is_configured = true
+  }
+
+  configure() {
     if (!otelConfigured()) {
-      console.debug("Telemetry not configured")
       return
     }
 
-    if (!otelEnabled()) {
-      console.debug("Telemetry disabled")
-      return
-    } else {
-      this.is_enabled = true
-    }
-
-    if (this.is_configured || !this.is_enabled) {
+    if (env.OTEL_SDK_DISABLED) {
       return
     }
 
-    console.debug("Initializing telemetry")
     this.setupEnv()
 
     // Create the node SDK with the context manager, the resource and the exporter.
-    const sdk = new NodeSDK({
+    this.sdk = new NodeSDK({
       serviceName: SERVICE_NAME,
     })
 
-    // Register the SDK to the OpenTelemetry API if OTEL is enabled
-    sdk.start()
-    console.debug("Telemetry initialized")
-    this.is_configured = true
-    this.sdk = sdk
+    // Register the SDK to the OpenTelemetry API
+    this.sdk.start()
   }
 
   /**
@@ -86,27 +64,17 @@ export class DaggerOtelConfigurator {
    * Setup environment for auto-configuring the SDK.
    */
   setupEnv() {
-    // Setup a default value for the service name.
-    if (!process.env[OtelEnv.OTEL_SERVICE_NAME]) {
-      process.env[OtelEnv.OTEL_SERVICE_NAME] = SERVICE_NAME
-    }
-
     // TODO: The insecure flag should be set by the shim instead of the SDK.
-    const endpoints = {
-      [OtelEnv.OTEL_EXPORTER_OTLP_ENDPOINT]:
-        OtelEnv.OTEL_EXPORTER_OTLP_INSECURE,
-      [OtelEnv.OTEL_EXPORTER_OTLP_METRICS_ENDPOINT]:
-        OtelEnv.OTEL_EXPORTER_OTLP_METRICS_INSECURE,
-      [OtelEnv.OTEL_EXPORTER_OTLP_LOGS_ENDPOINT]:
-        OtelEnv.OTEL_EXPORTER_OTLP_LOGS_INSECURE,
-      [OtelEnv.OTEL_EXPORTER_OTLP_TRACES_ENDPOINT]:
-        OtelEnv.OTEL_EXPORTER_OTLP_TRACES_INSECURE,
-    }
-
-    Object.entries(endpoints).forEach(([endpoint, insecure]) => {
-      const env = process.env[endpoint]
-      if (env && env.startsWith("http://")) {
-        process.env[insecure] = "true"
+    Object.entries(process.env).forEach(([key, value]) => {
+      if (
+        key.startsWith("OTEL_") &&
+        key.endsWith("_ENDPOINT") &&
+        value?.startsWith("http://")
+      ) {
+        const insecure = key.replace(/_ENDPOINT$/, "_INSECURE")
+        if (process.env[insecure] === undefined) {
+          process.env[insecure] = "true"
+        }
       }
     })
   }
