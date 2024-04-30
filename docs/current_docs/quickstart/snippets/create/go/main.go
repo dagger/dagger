82c832e89159fbcab830c3ef7ab598a2f8dfce64
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-05-01 03:29:31 +0530

    docs: Updates quickstart examples (#7172)
    * docs: Updates quickstart examples
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fixed capitalization according to new style guide
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Updates examples
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Copy fix
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Added feedback
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    ---------
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/quickstart/snippets/create/go/main.go
+++ docs/current_docs/quickstart/snippets/create/go/main.go
@@ -10,11 +10,26 @@ import (
 type Example struct{}
 
 // Build and publish a project using a Wolfi container
-func (m *Example) BuildAndPublish(ctx context.Context, buildSrc *Directory, buildArgs []string) (string, error) {
+func (m *Example) BuildAndPublish(
+	ctx context.Context,
+	buildSrc *Directory,
+	buildArgs []string,
+) (string, error) {
 	ctr := dag.Wolfi().Container()
 
 	return dag.
 		Golang().
-		BuildContainer(GolangBuildContainerOpts{Source: buildSrc, Args: buildArgs, Base: ctr}).
-		Publish(ctx, fmt.Sprintf("ttl.sh/my-hello-container-%.0f", math.Floor(rand.Float64()*10000000))) //#nosec
+		BuildContainer(
+			GolangBuildContainerOpts{
+				Source: buildSrc,
+				Args:   buildArgs,
+				Base:   ctr,
+			}).
+		Publish(
+			ctx,
+			fmt.Sprintf(
+				"ttl.sh/my-hello-container-%.0f",
+				math.Floor(rand.Float64()*10000000),
+			),
+		) //#nosec
 }
