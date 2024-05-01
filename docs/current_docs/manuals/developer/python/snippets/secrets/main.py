65727d8bfe0b8ff0c10acd9c803a5252ca47572b
Justin Chadwell <me@jedevc.com>
2024-05-01 19:32:10 +0100

    docs: don't use command line plaintext injection (#7232)
    This is a very bad default to recommend - essentially this "de-secrets"
    a value, and this could then be leaked accidentally:
    - Potentially into OTEL spans
    - Potentially into cache keys
    - Potentially into engine logs
    
    We should recommend using `WithSecretVariable` instead, which is much
    better - then rely on the shell to do the interpolation, which avoids
    all of these problems. The secret scrubber will then guarantee that we
    don't secrets leaked into logs.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- docs/current_docs/manuals/developer/python/snippets/secrets/main.py
+++ docs/current_docs/manuals/developer/python/snippets/secrets/main.py
@@ -6,16 +6,16 @@ from dagger import dag, function, object_type
 class MyModule:
     @function
     async def github_api(self, endpoint: str, token: dagger.Secret) -> str:
-        plaintext = await token.plaintext()
         return await (
             dag.container()
             .from_("alpine:3.17")
             .with_exec(["apk", "add", "curl"])
+            .with_secret_variable("GITHUB_TOKEN", token)
             .with_exec(
                 [
                     "sh",
                     "-c",
-                    f"""curl "{endpoint}" --header "Accept: application/vnd.github+json" --header "Authorization: Bearer {plaintext}" """,
+                    f"""curl "{endpoint}" --header "Accept: application/vnd.github+json" --header "Authorization: Bearer $GITHUB_TOKEN" """,
                 ]
             )
             .stdout()
