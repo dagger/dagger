88161f8b01872d1a706f29f72f812d8671b533b2
Justin Chadwell <me@jedevc.com>
2024-05-01 12:56:54 +0100

    docs: don't use command line plaintext injection
    This is a very bad default to recommend - essentially this "de-secrets"
    a value, and this could then be leaked accidentally:
    - Potentially into OTEL spans
    - Potentially into cache keys
    - Potentially into engine logs
    
    We should recommend using `WithSecretVariable` instead, which is much
    better - then rely on the shell to do the interpolation, which avoids
    all of these problems. The secret scrubber will then guarantee that we
    don't secrets leaked into logs.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- docs/current_docs/manuals/developer/go/snippets/secrets/main.go
+++ docs/current_docs/manuals/developer/go/snippets/secrets/main.go
@@ -8,13 +8,10 @@ import (
 type MyModule struct{}
 
 func (m *MyModule) GithubApi(ctx context.Context, endpoint string, token *Secret) (string, error) {
-	plaintext, err := token.Plaintext(ctx)
-	if err != nil {
-		return "", err
-	}
 	return dag.Container().
 		From("alpine:3.17").
 		WithExec([]string{"apk", "add", "curl"}).
-		WithExec([]string{"sh", "-c", fmt.Sprintf("curl \"%s\" --header \"Accept: application/vnd.github+json\" --header \"Authorization: Bearer %s\"", endpoint, plaintext)}).
+		WithSecretVariable("GITHUB_TOKEN", token).
+		WithExec([]string{"sh", "-c", fmt.Sprintf("curl \"%s\" --header \"Accept: application/vnd.github+json\" --header \"Authorization: Bearer $GITHUB_TOKEN\"", endpoint)}).
 		Stdout(ctx)
 }
