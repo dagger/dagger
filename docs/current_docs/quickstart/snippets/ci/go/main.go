a736c1ff320bc8aada4a9e228f396632d1de6304
Vikram Vaswani <vikram@dagger.io>
2024-05-08 22:50:18 +0530

    Updated content
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/quickstart/snippets/ci/go/main.go
+++ docs/current_docs/quickstart/snippets/ci/go/main.go
@@ -11,23 +11,19 @@ type HelloDagger struct{}
 
 // Tests, builds, packages and publishes the application
 func (m *HelloDagger) Ci(ctx context.Context, source *Directory) (string, error) {
-
 	// run tests
 	_, err := m.Test(ctx, source)
 	if err != nil {
 		return "", err
 	}
-
 	// obtain the build output directory
 	build := m.Build(source)
-
 	// create and publish a container with the build output
 	address, err := m.Package(build).
 		Publish(ctx, fmt.Sprintf("ttl.sh/hello-dagger-%.0f", math.Floor(rand.Float64()*10000000))) //#nosec
 	if err != nil {
 		return "", err
 	}
-
 	return address, nil
 }
 
