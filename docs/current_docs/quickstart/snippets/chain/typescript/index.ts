2638cecdc3b1c10de3248a5f436334bc67b2f4be
Vikram Vaswani <vikram@dagger.io>
2024-05-10 01:14:11 +0530

    Fix lint
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/quickstart/snippets/chain/typescript/index.ts
+++ docs/current_docs/quickstart/snippets/chain/typescript/index.ts
@@ -4,7 +4,7 @@ import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 class HelloDagger {
   @func()
   foo(): Directory {
-    return dag.container().from("cgr.dev/chainguard/wolfi-base").directory('/')
+    return dag.container().from("cgr.dev/chainguard/wolfi-base").directory("/")
   }
 
   @func()
