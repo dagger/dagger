13263b2e79ddcdb622e271336acec51b986315d3
Vikram Vaswani <vikram@dagger.io>
2024-05-15 00:54:06 +0530

    Add code
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/quickstart/snippets/publish/python/__init__.py
+++ docs/current_docs/quickstart/snippets/publish/python/__init__.py
@@ -7,51 +7,64 @@ from dagger import dag, function, object_type
 @object_type
 class HelloDagger:
     @function
-    async def ci(self, source: dagger.Directory) -> str:
-        """Tests, builds, packages and publishes the application"""
-        # run tests
+    async def publish(self, source: dagger.Directory) -> str:
+        """Tests, builds and publishes the application"""
+        # run unit tests
         self.test(source)
-        # create and publish a container with the build output
-        return await self.package(source).publish(
+        # build and publish the container
+        return await self.build(source).publish(
             f"ttl.sh/myapp-{random.randrange(10 ** 8)}"
         )
 
     @function
-    def package(self, source: dagger.Directory) -> dagger.Container:
-        """Returns a container with the production build"""
+    def build(self, source: dagger.Directory) -> dagger.Container:
+        """Returns a container with the production build and an NGINX service"""
+        # perform a multi-stage build
+        # stage 1
+        # use the build environment container
+        # build the application
+        # return the build output directory
+        build = (
+            self.build_env(source)
+            .with_exec(["npm", "run", "build"])
+            .directory("./dist")
+        )
+        # stage 2
+        # start from a base nginx container
+        # copy the build output directory to it
+        # expose container port 8080
         return (
             dag.container()
             .from_("nginx:1.25-alpine")
-            .with_directory("/usr/share/nginx/html", self.build(source))
-            .with_exposed_port(80)
-        )
-
-    @function
-    def build(self, source: dagger.Directory) -> dagger.Directory:
-        """Returns a directory with the production build"""
-        node_cache = dag.cache_volume("node")
-        return (
-            dag.container()
-            .from_("node:21-slim")
-            .with_directory("/src", source.without_directory("dagger"))
-            .with_workdir("/src")
-            .with_mounted_cache("/src/node_modules", node_cache)
-            .with_exec(["npm", "install"])
-            .with_exec(["npm", "run", "build"])
-            .directory("./dist")
+            .with_directory("/usr/share/nginx/html", build)
+            .with_exposed_port(8080)
         )
 
     @function
     async def test(self, source: dagger.Directory) -> str:
         """Returns the result of running unit tests"""
-        node_cache = dag.cache_volume("node")
+        # use the build environment container
+        # run unit tests
         return await (
+            self.build_env(source)
+            .with_exec(["npm", "run", "test:unit", "run"])
+            .stdout()
+        )
+
+    @function
+    def build_env(self, source: dagger.Directory) -> dagger.Container:
+        """Returns a container with the build environment"""
+        # create a Dagger cache volume for dependencies
+        node_cache = dag.cache_volume("node")
+        # create the build environment
+        # start from a base node container
+        # add source code
+        # install dependencies
+        return (
             dag.container()
             .from_("node:21-slim")
             .with_directory("/src", source.without_directory("dagger"))
-            .with_workdir("/src")
             .with_mounted_cache("/src/node_modules", node_cache)
+            .with_workdir("/src")
             .with_exec(["npm", "install"])
-            .with_exec(["npm", "run", "test:unit", "run"])
-            .stdout()
         )
--- docs/current_docs/quickstart/snippets/publish/typescript/index.ts
+++ docs/current_docs/quickstart/snippets/publish/typescript/index.ts
@@ -3,45 +3,36 @@ import { dag, Container, Directory, object, func } from "@dagger.io/dagger"
 @object()
 class HelloDagger {
   /**
-   * Tests, builds, packages and publishes the application
+   * Tests, builds and publishes the application
    */
   @func()
-  async ci(source: Directory): Promise<string> {
-    // run tests
+  async publish(source: Directory): Promise<string> {
+    // run unit tests
     this.test(source)
-    // create and publish a container with the build output
-    return await this.package(source).publish(
-      "ttl.sh/myapp-" + Math.floor(Math.random() * 10000000),
-    )
+    // build and publish the container
+    return await this.build(source).publish("ttl.sh/myapp-" + Math.floor(Math.random() * 10000000))
   }
 
   /**
-   * Returns a container with the production build
+   * Returns a container with the production build and an NGINX service
    */
   @func()
-  package(source: Directory): Container {
+  build(source: Directory): Container {
+    // perform a multi-stage build
+    // stage 1
+    // use the build environment container
+    // build the application
+    // return the build output directory
+    const build = this.buildEnv(source).withExec(["npm", "run", "build"]).directory("./dist")
+    // stage 2
+    // start from a base nginx container
+    // copy the build output directory to it
+    // expose container port 8080
     return dag
       .container()
       .from("nginx:1.25-alpine")
-      .withDirectory("/usr/share/nginx/html", this.build(source))
-      .withExposedPort(80)
-  }
-
-  /**
-   * Returns a directory with the production build
-   */
-  @func()
-  build(source: Directory): Directory {
-    const nodeCache = dag.cacheVolume("node")
-    return dag
-      .container()
-      .from("node:21-slim")
-      .withDirectory("/src", source.withoutDirectory("dagger"))
-      .withWorkdir("/src")
-      .withMountedCache("/src/node_modules", nodeCache)
-      .withExec(["npm", "install"])
-      .withExec(["npm", "run", "build"])
-      .directory("./dist")
+      .withDirectory("/usr/share/nginx/html", build)
+      .withExposedPort(8080)
   }
 
   /**
@@ -49,15 +40,28 @@ class HelloDagger {
    */
   @func()
   async test(source: Directory): Promise<string> {
+    // use the build environment container
+    // run unit tests
+    return this.buildEnv(source).withExec(["npm", "run", "test:unit", "run"]).stdout()
+  }
+
+  /**
+   * Returns a container with the build environment
+   */
+  @func()
+  buildEnv(source: Directory): Container {
+    // create a Dagger cache volume for dependencies
     const nodeCache = dag.cacheVolume("node")
+    // create the build environment
+    // start from a base node container
+    // add source code
+    // install dependencies
     return dag
       .container()
       .from("node:21-slim")
       .withDirectory("/src", source.withoutDirectory("dagger"))
-      .withWorkdir("/src")
       .withMountedCache("/src/node_modules", nodeCache)
+      .withWorkdir("/src")
       .withExec(["npm", "install"])
-      .withExec(["npm", "run", "test:unit", "run"])
-      .stdout()
   }
 }
