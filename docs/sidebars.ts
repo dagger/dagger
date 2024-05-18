6ae2bd05c8e61bfef84024c660c6b2459aee944d
Vikram Vaswani <vikram@dagger.io>
2024-05-18 19:06:57 +0530

    docs: Convert debugging page
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -369,6 +369,10 @@ module.exports = {
           "id": "manuals/developer/error-handling"
         },
         {
+          "type": "doc",
+          "id": "manuals/developer/debugging"
+        },
+        {
           "type": "category",
           "label": "Develop with Go",
           "link": {
@@ -387,10 +391,6 @@ module.exports = {
             {
               "type": "doc",
               "id": "manuals/developer/go/constructor"
-            },
-            {
-              "type": "doc",
-              "id": "manuals/developer/go/debugging"
             },
             {
               "type": "link",
@@ -424,10 +424,6 @@ module.exports = {
               "id": "manuals/developer/python/name-overrides"
             },
             {
-              "type": "doc",
-              "id": "manuals/developer/python/debugging"
-            },
-            {
               "type": "link",
               "label": "Python SDK Reference",
               "href": "https://dagger-io.readthedocs.org/"
@@ -457,14 +453,6 @@ module.exports = {
             {
               "type": "doc",
               "id": "manuals/developer/typescript/decorators"
-            },
-            {
-              "type": "doc",
-              "id": "manuals/developer/typescript/aliases"
-            },
-            {
-              "type": "doc",
-              "id": "manuals/developer/typescript/debugging"
             },
             {
               "type": "doc",
