f8cfecaa97ca8b321f898bfddc5725f175aabcd4
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-05-22 16:21:02 +0200

    feat: improve TS SDK runtime performances (#7096)
    Improving overall TypeScript SDK runtime performances:
    - Switched from npm to yarn v4 which in theory should make things faster.
    - Reorganized the SDK initialization to install the SDK before adding the user's source which should in theory improving the cache.
    - Improved the TS SDK code to be more readable and more clear.
    - Improved the user's package.json update using npm pkg set instead of a simple npm install which definitely give at least a second of improvement.
    - Moved @dagger.io/dagger from devDependencies to dependencies. That allows to use --production flag which in theory is faster than installing all dependencies.
    - Deduplicate the TS SDK yarn.lock to remove any no-essential dependencies from the lockfile, it's a way to reduce the overall download time.
    
    Theses changes seems to gives an average of 10-30% performances improvement.
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- sdk/typescript/runtime/main.go
+++ sdk/typescript/runtime/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"path"
 	"path/filepath"
 	"slices"
 
@@ -62,7 +63,7 @@ const (
 func (t *TypescriptSdk) ModuleRuntime(ctx context.Context, modSource *ModuleSource, introspectionJSON string) (*Container, error) {
 	ctr, err := t.CodegenBase(ctx, modSource, introspectionJSON)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create codegen base: %w", err)
 	}
 
 	subPath, err := modSource.SourceSubpath(ctx)
@@ -70,7 +71,7 @@ func (t *TypescriptSdk) ModuleRuntime(ctx context.Context, modSource *ModuleSour
 		return nil, fmt.Errorf("could not load module config: %w", err)
 	}
 
-	detectedRuntime, err := t.DetectRuntime(ctx, modSource, subPath)
+	detectedRuntime, err := t.detectRuntime(ctx, modSource, subPath)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create module runtime: %w", err)
 	}
@@ -84,12 +85,12 @@ func (t *TypescriptSdk) ModuleRuntime(ctx context.Context, modSource *ModuleSour
 	case Bun:
 		return ctr.
 			// Install dependencies
-			WithExec([]string{"bun", "install"}).
+			WithExec([]string{"bun", "install", "--no-verify", "--no-progress"}).
 			WithEntrypoint([]string{"bun", entrypointPath}), nil
 	case Node:
 		return ctr.
 			// Install dependencies
-			WithExec([]string{"npm", "install"}).
+			WithExec([]string{"yarn", "install", "--production"}).
 			// need to specify --tsconfig because final runtime container will change working directory to a separate scratch
 			// dir, without this the paths mapped in the tsconfig.json will not be used and js module loading will fail
 			// need to specify --no-deprecation because the default package.json has no main field which triggers a warning
@@ -105,15 +106,20 @@ func (t *TypescriptSdk) Codegen(ctx context.Context, modSource *ModuleSource, in
 	// Get base container
 	ctr, err := t.CodegenBase(ctx, modSource, introspectionJSON)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create codegen base: %w", err)
 	}
-	return dag.GeneratedCode(ctr.Directory(ModSourceDirPath)).
+	return dag.GeneratedCode(
+		dag.Directory().WithDirectory(
+			".",
+			ctr.Directory(ModSourceDirPath),
+			DirectoryWithDirectoryOpts{Exclude: []string{"**/node_modules/**"}}),
+	).
 		WithVCSGeneratedPaths([]string{
 			GenDir + "/**",
 		}).
 		WithVCSIgnoredPaths([]string{
 			GenDir,
-			"node_modules/**",
+			"**/node_modules/**",
 		}), nil
 }
 
@@ -131,79 +137,32 @@ func (t *TypescriptSdk) CodegenBase(ctx context.Context, modSource *ModuleSource
 		return nil, fmt.Errorf("could not load module config: %w", err)
 	}
 
-	detectedRuntime, err := t.DetectRuntime(ctx, modSource, subPath)
+	detectedRuntime, err := t.detectRuntime(ctx, modSource, subPath)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create codegen base: %w", err)
+		return nil, fmt.Errorf("failed to detect runtime: %w", err)
 	}
 
 	base, err := t.Base(detectedRuntime)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create codegen base: %w", err)
 	}
 
-	gen := base.
-		// Add codegen binary into a special path
-		WithMountedFile(codegenBinPath, t.SDKSourceDir.File("/codegen")).
-		// Add introspection file
-		WithNewFile(schemaPath, ContainerWithNewFileOpts{
-			Contents: introspectionJSON,
-		}).
-		// Execute the code generator using the given introspection file
-		WithExec([]string{
-			codegenBinPath,
-			"--lang", "typescript",
-			"--output", ModSourceDirPath,
-			"--module-name", name,
-			"--module-context-path", filepath.Join(ModSourceDirPath, subPath),
-			"--introspection-json-path", schemaPath,
-		}, ContainerWithExecOpts{
-			ExperimentalPrivilegedNesting: true,
-		}).
-		Directory(filepath.Join(ModSourceDirPath, subPath, GenDir))
+	// Get a directory with the SDK sources installed and the generated client.
+	sdk := t.
+		installedSDK(base, detectedRuntime).
+		WithDirectory(".", t.generateClient(base, name, introspectionJSON, subPath))
 
 	base = base.
-		// Add codegen binary into a special path
-		WithMountedFile(codegenBinPath, t.SDKSourceDir.File("/codegen")).
 		// Add template directory
 		WithMountedDirectory("/opt/module", dag.CurrentModule().Source().Directory(".")).
-		// Mount users' module
-		WithMountedDirectory(ModSourceDirPath, modSource.ContextDirectory()).
-		WithWorkdir(filepath.Join(ModSourceDirPath, subPath)).
-		// Add sdk source code
-		WithDirectory(GenDir, t.SDKSourceDir, ContainerWithDirectoryOpts{
-			Exclude: []string{"codegen", "runtime"},
-		}).
-		// Add generated code
-		WithDirectory(GenDir, gen)
+		// Mount users' module with SDK sources and generated client in it.
+		WithMountedDirectory(ModSourceDirPath, modSource.
+			ContextDirectory().
+			WithDirectory(filepath.Join(subPath, GenDir), sdk),
+		).
+		WithWorkdir(filepath.Join(ModSourceDirPath, subPath))
 
-	switch detectedRuntime {
-	case Bun:
-		base = base.
-			// Check if the project has existing source:
-			// if it does: add sdk as dev dependency
-			// if not: copy the template and replace QuickStart with the module name
-			WithExec([]string{"sh", "-c",
-				"if [ -f package.json ]; then  bun install ./sdk  --dev  && bun /opt/module/bin/__tsconfig.updator.ts; else cp -r /opt/module/template/*.json .; fi",
-			})
-	case Node:
-		base = base.
-			// Check if the project has existing source:
-			// if it does: add sdk as dev dependency
-			// if not: copy the template and replace QuickStart with the module name
-			WithExec([]string{"sh", "-c",
-				"if [ -f package.json ]; then  npm install --package-lock-only ./sdk  --dev  && tsx /opt/module/bin/__tsconfig.updator.ts; else cp -r /opt/module/template/*.json .; fi",
-			})
-	default:
-		return nil, fmt.Errorf("unknown runtime: %s", detectedRuntime)
-	}
-
-	return base.
-		// Check if there's a src directory with .ts files in it.
-		// If not, add the template file and replace QuickStart with the module name
-		// This cover the case where there's a package.json but no src directory.
-		WithExec([]string{"sh", "-c",
-			fmt.Sprintf("mkdir -p src && if ls src/*.ts >/dev/null 2>&1; then true; else cp /opt/module/template/src/index.ts src/index.ts && sed -i -e 's/QuickStart/%s/g' ./src/index.ts; fi", strcase.ToCamel(name))},
-		), nil
+	return t.setupModule(ctx, base, detectedRuntime, name)
 }
 
 // Base returns a Node or Bun container with cache setup for yarn or bun
@@ -218,15 +177,120 @@ func (t *TypescriptSdk) Base(runtime SupportedTSRuntime) (*Container, error) {
 		return dag.Container().
 			From(nodeImageRef).
 			WithoutEntrypoint().
+			// Enable corepack so we can use yarn v4 which is supposed to be faster than npm or yarn v1.
+			WithExec([]string{"corepack", "enable"}).
 			// Install default CA certificates and configure node to use them instead of its compiled in CA bundle.
 			// This enables use of custom CA certificates if configured in the dagger engine.
 			WithExec([]string{"apk", "add", "ca-certificates"}).
 			WithEnvVariable("NODE_OPTIONS", "--use-openssl-ca").
 			WithMountedCache("/root/.npm", dag.CacheVolume(fmt.Sprintf("mod-npm-cache-%s", nodeVersion))).
+			// Comment cache here, it seems it creates cache conflicts with yarn (v1 and v4).
+			// We should investigate this further and see if we hit the same issue with pnpm.
+			// WithMountedCache("/usr/local/share/.cache/yarn", dag.CacheVolume(fmt.Sprintf("mod-yarn-cache-%s", nodeVersion))).
 			WithExec([]string{"npm", "install", "-g", "tsx"}), nil
 	default:
 		return nil, fmt.Errorf("unknown runtime: %s", runtime)
 	}
+}
+
+// setupModule initialiaze the user's module.
+//
+// If the user's module has a package.json file, it will run the
+// __tsconfig.updator.ts script in order to add dagger to the tsconfig path so
+// the editor can give type hints and auto completion.
+// Otherwise, it will copy the template config files into the user's module directory.
+//
+// If there's no src directory or no typescript files in it, it will create one
+// and copy the template index.ts file in it.
+func (t *TypescriptSdk) setupModule(ctx context.Context, ctr *Container, runtime SupportedTSRuntime, name string) (*Container, error) {
+	moduleFiles, err := ctr.Directory(".").Entries(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not list rootfs entries: %w", err)
+	}
+
+	packageJSONExist := slices.Contains(moduleFiles, "package.json")
+
+	// If there's a package.json, run the tsconfig updator script and install the genDir.
+	// else, copy the template config files.
+	if packageJSONExist {
+		if runtime == Bun {
+			ctr = ctr.
+				WithExec([]string{"bun", "/opt/module/bin/__tsconfig.updator.ts"}).
+				WithExec([]string{"bun", "install", "--no-verify", "--no-progress", "--summary", "./sdk"})
+		} else {
+			ctr = ctr.
+				WithExec([]string{"tsx", "/opt/module/bin/__tsconfig.updator.ts"}).
+				WithExec([]string{"npm", "pkg", "set", "dependencies[@dagger.io/dagger]=./sdk"})
+		}
+	} else {
+		ctr = ctr.WithDirectory(".", ctr.Directory("/opt/module/template"), ContainerWithDirectoryOpts{Include: []string{"*.json"}})
+	}
+
+	// Check if there's a src directory and creates an empty directory if it doesn't exist.
+	if !slices.Contains(moduleFiles, "src") {
+		ctr = ctr.WithDirectory("src", dag.Directory())
+	}
+
+	// Get the list of files in the src directory.
+	moduleSourceFiles, err := ctr.Directory("src").Entries(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not list module source entries: %w", err)
+	}
+
+	// Check if there's a src directory with .ts files in it.
+	// If not, add the template file and replace QuickStart with the module name
+	if !slices.ContainsFunc(moduleSourceFiles, func(s string) bool {
+		return path.Ext(s) == ".ts"
+	}) {
+		return ctr.
+			WithDirectory("src", ctr.Directory("/opt/module/template/src"), ContainerWithDirectoryOpts{Include: []string{"*.ts"}}).
+			WithExec([]string{"sed", "-i", "-e", fmt.Sprintf("s/QuickStart/%s/g", strcase.ToCamel(name)), "src/index.ts"}), nil
+	}
+
+	return ctr, nil
+}
+
+// installedSDK returns a directory with the SDK sources and its dependencies installed.
+func (t *TypescriptSdk) installedSDK(ctr *Container, runtime SupportedTSRuntime) *Directory {
+	ctr = ctr.
+		WithWorkdir(ModSourceDirPath).
+		WithDirectory(".", t.SDKSourceDir, ContainerWithDirectoryOpts{
+			Exclude: []string{"codegen", "runtime"},
+		})
+
+	switch runtime {
+	case Bun:
+		return ctr.WithExec([]string{"bun", "install", "--no-verify", "--no-progress", "--summary"}).Directory(ModSourceDirPath)
+	case Node:
+		return ctr.WithExec([]string{"yarn", "workspaces", "focus", "--production"}).Directory(ModSourceDirPath)
+	default:
+		// Should never happen since we verify the runtime before calling this function.
+		return nil
+	}
+}
+
+// generateClient uses the given container to generate the client code.
+func (t *TypescriptSdk) generateClient(ctr *Container, name, introspectionJSON, subPath string) *Directory {
+	return ctr.
+		// Add dagger codegen binary.
+		WithMountedFile(codegenBinPath, t.SDKSourceDir.File("/codegen")).
+		// Write the introspection file.
+		WithNewFile(schemaPath, ContainerWithNewFileOpts{
+			Contents: introspectionJSON,
+		}).
+		// Execute the code generator using the given introspection file.
+		WithExec([]string{
+			codegenBinPath,
+			"--lang", "typescript",
+			"--output", ModSourceDirPath,
+			"--module-name", name,
+			"--module-context-path", filepath.Join(ModSourceDirPath, subPath),
+			"--introspection-json-path", schemaPath,
+		}, ContainerWithExecOpts{
+			ExperimentalPrivilegedNesting: true,
+		}).
+		// Return the generated code directory.
+		Directory(filepath.Join(ModSourceDirPath, subPath, GenDir))
 }
 
 // DetectRuntime returns the runtime(bun or node) detected for the user's module
@@ -234,7 +298,7 @@ func (t *TypescriptSdk) Base(runtime SupportedTSRuntime) (*Container, error) {
 // If a package-lock.json, yarn.lock, or pnpm-lock.yaml is present, node will be used.
 // If a bun.lockb is present, bun will be used.
 // If none of the above is present, node will be used.
-func (t *TypescriptSdk) DetectRuntime(ctx context.Context, modSource *ModuleSource, subPath string) (SupportedTSRuntime, error) {
+func (t *TypescriptSdk) detectRuntime(ctx context.Context, modSource *ModuleSource, subPath string) (SupportedTSRuntime, error) {
 	// Try to detect runtime from package.json
 	source := modSource.ContextDirectory().Directory(subPath)
 
