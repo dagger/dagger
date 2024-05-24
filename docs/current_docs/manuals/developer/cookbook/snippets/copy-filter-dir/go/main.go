20fb848ba91f57c609c280869349f720dbe58e22
Vikram Vaswani <vikram@dagger.io>
2024-05-24 21:26:52 +0530

    Add recipe
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/manuals/developer/cookbook/snippets/copy-filter-dir/go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"context"
+)
+
+type MyModule struct{}
+
+// Returns a container with a specified directory and an additional file
+func (m *MyModule) ModifyDirectory(ctx context.Context, d *Directory) *Container {
+	return dag.Container().
+		From("alpine:latest").
+		WithDirectory("/src", d).
+		WithExec([]string{"/bin/sh", "-c", `echo foo > /src/foo`})
+}
