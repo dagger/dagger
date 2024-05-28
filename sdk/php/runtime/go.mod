c6ea5100e89e66a4bac8401e1912f03d90b375ef
Carnage <t.carnage@gmail.com>
2024-05-28 13:02:22 +0100

    Experiments with getting php codegen working!

--- sdk/php/runtime/go.mod
+++ sdk/php/runtime/go.mod
@@ -1,3 +1,3 @@
 module php-sdk
 
-go 1.22.2
+go 1.21.7
--- sdk/php/runtime/main.go
+++ sdk/php/runtime/main.go
@@ -1,15 +1,18 @@
 // Runtime module for the PHP SDK
+// when done add php to core/schema/sdk.go:L93
 
 package main
 
 import (
 	"context"
+	"fmt"
+	//"path/filepath"
 )
 
 const (
-	DefaultImage          = "php:8.3-cli-alpine"
+	DefaultImage = "php:8.3-cli-alpine"
 	// TODO: figure out issue with Directory.Diff() "/" != "/src"
-	ModSourceDirPath      = "/"
+	ModSourceDirPath      = "/src"
 	RuntimeExecutablePath = "/"
 	GenDir                = "sdk"
 	GenPath               = "../generated"
@@ -18,24 +21,23 @@ const (
 )
 
 type PhpSdk struct {
-	SourceDir  *Directory
+	SourceDir     *Directory
 	RequiredPaths []string
-	Container *Container
+	Container     *Container
 }
 
 func New(
 	// Directory with the PHP SDK source code.
 	// +optional
-	sourceDir *Directory,
+	sdkSourceDir *Directory,
 ) *PhpSdk {
-	if sourceDir == nil {
-		sourceDir = dag.
-			Directory().
-			WithNewFile("index.php", "<?php\n echo 'hi';")
+	if sdkSourceDir == nil {
+		sdkSourceDir = dag.
+			Directory()
 	}
-	
+
 	return &PhpSdk{
-		SourceDir:  sourceDir,
+		SourceDir: sdkSourceDir,
 
 		RequiredPaths: []string{},
 
@@ -51,19 +53,47 @@ func New(
 	}
 }
 
-func (sdk *PhpSdk) Codegen() (*GeneratedCode, error) {
+func (sdk *PhpSdk) Codegen(ctx context.Context, modSource *ModuleSource /*, introspectionJSON string*/) (*GeneratedCode, error) {
 
 	/**
 	 * returns the container with a directory mounted at the given path
 	 * https://pkg.go.dev/dagger.io/dagger@v0.11.4#Container.WithMountedDirectory
-	 */ 
-	ctr := sdk.Container.WithMountedDirectory(ModSourceDirPath, sdk.SourceDir)
-	//		WithMountedDirectory(RuntimeExecutablePath, dag.CurrentModule().Source()).
-	//	WithMountedDirectory(GenDir, dag.CurrentModule().Source().Directory(GenPath))
-	
+	 */
+	ctr := sdk.Container
+	//.WithMountedDirectory(ModSourceDirPath, sdk.SourceDir)
+	//WithMountedDirectory(RuntimeExecutablePath, dag.CurrentModule().Source()).
+	//WithMountedDirectory(GenDir, dag.CurrentModule().Source().Directory(GenPath))
 
-	// ctr = ctr.WithMountedDirectory("/codegen", sdk.SourceDir.Directory("src")).
+	name, err := modSource.ModuleOriginalName(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not load module config: %w", err)
+	}
+
+	subPath, err := modSource.SourceSubpath(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("could not load module config: %w", err)
+	}
+
+	print(name)
+	print(subPath)
+
+	ctr = ctr.WithMountedDirectory("/codegen", modSource.ContextDirectory()).
+		//WithWorkdir("/codegen/sdk/php").
+		WithExec([]string{
+			"ls -al /codegen",
+		})
+	ctr.Stdout(ctx)
+	//WithNewFile("/codegen/schema.json", ContainerWithNewFileOpts{
+	//	Contents: introspectionJSON,
+	//}).
+	//WithMountedDirectory(ModSourceDirPath, modSource.ContextDirectory()).
+	//WithWorkdir(filepath.Join(ModSourceDirPath, subPath))).
+	//WithDirectory(GenDir, sdk.SourceDir, ContainerWithDirectoryOpts{
+	//	Exclude: []string{"codegen", "runtime"},
+	//})
 	//	WithMountedFile("/")
+
+	//ctr := sdk.Container.WithMountedDirectory("/opt/module", dag.CurrentModule().Source().Directory("."))
 
 	return dag.GeneratedCode(ctr.Directory(ModSourceDirPath)).
 			WithVCSGeneratedPaths([]string{GenDir + "/**"}).
@@ -81,8 +111,6 @@ func (sdk *PhpSdk) ModuleRuntime(
 
 	return ctr.WithEntrypoint([]string{RuntimeExecutablePath}), nil
 }
-
-
 
 // // Returns a container that echoes whatever string argument is provided
 // func (m *PhpSdk) ContainerEcho(stringArg string) *Container {
