491abca57b1fc60d0e327fc2840967d222dabc65
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-05-28 19:12:17 +0530

    Update docs/current_docs/manuals/developer/cookbook/snippets/copy-filter-dir/typescript/index.ts
    Co-authored-by: Vasek - Tom C <tom@quartz.technology>
    Signed-off-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>

--- docs/current_docs/manuals/developer/cookbook/snippets/copy-filter-dir/typescript/index.ts
+++ docs/current_docs/manuals/developer/cookbook/snippets/copy-filter-dir/typescript/index.ts
@@ -24,7 +24,7 @@ class MyModule {
     if (!excludeDirectory) {
       filteredSource = filteredSource.withoutDirectory(excludeDirectory)
     }
-    if (excludeFile != null) {
+    if (!excludeFile) {
       filteredSource = filteredSource.withoutFile(excludeFile)
     }
     return dag.container().
