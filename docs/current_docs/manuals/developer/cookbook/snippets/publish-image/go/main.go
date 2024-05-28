25cf8a4ec5e383f4efbb65c0539e3fe0f226a46f
Vikram Vaswani <vikram@dagger.io>
2024-05-28 20:43:46 +0530

    docs: Add publish recipes to cookbook
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/manuals/developer/cookbook/snippets/publish-image/go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type MyModule struct{}
+
+// Publish a container image to a private registry
+func (m *MyModule) Publish(
+	ctx context.Context,
+	// Registry address
+	registry string,
+	// Registry username
+	username string,
+	// Registry password
+	password *Secret,
+) (string, error) {
+	return dag.Container().
+		From("nginx:1.23-alpine").
+		WithNewFile("/usr/share/nginx/html/index.html", dagger.ContainerWithNewFileOpts{
+			Contents:    "Hello from Dagger!",
+			Permissions: 0o400,
+		}).
+		WithRegistryAuth(registry, username, password).
+		Publish(ctx, fmt.Sprintf("%s/%s/my-nginx", registry, username))
+}
---
+++ docs/current_docs/manuals/developer/cookbook/snippets/tag-publish-image/python/main.py
@@ -0,0 +1,31 @@
+from typing import Annotated, List
+
+import dagger
+from dagger import Doc, dag, function, object_type
+
+
+@object_type
+class MyModule:
+    @function
+    async def publish(
+        registry: Annotated[str, Doc("Registry address")],
+        username: Annotated[str, Doc("Registry username")],
+        password: Annotated[dagger.Secret, Doc("Registry password")],
+    ) -> List[str]:
+        """Tag a container image multiple times and publish it to a private registry"""
+        tags = ["latest", "1.0-alpine", "1.0", "1.0.0"]
+        addr = []
+        container = (
+            dag.container()
+            .from_("nginx:1.23-alpine")
+            .with_new_file(
+                "/usr/share/nginx/html/index.html",
+                contents="Hello from Dagger!",
+                permissions=0o400,
+            )
+            .with_registry_auth(registry, username, password)
+        )
+        for tag in tags:
+            a = await container.publish(f"{registry}/{username}/my-nginx:{tag}")
+            addr.append(a)
+        return addr
