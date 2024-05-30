7de7a9e201df41d5222f777a4942438ee867d3c2
Lev Lazinskiy <lev@dagger.io>
2024-05-30 13:44:16 -0700

    fix typescript linting issues
    Signed-off-by: Lev Lazinskiy <lev@dagger.io>

--- docs/current_docs/manuals/developer/cookbook/snippets/builds/multi-arch/typescript/index.ts
+++ docs/current_docs/manuals/developer/cookbook/snippets/builds/multi-arch/typescript/index.ts
@@ -1,49 +1,59 @@
-import { dag, Container, Directory, Platform, object, func, ContainerPublishOpts } from "@dagger.io/dagger"
+import {
+  dag,
+  Container,
+  Directory,
+  Platform,
+  object,
+  func,
+} from "@dagger.io/dagger"
 
 @object()
-class Ts {
+class MyModule {
+  /**
+   * Build and publish multi-platform image
+   * @param src source code location
+   */
   @func()
   async build(src: Directory): Promise<string> {
     // platforms to build for and push in a multi-platform image
     const platforms: Platform[] = [
-        "linux/amd64" as Platform, // a.k.a. x86_64
-        "linux/arm64" as Platform, // a.k.a. aarch64
-        "linux/s390x" as Platform, // a.k.a. IBM S/390 
+      "linux/amd64" as Platform, // a.k.a. x86_64
+      "linux/arm64" as Platform, // a.k.a. aarch64
+      "linux/s390x" as Platform, // a.k.a. IBM S/390
     ]
 
     // container registry for multi-platform image
     const imageRepo = "ttl.sh/myapp:latest"
-    
-    let platformVariants: Array<Container> = []
+    const platformVariants: Array<Container> = []
     for (const platform of platforms) {
       const ctr = dag
-        .container({platform: platform})
+        .container({ platform: platform })
         .from("golang:1.21-alpine")
-        // mount source 
+        // mount source
         .withDirectory("/src", src)
-        // mount empty dir where built binary will live 
+        // mount empty dir where built binary will live
         .withDirectory("/output", dag.directory())
         // ensure binary will be statically linked and thus executable
-        // in the final image 
+        // in the final image
         .withEnvVariable("CGO_ENABLED", "0")
         .withWorkdir("/src")
         .withExec(["go", "build", "-o", "/output/hello"])
-  
+
       // select output directory
       const outputDir = ctr.directory("/output")
 
-      // wrap output directory in a new empty container marked 
+      // wrap output directory in a new empty container marked
       // with the same platform
       const binaryCtr = await dag
-        .container({platform: platform})
+        .container({ platform: platform })
         .withRootfs(outputDir)
 
       platformVariants.push(binaryCtr)
     }
-    // publish to registry 
+    // publish to registry
     const imageDigest = await dag
       .container()
-      .publish(imageRepo, {platformVariants:platformVariants})
+      .publish(imageRepo, { platformVariants: platformVariants })
 
     return imageDigest
   }
