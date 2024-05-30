a7acfa60353f30679dc70d833b61682c245b4664
Alex Suraci <alex@dagger.io>
2024-05-29 23:16:48 -0400

    sdk/go: heartbeatinga (TODO replace)
    committing for now since I lost it at one point and it's better than
    nothing
    
    but really this responsibility should move outside of the SDK, probably
    with an exporter, in both the CLI and the engine
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- sdk/go/telemetry/live.go
+++ sdk/go/telemetry/live.go
@@ -2,26 +2,87 @@ package telemetry
 
 import (
 	"context"
+	"sync"
 	"time"
 
 	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
 )
 
 type LiveSpanProcessor struct {
 	sdktrace.SpanProcessor
+
+	activeSpans  map[spanKey]sdktrace.ReadOnlySpan
+	activeSpansL *sync.Mutex
+
+	heartbeatCtx    context.Context
+	heartbeatCancel func()
 }
 
+type spanKey struct {
+	traceID trace.TraceID
+	spanID  trace.SpanID
+}
+
+const HeartbeatInterval = 30 * time.Second
+
 func NewLiveSpanProcessor(exp sdktrace.SpanExporter) *LiveSpanProcessor {
-	return &LiveSpanProcessor{
+	lsp := &LiveSpanProcessor{
 		SpanProcessor: sdktrace.NewBatchSpanProcessor(
 			exp,
 			sdktrace.WithBatchTimeout(NearlyImmediate),
 		),
+		activeSpans:  map[spanKey]sdktrace.ReadOnlySpan{},
+		activeSpansL: &sync.Mutex{},
 	}
+
+	lsp.heartbeatCtx, lsp.heartbeatCancel = context.WithCancel(context.Background())
+
+	go lsp.heartbeat()
+
+	return lsp
 }
 
-func (p LiveSpanProcessor) OnStart(ctx context.Context, span sdktrace.ReadWriteSpan) {
-	p.SpanProcessor.OnEnd(liveSpan{span})
+func (p *LiveSpanProcessor) OnStart(ctx context.Context, span sdktrace.ReadWriteSpan) {
+	live := liveSpan{span}
+	p.SpanProcessor.OnEnd(live)
+	p.activeSpansL.Lock()
+	p.activeSpans[spanKey{
+		span.SpanContext().TraceID(),
+		span.SpanContext().SpanID(),
+	}] = live
+	p.activeSpansL.Unlock()
+}
+
+func (p *LiveSpanProcessor) OnEnd(span sdktrace.ReadOnlySpan) {
+	p.SpanProcessor.OnEnd(span)
+	p.activeSpansL.Lock()
+	delete(p.activeSpans, spanKey{
+		span.SpanContext().TraceID(),
+		span.SpanContext().SpanID(),
+	})
+	p.activeSpansL.Unlock()
+}
+
+func (p *LiveSpanProcessor) Shutdown(ctx context.Context) error {
+	p.heartbeatCancel()
+	return p.SpanProcessor.Shutdown(ctx)
+}
+
+func (p *LiveSpanProcessor) heartbeat() {
+	ticker := time.NewTicker(HeartbeatInterval)
+	for {
+		select {
+		case <-p.heartbeatCtx.Done():
+			return
+		case <-ticker.C:
+			p.activeSpansL.Lock()
+			for _, span := range p.activeSpans {
+				p.SpanProcessor.OnEnd(span)
+			}
+			p.activeSpansL.Unlock()
+		}
+	}
 }
 
 // liveSpan is a poor man's span snapshot, ensuring that we're adding an
