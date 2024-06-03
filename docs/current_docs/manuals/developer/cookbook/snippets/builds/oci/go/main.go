71282b4d69fb9b401deeae35de4829938a3d1991
Lev Lazinskiy <lev@dagger.io>
2024-06-03 12:08:50 -0700

    docs: add oci annotations example
    Signed-off-by: Lev Lazinskiy <lev@dagger.io>

---
+++ docs/current_docs/manuals/developer/cookbook/snippets/builds/oci/go/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"time"
+)
+
+type MyModule struct{}
+
+// Build and publish image with oci annotations
+func (m *MyModule) Build(
+	ctx context.Context,
+) (string, error) {
+	ref, err := dag.Container().
+		From("alpine").
+		WithLabel("org.opencontainers.image.title", "my-alpine").
+		WithLabel("org.opencontainers.image.version", "1.0").
+		WithLabel("org.opencontainers.image.created", time.Now().String()).
+		WithLabel("org.opencontainers.image.source", "https://github.com/alpinelinux/docker-alpine").
+		WithLabel("org.opencontainers.image.licenses", "MIT").
+		Publish(ctx, "ttl.sh/my-alpine")
+
+	if err != nil {
+		return "", err
+	}
+
+	return ref, nil
+}
