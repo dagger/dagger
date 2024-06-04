48322e129cca2ead278f3500cc97605425b6d559
Justin Chadwell <me@jedevc.com>
2024-06-04 16:20:38 +0100

    chore(python): throw error if session exits unexpectedly (#7473)
    * chore(python): throw error if session exits unexpectedly
    
    Also, don't close stdout/stderr before the process properly exits.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * Reuse Popen's cleanup of resources
    
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>
    
    * Adjust for test
    
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>
    Co-authored-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/src/dagger/_engine/session.py
+++ sdk/python/src/dagger/_engine/session.py
@@ -1,4 +1,5 @@
 import contextlib
+import dataclasses
 import json
 import logging
 import subprocess
@@ -30,13 +31,51 @@ def start_cli_session(cfg: dagger.Config, path: str):
     return SyncResource(start_cli_session_sync(cfg, path))
 
 
+@dataclasses.dataclass(slots=True)
+class Pclose(contextlib.AbstractContextManager):
+    """Close process by closing stdin and waiting for it to exit."""
+
+    proc: subprocess.Popen[str]
+
+    # Set a long timeout to give time for any cache exports to pack layers up
+    # which currently has to happen synchronously with the session.
+    timeout: int = 300
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        # Kill the child process by closing stdin, not via SIGKILL, so it has
+        # a chance to drain logs.
+        try:
+            if self.proc.stdin:
+                self.proc.stdin.close()
+        except AttributeError:
+            # FakeProcess doesn't have a stdin attribute (tests)
+            self.proc.terminate()
+
+        try:
+            self._wait()
+        except Exception:  # Including KeyboardInterrupt, wait handled that.
+            self.proc.kill()
+            # We don't call proc.wait() again as proc.__exit__ does that for us.
+            raise
+
+    def _wait(self):
+        # avoids raise-within-try (TRY301)
+        if self.proc.wait(self.timeout):
+            # non-zero exit code
+            msg = make_process_error_msg(self.proc, None, None)
+            raise dagger.SessionError(msg)
+
+
 @contextlib.contextmanager
 def start_cli_session_sync(cfg: dagger.Config, path: str):
     """Start an engine session with a provided CLI path."""
     logger.debug("Starting session using %s", path)
     try:
-        with run(cfg, path) as proc:
-            yield get_connect_params(proc)
+        with contextlib.ExitStack() as stack:
+            proc = stack.enter_context(run(cfg, path))
+            params = get_connect_params(proc)
+            stack.push(Pclose(proc))
+            yield params
     except (OSError, ValueError, TypeError) as e:
         raise dagger.SessionError(e) from e
 
@@ -93,7 +132,8 @@ def get_connect_params(proc: subprocess.Popen[str]) -> ConnectParams:
     # Check if subprocess exited with an error
     if proc.poll():
         stdout = conn + proc.stdout.read()
-        msg = make_process_error_msg(proc, stdout)
+        stderr = proc.stderr.read() if proc.stderr and proc.stderr.readable() else None
+        msg = make_process_error_msg(proc, stdout, stderr)
         raise dagger.SessionError(msg)
 
     if not conn:
@@ -107,15 +147,18 @@ def get_connect_params(proc: subprocess.Popen[str]) -> ConnectParams:
         raise dagger.SessionError(msg) from e
 
 
-def make_process_error_msg(proc: subprocess.Popen[str], out: str) -> str:
-    err = proc.stderr.read() if proc.stderr and proc.stderr.readable() else None
+def make_process_error_msg(
+    proc: subprocess.Popen[str],
+    stdout: str | None,
+    stderr: str | None,
+) -> str:
     args = cast(list[str], proc.args)
 
     # Reuse error message from CalledProcessError
     exc = subprocess.CalledProcessError(proc.returncode, " ".join(args))
 
     msg = str(exc)
-    detail = err or out
+    detail = stderr or stdout
     if detail and detail.strip():
         # `msg` ends in a period, just append
         msg = f"{msg} {detail.strip()}"
