8bc592b01908b38d79e10cfaeb3770a90cc133d9
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-06-05 03:40:01 +0530

    docs: Add service optimization and export recipes to cookbook (#7502)
    * docs: Add optimization and export recipes to cookbook
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fix linter
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Add feedback
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Update service state example
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fix linter
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Add Go example
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    ---------
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    Co-authored-by: Lev Lazinskiy <lev@dagger.io>

--- docs/current_docs/manuals/developer/snippets/services/persist-service-state/go/main.go
+++ docs/current_docs/manuals/developer/snippets/services/persist-service-state/go/main.go
@@ -7,7 +7,7 @@ import (
 type MyModule struct{}
 
 // Create Redis service and client
-func (m *MyModule) RedisService(ctx context.Context) (string, error) {
+func (m *MyModule) Redis(ctx context.Context) *Container {
 	redisSrv := dag.Container().
 		From("redis").
 		WithExposedPort(6379).
@@ -15,20 +15,35 @@ func (m *MyModule) RedisService(ctx context.Context) (string, error) {
 		WithWorkdir("/data").
 		AsService()
 
-	// create Redis client container
 	redisCLI := dag.Container().
 		From("redis").
 		WithServiceBinding("redis-srv", redisSrv).
 		WithEntrypoint([]string{"redis-cli", "-h", "redis-srv"})
 
-	// set and save value
-	redisCLI.
-		WithExec([]string{"set", "foo", "abc"}).
-		WithExec([]string{"save"}).
-		Sync(ctx)
+	return redisCLI
+}
 
-	// get value
-	return redisCLI.
-		WithExec([]string{"get", "foo"}).
+// Set key and value in Redis service
+func (m *MyModule) Set(
+	ctx context.Context,
+	// The cache key to set
+	key string,
+	// The cache value to set
+	value string,
+) (string, error) {
+	return m.Redis(ctx).
+		WithExec([]string{"set", key, value}).
+		WithExec([]string{"save"}).
+		Stdout(ctx)
+}
+
+// Get value from Redis service
+func (m *MyModule) Get(
+	ctx context.Context,
+	// The cache value to set
+	key string,
+) (string, error) {
+	return m.Redis(ctx).
+		WithExec([]string{"get", key}).
 		Stdout(ctx)
 }
