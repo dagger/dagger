765b761ddd2d6fbf53fffb4faba07f77c989c4be
Vikram Vaswani <vikram@dagger.io>
2024-06-04 19:28:55 +0530

    Updated error handling base example
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/manuals/developer/cookbook/snippets/handle-errors/go/main.go
+++ docs/current_docs/manuals/developer/cookbook/snippets/handle-errors/go/main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
-
-	"dagger.io/dagger/dag"
 )
 
 type MyModule struct{}
@@ -17,8 +16,11 @@ func (m *MyModule) Test(ctx context.Context) (string, error) {
 		// ERROR: cat: read error: Is a directory
 		WithExec([]string{"cat", "/"}).
 		Stdout(ctx)
-	if err != nil {
-		return "", fmt.Errorf("Test pipeline failure: %w", err)
+	var e *ExecError
+	if errors.As(err, &e) {
+		return fmt.Sprintf("Test pipeline failure: %s", e.Stderr), nil
+	} else if err != nil {
+		return "", err
 	}
 	return out, nil
 }
