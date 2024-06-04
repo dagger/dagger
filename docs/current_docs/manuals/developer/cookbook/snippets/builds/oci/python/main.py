3a90a244e511d621d945cf60eed899baf217bb0a
Lev Lazinskiy <lev@dagger.io>
2024-06-04 10:02:44 -0700

    docs: wrap up build cookbook (#7538)
    * docs: add oci annotations example
    * docs: add cache invalidation example
    
    Signed-off-by: Lev Lazinskiy <lev@dagger.io>

---
+++ docs/current_docs/manuals/developer/cookbook/snippets/builds/oci/python/main.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timezone
+
+from dagger import dag, function, object_type
+
+
+@object_type
+class MyModule:
+    @function
+    async def build(self) -> str:
+        """Build and publish image with oci labels"""
+        ref = (
+            dag.container()
+            .from_("alpine")
+            .with_label("org.opencontainers.image.title", "my-alpine")
+            .with_label("org.opencontainers.image.version", "1.0")
+            .with_label(
+                "org.opencontainers.image.created",
+                datetime.now(timezone.utc).isoformat(),
+            )
+            .with_label(
+                "org.opencontainers.image.source",
+                "https://github.com/alpinelinux/docker-alpine",
+            )
+            .with_label("org.opencontainers.image.licenses", "MIT")
+            .publish("ttl.sh/my-alpine")
+        )
+        return await ref
