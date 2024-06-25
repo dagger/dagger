5231aa7de19266430581da054604ce533cc85898
Justin Chadwell <me@jedevc.com>
2024-06-25 16:36:02 +0100

    chore: remove examples directory (#7752)
    * chore: remove examples directory
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * chore: remove references to examples dir
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    ---------
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- examples/sdk/go/gradle-build/src/main/java/hello/Greeter.java
+++
@@ -1,7 +0,0 @@
-package hello;
-
-public class Greeter {
-  public String sayHello() {
-  return "Hello world!";
-  }
-}
--- examples/sdk/go/gradle-build/src/main/java/hello/HelloWorld.java
+++
@@ -1,8 +0,0 @@
-package hello;
-
-public class HelloWorld {
-  public static void main(String[] args) {
-  Greeter greeter = new Greeter();
-  System.out.println(greeter.sayHello());
-  }
-}
