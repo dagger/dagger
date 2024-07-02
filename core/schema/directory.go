badd4384cd2c2932f6e10336f6bc902bfb3ee41b
Justin Chadwell <me@jedevc.com>
2024-07-02 12:35:46 +0100

    directory terminal should be in v0.12.0 as well
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- core/schema/directory.go
+++ core/schema/directory.go
@@ -92,23 +92,28 @@ func (s *directorySchema) Install() {
 			Doc(`Retrieves this directory with all file/dir timestamps set to the given time.`).
 			ArgDoc("timestamp", `Timestamp to set dir/files in.`,
 				`Formatted in seconds following Unix epoch (e.g., 1672531199).`),
-		dagql.NodeFunc("terminal", s.terminal).
-			Impure("Nondeterministic.").
-			Doc(`Opens an interactive terminal in new container with this directory mounted inside.`).
-			ArgDoc("container", `If set, override the default container used for the terminal.`).
-			ArgDoc("cmd", `If set, override the container's default terminal command and invoke these command arguments instead.`).
-			ArgDoc("experimentalPrivilegedNesting",
-				`Provides Dagger access to the executed command.`,
-				`Do not use this option unless you trust the command being executed;
+	}.Install(s.srv)
+
+	dagql.View(s.srv, containsVersion("v0.12.0"), func(srv dagql.ServerView) {
+		dagql.Fields[*core.Directory]{
+			dagql.NodeFunc("terminal", s.terminal).
+				Impure("Nondeterministic.").
+				Doc(`Opens an interactive terminal in new container with this directory mounted inside.`).
+				ArgDoc("container", `If set, override the default container used for the terminal.`).
+				ArgDoc("cmd", `If set, override the container's default terminal command and invoke these command arguments instead.`).
+				ArgDoc("experimentalPrivilegedNesting",
+					`Provides Dagger access to the executed command.`,
+					`Do not use this option unless you trust the command being executed;
 			the command being executed WILL BE GRANTED FULL ACCESS TO YOUR HOST
 			FILESYSTEM.`).
-			ArgDoc("insecureRootCapabilities",
-				`Execute the command with all root capabilities. This is similar to
+				ArgDoc("insecureRootCapabilities",
+					`Execute the command with all root capabilities. This is similar to
 			running a command with "sudo" or executing "docker run" with the
 			"--privileged" flag. Containerization does not provide any security
 			guarantees when using this option. It should only be used when
 			absolutely necessary and only with trusted commands.`),
-	}.Install(s.srv)
+		}.Install(srv)
+	})
 }
 
 type directoryPipelineArgs struct {
