a68ff4af7bfb7068a3ae0a4fdedee856eb7f8015
Justin Chadwell <me@jedevc.com>
2024-07-03 14:12:37 +0100

    chore: bump client/engine version compat to v0.12.0
    With changes to Terminal, and other related recent changes, we should
    bump the client/engine compat to the next release.
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- engine/version.go
+++ engine/version.go
@@ -19,11 +19,11 @@ var (
 
 	// MinimumEngineVersion is used by the client to determine the minimum
 	// allowed engine version that can be used by that client.
-	MinimumEngineVersion = "v0.11.8"
+	MinimumEngineVersion = "v0.12.0"
 
 	// MinimumClientVersion is used by the engine to determine the minimum
 	// allowed client version that can connect to that engine.
-	MinimumClientVersion = "v0.11.8"
+	MinimumClientVersion = "v0.12.0"
 )
 
 func init() {
