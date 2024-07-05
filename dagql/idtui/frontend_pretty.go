2dbcf8523978743e9d5ddb627e865e941405ff15
Alex Suraci <alex@dagger.io>
2024-07-05 12:59:18 -0400

    tui: render error logs beneath final print
    fixes #7007
    
    rather than printing log output in the tree view, which is annoying
    because it can't be copy-pasted, print error logs below it.
    
    this involves a mildly tedious process of hopping from unlazying spans
    (sync) to its unlazied effects (exec foo) in order to resurface it
    wherever it was installed in the tree (Go.gotestsum >
    Container.withExec).
    
    same logic as with Cloud.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/idtui/frontend_pretty.go
+++ dagql/idtui/frontend_pretty.go
@@ -218,6 +218,36 @@ func (fe *frontendPretty) runWithTUI(ctx context.Context, ttyIn *os.File, ttyOut
 	return fe.err
 }
 
+func (fe *frontendPretty) renderErrorLogs(out *termenv.Output) error {
+	if fe.rowsView == nil {
+		return nil
+	}
+	errTree := fe.db.CollectErrors(fe.rowsView)
+	var anyHasLogs bool
+	WalkTree(errTree, func(row *TraceTree, _ int) bool {
+		logs := fe.logs.Logs[row.Span.ID]
+		if logs != nil && logs.UsedHeight() > 0 {
+			anyHasLogs = true
+			return true
+		}
+		return false
+	})
+	if anyHasLogs {
+		fmt.Fprintln(out)
+		fmt.Fprintln(out, out.String("Error logs:").Bold())
+	}
+	WalkTree(errTree, func(row *TraceTree, _ int) bool {
+		logs := fe.logs.Logs[row.Span.ID]
+		if logs != nil && logs.UsedHeight() > 0 {
+			fmt.Fprintln(out)
+			fe.renderStep(out, row.Span, 0, "")
+			fe.renderLogs(out, logs, -1, logs.UsedHeight(), "")
+		}
+		return false
+	})
+	return nil
+}
+
 // finalRender is called after the program has finished running and prints the
 // final output after the TUI has exited.
 func (fe *frontendPretty) finalRender() error {
@@ -230,14 +260,23 @@ func (fe *frontendPretty) finalRender() error {
 	fe.focusedIdx = -1
 	fe.recalculateViewLocked()
 
+	// Render to stderr so stdout stays clean.
+	out := NewOutput(os.Stderr, termenv.WithProfile(fe.profile))
+
 	var renderedProgress bool
 	if fe.Debug || fe.Verbosity >= ShowCompletedVerbosity || fe.err != nil {
 		if fe.msgPreFinalRender.Len() > 0 {
 			fmt.Fprintf(os.Stderr, fe.msgPreFinalRender.String()+"\n\n")
 		}
-		// Render progress to stderr so stdout stays clean.
-		out := NewOutput(os.Stderr, termenv.WithProfile(fe.profile))
 		renderedProgress = fe.renderProgress(out, true, fe.window.Height, "")
+	}
+
+	// If there are errors, show log output.
+	if fe.err != nil && renderedProgress {
+		// Counter-intuitively, we don't want to render the primary output
+		// when there's an error, because the error is better represented by
+		// the progress output and error summary.
+		return fe.renderErrorLogs(out)
 	}
 
 	if renderedProgress {
@@ -846,18 +885,18 @@ func (fe *frontendPretty) renderLocked() {
 	fe.Render(fe.viewOut)
 }
 
-func (fe *frontendPretty) renderRow(out *termenv.Output, row *TraceRow, full bool, prefix string) {
+func (fe *frontendPretty) renderRow(out *termenv.Output, row *TraceRow, final bool, prefix string) {
 	fe.renderStep(out, row.Span, row.Depth, prefix)
+	if final {
+		// in the final render, we show logs beneath the progress tree instead
+		return
+	}
 	if row.IsRunningOrChildRunning || row.Span.Failed() || fe.Verbosity >= ShowSpammyVerbosity {
 		if logs := fe.logs.Logs[row.Span.ID]; logs != nil {
-			logLimit := fe.window.Height / 3
-			if full {
-				logLimit = logs.UsedHeight()
-			}
 			fe.renderLogs(out,
 				logs,
 				row.Depth,
-				logLimit,
+				fe.window.Height/3,
 				prefix,
 			)
 		}
