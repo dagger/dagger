5403273a62b3cbd0f45562ed60420963e9aa3ccf
Helder Correia <174525+helderco@users.noreply.github.com>
2024-07-19 11:26:00 +0000

    DNM: private sharing mode for uv's cache volume
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/runtime/main.go
+++ sdk/python/runtime/main.go
@@ -200,7 +200,13 @@ func (m *PythonSdk) WithBase() (*PythonSdk, error) {
 				Include: []string{"uv*"},
 			},
 		).
-		WithMountedCache("/root/.cache/uv", dag.CacheVolume("modpython-uv")).
+		WithMountedCache(
+			"/root/.cache/uv",
+			dag.CacheVolume("modpython-uv"),
+			dagger.ContainerWithMountedCacheOpts{
+				Sharing: dagger.Private,
+			},
+		).
 		WithEnvVariable("DAGGER_UV_IMAGE", uvAddr).
 		WithEnvVariable("UV_VERSION", uvTag).
 		WithEnvVariable("UV_SYSTEM_PYTHON", "1").
