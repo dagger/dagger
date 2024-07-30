baea38d4ba3f9920905b70585755e9486630b1ec
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-07-31 01:08:51 +0700

    feat(dev): add unused parameters to ps-analyzer in scripts lint
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- dev/scripts.go
+++ dev/scripts.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 
+	"github.com/dagger/dagger/dev/internal/dagger"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -20,7 +21,12 @@ func (s Scripts) Lint(ctx context.Context) error {
 	})
 	eg.Go(func() error {
 		return dag.PsAnalyzer().
-			Check(s.Dagger.Source().File("install.ps1")).
+			Check(s.Dagger.Source().File("install.ps1"), dagger.PsAnalyzerCheckOpts{
+				// Exclude the unused parameters for now due because PSScriptAnalyzer treat
+				// parameters in `Install-Dagger` as unused but the script won't run if we delete
+				// it.
+				ExcludeRules: []string{"PSReviewUnusedParameter"},
+			}).
 			Assert(ctx)
 	})
 	return eg.Wait()
