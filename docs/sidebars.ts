09d8d1854b764e74966d6872a7d9ef824e8870be
Vikram Vaswani <vikram@dagger.io>
2024-08-03 01:34:38 +0530

    Fix sidebar
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -221,7 +221,7 @@ module.exports = {
           "label": "Dagger Functions",
           "link": {
             "type": "doc",
-            "id": "manual/functions/structure"
+            "id": "manual/functions/initialization-structure-packaging"
           },
           "collapsible": true,
           "collapsed": true,
