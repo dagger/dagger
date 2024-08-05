0930a6ceacadcd45049a5c41142fa1c42ec85205
Vikram Vaswani <vikram@dagger.io>
2024-08-05 22:18:49 +0530

    Update sidebar
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -128,29 +128,18 @@ module.exports = {
         },
         {
           "type": "category",
-          "label": "Dagger API",
-          "link": {
-            "type": "doc",
-            "id": "manual/api/index"
-          },
+          "label": "Dagger CLI",
           "collapsible": true,
           "collapsed": true,
             items: [
               {
                 "type": "doc",
-                "id": "manual/api/queries"
+                "id": "manual/cli/visualization"
               },
               {
                 "type": "doc",
-                "id": "manual/api/state-representation"
-              },
-              {
-                "type": "doc",
-                "id": "manual/api/lazy-evaluation"
-              },
-              {
-                "type": "doc",
-                "id": "manual/api/types"
+                "label": "CLI Reference",
+                "id": "reference/cli"
               },
             ],
         },
@@ -186,6 +175,11 @@ module.exports = {
             },
             {
               "type": "doc",
+              "id": "manual/functions/debugging"
+            },
+
+            {
+              "type": "doc",
               "id": "manual/functions/documentation"
             },
             {
@@ -202,15 +196,23 @@ module.exports = {
             },
             {
               "type": "doc",
+              "id": "manual/functions/host-resources"
+            },
+            {
+              "type": "doc",
+              "id": "manual/functions/remote-resources"
+            },
+            {
+              "type": "doc",
               "id": "manual/functions/error-handling"
             },
             {
               "type": "doc",
-              "id": "manual/functions/debugging"
+              "id": "manual/functions/remote-resources"
             },
             {
               "type": "doc",
-              "id": "manual/functions/visualization"
+              "id": "manual/functions/publish"
             },
             {
               "type": "category",
@@ -221,6 +223,10 @@ module.exports = {
                 {
                   "type": "doc",
                   "id": "manual/functions/execution-environment"
+                },
+                {
+                  "type": "doc",
+                  "id": "manual/functions/structure-packaging"
                 },
                 {
                   "type": "doc",
@@ -246,39 +252,30 @@ module.exports = {
                   "type": "doc",
                   "id": "manual/functions/language-dependencies"
                 },
-                {
-                  "type": "doc",
-                  "id": "manual/functions/structure-packaging"
-                },
               ],
             },
-          ],
-        },
-        {
-          "type": "category",
-          "label": "Dagger Engine",
-          "link": {
-            "type": "doc",
-            "id": "manual/engine/index"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
             {
-              "type": "doc",
-              "id": "manual/engine/custom-ca"
-            },
-            {
-              "type": "doc",
-              "id": "manual/engine/custom-registry"
-            },
-            {
-              "type": "doc",
-              "id": "manual/engine/custom-runner"
-            },
-            {
-              "type": "doc",
-              "id": "manual/engine/proxy"
+              "type": "category",
+              "label": "Reference",
+              "collapsible": true,
+              "collapsed": true,
+              "items": [
+                {
+                  "type": "link",
+                  "label": "Go SDK Reference",
+                  "href": "https://pkg.go.dev/dagger.io/dagger"
+                },
+                {
+                  "type": "link",
+                  "label": "Python SDK Reference",
+                  "href": "https://dagger-io.readthedocs.org/"
+                },
+                {
+                  "type": "doc",
+                  "label": "TypeScript SDK Reference",
+                  "id": "reference/typescript/modules"
+                },
+              ]
             },
           ],
         },
@@ -298,6 +295,10 @@ module.exports = {
             },
             {
               "type": "doc",
+              "id": "manual/cloud/traces"
+            },
+            {
+              "type": "doc",
               "id": "manual/cloud/caching"
             },
             {
@@ -308,6 +309,71 @@ module.exports = {
               "type": "doc",
               "id": "manual/cloud/roles-permissions"
             },
+            {
+              "type": "doc",
+              "id": "manual/cloud/runs"
+            },
+          ],
+        },
+        {
+          "type": "category",
+          "label": "Dagger API",
+          "link": {
+            "type": "doc",
+            "id": "manual/api/index"
+          },
+          "collapsible": true,
+          "collapsed": true,
+            items: [
+              {
+                "type": "doc",
+                "id": "manual/api/queries"
+              },
+              {
+                "type": "doc",
+                "id": "manual/api/state-representation"
+              },
+              {
+                "type": "doc",
+                "id": "manual/api/lazy-evaluation"
+              },
+              {
+                "type": "doc",
+                "id": "manual/api/types"
+              },
+              {
+                "type": "link",
+                "label": "API Reference",
+                "href": "https://docs.dagger.io/api/reference"
+              },
+            ],
+        },
+        {
+          "type": "category",
+          "label": "Dagger Engine",
+          "link": {
+            "type": "doc",
+            "id": "manual/engine/index"
+          },
+          "collapsible": true,
+          "collapsed": true,
+          "items": [
+            {
+              "type": "doc",
+              "id": "manual/engine/custom-runner"
+            },
+            {
+              "type": "doc",
+              "id": "manual/engine/custom-registry"
+            },
+            {
+              "type": "doc",
+              "id": "manual/engine/custom-ca"
+            },
+            {
+              "type": "doc",
+              "id": "manual/engine/proxy"
+            },
           ],
         },
         {
@@ -315,407 +381,6 @@ module.exports = {
           "id": "manual/known-issues"
         },
       ]
-    },
-    /*
-    {
-      "type": "category",
-      "label": "User Manual",
-      "link": {
-        "type": "doc",
-        "id": "manuals/user/index"
-      },
-      "collapsible": true,
-      "collapsed": true,
-      "items": [
-        {
-          "type": "category",
-          "label": "Functions and Chaining",
-          "link": {
-            "type": "doc",
-            "id": "manuals/user/functions/functions"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "label": "Function Calls",
-              "id": "manuals/user/functions/call"
-            },
-            {
-              "type": "doc",
-              "label": "Arguments",
-              "id": "manuals/user/functions/arguments"
-            },
-            {
-              "type": "doc",
-              "label": "Chaining",
-              "id": "manuals/user/functions/chaining"
-            }
-          ]
-        },
-        {
-          "type": "category",
-          "label": "Just-in-Time Artifacts",
-          "collapsible": true,
-          "collapsed": true,
-          "link": {
-            "type": "doc",
-            "id": "manuals/user/artifacts/artifacts"
-          },
-          "items": [
-            {
-              "type": "category",
-              "label": "Produce and Inspect Artifacts",
-              "link": {
-                "type": "doc",
-                "id": "manuals/user/artifacts/production/produce"
-              },
-              "collapsible": true,
-              "collapsed": true,
-              "items": [
-                {
-                  "type": "doc",
-                  "label": "Just-in-Time Containers",
-                  "id": "manuals/user/artifacts/production/containers"
-                },
-                {
-                  "type": "doc",
-                  "label": "Just-in-Time Directories",
-                  "id": "manuals/user/artifacts/production/directories"
-                },
-                {
-                  "type": "doc",
-                  "label": "Just-in-Time Files",
-                  "id": "manuals/user/artifacts/production/files"
-                },
-                {
-                  "type": "doc",
-                  "label": "Artifact Inspection",
-                  "id": "manuals/user/artifacts/production/inspect"
-                },
-              ]
-            },
-            {
-              "type": "category",
-              "label": "Consume Artifacts",
-              "link": {
-                "type": "doc",
-                "id": "manuals/user/artifacts/consumption/consume"
-              },
-              "collapsible": true,
-              "collapsed": true,
-              "items": [
-                {
-                  "type": "doc",
-                  "label": "Artifact Export",
-                  "id": "manuals/user/artifacts/consumption/export"
-                },
-                {
-                  "type": "doc",
-                  "label": "Container Interaction",
-                  "id": "manuals/user/artifacts/consumption/terminal"
-                },
-                {
-                  "type": "doc",
-                  "label": "Container Publication",
-                  "id": "manuals/user/artifacts/consumption/publish"
-                },
-                {
-                  "type": "doc",
-                  "label": "Container Command Execution",
-                  "id": "manuals/user/artifacts/consumption/exec"
-                },
-                {
-                  "type": "doc",
-                  "label": "Containers as Services",
-                  "id": "manuals/user/artifacts/consumption/services"
-                },
-              ]
-            },
-          ]
-        },
-        {
-          "type": "category",
-          "label": "Host Access",
-          "link": {
-            "type": "doc",
-            "id": "manuals/user/host/host"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "label": "Host Filesystem Access",
-              "id": "manuals/user/host/host-fs"
-            },
-            {
-              "type": "doc",
-              "label": "Host Environment Access",
-              "id": "manuals/user/host/host-env"
-            },
-            {
-              "type": "doc",
-              "label": "Host Services Access",
-              "id": "manuals/user/host/host-services"
-            }
-          ]
-        },
-        {
-          "type": "category",
-          "label": "Remote Resources",
-          "link": {
-            "type": "doc",
-            "id": "manuals/user/remotes/remotes"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "label": "Remote Repositories",
-              "id": "manuals/user/remotes/remote-repositories"
-            },
-            {
-              "type": "doc",
-              "label": "Remote Container Images",
-              "id": "manuals/user/remotes/remote-images"
-            },
-
-          ]
-        },
-        {
-          "type": "category",
-          "label": "Visualization",
-          "link": {
-            "type": "doc",
-            "id": "manuals/user/visualization/visualization"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "label": "Dagger TUI",
-              "id": "manuals/user/visualization/tui"
-            },
-            {
-              "type": "doc",
-              "label": "Dagger Cloud",
-              "id": "manuals/user/visualization/cloud-get-started"
-            },
-          ]
-        },
-        {
-          "type": "doc",
-          "id": "manuals/user/troubleshooting",
-          "label": "Troubleshooting"
-        }
-      ]
-    },
-    {
-      "type": "category",
-      "label": "Developer Manual",
-      "collapsible": true,
-      "collapsed": true,
-      "link": {
-        "type": "doc",
-        "id": "manuals/developer/index"
-      },
-      "items": [
-        {
-          "type": "doc",
-          "id": "manuals/developer/modules-vs-functions"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/architecture"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/execution-environment"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/functions"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/ide-integration"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/module-structure"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/documentation"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/secrets"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/services"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/module-dependencies"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/chaining"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/cache-volumes"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/custom-types"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/enumerations"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/entrypoint-function"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/state-functions"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/runtimes"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/interfaces"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/language-dependencies"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/error-handling"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/debugging"
-        },
-        {
-          "type": "doc",
-          "label": "Publish Modules",
-          "id": "manuals/developer/publish-modules"
-        },
-        {
-          "type": "link",
-          "label": "Go SDK Reference",
-          "href": "https://pkg.go.dev/dagger.io/dagger"
-        },
-        {
-          "type": "link",
-          "label": "Python SDK Reference",
-          "href": "https://dagger-io.readthedocs.org/"
-        },
-        {
-          "type": "doc",
-          "label": "Reference",
-          "id": "reference/typescript/modules"
-        },
-        {
-          "type": "link",
-          "label": "API Reference",
-          "href": "https://docs.dagger.io/api/reference"
-        },
-        {
-          "type": "doc",
-          "id": "manuals/developer/known-issues"
-        }
-      ],
-    },
-    {
-      "type": "category",
-      "label": "Administrator Manual",
-      "link": {
-        "type": "doc",
-        "id": "manuals/administrator/index"
-      },
-      "collapsible": true,
-      "collapsed": true,
-      "items": [
-        {
-          "type": "doc",
-          "id": "manuals/administrator/ci"
-        },
-        {
-          "type": "category",
-          "label": "Dagger Engine",
-          "link": {
-            "type": "doc",
-            "id": "manuals/administrator/engine/index"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "label": "Custom Runner",
-              "id": "manuals/administrator/engine/custom-runner"
-            },
-            {
-              "type": "doc",
-              "label": "Custom Registry",
-              "id": "manuals/administrator/engine/custom-registry"
-            },
-            {
-              "type": "doc",
-              "label": "Proxy Configuration",
-              "id": "manuals/administrator/engine/proxy"
-            },
-            {
-              "type": "doc",
-              "label": "Custom Certificate Authorities",
-              "id": "manuals/administrator/engine/custom-ca"
-            }
-          ]
-        },
-        {
-          "type": "category",
-          "label": "Dagger Cloud",
-          "link": {
-            "type": "doc",
-            "id": "manuals/administrator/cloud/index"
-          },
-          "collapsible": true,
-          "collapsed": true,
-          "items": [
-            {
-              "type": "doc",
-              "id": "manuals/administrator/cloud/roles-permissions"
-            },
-            {
-              "type": "doc",
-              "id": "manuals/administrator/cloud/organizations"
-            },
-            {
-              "type": "doc",
-              "id": "manuals/administrator/cloud/caching"
-            }
-          ]
-        }
-      ]
-    },
-    */
-    {
-      "type": "doc",
-      "label": "CLI Reference",
-      "id": "reference/cli"
     },
     {
       "type": "doc",
