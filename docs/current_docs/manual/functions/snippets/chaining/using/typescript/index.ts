5754015cec19b034c79badb7e38921cc47cb1cdb
Vikram Vaswani <vikram@dagger.io>
2024-08-07 02:39:23 +0530

    Fix lint errors
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/manual/functions/snippets/chaining/using/typescript/index.ts
+++ docs/current_docs/manual/functions/snippets/chaining/using/typescript/index.ts
@@ -3,13 +3,7 @@ import { dag, Directory, object, func } from "@dagger.io/dagger"
 @object()
 class MyModule {
   @func()
-  example(
-    buildSrc: Directory,
-    buildArgs: string[],
-  ): Directory {
-    return dag
-      .golang()
-      .build({ source: buildSrc, args: buildArgs })
-      .terminal()
+  example(buildSrc: Directory, buildArgs: string[]): Directory {
+    return dag.golang().build({ source: buildSrc, args: buildArgs }).terminal()
   }
 }
