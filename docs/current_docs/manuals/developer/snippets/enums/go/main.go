deef1dc1a4512f9fbbe4ae92183e3b87078f015d
Tom Chauveau <tom@epitech.eu>
2024-08-06 13:45:09 +0200

    fix: linter
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- docs/current_docs/manuals/developer/snippets/enums/go/main.go
+++ docs/current_docs/manuals/developer/snippets/enums/go/main.go
@@ -10,38 +10,38 @@ type MyModule struct{}
 type Severity string
 
 const (
-  // Undetermined risk; analyze further.
-  Unknown Severity = "UNKNOWN"
+	// Undetermined risk; analyze further.
+	Unknown Severity = "UNKNOWN"
 
-  // Minimal risk; routine fix.
-  Low Severity = "LOW"
+	// Minimal risk; routine fix.
+	Low Severity = "LOW"
 
-  // Moderate risk; timely fix.
-  Medium Severity = "MEDIUM"
+	// Moderate risk; timely fix.
+	Medium Severity = "MEDIUM"
 
-  // Serious risk; quick fix needed.
-  High Severity = "HIGH"
+	// Serious risk; quick fix needed.
+	High Severity = "HIGH"
 
-  // Severe risk; immediate action.
-  Critical Severity = "CRITICAL"
+	// Severe risk; immediate action.
+	Critical Severity = "CRITICAL"
 )
 
 func (m *MyModule) Scan(ctx context.Context, ref string, severity Severity) (string, error) {
-  ctr := dag.Container().From(ref)
+	ctr := dag.Container().From(ref)
 
-  return dag.Container().
-    From("aquasec/trivy:0.50.4").
-    WithMountedFile("/mnt/ctr.tar", ctr.AsTarball()).
-    WithMountedCache("/root/.cache", dag.CacheVolume("trivy-cache")).
-    WithExec([]string{
-      "trivy",
-      "image",
-      "--format=json",
-      "--no-progress",
-      "--exit-code=1",
-      "--vuln-type=os,library",
-      "--severity=" + string(severity),
-      "--show-suppressed",
-      "--input=/mnt/ctr.tar",
-    }).Stdout(ctx)
+	return dag.Container().
+		From("aquasec/trivy:0.50.4").
+		WithMountedFile("/mnt/ctr.tar", ctr.AsTarball()).
+		WithMountedCache("/root/.cache", dag.CacheVolume("trivy-cache")).
+		WithExec([]string{
+			"trivy",
+			"image",
+			"--format=json",
+			"--no-progress",
+			"--exit-code=1",
+			"--vuln-type=os,library",
+			"--severity=" + string(severity),
+			"--show-suppressed",
+			"--input=/mnt/ctr.tar",
+		}).Stdout(ctx)
 }
