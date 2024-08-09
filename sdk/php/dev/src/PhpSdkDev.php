00939795bf23b94c9f573d83fa7663f9cf3a5568
John Charman <john.charman@imhotek.net>
2024-08-09 15:29:20 +0100

    Add DaggerFunction "format" to PhpSdkDev
    Refactor format to use always-exec module
    
    Update dependency and refactor test
    
    Signed-off-by: John Charman <john.charman@imhotek.net>

--- sdk/php/dev/src/PhpSdkDev.php
+++ sdk/php/dev/src/PhpSdkDev.php
@@ -9,12 +9,15 @@ use Dagger\Attribute\DaggerFunction;
 use Dagger\Attribute\DaggerObject;
 use Dagger\Container;
 use Dagger\Directory;
+use GraphQL\Exception\QueryError;
 
 use function Dagger\dag;
 
 #[DaggerObject]
 final class PhpSdkDev
 {
+    private const SDK_ROOT='/src/sdk/php';
+
     #[DaggerFunction('Run tests from source directory')]
     public function test(
         #[Argument('Run tests from the given source directory')]
@@ -28,9 +31,53 @@ final class PhpSdkDev
         );
     }
 
-    #[DaggerFunction('Run linter in source directory')]
+    #[DaggerFunction('Lint the source directory')]
     public function lint(Directory $source): Container {
         return $this->base($source)->withExec(['phpcs']);
+    }
+
+    /**
+     * PHPCBF exit codes:
+     * 0: No errors were found
+     * 1: Errors were found, all errors were fixed
+     * 2: Errors were found, not all errors could be fixed
+     * 3: General script execution error occurred
+     *
+     * All but exit code 3 are successful executions of the formatter.
+     * These exit codes cannot be customised in configuration:
+     * https://github.com/squizlabs/PHP_CodeSniffer/issues/1818#issuecomment-354420927
+     *
+     * @TODO simplify this script if/when phpcbf can customise exit codes.
+     * This will most likely occur on a 4.0 release:
+     * https://github.com/PHPCSStandards/PHP_CodeSniffer/issues/184
+     */
+    #[DaggerFunction('Format the source directory')]
+    public function format(Directory $source): Directory
+    {
+        $result = dag()->alwaysExec()->exec($this->base($source), ['phpcbf']);
+
+        if (dag()->alwaysExec()->lastExitCode($result) === '3') {
+            throw new QueryError(['errors' => [[
+                'message' => 'An error occured during execution of PHPCBF',
+            ]]]);
+        }
+
+        $original = $this->base($source)->directory(self::SDK_ROOT);
+        return $original->diff($result->directory(self::SDK_ROOT));
+    }
+
+    #[DaggerFunction('Format the source directory')]
+    public function formatStdout(Directory $source): string
+    {
+        $result = dag()->alwaysExec()->exec($this->base($source), ['phpcbf']);
+
+        if (dag()->alwaysExec()->lastExitCode($result) === '3') {
+            throw new QueryError(['errors' => [[
+                'message' => 'An error occured during execution of PHPCBF',
+            ]]]);
+        }
+
+        return dag()->alwaysExec()->stdout($result);
     }
 
     private function base(Directory $source): Container
@@ -46,8 +93,8 @@ final class PhpSdkDev
                 ->cacheVolume('composer-php:8.3-cli-alpine'))
             ->withEnvVariable('COMPOSER_HOME', '/root/.composer')
             ->withEnvVariable('COMPOSER_ALLOW_SUPERUSER', '1')
-            ->withMountedDirectory('/src/sdk/php', $source)
-            ->withWorkdir('/src/sdk/php')
+            ->withMountedDirectory(self::SDK_ROOT, $source)
+            ->withWorkdir(self::SDK_ROOT)
             ->withExec(['composer', 'install'])
             ->withEnvVariable('PATH', './vendor/bin:$PATH', expand: true);
     }
