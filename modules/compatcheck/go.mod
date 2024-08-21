98cb9773273119fe42d51c1b64d90676a257df2a
Rajat Jindal <rajatjindal83@gmail.com>
2024-08-21 06:58:12 +0530

    chore: add module to compare module schema between two given engine versions
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

---
+++ modules/compatcheck/go.mod
@@ -0,0 +1,71 @@
+module dagger/compatcheck
+
+go 1.22.5
+
+require (
+	github.com/99designs/gqlgen v0.17.49
+	github.com/Khan/genqlient v0.7.0
+	github.com/dagger/dagger v0.12.5
+	github.com/josephburnett/jd/v2 v2.0.0-20240818191833-6125a15c637a
+	github.com/moby/buildkit v0.15.2
+	github.com/tidwall/gjson v1.17.3
+	github.com/vektah/gqlparser/v2 v2.5.16
+	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.0.0-20240524160931-7c5e64cccc16
+	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.3.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
+	go.opentelemetry.io/otel/log v0.3.0
+	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/sdk/log v0.3.0
+	go.opentelemetry.io/otel/trace v1.27.0
+	go.opentelemetry.io/proto/otlp v1.3.1
+	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56
+	golang.org/x/mod v0.19.0
+	golang.org/x/sync v0.7.0
+	google.golang.org/grpc v1.64.0
+)
+
+require (
+	dagger.io/dagger v0.12.4 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/swag v0.21.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
+	github.com/iancoleman/strcase v0.3.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/lmittmann/tint v1.0.4 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	github.com/sosodev/duration v1.3.1 // indirect
+	github.com/sourcegraph/conc v0.3.0 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.uber.org/multierr v1.11.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/sys v0.22.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240521202816-d264139d666e // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240521202816-d264139d666e // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+)
+
+replace (
+	github.com/dagger/dagger/.dagger => ../../.dagger
+	github.com/dagger/dagger/engine/distconsts => ../../engine/distconsts
+)
---
+++ modules/compatcheck/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"context"
+	"dagger/compatcheck/internal/dagger"
+	"fmt"
+	"runtime"
+
+	"github.com/dagger/dagger/dagql/introspection"
+	"github.com/josephburnett/jd/v2"
+	"github.com/moby/buildkit/identity"
+	"github.com/tidwall/gjson"
+	"golang.org/x/exp/rand"
+	"golang.org/x/mod/semver"
+)
+
+type Compatcheck struct{}
+
+// Compare the schema of given module with different versions of engine
+func (m *Compatcheck) Run(ctx context.Context,
+	// ref of the module to compare
+	module,
+	// version of engine to compare with
+	versionA,
+	// version of engine to compare to
+	versionB string,
+	//+optional
+	// only required when one of the version to compare is 'dev' [optional]
+	source *dagger.Directory) error {
+	if (versionA == "dev" || versionB == "dev") && source == nil {
+		return fmt.Errorf("--source flag is required when one of the requested engine version is 'dev'")
+	}
+
+	schemaA, err := m.getSchemaForModuleForEngineVersion(ctx, module, versionA, source)
+	if err != nil {
+		return err
+	}
+
+	schemaB, err := m.getSchemaForModuleForEngineVersion(ctx, module, versionB, source)
+	if err != nil {
+		return err
+	}
+
+	a, _ := jd.ReadJsonString(schemaA)
+	b, _ := jd.ReadJsonString(schemaB)
+
+	diff := a.Diff(b).Render()
+	if diff != "" {
+		return fmt.Errorf("%s", diff)
+	}
+
+	return nil
+}
+
+// setup dagger engine/client with requested version and
+// fetches schema using dagger query
+func (m *Compatcheck) getSchemaForModuleForEngineVersion(ctx context.Context, module, engineVersion string, source *dagger.Directory) (string, error) {
+	var engineSvc *dagger.Service
+	var client *dagger.Container
+	var err error
+
+	if engineVersion == "dev" {
+		client = devEngineAndClient(source)
+	} else {
+		engineSvc = engineServiceWithVersion(engineVersion)
+		client, err = engineClientContainerWithVersion(ctx, engineSvc, engineVersion)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	rawIntrospection, err := client.WithNewFile("/schema-query.graphql", introspection.Query).
+		WithExec([]string{"dagger", "query", "-m", module, "--doc", "/schema-query.graphql"}).
+		Stdout(ctx)
+
+	if err != nil {
+		return "", err
+	}
+
+	return gjson.Get(rawIntrospection, "__schema").String(), nil
+}
+
+// returns a container with requested version of dagger cli
+func engineClientContainerWithVersion(ctx context.Context, devEngine *dagger.Service, version string) (*dagger.Container, error) {
+	endpoint, err := devEngine.Endpoint(ctx, dagger.ServiceEndpointOpts{Port: 1234, Scheme: "tcp"})
+	if err != nil {
+		return nil, err
+	}
+
+	// since release v0.12.5, dagger cli is bundled with the docker image of engine
+	if semver.Compare(version, "v0.12.5") >= 0 {
+		return dag.Container().From(fmt.Sprintf("ghcr.io/dagger/engine:%s", version)).
+			WithServiceBinding("dev-engine", devEngine).
+			WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint), nil
+	}
+
+	releaseArtifactName := fmt.Sprintf("dagger_%s_%s_%s", version, runtime.GOOS, runtime.GOARCH)
+	releaseArtifactTarFile := fmt.Sprintf("%s.tar.gz", releaseArtifactName)
+	releaseArtifactDownloadLink := fmt.Sprintf("https://github.com/dagger/dagger/releases/download/%s/%s", version, releaseArtifactTarFile)
+
+	return dag.Container().
+		From("alpine:latest").
+		WithExec([]string{"wget", releaseArtifactDownloadLink, "-O", releaseArtifactTarFile}).
+		WithExec([]string{"tar", "-xvf", releaseArtifactTarFile}).
+		WithExec([]string{"mv", "dagger", "/usr/bin/dagger"}).
+		WithServiceBinding("dev-engine", devEngine).
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint), nil
+}
+
+// returns a service with requested version of the dagger engine
+func engineServiceWithVersion(version string, withs ...func(*dagger.Container) *dagger.Container) *dagger.Service {
+	ctr := dag.Container().From(fmt.Sprintf("ghcr.io/dagger/engine:%s", version))
+	for _, with := range withs {
+		ctr = with(ctr)
+	}
+
+	deviceName, cidr := getUniqueNestedEngineNetwork()
+	return ctr.
+		WithMountedCache("/var/lib/dagger", dag.CacheVolume("dagger-dev-engine-state-"+identity.NewID())).
+		WithExposedPort(1234, dagger.ContainerWithExposedPortOpts{Protocol: dagger.Tcp}).
+		WithExec([]string{
+			"--addr", "tcp://0.0.0.0:1234",
+			"--addr", "unix:///var/run/buildkit/buildkitd.sock",
+			// // avoid network conflicts with other tests
+			"--network-name", deviceName,
+			"--network-cidr", cidr,
+		}, dagger.ContainerWithExecOpts{
+			UseEntrypoint:            true,
+			InsecureRootCapabilities: true,
+		}).AsService()
+}
+
+// returns a container with dev version of dagger engine and cli
+// requires path to root of dagger repository to be provided using
+// --source flag
+func devEngineAndClient(source *dagger.Directory) *dagger.Container {
+	return dag.DaggerDev(source).Dev()
+}
+
+// creates a network CIDR to use for running the engine
+func getUniqueNestedEngineNetwork() (deviceName string, cidr string) {
+	random := rand.Intn(240)
+	return fmt.Sprintf("dagger%d", random), fmt.Sprintf("10.89.%d.0/24", random)
+}
