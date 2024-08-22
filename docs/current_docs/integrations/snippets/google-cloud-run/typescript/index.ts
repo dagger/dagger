88a9e695a0219ffe073c3daaf82914819f48d2b5
Vikram Vaswani <vikram@dagger.io>
2024-08-22 23:33:56 +0530

    Fix lint errors
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/integrations/snippets/google-cloud-run/typescript/index.ts
+++ docs/current_docs/integrations/snippets/google-cloud-run/typescript/index.ts
@@ -3,7 +3,21 @@ import { dag, Secret, object, func } from "@dagger.io/dagger"
 @object()
 class MyModule {
   @func()
-  async deploy(projectName: string, serviceLocation: string, imageAddress: string, servicePort: number, credential: Secret): Promise<string> {
-    return await dag.googleCloudRun().createService(projectName, serviceLocation, imageAddress, servicePort, credential)
+  async deploy(
+    projectName: string,
+    serviceLocation: string,
+    imageAddress: string,
+    servicePort: number,
+    credential: Secret,
+  ): Promise<string> {
+    return await dag
+      .googleCloudRun()
+      .createService(
+        projectName,
+        serviceLocation,
+        imageAddress,
+        servicePort,
+        credential,
+      )
   }
 }
