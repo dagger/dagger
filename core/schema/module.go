71eab236eb610a566e975cf7c42d6001db48fd3e
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-08-25 01:02:47 +0700

    fix(core): drop leading slash from paths in withVCSGeneratedPaths API
    This fix comes from accidentally add a leading slash in the generated
    paths during fix the Elixir issue like this:
    
    ```go
    modName := "potato" // assume the name is this.
    dag.GeneratedCode(directory).
        WithVCSGeneratedPaths([]string{"/src/"+modName+"/lib/mix/tasks/dagger.invoke.ex"})
    ```
    
    The `.gitattributes` produces as:
    
    ```
    //src/potato/...
    ```
    
    It's happens because the API prepending it without detecting the path
    that user given. This fixes by dropping it if the path already
    contained.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- core/schema/module.go
+++ core/schema/module.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"path/filepath"
+	"strings"
 
 	"golang.org/x/sync/errgroup"
 
@@ -1035,6 +1036,7 @@ func (s *moduleSchema) updateCodegenAndRuntime(
 				// already has some config for the file
 				continue
 			}
+			fileName := strings.TrimPrefix(fileName, "/")
 			gitAttrsContents = append(gitAttrsContents,
 				[]byte(fmt.Sprintf("/%s linguist-generated\n", fileName))...,
 			)
