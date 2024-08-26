4e6d6e9da38dd156e448d8884950ab99894e5916
Guillaume de Rouville <guillaume@dagger.io>
2024-08-26 22:04:42 +0000

    chore: reintroduce tests: try killing subprocesses harder if they misbehave
    This PR was reverted in 8196, due to a race in setupPrivateRepoSSHAgent,
    which this PR supports.
    
    Now that the root of the race is fixed, we reintroduce it
    
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- core/integration/module_test.go
+++ core/integration/module_test.go
@@ -14,6 +14,7 @@ import (
 	"path/filepath"
 	"strings"
 	"sync"
+	"syscall"
 	"testing"
 	"time"
 
@@ -6987,10 +6988,41 @@ func cleanupExec(t testing.TB, cmd *exec.Cmd) {
 			t.Logf("never started: %v", cmd.Args)
 			return
 		}
-		t.Logf("interrupting: %v", cmd.Args)
-		cmd.Process.Signal(os.Interrupt)
-		t.Logf("waiting: %v", cmd.Args)
-		cmd.Wait()
+		done := make(chan struct{})
+		go func() {
+			cmd.Wait()
+			close(done)
+		}()
+
+		signals := []syscall.Signal{
+			syscall.SIGINT,
+			syscall.SIGTERM,
+			syscall.SIGKILL,
+		}
+		doSignal := func() {
+			if len(signals) == 0 {
+				return
+			}
+			var signal syscall.Signal
+			signal, signals = signals[0], signals[1:]
+			t.Logf("sending %s: %v", signal, cmd.Args)
+			cmd.Process.Signal(signal)
+		}
+		doSignal()
+
+		for {
+			select {
+			case <-done:
+				return
+			case <-time.After(30 * time.Second):
+				if !t.Failed() {
+					t.Errorf("process did not exit immediately")
+				}
+
+				// the process *still* isn't dead? try killing it harder.
+				doSignal()
+			}
+		}
 	})
 }
 
