ad34698b438601329f6e102d410664b6d0df468f
Alex Suraci <alex@dagger.io>
2024-08-27 10:30:49 -0400

    pin more OTel logging deps
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/integration/module_test.go
+++ core/integration/module_test.go
@@ -403,7 +403,16 @@ func (m *HasNotMainGo) Hello() string { return "Hello, world!" }
 		goMod, err := modGen.File("go.mod").Contents(ctx)
 		require.NoError(t, err)
 		require.Regexp(t, regexp.QuoteMeta(
-			`replace go.opentelemetry.io/otel/sdk/log => go.opentelemetry.io/otel/sdk/log v0.3.0`,
+			`go.opentelemetry.io/otel/log => go.opentelemetry.io/otel/log v0.3.0`,
+		), goMod)
+		require.Regexp(t, regexp.QuoteMeta(
+			`go.opentelemetry.io/otel/sdk/log => go.opentelemetry.io/otel/sdk/log v0.3.0`,
+		), goMod)
+		require.Regexp(t, regexp.QuoteMeta(
+			`go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc => go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.0.0-20240518090000-14441aefdf88`,
+		), goMod)
+		require.Regexp(t, regexp.QuoteMeta(
+			`go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp => go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.3.0`,
 		), goMod)
 	})
 
