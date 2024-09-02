9823a005d5c1c131345c3278bd6ef197c65d1c8b
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2024-09-02 19:59:07 +0530

    docs: Update Google Cloud Run integration page (#8211)
    * docs: Update Google Cloud Run integration page
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fix lint errors
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fix headings
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    ---------
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/integrations/snippets/google-cloud-run/typescript/index.ts
+++ docs/current_docs/integrations/snippets/google-cloud-run/typescript/index.ts
@@ -1,77 +1,23 @@
-import {
-  dag,
-  Container,
-  Directory,
-  Secret,
-  object,
-  func,
-} from "@dagger.io/dagger"
+import { dag, Secret, object, func } from "@dagger.io/dagger"
 
 @object()
 class MyModule {
-  /**
-   * Build an image
-   */
-  @func()
-  build(source: Directory): Container {
-    return dag
-      .container()
-      .from("node:21")
-      .withDirectory("/home/node", source)
-      .withWorkdir("/home/node")
-      .withExec(["npm", "install"])
-      .withEntrypoint(["npm", "start"])
-  }
-
-  /**
-   * Publish an image
-   *
-   * example: dagger call publish --source . --project PROJECT
-   *   --location LOCATION --repository REPOSITORY/APPNAME
-   *   --credential env:GOOGLE_JSON
-   */
-  @func()
-  async publish(
-    source: Directory,
-    project: string,
-    location: string,
-    repository: string,
-    credential: Secret,
-  ): Promise<string> {
-    const registry = `${location}-docker.pkg.dev/${project}/${repository}`
-    return await this.build(source)
-      .withRegistryAuth(`${location}-docker.pkg.dev`, "_json_key", credential)
-      .publish(registry)
-  }
-
-  /**
-   * Deploy an image to Google Cloud Run
-   *
-   * example: dagger call deploy --source . --project PROJECT
-   *  --registry-location LOCATION --repository REPOSITORY/APPNAME
-   *  --service-location LOCATION --service SERVICE
-   *  --credential env:GOOGLE_JSON
-   */
   @func()
   async deploy(
-    source: Directory,
-    project: string,
-    registryLocation: string,
-    repository: string,
+    projectName: string,
     serviceLocation: string,
-    service: string,
+    imageAddress: string,
+    servicePort: number,
     credential: Secret,
   ): Promise<string> {
-    const addr = await this.publish(
-      source,
-      project,
-      registryLocation,
-      repository,
-      credential,
-    )
-
-    return dag
+    return await dag
       .googleCloudRun()
-      .updateService(project, serviceLocation, service, addr, 3000, credential)
+      .createService(
+        projectName,
+        serviceLocation,
+        imageAddress,
+        servicePort,
+        credential,
+      )
   }
 }
