142892b883205b43a8ebcebbe44bccbbd5e6bea8
Vikram Vaswani <vikram@dagger.io>
2024-09-11 19:15:06 +0530

    Added code
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/manuals/developer/snippets/functions/arguments-contextual-dir/python/main.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+
+import dagger
+from dagger import  DefaultPath, function, object_type
+
+@object_type
+class MyModule:
+    @function
+    async def readDir(
+        self,
+        source: Annotated[dagger.Directory, DefaultPath("/")],
+    ) -> list[str]:
+        return await source.entries()
