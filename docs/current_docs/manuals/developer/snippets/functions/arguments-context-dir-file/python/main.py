041fd3611812a5e5fa5ee66c4d0ccb1e8122457f
Vikram Vaswani <vikram@dagger.io>
2024-09-12 02:10:02 +0530

    Fix lint error
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/manuals/developer/snippets/functions/arguments-context-dir-file/python/main.py
+++ docs/current_docs/manuals/developer/snippets/functions/arguments-context-dir-file/python/main.py
@@ -6,14 +6,14 @@ from dagger import  DefaultPath, function, object_type
 @object_type
 class MyModule:
     @function
-    async def readDir(
+    async def read_dir(
         self,
         source: Annotated[dagger.Directory, DefaultPath("/")],
     ) -> list[str]:
         return await source.entries()
 
     @function
-    async def readFile(
+    async def read_file(
         self,
         source: Annotated[dagger.File, DefaultPath("/README.md")],
     ) -> list[str]:
