bd08bf5c0f497e0a460c853ed9e9408b4ef2d6d4
Vasek - Tom C <tom.chauveau@epitech.eu>
2024-09-16 20:13:20 +0200

    fix: TS regression introduce by grapql-request dep bump (#8467)
    Rawcode spotted an issue https://discord.com/channels/707636530424053791/1284180822135406656
    where traces where no longer published to traces.
    The issue is introduced by a change of the type from a plain JSON object
    to `Headers` which break the current opentelemetry propagator.
    This PR fixes that issue by using a custom injector to set the `traceparent` header
    
    Signed-off-by: Vasek - Tom C <tom.chauveau@epitech.eu>

--- sdk/typescript/graphql/client.ts
+++ sdk/typescript/graphql/client.ts
@@ -1,6 +1,17 @@
 import * as opentelemetry from "@opentelemetry/api"
 import { GraphQLClient } from "graphql-request"
 
+/**
+ * Customer setter to inject trace parent into the request headers
+ * This is required because `graphql-request` 7.0.1 changes its header
+ * type to `Headers` which break the default open telemetry propagator.
+ */
+class CustomSetter {
+  set(carrier: Headers, key: string, value: string): void {
+    carrier.set(key, value)
+  }
+}
+
 export function createGQLClient(port: number, token: string): GraphQLClient {
   const client = new GraphQLClient(`http://127.0.0.1:${port}/query`, {
     headers: {
@@ -11,6 +22,7 @@ export function createGQLClient(port: number, token: string): GraphQLClient {
       opentelemetry.propagation.inject(
         opentelemetry.context.active(),
         req.headers,
+        new CustomSetter(),
       )
 
       return req
