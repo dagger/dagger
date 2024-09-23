0384e6f658742961de280569c1deedeb363beba1
Alex Suraci <alex@dagger.io>
2024-09-23 11:35:53 -0400

    ensure spans are sorted chronologically
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/dagui/ordered_set.go
+++ dagql/dagui/ordered_set.go
@@ -5,9 +5,10 @@ import (
 )
 
 type OrderedSet[K, V comparable] struct {
-	Order   []V
-	KeyFunc func(V) K
-	Map     map[K]V
+	Order    []V
+	KeyFunc  func(V) K
+	LessFunc func(V, V) bool
+	Map      map[K]V
 }
 
 func NewSet[T comparable]() *OrderedSet[T, T] {
@@ -16,6 +17,28 @@ func NewSet[T comparable]() *OrderedSet[T, T] {
 		KeyFunc: func(v T) T { return v },
 		Map:     map[T]T{},
 	}
+}
+
+func NewOrderedSet[K comparable, V comparable](keyFunc func(V) K, vs ...V) *OrderedSet[K, V] {
+	set := &OrderedSet[K, V]{
+		Order:   []V{},
+		KeyFunc: keyFunc,
+		Map:     map[K]V{},
+	}
+	for _, v := range vs {
+		set.Add(v)
+	}
+	return set
+}
+
+func NewSpanSet(spans ...*Span) *OrderedSet[SpanID, *Span] {
+	set := NewOrderedSet(func(span *Span) SpanID {
+		return span.ID
+	}, spans...)
+	set.LessFunc = func(a, b *Span) bool {
+		return a.StartTime.Before(b.StartTime)
+	}
+	return set
 }
 
 func (set *OrderedSet[K, V]) MarshalJSON() ([]byte, error) {
@@ -39,7 +62,11 @@ func (set *OrderedSet[K, V]) Add(value V) {
 		return
 	}
 	set.Map[key] = value
-	set.Order = append(set.Order, value)
+	if set.LessFunc != nil {
+		set.Order = insert(set.Order, value, set.LessFunc)
+	} else {
+		set.Order = append(set.Order, value)
+	}
 }
 
 func (set *OrderedSet[K, V]) Remove(value V) {
@@ -63,20 +90,15 @@ func (set *OrderedSet[K, V]) Clear() {
 	clear(set.Map)
 }
 
-func NewOrderedSet[K comparable, V comparable](keyFunc func(V) K, vs ...V) *OrderedSet[K, V] {
-	set := &OrderedSet[K, V]{
-		Order:   []V{},
-		KeyFunc: keyFunc,
-		Map:     map[K]V{},
+func insert[T any](slice []T, value T, less func(a, b T) bool) []T {
+	var i int
+	for i = 0; i < len(slice); i++ {
+		if !less(slice[i], value) {
+			break
+		}
 	}
-	for _, v := range vs {
-		set.Add(v)
-	}
-	return set
-}
-
-func NewSpanSet(spans ...*Span) *OrderedSet[SpanID, *Span] {
-	return NewOrderedSet(func(span *Span) SpanID {
-		return span.ID
-	}, spans...)
+	slice = append(slice, value)
+	copy(slice[i+1:], slice[i:])
+	slice[i] = value
+	return slice
 }
