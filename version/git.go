88775c2714abbaf71e79d882f4f58ce5c9d03dd5
Justin Chadwell <me@jedevc.com>
2024-09-26 13:56:33 +0100

    chore: refactor and produce consistent versions
    Signed-off-by: Justin Chadwell <me@jedevc.com>

---
+++ version/git.go
@@ -0,0 +1,229 @@
+package main
+
+import (
+	"context"
+	"dagger/version/internal/dagger"
+	"slices"
+	"strings"
+
+	"golang.org/x/mod/semver"
+)
+
+func (v *Version) Git() *Git {
+	ctr := dag.Wolfi().
+		Container(dagger.WolfiContainerOpts{Packages: []string{"git"}}).
+		WithWorkdir("/src")
+	if v.Inputs != nil {
+		ctr = ctr.WithDirectory(".", v.Inputs)
+	}
+	if v.GitDir != nil {
+		ctr = ctr.WithDirectory(".", v.GitDir)
+
+		// do various unshallowing operations (only the bare minimum is
+		// provided by the core git functions which are used by our remote git
+		// module sources)
+		remote := "https://github.com/dagger/dagger.git"
+		ctr = ctr.
+			// we need all the tags and the unshallowed repo, so we can
+			// determine which tags are in HEAD's history later
+			WithExec([]string{"git", "fetch", "--tags", "--unshallow", remote}).
+			// we need main, so we can determine the merge base for it later
+			WithExec([]string{"git", "fetch", "--no-tags", remote, "refs/heads/main:refs/heads/main"})
+	}
+	return &Git{ctr}
+}
+
+// Git is an opinionated helper for performing various commands on our dagger repo.
+type Git struct {
+	Container *dagger.Container
+}
+
+func (git *Git) valid(ctx context.Context) (bool, error) {
+	entries, err := git.Container.Directory(".").Entries(ctx)
+	if err != nil {
+		return false, err
+	}
+	return slices.Contains(entries, ".git"), nil
+}
+
+type VersionTag struct {
+	// The component this belongs to.
+	Component string
+	// The semver version for this component.
+	Version string
+	// The commit hash.
+	Commit string
+
+	// The tag creator date.
+	// Distinct from *author* date, and not to be confused with the underlying commit date.
+	Date string
+}
+
+// VersionTagLatests gets the latest version tag for a given component
+func (git *Git) VersionTagLatest(
+	ctx context.Context,
+
+	component string, // +optional
+) (*VersionTag, error) {
+	versions, err := git.VersionTags(ctx, component)
+	if err != nil {
+		return nil, err
+	}
+	if len(versions) == 0 {
+		return nil, nil
+	}
+	version := versions[len(versions)-1]
+	return &version, nil
+}
+
+// VersionTags gets all version tags for a given component - the resulting
+// versions are sorted in ascending order
+func (git *Git) VersionTags(
+	ctx context.Context,
+	component string, // +optional
+) ([]VersionTag, error) {
+	if ok, err := git.valid(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
+	component = strings.TrimSuffix(component, "/")
+	componentFilter := "v*"
+	if component != "" {
+		componentFilter = component + "/" + componentFilter
+	}
+
+	tagsRaw, err := git.Container.
+		WithExec([]string{
+			"git",
+			"tag",
+			// filter to only the desired component
+			"-l", componentFilter,
+			// filter to reachable commits from HEAD
+			"--merged=HEAD",
+			// format as "<tag> <commit> <date>"
+			"--format", "%(refname:lstrip=2) %(objectname) %(creatordate:iso-strict)",
+			// sort by ascending semver
+			"--sort", "version:refname",
+		}).
+		Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	var versionTags []VersionTag
+	for _, line := range strings.Split(tagsRaw, "\n") {
+		parts := strings.Split(line, " ")
+		if len(parts) != 3 {
+			continue
+		}
+		tag, commit, date := parts[0], parts[1], parts[2]
+		version := strings.TrimPrefix(tag, component+"/")
+		if !semver.IsValid(version) {
+			continue
+		}
+
+		versionTag := VersionTag{
+			Component: component,
+			Version:   version,
+			Date:      date,
+			Commit:    commit,
+		}
+		versionTags = append(versionTags, versionTag)
+	}
+
+	return versionTags, nil
+}
+
+type Commit struct {
+	// The commit hash.
+	Commit string
+
+	// The commit commit date.
+	// Distinct from the *author* date.
+	Date string
+}
+
+func (git *Git) Head(ctx context.Context) (*Commit, error) {
+	return git.Commit(ctx, "HEAD")
+}
+
+func (git *Git) Commit(ctx context.Context, ref string) (*Commit, error) {
+	if ok, err := git.valid(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
+	raw, err := git.Container.
+		WithExec([]string{
+			"git",
+			"show",
+			// skip the pretty output
+			"-s",
+			// format as "<commit> <date>"
+			"--format=%H %cI",
+			ref,
+		}).
+		Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+	raw = strings.TrimSpace(raw)
+
+	commit, date, _ := strings.Cut(raw, " ")
+	return &Commit{
+		Commit: commit,
+		Date:   date,
+	}, nil
+}
+
+func (git *Git) MergeBase(ctx context.Context, ref string, ref2 string) (*Commit, error) {
+	if ok, err := git.valid(ctx); err != nil {
+		return nil, err
+	} else if !ok {
+		return nil, nil
+	}
+
+	raw, err := git.Container.
+		WithExec([]string{
+			"git",
+			"merge-base",
+			ref, ref2,
+		}).
+		Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+	raw = strings.TrimSpace(raw)
+
+	return git.Commit(ctx, raw)
+}
+
+// Return whether the current git state is dirty
+func (git *Git) Dirty(ctx context.Context) (bool, error) {
+	status, err := git.status(ctx)
+	if err != nil {
+		return false, err
+	}
+	return len(status) > 0, nil
+}
+
+func (git *Git) status(ctx context.Context) (string, error) {
+	if ok, err := git.valid(ctx); err != nil {
+		return "", err
+	} else if !ok {
+		return "", nil
+	}
+
+	args := []string{"git", "status", "--porcelain", "--"}
+	for _, ignore := range ignores {
+		args = append(args, ":(exclude)"+ignore)
+	}
+	result, err := git.Container.WithExec(args).Stdout(ctx)
+	if err != nil {
+		return "", err
+	}
+	return strings.Trim(result, "\n"), nil
+}
--- version/main.go
+++ version/main.go
@@ -6,14 +6,12 @@ package main
 
 import (
 	"context"
-	"crypto/sha1"
 	"dagger/version/internal/dagger"
-	"encoding/hex"
 	"fmt"
-	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
+	"time"
 
 	"golang.org/x/mod/semver"
 )
@@ -30,7 +28,7 @@ func New(
 	inputs *dagger.Directory,
 	// +optional
 	// +defaultPath="/"
-	// +ignore=["*", "!.git"]
+	// +ignore=["*", "!.git", "!**/.gitignore"]
 	gitDir *dagger.Directory,
 	// .changes file used to extract version information
 	// +optional
@@ -39,12 +37,18 @@ func New(
 	changes *dagger.Directory,
 ) Version {
 	return Version{
-		// FIXME: upload the whole git dir is inefficient.
-		// We can stop doing it once this is shipped:  https://github.com/dagger/dagger/issues/8520
+		// NOTE: uploading the whole git dir is inefficient.
+		// we can stop doing it once dagger/dagger#8520 ships
 		GitDir:  gitDir,
 		Inputs:  inputs,
 		Changes: changes,
 	}
+}
+
+// FIXME: this is copy-pasted from top-level +ignore. Find a way to DRY this up.
+// FIXME: investigate behavior difference between dagger and git on: '**/testdata': dagger ignores, git requires extra /**
+var ignores = []string{
+	"**_test.go", "**/.git*", "**/.venv", "**/.dagger", ".*", "bin", "**/node_modules", "**/testdata/**", "**/.changes/**", "docs", "helm", "release", "version", "modules", "*.md", "LICENSE", "NOTICE", "hack",
 }
 
 type Version struct {
@@ -54,241 +58,131 @@ type Version struct {
 	Changes *dagger.Directory
 }
 
-// Return whether the current version is a development version or not
-// The definition is:
-// - If there are uncommitted changes to the inputs files, dev=true
-// - otherwise, dev=false
-func (v Version) Dev(ctx context.Context) (bool, error) {
-	diff, err := v.diff(ctx)
-	if err != nil {
-		return false, err
-	}
-	return len(strings.Trim(diff, "\n")) != 0, nil
-}
-
-// Return any uncommitted changes to input files
-func (v Version) diff(ctx context.Context) (string, error) {
-	ignores := []string{
-		// FIXME: this is copy-pasted from +ignore above. Find a way to DRY this up.
-		// FIXME: investigate behavior difference between dagger and git on: '**/testdata': dagger ignores, git requires extra /**
-		"**_test.go", "**/.git*", "**/.venv", "**/.dagger", ".*", "bin", "**/node_modules", "**/testdata/**", "**/.changes/**", "docs", "helm", "release", "version", "modules", "*.md", "LICENSE", "NOTICE", "hack",
-	}
-	args := []string{"git", "diff", "--"}
-	for _, ignore := range ignores {
-		args = append(args, ":(exclude)"+ignore)
-	}
-	return dag.Wolfi().
-		Container(dagger.WolfiContainerOpts{Packages: []string{"git"}}).
-		WithDirectory("/src", dag.Directory()).
-		WithWorkdir("/src").
-		WithDirectory(".", v.Inputs).
-		WithDirectory(".", v.GitDir).
-		WithExec(args).
-		Stdout(ctx)
-}
-
 // Generate a version string from the current context
-// - If the current commit is a semver tag, the version is that tag
-// - If no git information is available, the version is "<next-version>-<diget>"
-// - In all other cases, the version is "<next-version>-<commit>-<digest>", where:
-//   - <next-version> is inferred from .changes/
-//   - <digest> is computed from the inputs directory
 func (v Version) Version(ctx context.Context) (string, error) {
-	if v.gitDirExists(ctx) {
-		tag, err := v.VersionTag(ctx)
+	dirty, err := v.Git().Dirty(ctx)
+	if err != nil {
+		return "", err
+	}
+	head, err := v.Git().Head(ctx)
+	if err != nil {
+		return "", err
+	}
+	if dirty || head == nil {
+		// this is a dev version - git state is dirty, or we have no git state at all
+		// (v<major>.<minor>.<patch>-<timestamp>-dev-<inputdigest>)
+		next, err := v.NextReleaseVersion(ctx)
 		if err != nil {
 			return "", err
 		}
-		if tag != "" {
-			// FIXME: we don't handle dirty checkout of a semvar tag checkout
-			return tag, nil
+		digest, err := v.Inputs.Digest(ctx)
+		if err != nil {
+			return "", err
 		}
+		if _, newDigest, ok := strings.Cut(digest, ":"); ok {
+			digest = newDigest
+		}
+		// NOTE: the timestamp is empty here to prevent unnecessary rebuilds
+		return fmt.Sprintf("%s-%s-dev-%s", next, pseudoversionTimestamp(time.Time{}), digest[:12]), nil
 	}
-	next, err := v.NextVersion(ctx)
+
+	lastVersion, err := v.Git().VersionTagLatest(ctx, "")
 	if err != nil {
 		return "", err
 	}
-	digest, err := v.InputsDigest(ctx)
+	if lastVersion != nil && lastVersion.Commit == head.Commit {
+		// this is a tagged release - we got a release tag for this commit
+		// (v<major>.<minor>.<patch>)
+		return lastVersion.Version, nil
+	}
+
+	// this is an untagged release - we didn't find a release tag
+	// (v<major>.<minor>.<patch>-<timestamp>-<commit>)
+	next, err := v.NextReleaseVersion(ctx)
 	if err != nil {
 		return "", err
 	}
-	if !v.gitDirExists(ctx) {
-		return fmt.Sprintf("%s-%s", next, digest), nil
-	}
-	commit, err := v.Commit(ctx)
+	commitDate, err := time.Parse(time.RFC3339, head.Date)
 	if err != nil {
 		return "", err
 	}
-	// FIXME we don't differentiate between clean and dirty checkout:
-	// instead we always add the commit + digest
-	return fmt.Sprintf("%s-%s-%s", next, commit, digest), nil
+	return fmt.Sprintf("%s-%s-%s", next, pseudoversionTimestamp(commitDate), head.Commit[:12]), nil
+}
+
+func pseudoversionTimestamp(t time.Time) string {
+	// go time formatting is bizarre - this translates to "yymmddhhmmss"
+	return t.Format("060102150405")
 }
 
 // Return the tag to use when auto-downloading the engine image from the CLI
 func (v Version) ImageTag(ctx context.Context) (string, error) {
-	dev, err := v.Dev(ctx)
+	head, err := v.Git().Head(ctx)
 	if err != nil {
 		return "", err
 	}
-	if !dev {
-		// For non-dev release, the image tag is the version tag
-		return v.VersionTag(ctx)
-	}
-	commit, err := v.Commit(ctx)
-	if err != nil {
-		return "", err
-	}
-	if commit != "" {
-		// For dev releases with available git dir: image tag is git commit
-		return commit, nil
-	}
-	// If no commit is available, return the input digest
-	// At the moment there is no scenario where this will be used,
-	// because no build without git information will be pushed to any official registry.
-	// But in the future this could be used for local auto-download registry perhaps.
-	return v.InputsDigest(ctx)
-}
-
-func (v Version) gitRepo() *dagger.SupergitRepo {
-	return dag.Supergit().
-		Load(v.GitDir, dagger.SupergitLoadOpts{
-			Worktree: v.Inputs,
-		}).
-		// Fetch all tag references, otherwise we'll miss tags if the repo is fetched by core git function
-		WithCommand([]string{"fetch", "--no-tags", "origin", "+refs/tags/*:refs/tags/*", "--depth=1"})
-}
-
-type VersionTag struct {
-	Component string
-	Version   string
-	Commit    string
-}
-
-// Return a list of version tags pointing to the current commit
-// A version tag is of the form [PATH/]SEMVER. Examples:
-//   - "v0.1.0"
-//   - "sdk/php/v0.1.1"
-//   - "bla/v1.1.0"
-func (v Version) VersionTags(ctx context.Context) ([]VersionTag, error) {
-	if !v.gitDirExists(ctx) {
-		return nil, nil
-	}
-	commit, err := v.Commit(ctx)
-	if err != nil {
-		return nil, err
-	}
-	tagsRaw, err := v.gitRepo().
-		Command([]string{"tag", "--points-at", "HEAD"}).
-		Stdout(ctx)
-	if err != nil {
-		return nil, err
-	}
-	var tags []VersionTag
-	for _, tag := range strings.Split(strings.Trim(tagsRaw, "\n"), "\n") {
-		if semver.IsValid(tag) {
-			tags = append(tags, VersionTag{Version: tag, Commit: commit})
-			continue
-		}
-		// Split the path by "/" and get the last part
-		parts := strings.Split(tag, "/")
-		if len(parts) < 2 {
-			continue // Ensure there are at least two path parts and the version
-		}
-		version := parts[len(parts)-1]
-		// Check if the last part is a valid semver
-		if !semver.IsValid(version) {
-			continue
-		}
-		tags = append(tags, VersionTag{
-			Version:   version,
-			Component: strings.Join(parts[:len(parts)-1], "/"),
-			Commit:    commit,
-		})
-	}
-	return tags, nil
-}
-
-// Return all tags pointing at the current commit
-func (v Version) Tags(ctx context.Context) ([]string, error) {
-	if !v.gitDirExists(ctx) {
-		return nil, nil
-	}
-	commit, err := v.Commit(ctx)
-	if err != nil {
-		return nil, err
-	}
-	tagsRaw, err := v.gitRepo().
-		Command([]string{"tag", "--points-at", commit}).
-		Stdout(ctx)
-	if err != nil {
-		return nil, err
-	}
-	return strings.Split(strings.Trim(tagsRaw, "\n"), "\n"), nil
-}
-
-// Return the semver-compatible tag pointing to the current tag, or an empty string
-func (v Version) VersionTag(ctx context.Context) (string, error) {
-	tags, err := v.VersionTags(ctx)
-	if err != nil {
-		return "", err
-	}
-	if len(tags) == 0 {
+	if head == nil {
+		// no git metadata
+		// (empty)
 		return "", nil
 	}
-	for _, tag := range tags {
-		if tag.Component == "" {
-			return tag.Version, nil
+
+	dirty, err := v.Git().Dirty(ctx)
+	if err != nil {
+		return "", err
+	}
+	if dirty {
+		// this is a dev version - get the last commit from main on this branch
+		// (<commit>)
+		mergeBase, err := v.Git().MergeBase(ctx, "main", head.Commit)
+		if err != nil {
+			return "", err
 		}
+		return mergeBase.Commit, nil
 	}
-	return "", nil
-}
 
-func (v Version) gitDirExists(ctx context.Context) bool {
-	_, err := v.GitDir.Directory(".git").Entries(ctx)
-	return err == nil
-}
-
-// Return the current git commit
-func (v Version) Commit(ctx context.Context) (string, error) {
-	if !v.gitDirExists(ctx) {
-		return "", nil
-	}
-	cmd := []string{"rev-parse", "HEAD"}
-	// FIXME: contribute GitRepo.head() uptsream
-	commit, err := v.gitRepo().Command(cmd).Stdout(ctx)
+	lastVersion, err := v.Git().VersionTagLatest(ctx, "")
 	if err != nil {
 		return "", err
 	}
-	return strings.Trim(commit, "\n"), nil
-}
+	if lastVersion != nil && lastVersion.Commit == head.Commit {
+		// this is a tagged release
+		// (v<major>.<minor>.<patch>)
+		return lastVersion.Version, nil
+	}
 
-// Compute a stable digest of the input files, to differentiate dev builds on the same commit
-// FIXME: the digest doesn't seem very stable...
-func (v Version) InputsDigest(ctx context.Context) (string, error) {
-	id, err := v.Inputs.ID(ctx)
+	// this is an untagged release - get the last commit from main on this branch
+	// <commit>
+	mergeBase, err := v.Git().MergeBase(ctx, "main", head.Commit)
 	if err != nil {
 		return "", err
 	}
-	h := sha1.New() //nolint:gosec
-	h.Write([]byte(id))
-	dgst := hex.EncodeToString(h.Sum(nil))
-	return dgst, nil
+	return mergeBase.Commit, nil
 }
 
-// Determine the "next" version to be released
+// Determine the last released version.
+func (v Version) LastReleaseVersion(ctx context.Context) (string, error) {
+	tag, err := v.Git().VersionTagLatest(ctx, "")
+	if err != nil {
+		return "", err
+	}
+	if tag == nil {
+		return "", fmt.Errorf("no releases found")
+	}
+	return tag.Version, nil
+}
+
+// Determine the "next" version to be released.
+//
 // It first attempts to use the version in .changes/.next, but if this fails,
 // or that version seems to have already been released, then we automagically
 // calculate the next patch release in the current series.
-func (v Version) NextVersion(ctx context.Context) (string, error) {
-	// this is kinda meh, since it assumes changie releases match up with git
-	// tags - thankfully this is true for us (otherwise, we'd have to look at
-	// *all* the tags in the source, which would be slow)
+func (v Version) NextReleaseVersion(ctx context.Context) (string, error) {
+	// if there's a defined next version, try and use that
 	entries, err := v.Changes.Directory(".changes").Entries(ctx)
 	if err != nil {
 		return "", err
 	}
-	// if there's a defined next version, try and use that
-	var definedNextVersion string
+	var nextVersion string
 	if slices.Contains(entries, ".next") {
 		content, err := v.Changes.File(".changes/.next").Contents(ctx)
 		if err != nil {
@@ -305,188 +199,41 @@ func (v Version) NextVersion(ctx context.Context) (string, error) {
 				continue
 			}
 
-			definedNextVersion = baseVersion(line)
+			nextVersion = baseVersion(line)
 			break
 		}
 	}
-	// also try and determine what the last version was, so we can
-	// auto-determine a next version from that
-	var lastVersion string
-	for _, entry := range entries {
-		entry, _ := strings.CutSuffix(entry, filepath.Ext(entry))
-		if semver.Compare(entry, lastVersion) > 0 {
-			lastVersion = entry
-		}
-	}
-	if lastVersion == "" {
-		return "", fmt.Errorf("could not find any valid versions")
-	}
-	lastVersion = baseVersion(lastVersion)
 
-	majorMinor := semver.MajorMinor(lastVersion)
-	patchStr, _ := strings.CutPrefix(lastVersion, majorMinor+".")
-	patch, err := strconv.Atoi(patchStr)
+	// also try and determine what the last version from git was, so we can
+	// auto-determine a next version from that
+	lastVersion, err := v.Git().VersionTagLatest(ctx, "")
 	if err != nil {
 		return "", err
 	}
-	nextVersion := fmt.Sprintf("%s.%d", majorMinor, patch+1)
-
-	if semver.Compare(definedNextVersion, nextVersion) > 0 {
-		// if the defined next version is larger than the auto-generated one,
-		// override it - this'll be the case for when we plan to bump to a
-		// minor version
-		nextVersion = definedNextVersion
-	}
-	return nextVersion, nil
-}
-
-func baseVersion(version string) string {
-	version = strings.TrimSuffix(version, semver.Build(version))
-	version = strings.TrimSuffix(version, semver.Prerelease(version))
-	return version
-}
-
-/*
-
-	// If we get a commit, make sure it's valid (regardless of if we use it)
-	if (commit != "") && (!commitRegexp.MatchString(commit)) {
-		return nil, fmt.Errorf("invalid commit sha: %s", commit)
-	}
-	// If we have a semver tag, use just that
-	// Example: "v0.1.0"
-	if !semver.IsValid(tag) {
-		return &Version{Tag: tag}, nil
-	}
-	// If we have a valid commit sha, use that + next version
-	// Example: "v0.2.0-ad997972f96272f3e140e12b12e00ef4d6e9450b"
-	if commit != "" {
-		next, err := nextVersion(ctx, changes)
-		if err != nil {
-			return nil, err
+	if lastVersion != nil {
+		maybeNextVersion := bumpVersion(lastVersion.Version)
+		if semver.Compare(maybeNextVersion, nextVersion) > 0 {
+			// if the auto-bumped last version is greater than the defined
+			// version, we've probably forgotten to update `.changes/.next`
+			nextVersion = maybeNextVersion
 		}
-		return &Version{
-			Commit:    commit,
-			Next:      next,
-			Timestamp: pseudoversionTimestamp(time.Now().UTC()),
-		}, nil
-	}
-	// Fall back to input hash + next version
-	// Example: "v0.2.0-deadbeefdeadbeefdeadbeef"
-	dgst, err := dirhash(ctx, inputs)
-	if err != nil {
-		return nil, err
-	}
-	next, err := nextVersion(ctx, changes)
-	if err != nil {
-		return nil, err
-	}
-	return &Version{
-		Dev:       dgst,
-		Next:      next,
-		Timestamp: pseudoversionTimestamp(time.Time{}),
-	}, nil
-}
-
-var commitRegexp = regexp.MustCompile("^[0-9a-f]{40}$")
-
-// Complete version string
-func (info *Version) Version() string {
-	if info.Tag != "" {
-		return info.Tag
 	}
 
-	nextVersion := info.Next
 	if nextVersion == "" {
-		nextVersion = "v0.0.0"
-	}
-	timestamp := info.Timestamp
-	if timestamp == "" {
-		timestamp = pseudoversionTimestamp(time.Time{})
-	}
-
-	var rest string
-	switch {
-	case info.Commit != "":
-		rest = info.Commit[:12]
-	case info.Dev != "":
-		rest = "dev-" + info.Dev[:12]
-	default:
-		rest = "dev-" + strings.Repeat("0", 12)
-	}
-	return fmt.Sprintf("%s-%s-%s", nextVersion, timestamp, rest)
-}
-
-// nextVersion determines the "next" version to be released.
-//
-// It first attempts to use the version in .changes/.next, but if this fails,
-// or that version seems to have already been released, then we automagically
-// calculate the next patch release in the current series.
-func nextVersion(ctx context.Context, dir *dagger.Directory) (string, error) {
-	// this is kinda meh, since it assumes changie releases match up with git
-	// tags - thankfully this is true for us (otherwise, we'd have to look at
-	// *all* the tags in the source, which would be slow)
-	entries, err := dir.Directory(".changes").Entries(ctx)
-	if err != nil {
-		return "", err
-	}
-
-	// if there's a defined next version, try and use that
-	var definedNextVersion string
-	if slices.Contains(entries, ".next") {
-		content, err := dir.File(".changes/.next").Contents(ctx)
-		if err != nil {
-			return "", err
-		}
-		for _, line := range strings.Split(content, "\n") {
-			line = strings.TrimSpace(line)
-			if len(line) == 0 {
-				// empty
-				continue
-			}
-			if strings.HasPrefix(line, "#") {
-				// comment
-				continue
-			}
-
-			definedNextVersion = baseVersion(line)
-			break
-		}
-	}
-
-	// also try and determine what the last version was, so we can
-	// auto-determine a next version from that
-	var lastVersion string
-	for _, entry := range entries {
-		entry, _ := strings.CutSuffix(entry, filepath.Ext(entry))
-		if semver.Compare(entry, lastVersion) > 0 {
-			lastVersion = entry
-		}
-	}
-	if lastVersion == "" {
-		return "", fmt.Errorf("could not find any valid versions")
-	}
-	lastVersion = baseVersion(lastVersion)
-
-	majorMinor := semver.MajorMinor(lastVersion)
-	patchStr, _ := strings.CutPrefix(lastVersion, majorMinor+".")
-	patch, err := strconv.Atoi(patchStr)
-	if err != nil {
-		return "", err
-	}
-	nextVersion := fmt.Sprintf("%s.%d", majorMinor, patch+1)
-
-	if semver.Compare(definedNextVersion, nextVersion) > 0 {
-		// if the defined next version is larger than the auto-generated one,
-		// override it - this'll be the case for when we plan to bump to a
-		// minor version
-		nextVersion = definedNextVersion
+		return "", fmt.Errorf("could not determine next version")
 	}
 	return nextVersion, nil
 }
 
-func pseudoversionTimestamp(t time.Time) string {
-	// go time formatting is bizarre - this translates to "yymmddhhmmss"
-	return t.Format("060102150405")
+func bumpVersion(version string) string {
+	if !semver.IsValid(version) {
+		return version // cannot bump a non-semver version
+	}
+	version = baseVersion(version)
+	majorMinor := semver.MajorMinor(version)
+	patchStr, _ := strings.CutPrefix(version, majorMinor+".")
+	patch, _ := strconv.Atoi(patchStr)
+	return fmt.Sprintf("%s.%d", majorMinor, patch+1)
 }
 
 func baseVersion(version string) string {
@@ -494,4 +241,3 @@ func baseVersion(version string) string {
 	version = strings.TrimSuffix(version, semver.Prerelease(version))
 	return version
 }
-*/
