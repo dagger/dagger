82aadb6c27f85002dad56713dd2cf67c2abb22ad
Vikram Vaswani <vikram@dagger.io>
2024-09-27 18:13:26 +0530

    Rebased feature pages
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/features/snippets/services-2/python/main.py
+++ docs/current_docs/features/snippets/services-2/python/main.py
@@ -1,33 +1,11 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import dagger
 from dagger import dag, function, object_type
-=======
-from typing import Annotated
-
-import dagger
-from dagger import Doc, dag, function, object_type
->>>>>>> 81388975a (Added code snippets to feature pages)
-=======
-import dagger
-from dagger import dag, function, object_type
->>>>>>> 2f22413a8 (Fix linter)
 
 
 @object_type
 class MyModule:
     @function
-<<<<<<< HEAD
-<<<<<<< HEAD
     async def user_list(self, svc: dagger.Service) -> str:
-=======
-    async def user_list(
-        self, svc: dagger.Service
-    ) -> str:
->>>>>>> 81388975a (Added code snippets to feature pages)
-=======
-    async def user_list(self, svc: dagger.Service) -> str:
->>>>>>> 2f22413a8 (Fix linter)
         return await (
             dag.container()
             .from_("mariadb:10.11.2")
