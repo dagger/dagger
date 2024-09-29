6be4d8f925cf9a07766e5a3934b969c95e7a1460
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-09-29 23:33:27 +0700

    cleanup(sdk/dotnet): cleanup existing project
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- sdk/dotnet/DaggerSDKCodeGen/Models/TypeDef.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DaggerSDKCodeGen.Models;
-
-public class TypeDef
-{
-    public string? Kind { get; set; }
-    public string? Name { get; set; }
-    public TypeDef? OfType { get; set; }
-}
