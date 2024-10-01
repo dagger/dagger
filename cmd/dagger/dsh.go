e7d7bdd5d19c6360a873d17fca20cfae61e56f69
Solomon Hykes <solomon@dagger.io>
2024-10-01 09:56:31 -0700

    merge POC implementation in the dagger command
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ cmd/dagger/dsh.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/chzyer/readline"
+	"mvdan.cc/sh/v3/expand"
+	"mvdan.cc/sh/v3/interp"
+	"mvdan.cc/sh/v3/syntax"
+)
+
+func dsh(args []string) error {
+	if err := shell("> "); err != nil {
+		return err
+	}
+	return nil
+}
+
+func execDagger(ctx context.Context, module string, args []string) error {
+	if module != "" {
+		args = append([]string{"-m", module}, args...)
+	}
+	cmd := exec.CommandContext(ctx, "dagger", args...)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	return cmd.Run()
+}
+
+func dshExec(ctx context.Context, args []string) error {
+	fmt.Fprintf(os.Stderr, "# %s\n", strings.Join(args, " "))
+	if len(args) == 0 {
+		return nil
+	}
+	switch args[0] {
+	case "install":
+		return execDagger(ctx, "", args)
+	default:
+		module := args[0]
+		args = append([]string{"call"}, args[1:]...)
+		return execDagger(ctx, module, args)
+	}
+	return nil // Returning nil to indicate successful execution; adjust as needed
+}
+
+func shell(prompt string) error {
+	rl, err := readline.New(prompt)
+	if err != nil {
+		panic(err)
+	}
+	defer rl.Close()
+
+	runner, err := interp.New(
+		interp.StdIO(os.Stdin, os.Stdout, os.Stderr),
+		interp.ExecHandler(dshExec),
+		interp.Env(expand.ListEnviron("FOO=bar")),
+	)
+	if err != nil {
+		return fmt.Errorf("Error setting up interpreter: %s", err)
+	}
+	for {
+		line, err := rl.Readline()
+		if err != nil { // EOF or Ctrl-D to exit
+			break
+		}
+		parser := syntax.NewParser()
+		file, err := parser.Parse(strings.NewReader(line), "")
+		if err != nil {
+			return fmt.Errorf("Error parsing command: %s", err)
+		}
+		// Run the parsed command file
+		if err := runner.Run(context.Background(), file); err != nil {
+			fmt.Fprintf(os.Stderr, "%s\n", err)
+		}
+	}
+	return nil
+}
