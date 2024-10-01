7f669f9d04bc304fff0a31612721567c2ccda668
Helder Correia <174525+helderco@users.noreply.github.com>
2024-10-01 16:54:13 -0700

    wip: introspection
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- cmd/dagger/shell.go
+++ cmd/dagger/shell.go
@@ -12,6 +12,7 @@ import (
 	"github.com/chzyer/readline"
 	"github.com/dagger/dagger/engine/client"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 	"mvdan.cc/sh/v3/expand"
 	"mvdan.cc/sh/v3/interp"
 	"mvdan.cc/sh/v3/syntax"
@@ -27,20 +28,37 @@ var shellCmd = &cobra.Command{
 	},
 }
 
+var shellModConf *moduleDef
+
 // Interactive shell main loop
 func shell(ctx context.Context, engineClient *client.Client, args []string) error {
 	// FIXME 1: introspect all dependencies & types
 	// FIXME 2: cool interactive repl
+    dag := engineClient.Dagger()
+
+	modDef, err := initializeModule(ctx, dag, true)
+	if err != nil {
+		return fmt.Errorf("error initializing module: %s", err)
+	}
+	shellModConf = modDef
+
 	prompt := "> "
 	rl, err := readline.New(prompt)
 	if err != nil {
 		panic(err)
 	}
 	defer rl.Close()
+
+    handler := func(next interp.ExecHandlerFunc) interp.ExecHandlerFunc {
+        return func(ctx context.Context, args []string) error {
+            return shellCall(ctx, dag, modDef, args, )
+        }
+    }
+
 	runner, err := interp.New(
 		interp.StdIO(nil, os.Stdout, os.Stderr),
-		interp.ExecHandlers(shellDebug, shellBuiltin, shellCall),
-		interp.Env(expand.ListEnviron("FOO=bar")),
+		interp.ExecHandlers(shellDebug, shellBuiltin, handler),
+        interp.Env(expand.ListEnviron("FOO=bar")),
 	)
 	if err != nil {
 		return fmt.Errorf("Error setting up interpreter: %s", err)
@@ -114,43 +132,91 @@ func shellLog(ctx context.Context, msg string, args ...interface{}) {
 	fmt.Fprintf(hctx.Stderr, msg, args...)
 }
 
-func shellCall(next interp.ExecHandlerFunc) interp.ExecHandlerFunc {
-	return func(ctx context.Context, args []string) error {
-		input, err := readObject(ctx)
+func shellCall(ctx context.Context, dag *dagger.Client, modDef *moduleDef, args []string) error {
+		o, err := readObject(ctx)
 		if err != nil {
 			return err
 		}
-		var output Object
-		if input == nil {
+		if o == nil {
 			shellLog(ctx, "[%s] ENTRYPOINT!!\n", args[0])
 			// You're the entrypoint
 			// 1. Interpret args as same-module call (eg. 'build')
 			// 2. If no match: interpret args as core function call (eg. 'git')
 			// 3. If no match (to be done later): interpret args as dependency short name (eg. 'wolfi container')
 			// --> craft the call
-			output = Object{
-				Type: "FIXME-ENTRYPOINT",
-				Calls: []Call{
-					Call{
-						Function:  args[0],                      // FIXME: handle cases 2 and 3
-						Arguments: make(map[string]interface{}), // FIXME
-					},
-				},
+
+			o = &Object{
+				Type: modDef.MainObject.AsObject.Name,
 			}
-		} else if input.Type == "EOF" { // debug hack
-			shellLog(ctx, "[%s] eof\n", args[0])
-			return nil
-		} else {
-			// You're chaining
-			var call = Call{
-				Function:  args[0],
-				Arguments: make(map[string]interface{}), // FIXME
-			}
-			output = input.WithCall(call)
-			output.Type = "FIXME CHAINED"
 		}
-		return output.Write(ctx)
+
+		objDef := shellModConf.GetObject(o.Type)
+		if objDef == nil {
+			return fmt.Errorf("could not find object type %q", o.Type)
+		}
+
+		fnDef, err := GetFunction(objDef, args[0])
+		if err != nil {
+			return fmt.Errorf("%q does not have a %q function", args[0], o.Type)
+		}
+
+		o.WithCall(newCall(ctx, fnDef, args[1:]))
+		return o.Write(ctx)
 	}
+}
+
+func newCall(ctx context.Context, modFunc *modFunction, args []string) (*Call, error) {
+	flags := pflag.NewFlagSet(modFunc.Name, pflag.ContinueOnError)
+
+	var reqs []*modFunctionArg
+
+	for _, arg := range modFunc.Args {
+		if !arg.IsRequired() {
+			continue
+		}
+
+		shellModConf.LoadTypeDef(arg.TypeDef)
+
+		if err := arg.AddFlag(flags); err != nil {
+			return nil, fmt.Errorf("error addding flag: %w", err)
+		}
+
+		reqs = append(reqs, arg)
+	}
+
+	if len(reqs) > len(args) {
+		return nil, fmt.Errorf("not enough arguments: expected %d, got %d", len(reqs), len(args))
+	}
+
+	if err := flags.Parse(args); err != nil {
+		return nil, fmt.Errorf("error parsing flags: %w", err)
+	}
+
+	for i, arg := range reqs {
+		argDef := reqs[i]
+
+		flag := flags.Lookup(argDef.FlagName())
+		val := flag.Value
+
+		switch v := val.(type) {
+		case DaggerValue:
+			obj, err := v.Get(ctx, dag, shellModConf.Source, arg)
+			if err != nil {
+				return nil, fmt.Errorf("failed to get value for argument %q: %w", arg.FlagName(), err)
+			}
+			if obj == nil {
+				return nil, fmt.Errorf("no value for argument: %s", arg.FlagName())
+			}
+			val = obj
+		case pflag.SliceValue:
+			val = v.GetSlice()
+		}
+	}
+
+	return &Call{
+		Function:  "",
+		Arguments: make(map[string]interface{}),
+	}, nil
 }
 
 func shellBuiltin(next interp.ExecHandlerFunc) interp.ExecHandlerFunc {
