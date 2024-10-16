a22eb223339180def849c8a22d94140d17d3b554
Erik Sipsma <erik@sipsma.dev>
2024-10-15 23:32:16 -0700

    ci: implement Alpine module w/ go apk lib for better caching
    The previous module implementation called apko in a withExec, which
    resulted in every permutation of a given set of alpine packages creating
    a new tarball on disk (which added up to multiple GBs in our CI) and
    importing those tarballs to their own, single-layer containers.
    
    The new implementation uses the apk go bindings inside apko to directly
    download, unpack and merge packages together all natively in Dagger.
    
    The end result is that we only download+unpack packages once and use
    merge-op to efficiently combine them into different permutations of
    package installs, getting rid of the previous duplication and improving
    caching.
    
    This just updates our in-repo Alpine module for now. We should also do
    the same for the Wolfi module so we can get rid of all usages of
    wrapping apko in a withExec.
    
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- modules/alpine/go.mod
+++ modules/alpine/go.mod
@@ -1,43 +1,90 @@
 module github.com/dagger/dagger/dev/alpine
 
-go 1.22.5
+go 1.23.1
 
 require (
+	chainguard.dev/apko v0.19.2
 	github.com/99designs/gqlgen v0.17.49
 	github.com/Khan/genqlient v0.7.0
 	github.com/vektah/gqlparser/v2 v2.5.16
-	go.opentelemetry.io/otel v1.27.0
+	go.opentelemetry.io/otel v1.30.0
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.0.0-20240518090000-14441aefdf88
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.3.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.29.0
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.28.0
 	go.opentelemetry.io/otel/log v0.3.0
-	go.opentelemetry.io/otel/sdk v1.27.0
+	go.opentelemetry.io/otel/sdk v1.29.0
 	go.opentelemetry.io/otel/sdk/log v0.3.0
-	go.opentelemetry.io/otel/trace v1.27.0
+	go.opentelemetry.io/otel/trace v1.30.0
 	go.opentelemetry.io/proto/otlp v1.3.1
-	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
-	golang.org/x/mod v0.18.0
-	golang.org/x/sync v0.7.0
-	google.golang.org/grpc v1.64.0
+	golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
+	golang.org/x/mod v0.20.0
+	golang.org/x/sync v0.8.0
+	google.golang.org/grpc v1.66.2
 )
 
 require (
+	chainguard.dev/go-grpc-kit v0.17.6 // indirect
+	chainguard.dev/sdk v0.1.25 // indirect
+	cloud.google.com/go/auth v0.9.4 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.4 // indirect
+	cloud.google.com/go/compute/metadata v0.5.2 // indirect
+	filippo.io/edwards25519 v1.1.0 // indirect
+	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
-	github.com/go-logr/logr v1.4.1 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/chainguard-dev/clog v1.5.1-0.20240811185937-4c523ae4593f // indirect
+	github.com/charmbracelet/lipgloss v0.10.0 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/google/uuid v1.6.0 // indirect
-	github.com/grpc-ecosystem/grpc-gateway/v2 v2.20.0 // indirect
-	github.com/sergi/go-diff v1.3.2-0.20230802210424-5b0b94c5c0d3 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0 // indirect
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.1-0.20210315223345-82c243799c99 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.22.0 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/muesli/reflow v0.3.0 // indirect
+	github.com/muesli/termenv v0.15.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/prometheus/client_golang v1.20.3 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/common v0.57.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/psanford/memfs v0.0.0-20230130182539-4dbf7e3e865e // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/sosodev/duration v1.3.1 // indirect
-	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0 // indirect
-	go.opentelemetry.io/otel/metric v1.27.0 // indirect
-	golang.org/x/net v0.26.0 // indirect
-	golang.org/x/sys v0.21.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-	google.golang.org/genproto/googleapis/api v0.0.0-20240520151616-dc85e6b867a5 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20240515191416-fc5f0ca64291 // indirect
-	google.golang.org/protobuf v1.34.1 // indirect
+	go.lsp.dev/uri v0.3.0 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.29.0 // indirect
+	go.opentelemetry.io/otel/metric v1.30.0 // indirect
+	go.step.sm/crypto v0.52.0 // indirect
+	golang.org/x/crypto v0.27.0 // indirect
+	golang.org/x/net v0.29.0 // indirect
+	golang.org/x/oauth2 v0.23.0 // indirect
+	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/text v0.18.0 // indirect
+	golang.org/x/time v0.6.0 // indirect
+	google.golang.org/api v0.198.0 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20240827150818-7e3bb234dfed // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240903143218-8af14fe29dc1 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
 )
 
 replace go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc => go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.0.0-20240518090000-14441aefdf88
--- modules/alpine/main.go
+++ modules/alpine/main.go
@@ -3,13 +3,25 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
+	"path/filepath"
 	"runtime"
+	"strings"
 	"time"
 
+	goapk "chainguard.dev/apko/pkg/apk/apk"
 	"github.com/dagger/dagger/dev/alpine/internal/dagger"
 	"golang.org/x/mod/semver"
+	"golang.org/x/sync/errgroup"
+)
+
+const (
+	alpineRepository  = "https://dl-cdn.alpinelinux.org/alpine"
+	alpineReleasesURL = "https://alpinelinux.org/releases.json"
 )
 
 func New(
@@ -26,6 +38,10 @@ func New(
 ) (Alpine, error) {
 	if arch == "" {
 		arch = runtime.GOARCH
+	}
+	goArch := arch
+	if apkArch := goapk.ArchToAPK(arch); apkArch != arch {
+		arch = apkArch
 	}
 
 	switch {
@@ -44,6 +60,8 @@ func New(
 		Branch:   branch,
 		Arch:     arch,
 		Packages: packages,
+
+		GoArch: goArch,
 	}, nil
 }
 
@@ -55,64 +73,183 @@ type Alpine struct {
 	Branch string
 	// The APK packages to install
 	Packages []string
+
+	// the GOARCH equivalent of Arch
+	//+private
+	GoArch string
 }
 
 // Build an Alpine Linux container
-func (m *Alpine) Container() *dagger.Container {
-	return dag.
-		Container(dagger.ContainerOpts{
-			Platform: dagger.Platform("linux/" + m.Arch),
-		}).
-		Import(m.Archive())
-}
-
-// Build an Alpine Linux OCI archive
-func (m *Alpine) Archive() *dagger.File {
-	return dag.
-		Container().
-		From("cgr.dev/chainguard/apko").
-		WithMountedCache("/apkache", dagger.Connect().CacheVolume("apko")).
-		WithFile("apko.yml", m.ApkoConfig()).
-		// HACK: wrapping the apko build with this time based env ensures that
-		// the cache is invalidated once-per day. Without this, we can't ensure
-		// that we get the newest versions.
-		WithEnvVariable("DAGGER_APK_CACHE_BUSTER", fmt.Sprintf("%d", time.Now().Truncate(24*time.Hour).Unix())).
-		WithExec([]string{
-			"apko", "build",
-			"--cache-dir", "/apkache/",
-			"apko.yml",
-			"latest",
-			"img.tar",
-		}).
-		File("img.tar")
-}
-
-// Generate the APKO build configuration
-func (m Alpine) ApkoConfig() *dagger.File {
-	configJSON, _ := json.Marshal(m.apkoConfig())
-	return dag.
-		Directory().
-		WithNewFile("apko.yml", string(configJSON)).
-		File("apko.yml")
-}
-
-func (m Alpine) apkoConfig() APKOConfig {
-	var config APKOConfig
-	config.Contents.Packages = append([]string{"alpine-base"}, m.Packages...)
-	for _, path := range []string{"main", "community"} {
-		repo := "https://dl-cdn.alpinelinux.org/alpine/" + m.Branch + "/" + path
-		config.Contents.Repositories = append(config.Contents.Repositories, repo)
+func (m *Alpine) Container(ctx context.Context) (*dagger.Container, error) {
+	keys, err := m.keys()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get alpine keys: %w", err)
 	}
-	config.Archs = []string{m.Arch}
-	return config
+
+	mainRepo := goapk.NewRepositoryFromComponents(
+		alpineRepository,
+		m.Branch,
+		"main",
+		m.Arch,
+	)
+	communityRepo := goapk.NewRepositoryFromComponents(
+		alpineRepository,
+		m.Branch,
+		"community",
+		m.Arch,
+	)
+
+	indexes, err := goapk.GetRepositoryIndexes(ctx, []string{mainRepo.URI, communityRepo.URI}, keys, "", goapk.WithHTTPClient(http.DefaultClient))
+	if err != nil {
+		return nil, fmt.Errorf("failed to get alpine main indexes: %w", err)
+	}
+
+	pkgResolver := goapk.NewPkgResolver(ctx, indexes)
+
+	pkgs := append([]string{"alpine-baselayout", "alpine-release", "busybox"}, m.Packages...)
+
+	repoPkgs, conflicts, err := pkgResolver.GetPackagesWithDependencies(ctx, pkgs, nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get alpine packages: %w", err)
+	}
+	if len(conflicts) > 0 {
+		// TODO: confirm that ignoring also matches apk add behavior (seems like it does)
+		fmt.Printf("alpine package conflicts: %v\n", conflicts)
+	}
+
+	setupBase := dag.Container().From("busybox:latest")
+
+	type alpinePackage struct {
+		dir         *dagger.Directory
+		preInstall  *dagger.File
+		postInstall *dagger.File
+		trigger     *dagger.File
+		rmFileNames []string
+	}
+
+	var eg errgroup.Group
+	alpinePkgs := make([]*alpinePackage, len(repoPkgs))
+	for i, pkg := range repoPkgs {
+		eg.Go(func() error {
+			url := pkg.URL()
+			mntPath := filepath.Join("/mnt", filepath.Base(url))
+			outDir := "/out"
+
+			unpacked := setupBase.
+				WithMountedFile(mntPath, dag.HTTP(url)).
+				WithMountedDirectory(outDir, dag.Directory()).
+				WithWorkdir(outDir).
+				WithExec([]string{"tar", "-xf", mntPath})
+
+			alpinePkg := &alpinePackage{
+				dir: unpacked.Directory(outDir),
+			}
+
+			entries, err := alpinePkg.dir.Entries(ctx)
+			if err != nil {
+				return fmt.Errorf("failed to get alpine package entries: %w", err)
+			}
+
+			for _, entry := range entries {
+				if !strings.HasPrefix(entry, ".") {
+					continue
+				}
+				alpinePkg.rmFileNames = append(alpinePkg.rmFileNames, entry)
+				switch entry {
+				case ".pre-install":
+					alpinePkg.preInstall = unpacked.File(filepath.Join(outDir, entry))
+				case ".post-install":
+					alpinePkg.postInstall = unpacked.File(filepath.Join(outDir, entry))
+				case ".trigger":
+					alpinePkg.trigger = unpacked.File(filepath.Join(outDir, entry))
+				}
+			}
+
+			alpinePkgs[i] = alpinePkg
+			return nil
+		})
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, fmt.Errorf("failed to get alpine packages: %w", err)
+	}
+
+	ctr := dag.Container(dagger.ContainerOpts{Platform: dagger.Platform(m.GoArch)})
+	for _, pkg := range alpinePkgs {
+		if pkg.preInstall != nil {
+			ctr = ctr.
+				WithMountedFile("/tmp/script", pkg.preInstall).
+				WithExec([]string{"/tmp/script"}).
+				WithoutMount("/tmp/script")
+		}
+		ctr = ctr.WithDirectory("/", pkg.dir, dagger.ContainerWithDirectoryOpts{
+			Exclude: pkg.rmFileNames,
+		})
+		if pkg.postInstall != nil {
+			ctr = ctr.
+				WithMountedFile("/tmp/script", pkg.postInstall).
+				WithExec([]string{"/tmp/script"}).
+				WithoutMount("/tmp/script")
+		}
+	}
+	for _, pkg := range alpinePkgs {
+		if pkg.trigger != nil {
+			ctr = ctr.
+				WithMountedFile("/tmp/script", pkg.trigger).
+				WithExec([]string{"/tmp/script"}).
+				WithoutMount("/tmp/script")
+		}
+	}
+
+	return ctr, nil
 }
 
-type APKOConfig struct {
-	Contents APKOConfigContents `json:"contents"`
-	Archs    []string           `json:"archs"`
+func (m *Alpine) keys() (map[string][]byte, error) {
+	releases, err := m.releases()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get alpine releases: %w", err)
+	}
+	branch := releases.GetReleaseBranch(m.Branch)
+	if branch == nil {
+		return nil, fmt.Errorf("failed to get alpine branch for version %s", m.Branch)
+	}
+	urls := branch.KeysFor(m.Arch, time.Now())
+
+	keys := make(map[string][]byte)
+	for _, u := range urls {
+		res, err := http.Get(u)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get alpine key at %s: %w", u, err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("unable to get alpine key at %s: %v", u, res.Status)
+		}
+		keyBytes, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read alpine key at %s: %w", u, err)
+		}
+		keys[filepath.Base(u)] = keyBytes
+	}
+	return keys, nil
 }
 
-type APKOConfigContents struct {
-	Repositories []string `json:"repositories"`
-	Packages     []string `json:"packages"`
+func (m *Alpine) releases() (*goapk.Releases, error) {
+	res, err := http.Get(alpineReleasesURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get alpine releases: %w", err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unable to get alpine releases at %s: %v", alpineReleasesURL, res.Status)
+	}
+	b, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read alpine releases: %w", err)
+	}
+	var releases goapk.Releases
+	if err := json.Unmarshal(b, &releases); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal alpine releases: %w", err)
+	}
+
+	return &releases, nil
 }
