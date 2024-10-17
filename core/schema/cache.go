0a704ba04537d27f394fd90e4f525466424aa05a
Rajat Jindal <rajatjindal83@gmail.com>
2024-10-17 10:12:19 +0530

    feat(engine): ensure cache volumes are namespaced
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- core/schema/cache.go
+++ core/schema/cache.go
@@ -2,6 +2,7 @@ package schema
 
 import (
 	"context"
+	"errors"
 
 	"github.com/dagger/dagger/core"
 	"github.com/dagger/dagger/dagql"
@@ -19,8 +20,9 @@ func (s *cacheSchema) Name() string {
 
 func (s *cacheSchema) Install() {
 	dagql.Fields[*core.Query]{
-		dagql.Func("cacheVolume", s.cacheVolume).
+		dagql.NodeFunc("cacheVolume", s.cacheVolume).
 			Doc("Constructs a cache volume for a given cache key.").
+			Impure("cache volume should be namespaced").
 			ArgDoc("key", `A string identifier to target this cache volume (e.g., "modules-cache").`),
 	}.Install(s.srv)
 
@@ -32,13 +34,59 @@ func (s *cacheSchema) Dependencies() []SchemaResolvers {
 }
 
 type cacheArgs struct {
-	Key string
+	Key       string
+	Namespace string `default:""`
 }
 
-func (s *cacheSchema) cacheVolume(ctx context.Context, parent *core.Query, args cacheArgs) (*core.CacheVolume, error) {
-	// TODO(vito): inject some sort of scope/session/project/user derived value
-	// here instead of a static value
-	//
-	// we have to inject something so we can tell it's a valid ID
-	return core.NewCache(args.Key), nil
+func (s *cacheSchema) cacheVolume(ctx context.Context, parent dagql.Instance[*core.Query], args cacheArgs) (dagql.Instance[*core.CacheVolume], error) {
+	var inst dagql.Instance[*core.CacheVolume]
+
+	if args.Namespace != "" {
+		return dagql.NewInstanceForCurrentID(ctx, s.srv, parent, core.NewCache(args.Namespace+":"+args.Key))
+	}
+
+	m, err := parent.Self.Server.CurrentModule(ctx)
+	if err != nil && !errors.Is(err, core.ErrNoCurrentModule) {
+		return inst, err
+	}
+
+	namespaceKey := ""
+	if m != nil {
+		name, err := m.Source.Self.ModuleName(ctx)
+		if err != nil {
+			return inst, err
+		}
+
+		symbolic, err := m.Source.Self.Symbolic()
+		if err != nil {
+			return inst, err
+		}
+
+		namespaceKey = name + symbolic
+	}
+
+	// if no namespace key, just return the NewCache based on key
+	if namespaceKey == "" {
+		return dagql.NewInstanceForCurrentID(ctx, s.srv, parent, core.NewCache(args.Key))
+	}
+
+	err = s.srv.Select(ctx, s.srv.Root(), &inst, dagql.Selector{
+		Field: "cacheVolume",
+		Pure:  true,
+		Args: []dagql.NamedInput{
+			{
+				Name:  "namespace",
+				Value: dagql.NewString(namespaceKey),
+			},
+			{
+				Name:  "key",
+				Value: dagql.NewString(args.Key),
+			},
+		},
+	})
+	if err != nil {
+		return inst, err
+	}
+
+	return inst, nil
 }
