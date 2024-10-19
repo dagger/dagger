92d8e57c431eea1180130ee91884ff9752ebb550
Vikram Vaswani <vikram@dagger.io>
2024-10-19 20:27:39 +0530

    docs: Add directory filtering page
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/api/snippets/filesystem-filters/post-call/go/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"context"
+	"dagger/my-module/internal/dagger"
+
+	"dagger.io/dagger/dag"
+)
+
+type MyModule struct{}
+
+func (m *MyModule) Foo(
+	ctx context.Context,
+	source *dagger.Directory,
+) *dagger.Container {
+	builder := dag.
+		Container().
+		From("golang:latest").
+		WithDirectory("/src", source, dagger.ContainerWithDirectoryOpts{Exlcude: []string{"*.git", "internal"}}).
+		WithWorkdir("/src/hello").
+		WithExec([]string{"go", "build", "-o", "hello.bin", "."})
+	return dag.
+		Container().
+		From("alpine:latest").
+		WithDirectory("/app", builder.Directory("/src/hello"), dagger.ContainerWithDirectoryOpts{Include: []string{"hello.bin"}}).
+		WithEntrypoint([]string{"/app/hello.bin"})
+}
---
+++ docs/current_docs/api/snippets/filesystem-filters/pre-call/typescript/index.ts
@@ -0,0 +1,15 @@
+import { dag, object, argument, func, Directory, Container } from "@dagger.io/dagger"
+
+@object()
+class MyModule {
+  @func()
+  async foo(
+    @argument({ ignore: ["*", "!**/*.ts"] }) source: Directory
+  ): Promise<Container> {
+    return await dag
+      .container()
+      .from("alpine:latest")
+      .withDirectory("/src", source)
+		  .sync()
+  }
+}
