e7322d116d0811b931b11d687774fca9bc184b90
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-10-24 15:26:47 +0700

    feat(sdk/elixir): move entrypoint invoke task to dagger_sdk
    Before this changeset, the invoke task living inside a user defined
    module. This is confuses for someone who read the module source code.
    
    So, this changeset move the invoke task to the `dagger_sdk` and modify
    the runtime to use this task instead.
    
    And the runtime is no longer generate mix task anymore.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ sdk/elixir/lib/mix/tasks/dagger.entrypoint.invoke.ex
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.Dagger.Entrypoint.Invoke do
+  @shortdoc "Main entrypoint for invoking a Dagger Module."
+
+  @moduledoc """
+  Main entrypoint for invoking a Dagger Module.
+
+  NOTE: This task can run only inside Dagger Elixir runtime.
+
+  ## Arguments
+
+  - `otp_app` - The OTP application to load before invoke.
+  - `module` - A main module to invoke. (e.g. `Potato`)
+  """
+
+  use Mix.Task
+
+  def run([module]) do
+    Mix.Task.run("compile")
+    Mix.Task.reenable("dagger.entrypoint.invoke")
+
+    module =
+      module
+      |> String.split(".")
+      |> Module.concat()
+
+    Code.ensure_loaded?(module) ||
+      Mix.raise("Cannot find module #{module}")
+
+    Dagger.Mod.invoke(module)
+  end
+end
