7d13db2193781c6d9a11583196698b7478da659c
Vikram Vaswani <vikram@dagger.io>
2024-10-25 13:19:54 +0530

    docs: Add pre-call filter example to cookbook
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/cookbook/snippets/copy-filter-dir-pre/go/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"context"
+	"dagger/my-module/internal/dagger"
+)
+
+type MyModule struct{}
+
+func (m *MyModule) CopyDirectoryWithExclusions(
+	ctx context.Context,
+	// +ignore=["*", "!**/*.mdx"]
+	source *dagger.Directory,
+) (*dagger.Container, error) {
+	return dag.
+		Container().
+		From("alpine:latest").
+		WithDirectory("/src", source).
+		Sync(ctx)
+}
