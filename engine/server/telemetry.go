7126ededa1ae5bec30c9aae1874f370a8b615f7d
Alex Suraci <alex@dagger.io>
2024-11-05 17:20:47 -0500

    don't bother with transactions for client DB
    Some users are still hitting 'database is locked' errors despite SQLite
    being tuned properly as far as I can tell. We really don't need
    transactions for this, so just remove them.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- engine/server/telemetry.go
+++ engine/server/telemetry.go
@@ -332,13 +332,7 @@ func spanNames(spans []sdktrace.ReadOnlySpan) []string {
 func (ps SpansPubSub) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnlySpan) error {
 	slog.ExtraDebug("pubsub exporting spans", "client", ps.client.clientID, "count", len(spans))
 
-	tx, err := ps.client.db.Begin()
-	if err != nil {
-		return fmt.Errorf("export spans %+v: begin tx: %w", spanNames(spans), err)
-	}
-	defer tx.Rollback()
-
-	queries := clientdb.New(tx)
+	queries := clientdb.New(ps.client.db)
 
 	for _, span := range spans {
 		traceID := span.SpanContext().TraceID().String()
@@ -417,10 +411,6 @@ func (ps SpansPubSub) ExportSpans(ctx context.Context, spans []sdktrace.ReadOnly
 		}
 	}
 
-	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
-	}
-
 	return nil
 }
 
@@ -442,13 +432,7 @@ type LogsPubSub struct {
 func (ps LogsPubSub) Export(ctx context.Context, logs []sdklog.Record) error {
 	slog.ExtraDebug("pubsub exporting logs", "client", ps.client.clientID, "count", len(logs))
 
-	tx, err := ps.client.db.Begin()
-	if err != nil {
-		return fmt.Errorf("export logs %+v: begin tx: %w", logs, err)
-	}
-	defer tx.Rollback()
-
-	queries := clientdb.New(tx)
+	queries := clientdb.New(ps.client.db)
 
 	for _, rec := range logs {
 		traceID := rec.TraceID().String()
@@ -457,6 +441,7 @@ func (ps LogsPubSub) Export(ctx context.Context, logs []sdklog.Record) error {
 		severity := int64(rec.Severity())
 
 		var body []byte
+		var err error
 		if !rec.Body().Empty() {
 			body, err = proto.Marshal(telemetry.LogValueToPB(rec.Body()))
 			if err != nil {
@@ -515,10 +500,6 @@ func (ps LogsPubSub) Export(ctx context.Context, logs []sdklog.Record) error {
 		}
 	}
 
-	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
-	}
-
 	return nil
 }
 
@@ -543,13 +524,7 @@ func (ps MetricsPubSub) Export(ctx context.Context, metrics *metricdata.Resource
 		return nil
 	}
 
-	tx, err := ps.client.db.Begin()
-	if err != nil {
-		return fmt.Errorf("export metrics %+v: begin tx: %w", metrics, err)
-	}
-	defer tx.Rollback()
-
-	queries := clientdb.New(tx)
+	queries := clientdb.New(ps.client.db)
 
 	pbMetrics, err := telemetry.ResourceMetricsToPB(metrics)
 	if err != nil {
@@ -564,10 +539,6 @@ func (ps MetricsPubSub) Export(ctx context.Context, metrics *metricdata.Resource
 	_, err = queries.InsertMetric(ctx, metricsPBBytes)
 	if err != nil {
 		return fmt.Errorf("insert metrics: %w", err)
-	}
-
-	if err := tx.Commit(); err != nil {
-		return fmt.Errorf("commit tx: %w", err)
 	}
 
 	return nil
