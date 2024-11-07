715f887def8dea0f9f1a6da12a2d9605c1a69d42
Solomon Hykes <solomon@dagger.io>
2024-11-06 16:06:23 -0800

    Improve our Dagger module for Go
    Major improvements to github.com/dagger/dagger/modules/go, with all required features to build and test Dagger.
    
    New configuration settings:
    
    - Download and build caches
    - Base container
    - Lldflags
    - Injected values
    - CGO support
    - Race detector
    - DWARF generation
    - Symbol table
    
    New functions:
    
    - Build: build specified packages and return a directory
    - Binary: build the specified binary and return a file
    - Download: download dependencies to the cache
    - Test: run tests for the specified packages (supports test filtering, parallel, timeout, failfast)
    - Tests: list available tests
    
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- modules/go/go.mod
+++ modules/go/go.mod
@@ -1,11 +1,14 @@
 module github.com/dagger/dagger/modules/go
 
-go 1.22
+go 1.22.5
+
+toolchain go1.23.2
 
 require (
-	github.com/99designs/gqlgen v0.17.49
+	github.com/99designs/gqlgen v0.17.55
 	github.com/Khan/genqlient v0.7.0
-	github.com/vektah/gqlparser/v2 v2.5.16
+	github.com/containerd/platforms v0.2.1
+	github.com/vektah/gqlparser/v2 v2.5.17
 	go.opentelemetry.io/otel v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.27.0
@@ -14,6 +17,13 @@ require (
 	golang.org/x/exp v0.0.0-20231110203233-9a3e6036ecaa
 	golang.org/x/sync v0.8.0
 	google.golang.org/grpc v1.65.0
+)
+
+require (
+	github.com/containerd/log v0.1.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.1.0-rc5 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 )
 
 require (
@@ -26,13 +36,16 @@ require (
 	github.com/sosodev/duration v1.3.1 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploggrpc v0.0.0-20240518090000-14441aefdf88
 	go.opentelemetry.io/otel/exporters/otlp/otlplog/otlploghttp v0.3.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetricgrpc v1.27.0
+	go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp v1.27.0
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.27.0 // indirect
 	go.opentelemetry.io/otel/log v0.3.0
-	go.opentelemetry.io/otel/metric v1.27.0 // indirect
+	go.opentelemetry.io/otel/metric v1.27.0
 	go.opentelemetry.io/otel/sdk/log v0.3.0
+	go.opentelemetry.io/otel/sdk/metric v1.27.0
 	go.opentelemetry.io/proto/otlp v1.3.1
 	golang.org/x/net v0.29.0 // indirect
-	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/sys v0.26.0 // indirect
 	golang.org/x/text v0.18.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20240814211410-ddb44dafa142 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240814211410-ddb44dafa142 // indirect
--- modules/go/main.go
+++ modules/go/main.go
@@ -2,12 +2,20 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"path"
+	"strings"
+	"time"
 
 	"go.opentelemetry.io/otel/codes"
 	"golang.org/x/sync/errgroup"
 
+	"github.com/containerd/platforms"
 	"github.com/dagger/dagger/modules/go/internal/dagger"
+)
+
+const (
+	defaultPlatform = dagger.Platform("")
 )
 
 func New(
@@ -17,13 +25,82 @@ func New(
 	// +optional
 	// +default="1.23.2"
 	version string,
-) *Go {
+	// Use a custom module cache
+	// +optional
+	moduleCache *dagger.CacheVolume,
+
+	// Use a custom build cache
+	// +optional
+	buildCache *dagger.CacheVolume,
+
+	// Use a custom base container.
+	// The container must have Go installed.
+	// +optional
+	base *dagger.Container,
+
+	// Pass arguments to 'go build -ldflags''
+	// +optional
+	ldflags []string,
+
+	// Add string value definition of the form importpath.name=value
+	// Example: "github.com/my/module.Foo=bar"
+	// +optional
+	values []string,
+
+	// Enable CGO
+	// +optional
+	cgo bool,
+
+	// Enable race detector. Implies cgo=true
+	// +optional
+	race bool,
+) Go {
 	if source == nil {
 		source = dag.Directory()
 	}
-	return &Go{
-		Version: version,
-		Source:  source,
+	if moduleCache == nil {
+		// Cache volumes should be namespaced by module, but they aren't (yet).
+		// For now, we namespace them explicitly here.
+		moduleCache = dag.CacheVolume("github.com/dagger/dagger/modules/go:modules")
+	}
+	if buildCache == nil {
+		// Cache volumes should be namespaced by module, but they aren't (yet).
+		// For now, we namespace them explicitly here.
+		buildCache = dag.CacheVolume("github.com/dagger/dagger/modules/go:build")
+	}
+	if base == nil {
+		base = dag.
+			Wolfi().
+			Container(dagger.WolfiContainerOpts{Packages: []string{
+				"go~" + version,
+				// gcc is needed to run go test -race https://github.com/golang/go/issues/9918 (???)
+				"build-base",
+				// adding the git CLI to inject vcs info into the go binaries
+				"git",
+				// Install protoc for protobug support by default
+				// The specific version is dictated by Dagger's own requirement
+				// FIXME: make this optional with overlay support
+				"protoc~3.21.12",
+			}}).
+			WithEnvVariable("GOLANG_VERSION", version).
+			WithEnvVariable("GOPATH", "/go").
+			WithEnvVariable("PATH", "${GOPATH}/bin:${PATH}", dagger.ContainerWithEnvVariableOpts{Expand: true}).
+			WithDirectory("/usr/local/bin", dag.Directory()).
+			// Configure caches
+			WithMountedCache("/go/pkg/mod", moduleCache).
+			WithMountedCache("/root/.cache/go-build", buildCache).
+			WithWorkdir("/app")
+	}
+	return Go{
+		Version:     version,
+		Source:      source,
+		ModuleCache: moduleCache,
+		BuildCache:  buildCache,
+		Base:        base,
+		Ldflags:     ldflags,
+		Values:      values,
+		Cgo:         cgo,
+		Race:        race,
 	}
 }
 
@@ -34,57 +111,288 @@ type Go struct {
 
 	// Project source directory
 	Source *dagger.Directory
+
+	// Go module cache
+	ModuleCache *dagger.CacheVolume
+
+	// Go build cache
+	BuildCache *dagger.CacheVolume
+
+	// Base container from which to run all operations
+	Base *dagger.Container
+
+	// Pass arguments to 'go build -ldflags''
+	Ldflags []string
+
+	// Add string value definition of the form importpath.name=value
+	Values []string
+
+	// Enable CGO
+	Cgo bool
+
+	// Enable race detector
+	Race bool
 }
 
-// Build a base container with Go installed and configured
-func (p *Go) Base(
-	// Any extra apk packages that should be included in the base image
-	// +optional
-	extraPackages []string,
-) *dagger.Container {
-	pkgs := []string{
-		"go~" + p.Version,
-		// gcc is needed to run go test -race https://github.com/golang/go/issues/9918 (???)
-		"build-base",
-		// adding the git CLI to inject vcs info into the go binaries
-		"git",
-	}
-	pkgs = append(pkgs, extraPackages...)
-	return dag.
-		Wolfi().
-		Container(dagger.WolfiContainerOpts{Packages: pkgs}).
-		WithEnvVariable("GOLANG_VERSION", p.Version).
-		WithEnvVariable("GOPATH", "/go").
-		WithEnvVariable("PATH", "${GOPATH}/bin:${PATH}", dagger.ContainerWithEnvVariableOpts{Expand: true}).
-		WithDirectory("/usr/local/bin", dag.Directory()).
-		WithMountedCache("/go/pkg/mod", p.goModCache()).
-		WithMountedCache("/root/.cache/go-build", p.goBuildCache())
-}
-
-// Prepare a build environment for the given Go source code
-//   - Build a base container with Go tooling installed and configured
-//   - Mount the source code
-//   - Download dependencies
-func (p *Go) Env(
-	// Any extra apk packages that should be included in the base image
-	// +optional
-	extraPackages []string,
-) *dagger.Container {
-	return p.
-		Base(extraPackages).
-		WithEnvVariable("CGO_ENABLED", "0").
-		WithWorkdir("/app").
+// Download dependencies into the module cache
+func (p Go) Download(ctx context.Context) (Go, error) {
+	_, err := p.Base.
 		// run `go mod download` with only go.mod files (re-run only if mod files have changed)
-		WithDirectory("/app", p.Source, dagger.ContainerWithDirectoryOpts{
+		WithDirectory("", p.Source, dagger.ContainerWithDirectoryOpts{
 			Include: []string{"**/go.mod", "**/go.sum"},
 		}).
+		WithEnvVariable("CACHE_BUSTER", time.Now().Format("20060102-150405.000")).
 		WithExec([]string{"go", "mod", "download"}).
-		// run `go build` with all source
-		WithMountedDirectory("/app", p.Source)
+		Sync(ctx)
+	if err != nil {
+		return p, err
+	}
+	return p, nil
+}
+
+// Prepare a build environment for the given Go source code:
+//   - Build a base container with Go tooling installed and configured
+//   - Apply configuration
+//   - Mount the source code
+func (p Go) Env(
+	// +optional
+	platform dagger.Platform,
+) *dagger.Container {
+	return p.Base.
+		// Configure CGO
+		WithEnvVariable("CGO_ENABLED", func() string {
+			if p.Cgo {
+				return "1"
+			}
+			return "0"
+		}()).
+		// Configure platform
+		With(func(c *dagger.Container) *dagger.Container {
+			if platform == "" {
+				return c
+			}
+			spec := platforms.Normalize(platforms.MustParse(string(platform)))
+			c = c.
+				WithEnvVariable("GOOS", spec.OS).
+				WithEnvVariable("GOARCH", spec.Architecture)
+			switch spec.Architecture {
+			case "arm", "arm64":
+				switch spec.Variant {
+				case "", "v8":
+				default:
+					c = c.WithEnvVariable("GOARM", strings.TrimPrefix(spec.Variant, "v"))
+				}
+			}
+			return c
+		}).
+		WithMountedDirectory("", p.Source)
+}
+
+// List tests
+func (p Go) Tests(
+	ctx context.Context,
+	// Packages to list tests from (default all packages)
+	// +optional
+	// +default=["./..."]
+	pkgs []string,
+) (string, error) {
+	script := "go test -list=. " + strings.Join(pkgs, " ") + " | grep ^Test | sort"
+	return p.
+		Env(defaultPlatform).
+		WithExec([]string{"sh", "-c", script}).
+		Stdout(ctx)
+}
+
+// Build the given main packages, and return the build directory
+func (p Go) Build(
+	ctx context.Context,
+	// Which targets to build (default all main packages)
+	// +optional
+	// +default=["./..."]
+	pkgs []string,
+	// Disable symbol table
+	// +optional
+	noSymbols bool,
+	// Disable DWARF generation
+	// +optional
+	noDwarf bool,
+	// Target build platform
+	// +optional
+	platform dagger.Platform,
+	// Output directory
+	// +optional
+	// +default="./bin/"
+	output string,
+) (*dagger.Directory, error) {
+	if p.Race {
+		p.Cgo = true
+	}
+	mainPkgs, err := p.ListPackages(ctx, pkgs, true)
+	if err != nil {
+		return nil, err
+	}
+	ldflags := p.Ldflags
+	if noSymbols {
+		ldflags = append(ldflags, "-s")
+	}
+	if noDwarf {
+		ldflags = append(ldflags, "-w")
+	}
+	if _, err := p.Download(ctx); err != nil {
+		return nil, err
+	}
+	env := p.Env(platform)
+	cmd := []string{"go", "build", "-o", output}
+	for _, pkg := range mainPkgs {
+		env = env.WithExec(goCommand(cmd, []string{pkg}, ldflags, p.Values, p.Race))
+	}
+	return dag.Directory().WithDirectory(output, env.Directory(output)), nil
+}
+
+// Build a single main package, and return the compiled binary
+func (p Go) Binary(
+	ctx context.Context,
+	// Which package to build
+	pkg string,
+	// Disable symbol table
+	// +optional
+	noSymbols bool,
+	// Disable DWARF generation
+	// +optional
+	noDwarf bool,
+	// Target build platform
+	// +optional
+	platform dagger.Platform,
+) (*dagger.File, error) {
+	dir, err := p.Build(
+		ctx,
+		[]string{pkg},
+		noSymbols,
+		noDwarf,
+		platform,
+		"./bin/",
+	)
+	if err != nil {
+		return nil, err
+	}
+	// The binary might be called dagger or dagger.exe
+	files, err := dir.Glob(ctx, "bin/"+path.Base(pkg)+"*")
+	if err != nil {
+		return nil, err
+	}
+	if len(files) == 0 {
+		return nil, fmt.Errorf("no matching binary in %q", files)
+	}
+	return dir.File(files[0]), nil
+}
+
+// Run tests for the given packages
+func (p Go) Test(
+	ctx context.Context,
+	// Only run these tests
+	// +optional
+	run string,
+	// Skip these tests
+	// +optional
+	skip string,
+	// Abort test run on first failure
+	// +optional
+	failfast bool,
+	// How many tests to run in parallel - defaults to the number of CPUs
+	// +optional
+	// +default=0
+	parallel int,
+	// How long before timing out the test run
+	// +optional
+	// +default="30m"
+	timeout string,
+	// +optional
+	// +default=1
+	count int,
+	// Which packages to test
+	// +optional
+	// +default=["./..."]
+	pkgs []string,
+) error {
+	if p.Race {
+		p.Cgo = true
+	}
+	cmd := []string{"go", "test", "-v"}
+	if parallel != 0 {
+		cmd = append(cmd, fmt.Sprintf("-parallel=%d", parallel))
+	}
+	cmd = append(cmd, fmt.Sprintf("-timeout=%s", timeout))
+	cmd = append(cmd, fmt.Sprintf("-count=%d", count))
+	if run != "" {
+		cmd = append(cmd, "-run", run)
+	}
+	if failfast {
+		cmd = append(cmd, "-failfast")
+	}
+	if skip != "" {
+		cmd = append(cmd, "-skip", skip)
+	}
+	if _, err := p.Download(ctx); err != nil {
+		return err
+	}
+	_, err := p.
+		Env(defaultPlatform).
+		WithExec(goCommand(cmd, pkgs, p.Ldflags, p.Values, p.Race)).
+		Sync(ctx)
+	return err
+}
+
+// List packages matching the specified criteria
+func (p Go) ListPackages(
+	ctx context.Context,
+	// Filter by name or pattern. Example './foo/...'
+	// +optional
+	// +default=["./..."]
+	pkgs []string,
+	// Only list main packages
+	// +optional
+	onlyMain bool,
+) ([]string, error) {
+	args := []string{"go", "list"}
+	if onlyMain {
+		args = append(args, "-f", `{{if eq .Name "main"}}{{.Dir}}{{end}}`)
+	} else {
+		args = append(args, "-f", `{{.Dir}}`)
+	}
+	args = append(args, pkgs...)
+	out, err := p.Env(defaultPlatform).WithExec(args).Stdout(ctx)
+	if err != nil {
+		return nil, err
+	}
+	result := strings.Split(strings.Trim(out, "\n"), "\n")
+	for i := range result {
+		result[i] = strings.Replace(result[i], "/app/", "./", 1)
+	}
+	return result, nil
+}
+
+func goCommand(
+	cmd []string,
+	pkgs []string,
+	ldflags []string,
+	values []string,
+	race bool,
+) []string {
+	for _, val := range values {
+		ldflags = append(ldflags, "-X '"+val+"'")
+	}
+	if len(ldflags) > 0 {
+		cmd = append(cmd, "-ldflags", strings.Join(ldflags, " "))
+	}
+	if race {
+		cmd = append(cmd, "-race")
+	}
+	cmd = append(cmd, pkgs...)
+	return cmd
 }
 
 // Lint the project
-func (p *Go) Lint(
+func (p Go) Lint(
 	ctx context.Context,
 	packages []string, // +optional
 ) error {
@@ -97,8 +405,8 @@ func (p *Go) Lint(
 				Golangci().
 				Lint(p.Source, dagger.GolangciLintOpts{
 					Path:         pkg,
-					GoModCache:   p.goModCache(),
-					GoBuildCache: p.goBuildCache(),
+					GoModCache:   p.ModuleCache,
+					GoBuildCache: p.BuildCache,
 				}).
 				Assert(ctx)
 		})
@@ -106,7 +414,7 @@ func (p *Go) Lint(
 			ctx, span := Tracer().Start(ctx, "tidy "+path.Clean(pkg))
 			defer span.End()
 			beforeTidy := p.Source.Directory(pkg)
-			afterTidy := p.Env(nil).WithWorkdir(pkg).WithExec([]string{"go", "mod", "tidy"}).Directory(".")
+			afterTidy := p.Env(defaultPlatform).WithWorkdir(pkg).WithExec([]string{"go", "mod", "tidy"}).Directory(".")
 			err := dag.Dirdiff().AssertEqual(ctx, beforeTidy, afterTidy, []string{"go.mod", "go.sum"})
 			if err != nil {
 				span.SetStatus(codes.Error, err.Error())
@@ -115,12 +423,4 @@ func (p *Go) Lint(
 		})
 	}
 	return eg.Wait()
-}
-
-func (p *Go) goModCache() *dagger.CacheVolume {
-	return dag.CacheVolume("go-mod")
-}
-
-func (p *Go) goBuildCache() *dagger.CacheVolume {
-	return dag.CacheVolume("go-build")
 }
