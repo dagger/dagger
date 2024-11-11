04481533b71eea7b413c4bb7db6d46719eb48675
Vikram Vaswani <vikram@dagger.io>
2024-11-12 01:47:14 +0530

    Fixed linter
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/cookbook/snippets/copy-dir-runtime/typescript/index.ts
+++ docs/current_docs/cookbook/snippets/copy-dir-runtime/typescript/index.ts
@@ -9,6 +9,6 @@ class MyModule {
     await source.export("foo.txt")
     // your custom logic here
     // for example, read and print the file in the Dagger Engine container
-    console.log(fs.readFileSync("foo.txt", "utf8"));
+    console.log(fs.readFileSync("foo.txt", "utf8"))
   }
 }
