ca3c47e61657a7b559a8222ab0d37f2779b37302
Justin Chadwell <me@jedevc.com>
2024-11-20 14:33:30 +0000

    tests: add provisioning test suite for docker driver
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- core/integration/provision_test.go
+++ core/integration/provision_test.go
@@ -7,12 +7,99 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+	"testing"
 
 	"dagger.io/dagger"
+	"github.com/dagger/dagger/engine"
 	"github.com/dagger/dagger/testctx"
 	"github.com/moby/buildkit/identity"
 	"github.com/stretchr/testify/require"
+	"github.com/tidwall/gjson"
+	"golang.org/x/mod/semver"
 )
+
+type ProvisionSuite struct{}
+
+func TestProvision(t *testing.T) {
+	testctx.Run(testCtx, t, ProvisionSuite{}, Middleware()...)
+}
+
+func (ProvisionSuite) TestDockerDriver(ctx context.Context, t *testctx.T) {
+	t.Run("default image", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+		dockerd := dockerService(t, c, "", nil)
+		dockerc := dockerClient(ctx, t, c, dockerd, "", nil)
+		dockerc = dockerc.WithMountedFile("/bin/dagger", daggerCliFile(t, c))
+
+		out, err := dockerc.
+			WithExec([]string{"dagger", "query"}, dagger.ContainerWithExecOpts{Stdin: "{version}"}).Stdout(ctx)
+		require.NoError(t, err)
+
+		version := gjson.Get(out, "version").String()
+		require.True(t, semver.IsValid(version))
+	})
+
+	t.Run("specified image", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+		dockerd := dockerService(t, c, "", nil)
+		dockerc := dockerClient(ctx, t, c, dockerd, "", nil)
+		dockerc = dockerc.WithMountedFile("/bin/dagger", daggerCliFile(t, c))
+
+		version := "v0.14.0"
+		out, err := dockerc.
+			WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", "docker-image://registry.dagger.io/engine:"+version).
+			WithExec([]string{"dagger", "query"}, dagger.ContainerWithExecOpts{Stdin: "{version}"}).Stdout(ctx)
+		require.NoError(t, err)
+		require.JSONEq(t, `{"version":"`+version+`"}`, out)
+	})
+
+	t.Run("current image", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+		dockerd := dockerService(t, c, "", nil)
+		dockerc := dockerClient(ctx, t, c, dockerd, "", nil)
+		dockerc = dockerc.WithMountedFile("/bin/dagger", daggerCliFile(t, c))
+		dockerc, err := dockerLoadEngine(ctx, c, dockerc, "registry.dagger.io/engine:dev")
+		require.NoError(t, err)
+
+		out, err := dockerc.
+			WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", "docker-image://registry.dagger.io/engine:dev").
+			WithExec([]string{"dagger", "query"}, dagger.ContainerWithExecOpts{Stdin: "{version}"}).Stdout(ctx)
+		require.NoError(t, err)
+		require.JSONEq(t, `{"version":"`+engine.Version+`"}`, out)
+	})
+}
+
+func (ProvisionSuite) TestDockerDriverConfig(ctx context.Context, t *testctx.T) {
+	c := connect(ctx, t)
+
+	configContents := `{"gc":{"reservedSpace": 1000, "maxUsedSpace": 2000, "minFreeSpace": 3000}}`
+	middleware := func(ctr *dagger.Container) *dagger.Container {
+		// this mounts the file into both the client+server containers
+		return ctr.WithNewFile("/root/.config/dagger/engine.json", configContents)
+	}
+
+	dockerd := dockerService(t, c, "", middleware)
+	dockerc := dockerClient(ctx, t, c, dockerd, "", middleware)
+	dockerc, err := dockerLoadEngine(ctx, c, dockerc, "registry.dagger.io/engine:dev")
+	require.NoError(t, err)
+	dockerc = dockerc.
+		WithMountedFile("/bin/dagger", daggerCliFile(t, c)).
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", "docker-image://registry.dagger.io/engine:dev")
+
+	// check that the config was used by the engine
+	out, err := dockerc.WithExec([]string{"dagger", "query"}, dagger.ContainerWithExecOpts{Stdin: "{engine{localCache{reservedSpace,maxUsedSpace,minFreeSpace}}}"}).Stdout(ctx)
+	require.NoError(t, err)
+	require.JSONEq(t, `{"engine": {"localCache": {"reservedSpace": 1000, "maxUsedSpace": 2000, "minFreeSpace": 3000}}}`, out)
+
+	// also, just for good measure, check that the file was propagated to the right place
+	ctrid, err := dockerc.WithExec([]string{"docker", "ps", "-n1", "--format={{.ID}}"}).Stdout(ctx)
+	require.NoError(t, err)
+	ctrid = strings.TrimSpace(ctrid)
+	require.NotEmpty(t, ctrid)
+	result, err := dockerc.WithExec([]string{"docker", "exec", ctrid, "cat", "/etc/dagger/engine.json"}).Stdout(ctx)
+	require.NoError(t, err)
+	require.JSONEq(t, configContents, result)
+}
 
 func dockerService(t *testctx.T, dag *dagger.Client, dockerVersion string, f func(*dagger.Container) *dagger.Container) *dagger.Service {
 	tag := "dind"
