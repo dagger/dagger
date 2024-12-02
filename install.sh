1259e53a8fbef582fb6aae6a496dd67a5572ea22
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-12-02 17:54:05 +0700

    fix(install.sh): remove duplicate functions (#9078)
    Remove duplicate functions, `os_uname` and `os_arch`.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- install.sh
+++ install.sh
@@ -255,31 +255,6 @@ Install specified dev build <commit sha>:
 EOF
 }
 
-uname_os() {
-  os=$(uname -s | tr '[:upper:]' '[:lower:]')
-  case "$os" in
-    cygwin_nt*) os="windows" ;;
-    mingw*) os="windows" ;;
-    msys_nt*) os="windows" ;;
-  esac
-  echo "$os"
-}
-
-uname_arch() {
-  arch=$(uname -m)
-  case $arch in
-    x86_64) arch="amd64" ;;
-    x86) arch="386" ;;
-    i686) arch="386" ;;
-    i386) arch="386" ;;
-    aarch64) arch="arm64" ;;
-    armv5*) arch="armv5" ;;
-    armv6*) arch="armv6" ;;
-    armv7*) arch="armv7" ;;
-  esac
-  echo "$arch"
-}
-
 latest_version() {
   curl -sfL "${base}/${name}/latest_version"
 }
