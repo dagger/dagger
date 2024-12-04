a36ff6446de0924da2b5046c6e628261ff971776
Tom Chauveau <tom@epitech.eu>
2024-12-04 19:38:16 +0100

    feat: big improvement on the provisionning.
    - Refactor the connection logic to work with callback to handle the
    closing in a more elegent way.
    It also unify the connection system to simplify even more provisioning.
    - Move connection & query tree inside Context.
    - Move compute query from the `api` directory to `common`
    - Handle query execution and query tree build logic inside context to
    simplify codegen (2K line saved)
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- sdk/typescript/src/common/context/context.ts
+++ sdk/typescript/src/common/context/context.ts
@@ -1,75 +1,30 @@
 import { GraphQLClient } from "graphql-request"
 
-import { ConnectOpts } from "../../connectOpts.js"
-import { EngineConn, loadProvioningLibrary } from "../../provisioning/index.js"
+import { computeQuery, QueryTree } from "../graphql/compute_query.js"
+import { globalConnection } from "../graphql/connection.js"
 
-interface ContextConfig {
-  client?: GraphQLClient
-  engineConn?: EngineConn
-}
-
-/**
- * Context abstracts the connection to the engine.
- *
- * It's required to implement the default global SDK.
- * Its purpose is to store and returns the connection to the graphQL API, if
- * no connection is set, it can create its own.
- *
- * This is also useful for lazy evaluation with the default global client,
- * this one should only run the engine if it actually executes something.
- */
 export class Context {
-  private _client?: GraphQLClient
-  private _engineConn?: EngineConn
+  constructor(
+    private _queryTree: QueryTree[] = [],
+    private _connection = globalConnection,
+  ) {}
 
-  constructor(config?: ContextConfig) {
-    this._client = config?.client
-    this._engineConn = config?.engineConn
+  getGQLClient(): GraphQLClient {
+    return this._connection.getGQLClient()
   }
 
-  /**
-   * Returns a GraphQL client connected to the engine.
-   *
-   * If no client is set, it will create one.
-   */
-  public async connection(cfg: ConnectOpts = {}): Promise<GraphQLClient> {
-    if (!this._client) {
-      const engineConn = await loadProvioningLibrary()
-
-      const client = await engineConn.Connect(cfg)
-
-      this._client = client
-      this._engineConn = engineConn
-    }
-
-    return this._client
+  getConnection() {
+    return this._connection
   }
 
-  public getGQLClient(): GraphQLClient {
-    if (!this._client) {
-      throw new Error(
-        "graphQL connection not established yet, please use it inside a connect or connection function.",
-      )
-    }
-
-    return this._client
+  select(operation: string, args?: Record<string, unknown>): Context {
+    return new Context(
+      [...this._queryTree, { operation, args }],
+      this._connection,
+    )
   }
 
-  /**
-   * Close the connection and the engine if this one was started by the node
-   * SDK.
-   */
-  public close(): void {
-    if (this._engineConn) {
-      this._engineConn.Close()
-    }
-
-    // Reset client, so it can restart a new connection if necessary
-    this._client = undefined
+  execute<T>(): Promise<T> {
+    return computeQuery(this._queryTree, this._connection.getGQLClient())
   }
 }
-
-/**
- * Expose a default context for the global client
- */
-export const defaultContext = new Context()
