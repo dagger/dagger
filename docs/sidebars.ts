017122703ab61111de8c3976e73256046c7ccd48
Justin Chadwell <me@jedevc.com>
2024-12-04 15:48:21 +0000

    docs: rework custom configuration docs with `engine.json` (#9022)
    * docs: rework custom configuration docs
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * chore: apply suggestions from code review
    
    Co-authored-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * Copy edit
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Add Engine API page
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * Fix typo
    
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    
    * docs: update faq configuration to new engine config
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * update version for new engine.json file
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    ---------
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>
    Co-authored-by: vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
    Co-authored-by: Vikram Vaswani <vikram@dagger.io>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -98,6 +98,7 @@ module.exports = {
             "api/interfaces",
             "api/custom-types",
             "api/state",
+            "api/engine",
           ]
         },
         {
@@ -113,6 +114,11 @@ module.exports = {
               "type": "link",
               "label": "Module Configuration File Reference",
               "href": "https://docs.dagger.io/reference/dagger.schema.json"
+            },
+            {
+              "type": "link",
+              "label": "Engine Configuration File Reference",
+              "href": "https://docs.dagger.io/reference/engine.schema.json"
             }
           ]
         },
@@ -197,8 +203,8 @@ module.exports = {
       "collapsible": true,
       "collapsed": true,
       "items": [
+        "configuration/engine",
         "configuration/custom-runner",
-        "configuration/custom-registry",
         "configuration/custom-ca",
         "configuration/proxy",
         "configuration/cloud",
