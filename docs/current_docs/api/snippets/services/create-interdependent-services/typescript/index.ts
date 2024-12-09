8a1d87b642ffe021bd1f009391f88ec2fefaa56f
Vikram Vaswani <vikram@dagger.io>
2024-12-09 10:15:29 +0530

    Fixed linter errors
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/api/snippets/services/create-interdependent-services/typescript/index.ts
+++ docs/current_docs/api/snippets/services/create-interdependent-services/typescript/index.ts
@@ -9,11 +9,13 @@ class MyModule {
       .container()
       .from("nginx")
       .withExposedPort(80)
-      .asService({args: [
-        "sh",
-        "-c",
-        `nginx & while true; do curl svcb:80 && sleep 1; done`,
-      ]})
+      .asService({
+        args: [
+          "sh",
+          "-c",
+          `nginx & while true; do curl svcb:80 && sleep 1; done`,
+        ],
+      })
       .withHostname("svca")
 
     await svcA.start()
@@ -22,11 +24,13 @@ class MyModule {
       .container()
       .from("nginx")
       .withExposedPort(80)
-      .asService({args: [
-        "sh",
-        "-c",
-        `nginx & while true; do curl svca:80 && sleep 1; done`,
-      ]})
+      .asService({
+        args: [
+          "sh",
+          "-c",
+          `nginx & while true; do curl svca:80 && sleep 1; done`,
+        ],
+      })
       .withHostname("svcb")
 
     await svcB.start()
--- docs/current_docs/api/snippets/services/persist-service-state/typescript/index.ts
+++ docs/current_docs/api/snippets/services/persist-service-state/typescript/index.ts
@@ -13,7 +13,7 @@ class MyModule {
       .withExposedPort(6379)
       .withMountedCache("/data", dag.cacheVolume("my-redis"))
       .withWorkdir("/data")
-      .asService({useEntrypoint: true})
+      .asService({ useEntrypoint: true })
 
     const redisCLI = dag
       .container()
