26ec17509f3dcb7b87a08a48d1516c18a0fca638
Gerhard Lazu <gerhard@users.noreply.github.com>
2024-12-10 11:37:50 +0000

    ci: Add bump-dagger-version function to daggerverse (#8947)
    Upgrades Dagger version for Daggerverse in:
    - GitHub workflows
    - Daggerverse modules
    - Daggerverse app
    - Infra config
    
    It also deploys a Daggerverse preview environment with a development
    version of Dagger (this implies the version that we are upgrading to).
    
    This now runs automatically part of the publish workflow. When no
    `--from` is specified, it looks inside the `.changes` directory and
    picks the one but last version (n-1).
    
    Updated the publish workflow runs-on to only use our own runners. This
    workflow is never meant to run in forks.
    
    Signed-off-by: Gerhard Lazu <gerhard@dagger.io>

--- modules/daggerverse/main.go
+++ modules/daggerverse/main.go
@@ -4,71 +4,86 @@ import (
 	"context"
 	"dagger/daggerverse/internal/dagger"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/google/go-github/v66/github"
 )
 
-type Daggerverse struct{}
+type Daggerverse struct {
+	// +private
+	Gh *dagger.Gh
+	// +private
+	GitHubUser string
+	// +private
+	GitHubUsername string
+	// +private
+	GitHubUserEmail string
+	// +private
+	Repo string
+}
 
-// Deploy preview environment running Dagger main: dagger call deploy-preview-with-dagger-main --github-token=env:GITHUB_PAT
-func (h *Daggerverse) DeployPreviewWithDaggerMain(
+func New(
 	ctx context.Context,
+	// GitHub Personal Access Token which access to dagger/dagger.io repo
 	githubToken *dagger.Secret,
-) error {
+) (*Daggerverse, error) {
 	token, err := githubToken.Plaintext(ctx)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// get user config from githubToken
 	ghc := github.NewClient(nil).WithAuthToken(token)
 	user, _, err := ghc.Users.Get(ctx, "")
 	if err != nil {
-		return err
+		return nil, err
+	}
+	repo := "github.com/dagger/dagger.io"
+	dgvs := &Daggerverse{
+		GitHubUsername: *user.Login,
+		GitHubUser:     *user.Name,
+		Repo:           repo,
+		Gh: dag.Gh(dagger.GhOpts{
+			Token: githubToken,
+			Repo:  repo,
+		}),
 	}
 
 	emails, _, err := ghc.Users.ListEmails(ctx, &github.ListOptions{})
 	if err != nil {
-		return err
+		return nil, err
 	}
+	dgvs.GitHubUserEmail = *emails[0].Email
 
-	today := time.Now()
-	date := today.Format("2006-01-02")
+	return dgvs, nil
+}
 
-	// clone dagger.io - private repository, requires a github token
-	repo := "github.com/dagger/dagger.io"
-	// clone dagger.io - private repository, requires a github token
-	gh := dag.Gh(dagger.GhOpts{
-		Token: githubToken,
-		Repo:  repo,
-	})
-
+// Deploy preview environment running Dagger main: dagger call --github-token=env:GITHUB_PAT deploy-preview-with-dagger-main
+func (h *Daggerverse) DeployPreviewWithDaggerMain(
+	ctx context.Context,
+) error {
 	// make a change so that a new Daggerverse deployment will be created
-	daggerio := gh.Repo().Clone(
-		repo,
-		dagger.GhRepoCloneOpts{
-			Args: []string{"--depth=1"},
-		}).
-		WithNewFile("daggerverse/CREATE_PREVIEW_ENVIRONMENT", today.String())
+	daggerio := h.clone().
+		WithNewFile("daggerverse/CREATE_PREVIEW_ENVIRONMENT", time.Now().String())
 
-	branch := fmt.Sprintf("dgvs-test-with-dagger-main-%s", date)
+	branch := fmt.Sprintf("dgvs-test-with-dagger-main-%s", h.date())
 	commitMsg := fmt.Sprintf(`dgvs: Test Dagger Engine main @ %s
 
-daggerverse-checks in GitHub Actions ensures that module crawling works as expected. Should complete within 5 mins.`, date)
+daggerverse-checks in GitHub Actions ensures that module crawling works as expected. Should complete within 5 mins.`, h.date())
 
 	// open a PR so that it creates a new Daggerverse preview environment running Dagger main
-	err = gh.WithSource(daggerio).
+	err := h.Gh.WithSource(daggerio).
 		WithGitExec([]string{"checkout", "-b", branch}).
 		WithGitExec([]string{"add", "daggerverse/CREATE_PREVIEW_ENVIRONMENT"}).
-		WithGitExec([]string{"config", "user.email", *emails[0].Email}).
-		WithGitExec([]string{"config", "user.name", *user.Name}).
+		WithGitExec([]string{"config", "user.email", h.GitHubUserEmail}).
+		WithGitExec([]string{"config", "user.name", h.GitHubUser}).
 		WithGitExec([]string{"commit", "-am", commitMsg}).
-		WithGitExec([]string{"push", "--force", "origin", branch}).
+		WithGitExec([]string{"push", "origin", branch}).
 		PullRequest().Create(
 		ctx,
 		dagger.GhPullRequestCreateOpts{
-			Assignees: []string{*user.Login},
+			Assignees: []string{h.GitHubUsername},
 			Fill:      true,
 			Labels:    []string{"preview", "area/daggerverse"},
 			Head:      branch,
@@ -76,4 +91,118 @@ daggerverse-checks in GitHub Actions ensures that module crawling works as expec
 	)
 
 	return err
+}
+
+// Bump Dagger version: dagger call --github-token=env:GITHUB_PAT bump-dagger-version --from=0.13.7 --to=0.14.0
+func (h *Daggerverse) BumpDaggerVersion(
+	ctx context.Context,
+	// +defaultPath="../../.changes"
+	releases *dagger.Directory,
+	// Which version of Dagger are we bumping from - defaults to version n-1
+	// +optional
+	from string,
+	// Which version of Dagger are we bumping to
+	to string,
+) (err error) {
+	if from == "" {
+		from, err = dag.Container().From("alpine").
+			WithDirectory("/releases", releases).
+			WithWorkdir("/releases").
+			WithExec([]string{"sh", "-c", "ls v* | awk -F'[v.]' '{ print $2\".\"$3.\".\"$4 }' | sort -V | tail -n 2 | head -n 1"}).
+			Stdout(ctx)
+		if err != nil {
+			return err
+		}
+		from = strings.TrimSpace(from)
+	}
+
+	fromDashed := strings.ReplaceAll(from, ".", "-")
+	toDashed := strings.ReplaceAll(to, ".", "-")
+	engineImage := fmt.Sprintf("registry.dagger.io/engine:v%s", to)
+
+	engine := dag.Container().From(engineImage).
+		WithExposedPort(1234).
+		WithExec([]string{
+			"--addr", "tcp://0.0.0.0:1234",
+			"--addr", "unix:///var/run/buildkit/buildkitd.sock",
+			"--network-cidr", "10.12.34.0/24",
+		}, dagger.ContainerWithExecOpts{
+			UseEntrypoint:            true,
+			InsecureRootCapabilities: true,
+		}).AsService()
+
+	daggerio, err := dag.Container().From(engineImage).
+		WithDirectory("/dagger.io", h.clone()).
+		WithWorkdir("/dagger.io").
+		WithExec([]string{"sh", "-c",
+			fmt.Sprintf("find .github/workflows -name '*daggerverse*' -exec sed -i 's/%s/%s/g' {} +", fromDashed, toDashed),
+		}).
+		WithExec([]string{"sh", "-c",
+			fmt.Sprintf("sed -i 's/\\(DaggerVersion\\s*=\\s*\"\\)%s\"/\\1%s\"/' daggerverse/dag/main.go", from, to),
+		}).
+		WithServiceBinding("daggerverse-engine", engine).
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", "tcp://daggerverse-engine:1234").
+		WithExec([]string{"nc", "-vzw", "1", "daggerverse-engine", "1234"}).
+		WithExec([]string{"dagger", "version"}).
+		WithExec([]string{"dagger", "core", "version"}).
+		WithExec([]string{"dagger", "--mod=daggerverse/dag", "develop"}).
+		WithExec([]string{"dagger", "--mod=daggerverse", "develop"}).
+		WithExec([]string{"sh", "-c",
+			fmt.Sprintf("sed -i 's/v[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*/v%s/' infra/ci/*/argocd/daggerverse-preview/appset.yaml", to),
+		}).
+		WithExec([]string{"sh", "-c",
+			fmt.Sprintf("sed -i 's/registry\\.dagger\\.io\\/engine:v[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*/registry\\.dagger\\.io\\/engine:v%s/' infra/ci/*/argocd/daggerverse-preview/manifests/deployment.base.yaml", to),
+		}).
+		WithExec([]string{"sh", "-c",
+			fmt.Sprintf("sed -i 's/registry\\.dagger\\.io\\/engine:v[0-9][0-9]*\\.[0-9][0-9]*\\.[0-9][0-9]*/registry\\.dagger\\.io\\/engine:v%s/' infra/prod/*/argocd/daggerverse/deployment.yaml", to),
+		}).
+		Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	daggerverse, err := dag.Go(daggerio.Directory("daggerverse")).Env().
+		WithExec([]string{"go", "get", fmt.Sprintf("dagger.io/dagger@v%s", to)}).
+		Sync(ctx)
+	if err != nil {
+		return err
+	}
+
+	updated := daggerio.
+		WithDirectory("/dagger.io/daggerverse", daggerverse.Directory(".")).
+		Directory("/dagger.io")
+
+	branch := fmt.Sprintf("dgvs-bump-dagger-from-%s-to-%s-with-dagger-main", from, to)
+	commitMsg := fmt.Sprintf("dgvs: Bump Dagger from %s to %s", from, to)
+
+	err = h.Gh.WithSource(updated).
+		WithGitExec([]string{"checkout", "-b", branch}).
+		WithGitExec([]string{"add", ".github", "daggerverse", "infra"}).
+		WithGitExec([]string{"config", "user.email", h.GitHubUserEmail}).
+		WithGitExec([]string{"config", "user.name", h.GitHubUser}).
+		WithGitExec([]string{"commit", "-am", commitMsg}).
+		WithGitExec([]string{"push", "origin", branch}).
+		PullRequest().Create(
+		ctx,
+		dagger.GhPullRequestCreateOpts{
+			Assignees: []string{h.GitHubUsername},
+			Fill:      true,
+			Labels:    []string{"preview", "area/daggerverse"},
+			Head:      branch,
+		},
+	)
+
+	return err
+}
+
+func (h *Daggerverse) date() string {
+	return time.Now().Format("2006-01-02")
+}
+
+func (h *Daggerverse) clone() *dagger.Directory {
+	return h.Gh.Repo().Clone(
+		h.Repo,
+		dagger.GhRepoCloneOpts{
+			Args: []string{"--depth=1"},
+		})
 }
