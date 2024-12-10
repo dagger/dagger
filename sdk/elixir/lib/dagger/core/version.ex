8b9fbf5c643285eba1887292a79050540e399955
Justin Chadwell <me@jedevc.com>
2024-12-10 14:10:04 +0000

    chore: prep for v0.15.0 (#9158)
    * chore: bump dependencies to v0.15.0
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * chore: add release notes for v0.15.0
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    * chore: bump next version to v0.15.1
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>
    
    ---------
    
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- sdk/elixir/lib/dagger/core/version.ex
+++ sdk/elixir/lib/dagger/core/version.ex
@@ -1,7 +1,7 @@
 defmodule Dagger.Core.Version do
   @moduledoc false
 
-  @dagger_cli_version "0.14.0"
+  @dagger_cli_version "0.15.0"
 
   def engine_version(), do: @dagger_cli_version
 end
--- sdk/go/internal/engineconn/version.gen.go
+++ sdk/go/internal/engineconn/version.gen.go
@@ -2,4 +2,4 @@
 
 package engineconn
 
-const CLIVersion = "0.14.0"
+const CLIVersion = "0.15.0"
