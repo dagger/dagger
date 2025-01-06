1800107827c95321d7158f8044ee55aa397e242e
Andrea Luzzardi <al@dagger.io>
2025-01-06 16:09:23 +0100

    file secretprovider: home path expansion
    Signed-off-by: Andrea Luzzardi <al@dagger.io>

--- engine/client/secretprovider/file.go
+++ engine/client/secretprovider/file.go
@@ -2,9 +2,21 @@ package secretprovider
 
 import (
 	"context"
+	"fmt"
 	"os"
+
+	"github.com/mitchellh/go-homedir"
 )
 
 func fileProvider(_ context.Context, path string) ([]byte, error) {
-	return os.ReadFile(path)
+	path, err := homedir.Expand(path)
+	if err != nil {
+		return nil, err
+	}
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read secret file %q: %w", path, err)
+	}
+
+	return data, nil
 }
