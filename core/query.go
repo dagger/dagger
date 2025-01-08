965267c25b530ede48738fcce9ce17b286f191eb
Alex Suraci <alex@dagger.io>
2025-01-08 18:14:45 -0500

    remove Span.query - not needed anymore
    Previously this was used when chaining sub-spans from Spans. Now it's
    dependent on context.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/query.go
+++ core/query.go
@@ -86,7 +86,7 @@ func (c SpanContext) ToContext(ctx context.Context) context.Context {
 type Span struct {
 	Name string `field:"true"`
 
-	Query *Query `field:"true"`
+	Query *Query
 
 	Span trace.Span
 }
