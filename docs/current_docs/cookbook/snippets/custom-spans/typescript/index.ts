09616fc27886dc27642ab53458a65a3ad824479e
Vikram Vaswani <vikram@dagger.io>
2025-01-09 02:03:08 +0530

    Fixed lint errors
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/cookbook/snippets/custom-spans/typescript/index.ts
+++ docs/current_docs/cookbook/snippets/custom-spans/typescript/index.ts
@@ -6,17 +6,19 @@ class MyModule {
   @func()
   async foo(): Promise<void> {
     // clone the source code repository
-    const source = dag.git("https://github.com/dagger/hello-dagger")
+    const source = dag
+      .git("https://github.com/dagger/hello-dagger")
       .branch("main")
-      .tree();
+      .tree()
 
     // set up a container with the source code mounted
     // install dependencies
-    const container = dag.container()
+    const container = dag
+      .container()
       .from("node:latest")
       .withDirectory("/src", source)
       .withWorkdir("/src")
-      .withExec(["npm", "install"]);
+      .withExec(["npm", "install"])
 
     // run tasks in parallel
     // emit a span for each
@@ -25,60 +27,66 @@ class MyModule {
       this.typecheck(container),
       this.format(container),
       this.test(container),
-    ];
+    ]
 
-    await Promise.all(tasks);
+    await Promise.all(tasks)
   }
 
   private async lint(container: Container): Promise<void> {
     const tracer = trace.getTracer("dagger-otel")
     const span = tracer.startSpan("lint code")
     try {
-      const result = await container.withExec(["npm", "run", "lint"]).sync();
+      const result = await container.withExec(["npm", "run", "lint"]).sync()
       if (result.exitCode !== 0) {
-        throw new Error(`Linting failed with exit code ${result.exitCode}`);
+        throw new Error(`Linting failed with exit code ${result.exitCode}`)
       }
     } finally {
-      span.end();
+      span.end()
     }
   }
 
   private async typecheck(container: Container): Promise<void> {
     const tracer = trace.getTracer("dagger-otel")
-    const span = tracer.startSpan("check types");
+    const span = tracer.startSpan("check types")
     try {
-      const result = await container.withExec(["npm", "run", "type-check"]).sync();
+      const result = await container
+        .withExec(["npm", "run", "type-check"])
+        .sync()
       if (result.exitCode !== 0) {
-        throw new Error(`Type check failed with exit code ${result.exitCode}`);
+        throw new Error(`Type check failed with exit code ${result.exitCode}`)
       }
     } finally {
-      span.end();
+      span.end()
     }
   }
 
   private async format(container: Container): Promise<void> {
     const tracer = trace.getTracer("dagger-otel")
-    const span = tracer.startSpan("format code");
+    const span = tracer.startSpan("format code")
     try {
-      const result = await container.withExec(["npm", "run", "format"]).sync();
+      const result = await container.withExec(["npm", "run", "format"]).sync()
       if (result.exitCode !== 0) {
-        throw new Error(`Code formatting failed with exit code ${result.exitCode}`);
+        throw new Error(
+          `Code formatting failed with exit code ${result.exitCode}`,
+        )
       }
     } finally {
-      span.end();
+      span.end()
     }
   }
 
   private async test(container: Container): Promise<void> {
     const tracer = trace.getTracer("dagger-otel")
-    const span = tracer.startSpan("run unit tests");
+    const span = tracer.startSpan("run unit tests")
     try {
-      const result = await container.withExec(["npm", "run", "test:unit", "run"]).sync();
+      const result = await container
+        .withExec(["npm", "run", "test:unit", "run"])
+        .sync()
       if (result.exitCode !== 0) {
-        throw new Error(`Tests failed with exit code ${result.exitCode}`);
+        throw new Error(`Tests failed with exit code ${result.exitCode}`)
       }
     } finally {
-      span.end();
+      span.end()
     }
   }
 }
