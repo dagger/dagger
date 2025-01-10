20a6c59e804579016cc41380982361f3f710c79f
Tom Chauveau <tom@epitech.eu>
2025-01-10 14:36:00 +0100

    chore: fix linter
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- docs/current_docs/api/snippets/interfaces/usages/typescript/index.ts
+++ docs/current_docs/api/snippets/interfaces/usages/typescript/index.ts
@@ -4,7 +4,7 @@ import { dag, func, object } from "@dagger.io/dagger"
 export class Usage {
   @func()
   async test(): Promise<string> {
-    // Because `Example` implements `Fooer`, the conversion function 
+    // Because `Example` implements `Fooer`, the conversion function
     // `AsMyModuleFooer` has been generated.
     return dag.myModule().foo(dag.example().asMyModuleFooer())
   }
