c6dd260e36f6ddb7225ee92c1911066015ba4225
Alex Suraci <alex@dagger.io>
2025-01-10 00:30:09 -0500

    fix ts lint
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- sdk/typescript/src/api/client.gen.ts
+++ sdk/typescript/src/api/client.gen.ts
@@ -7894,8 +7894,10 @@ export class Span extends BaseClient {
     let spanError: Error | undefined = undefined
     try {
       return await opentelemetry.context.with(newContext, fn, this, started)
-    } catch (e) {
-      spanError = dag.error(e.message)
+    } catch (e: unknown) {
+      if (e instanceof globalThis.Error) {
+        spanError = dag.error(e.message)
+      }
       throw e
     } finally {
       await started.end({ error: spanError })
