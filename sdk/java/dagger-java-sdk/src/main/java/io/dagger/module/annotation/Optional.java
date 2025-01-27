b81e82da82a2cc4fac25c250104587c324d6adfe
Yves Brissaud <gh@lgtd.io>
2025-01-27 18:17:37 +0100

    allow to set a default value to parameters
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/annotation/Optional.java
+++ sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/annotation/Optional.java
@@ -7,4 +7,15 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.SOURCE)
-public @interface Optional {}
+public @interface Optional {
+  /**
+   * Default value of the parameter. This must be a <b>valid JSON string</b>.
+   *
+   * <p>Be careful with string values. For instance <code>defaultValue="true"</code> is a boolean,
+   * while <code>defaultValue="\"true\""</code> is a string.
+   *
+   * <p>You <b>must</b> put string values under escaped quotes in order to make it a valid json
+   * string.
+   */
+  String defaultValue() default "";
+}
