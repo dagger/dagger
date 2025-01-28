d9a8515eec90c172749f44e03e0474a7ea5702b0
Alex Suraci <alex@dagger.io>
2025-01-28 16:26:25 -0500

    use DecodeInput to convert tool args to selector
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/server.go
+++ dagql/server.go
@@ -1018,7 +1018,7 @@ func setInputObjectFields(obj any, vals map[string]any) error {
 		}
 		if input != nil { // will be nil for optional fields
 			if err := assignInputField(fieldV, input); err != nil {
-				return fmt.Errorf("assign %q: %w", fieldT.Name, err)
+				return fmt.Errorf("assign input object %q as %+v (%T): %w", fieldT.Name, input, input, err)
 			}
 		}
 	}
