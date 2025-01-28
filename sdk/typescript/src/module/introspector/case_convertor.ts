5743b31b95f17c391e539b30b8fe55ad938e691a
Vasek - Tom C <tom.chauveau@epitech.eu>
2025-01-28 20:00:49 +0100

    fix(ts-sdk): object name conversion with numbers (#9459)
    Fixes #9451
    
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- sdk/typescript/src/module/introspector/case_convertor.ts
+++ sdk/typescript/src/module/introspector/case_convertor.ts
@@ -1,18 +1,15 @@
 export function convertToPascalCase(input: string): string {
+  // Handle empty string case
+  if (!input) {
+    return ""
+  }
+
+  // Split on word boundaries before uppercase letters, numbers, and special characters
   const words = input
-    .replace(/[^a-zA-Z0-9]/g, " ") // Replace non-alphanumeric characters with spaces
-    .split(/\s+/)
+    .split(/(?=[A-Z0-9])|[^a-zA-Z0-9]|(?<=[a-zA-Z])(?=\d)|(?<=\d)(?=[a-zA-Z])/g)
     .filter((word) => word.length > 0)
 
-  if (words.length === 0) {
-    return "" // No valid words found
-  }
-
-  // It's an edge case when moduleName is already in PascalCase or camelCase
-  if (words.length === 1) {
-    return words[0].charAt(0).toUpperCase() + words[0].slice(1)
-  }
-
+  // Convert each word to proper case
   const pascalCase = words
     .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
     .join("")
--- sdk/typescript/src/module/introspector/test/case_convertor.spec.ts
+++ sdk/typescript/src/module/introspector/test/case_convertor.spec.ts
@@ -33,5 +33,37 @@ describe("case convertor", function () {
       const result = convertToPascalCase("HelloWorld")
       assert.equal(result, "HelloWorld")
     })
+
+    it("should correctly handle numbers as spacers", function () {
+      const result = convertToPascalCase("hello123world")
+      assert.equal(result, "Hello123World")
+    })
+
+    it("should correctly handle number as first word", function () {
+      const result = convertToPascalCase("123helloworld")
+      assert.equal(result, "123Helloworld")
+    })
+
+    it("should correctly handle number as last word", function () {
+      const result = convertToPascalCase("helloWorld123")
+      assert.equal(result, "HelloWorld123")
+    })
+
+    it("should correctly handle special characters as spacers", function () {
+      const result = convertToPascalCase("hello world")
+      assert.equal(result, "HelloWorld")
+    })
+
+    it("should correctly handle mix cases", function () {
+      const result = convertToPascalCase("123hello-world_here")
+      assert.equal(result, "123HelloWorldHere")
+    })
+
+    it("should correctly handle multiple uppercase words", function () {
+      // This will still not work in module because the generated class name would be `IntrospectionJson`
+      // See https://github.com/dagger/dagger/issues/7941
+      const result = convertToPascalCase("introspectionJSON")
+      assert.equal(result, "IntrospectionJSON")
+    })
   })
 })
