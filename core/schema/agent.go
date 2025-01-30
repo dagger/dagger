03256484f1ce170866ac636aab4b0d42a9ffa460
Solomon Hykes <solomon@dagger.io>
2025-01-30 14:03:11 -0800

    agent: remove old schema code
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- core/schema/agent.go
+++ core/schema/agent.go
@@ -1,8 +1,6 @@
 package schema
 
 import (
-	"context"
-
 	"github.com/dagger/dagger/core"
 	"github.com/dagger/dagger/dagql"
 )
@@ -15,41 +13,4 @@ var _ SchemaResolvers = &agentSchema{}
 
 func (s agentSchema) Install() {
 	s.srv.SetMiddleware(core.AgentMiddleware{Server: s.srv})
-}
-
-func (s *agentSchema) withLlm(ctx context.Context, parent *core.Query, args core.LlmConfig) (*core.Query, error) {
-	// FIXME: hack
-	core.GlobalLLMConfig = args
-	return parent.WithLlmConfig(args), nil
-}
-
-type agentWithPromptArgs struct {
-	Prompt string
-}
-
-func (s *agentSchema) withPrompt(ctx context.Context, parent *core.Agent, args agentWithPromptArgs) (*core.Agent, error) {
-	return parent.WithPrompt(args.Prompt), nil
-}
-
-func (s *agentSchema) withSystemPrompt(ctx context.Context, parent *core.Agent, args agentWithPromptArgs) (*core.Agent, error) {
-	return parent.WithSystemPrompt(args.Prompt), nil
-}
-
-type agentRunArgs struct{}
-
-func (s *agentSchema) run(ctx context.Context, parent *core.Agent, args agentRunArgs) (*core.Agent, error) {
-	// FIXME: make maxLoops configurable
-	return parent.Run(ctx, 0)
-}
-
-type agentHistoryArgs struct{}
-
-func (s *agentSchema) history(ctx context.Context, parent *core.Agent, args agentHistoryArgs) ([]string, error) {
-	return parent.History()
-}
-
-type agentAsObjectArgs struct{}
-
-func (s *agentSchema) asObject(ctx context.Context, parent *core.Agent, args agentHistoryArgs) (dagql.Object, error) {
-	return parent.Self(ctx), nil
 }
