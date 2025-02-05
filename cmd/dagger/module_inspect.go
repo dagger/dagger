48a9e3d4c45f157d6e167b77b729711b041446c9
Helder Correia <174525+helderco@users.noreply.github.com>
2025-02-05 11:41:33 -0100

    cli: trivial cleanups
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- cmd/dagger/module_inspect.go
+++ cmd/dagger/module_inspect.go
@@ -947,7 +947,7 @@ func (r *modFunctionArg) Long() string {
 		} else if sb.Len() > 0 {
 			sb.WriteString(" ")
 		}
-		sb.WriteString(fmt.Sprintf("(default: %s)", defVal))
+		fmt.Fprintf(sb, "(default: %s)", defVal)
 	}
 
 	if r.TypeDef.Kind == dagger.TypeDefKindEnumKind {
@@ -957,7 +957,7 @@ func (r *modFunctionArg) Long() string {
 		} else if sb.Len() > 0 {
 			sb.WriteString(" ")
 		}
-		sb.WriteString(fmt.Sprintf("(possible values: %s)", names))
+		fmt.Fprintf(sb, "(possible values: %s)", names)
 	}
 
 	return sb.String()
