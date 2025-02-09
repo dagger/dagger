6d1e52bdb2d64c8100f9a643c759ec7c83b97f6a
Alex Suraci <alex@dagger.io>
2025-02-09 17:15:44 -0500

    reveal llm spans
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- sdk/go/telemetry/span.go
+++ sdk/go/telemetry/span.go
@@ -14,6 +14,12 @@ func Encapsulate() trace.SpanStartOption {
 	return trace.WithAttributes(attribute.Bool(UIEncapsulateAttr, true))
 }
 
+// Reveal can be applied to a span to indicate that this span should
+// collapse its children by default.
+func Reveal() trace.SpanStartOption {
+	return trace.WithAttributes(attribute.Bool(UIRevealAttr, true))
+}
+
 // Encapsulated can be applied to a child span to indicate that it should be
 // collapsed by default.
 func Encapsulated() trace.SpanStartOption {
