83580ca785831b076038f2a4d2eddf454aac6c1a
Yves Brissaud <gh@lgtd.io>
2025-02-11 08:59:15 +0100

    allow to set a default path for directories
        @DefaultPath("./dagger")
    
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/info/ParameterInfo.java
+++ sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/info/ParameterInfo.java
@@ -1,9 +1,11 @@
 package io.dagger.module.info;
 
+import java.util.Optional;
+
 public record ParameterInfo(
     String name,
     String description,
     TypeInfo type,
     boolean optional,
-    boolean hasDefaultValue,
-    String defaultValue) {}
+    Optional<String> defaultValue,
+    Optional<String> defaultPath) {}
