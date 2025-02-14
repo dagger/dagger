b804c6a74a27ce18d79a650e0e22660ebfa4f6c4
Tom Chauveau <tom@epitech.eu>
2025-02-14 01:04:44 +0100

    chore: fix linter
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- cmd/dagger/engine.go
+++ cmd/dagger/engine.go
@@ -67,18 +67,18 @@ func withEngine(
 			if err != nil {
 				return fmt.Errorf("failed to get current working directory: %w", err)
 			}
-	
+
 			moduleSrcPath, configExist, err := findUp(cwd)
 			if err != nil {
 				return fmt.Errorf("failed to look for dagger.json file: %w", err)
 			}
-	
+
 			if configExist {
 				mod, err := initializeClientGeneratorModule(ctx, sess.Dagger(), moduleSrcPath, false)
 				if err != nil {
 					return fmt.Errorf("failed to initialize current module: %w", err)
 				}
-	
+
 				for _, dep := range mod.Dependencies {
 					err := dep.Source.AsModule().Initialize().Serve(ctx)
 					if err != nil {
@@ -87,7 +87,6 @@ func withEngine(
 				}
 			}
 		}
-
 
 		return fn(ctx, sess)
 	})
