2262b0549c8fa73f0626da75536da57e14d2407a
Tom Chauveau <tom@epitech.eu>
2025-02-14 01:48:41 +0100

    chore: disable module init on session
    Signed-off-by: Tom Chauveau <tom@epitech.eu>

--- cmd/dagger/session.go
+++ cmd/dagger/session.go
@@ -84,7 +84,7 @@ func EngineSession(cmd *cobra.Command, args []string) error {
 	return withEngine(ctx, client.Params{
 		SecretToken: sessionToken.String(),
 		Version:     sessionVersion,
-		AutoInit:    true,
+		AutoInit:    false,
 	}, func(ctx context.Context, sess *client.Client) error {
 		// Requests maintain their original trace context from the client, rather
 		// than appearing beneath the dagger session span, so in order to see any
