09516b4fce429f2776dfb5950208a0272989550c
carnage <carnage@users.noreply.github.com>
2025-02-21 14:18:23 +0000

    PHP: Ensure up to date dependencies of the generated code
    Signed-off-by: Chris Riley <t.carnage@gmail.com>
    Co-authored-by: John Charman <102669158+charjr@users.noreply.github.com>

--- sdk/php/runtime/main.go
+++ sdk/php/runtime/main.go
@@ -124,7 +124,7 @@ func (m *PhpSdk) CodegenBase(
 		WithWorkdir(srcPath).
 		WithExec([]string{"/init-template.sh", name}).
 		// composer install adds the lock file so we want this even in Codegen.
-		WithExec([]string{"composer", "install"}).
+		WithExec([]string{"composer", "update", "--with-all-dependencies", "--minimal-changes", "dagger/dagger"}).
 		WithEntrypoint([]string{filepath.Join(srcPath, "entrypoint.php")})
 
 	return ctr, nil
