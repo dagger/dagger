e87284b75cb9b23e6c340380e087460cf5841743
Justin Chadwell <me@jedevc.com>
2025-02-27 15:16:13 +0000

    tests: add Scripts.test for testing install.sh
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- .dagger/scripts.go
+++ .dagger/scripts.go
@@ -2,8 +2,13 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"strings"
 
+	"github.com/containerd/platforms"
 	"github.com/dagger/dagger/.dagger/internal/dagger"
+	"go.opentelemetry.io/otel/codes"
+	"golang.org/x/mod/semver"
 	"golang.org/x/sync/errgroup"
 )
 
@@ -30,4 +35,139 @@ func (s Scripts) Lint(ctx context.Context) error {
 			Assert(ctx)
 	})
 	return eg.Wait()
+}
+
+func (s Scripts) Test(ctx context.Context) error {
+	ctr := dag.Alpine(
+		dagger.AlpineOpts{
+			Packages: []string{"curl"},
+		}).
+		Container().
+		WithWorkdir("/opt/dagger").
+		WithFile("/usr/local/bin/install.sh", s.Dagger.Source().File("install.sh"), dagger.ContainerWithFileOpts{
+			Permissions: 0755,
+		})
+
+	lastRelease, err := dag.Version().LastReleaseVersion(ctx)
+	if err != nil {
+		return err
+	}
+
+	eg := errgroup.Group{}
+
+	test := func(name string, f func(ctx context.Context) error) {
+		ctx, span := Tracer().Start(ctx, name)
+		eg.Go(func() (rerr error) {
+			defer func() {
+				if rerr != nil {
+					span.SetStatus(codes.Error, rerr.Error())
+				}
+				span.End()
+			}()
+			return f(ctx)
+		})
+	}
+
+	test("default install", func(ctx context.Context) error {
+		ctr := ctr.
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "/opt/dagger/bin/dagger", nil)
+	})
+
+	test("install to location", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("BIN_DIR", "/opt/special-bin").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "/opt/special-bin/dagger", nil)
+	})
+
+	// install by semver
+	test("install vX.Y.Z", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_VERSION", "v0.16.1").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchExactVersion("v0.16.1"))
+	})
+	test("install vX.Y", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_VERSION", "v0.15").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchExactVersion("v0.15.4"))
+	})
+	test("install X.Y.Z without v", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_VERSION", "0.16.1").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchExactVersion("v0.16.1"))
+	})
+
+	// install latest
+	test("install latest", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_VERSION", "latest").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchAtLeastVersion(lastRelease))
+	})
+
+	test("install git sha", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_COMMIT", "976cd0bf4be8d1cacbc3ee23a7ab057e8868ac2d").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchExactVersion("v0.16.2-250227135944-976cd0bf4be8"))
+	})
+	test("install git head", func(ctx context.Context) error {
+		ctr := ctr.
+			WithEnvVariable("DAGGER_COMMIT", "head").
+			WithExec([]string{"install.sh"})
+		return checkDaggerVersion(ctx, ctr, "./bin/dagger", matchAtLeastVersion(lastRelease))
+	})
+
+	return eg.Wait()
+}
+
+func matchExactVersion(target string) func(string) error {
+	return func(v string) error {
+		if semver.Compare(target, v) != 0 {
+			return fmt.Errorf(`expected %q to match semver %q`, v, target)
+		}
+		return nil
+	}
+}
+
+func matchAtLeastVersion(target string) func(string) error {
+	return func(v string) error {
+		if semver.Compare(target, v) > 0 {
+			return fmt.Errorf(`expected %q to exceed semver %q`, v, target)
+		}
+		return nil
+	}
+}
+
+func checkDaggerVersion(ctx context.Context, ctr *dagger.Container, path string, version func(string) error) error {
+	out, err := ctr.
+		WithExec([]string{path, "version"}).
+		Stdout(ctx)
+	if err != nil {
+		return err
+	}
+	out = strings.TrimSpace(out)
+
+	fields := strings.Fields(out)
+	if fields[0] != "dagger" {
+		return fmt.Errorf(`expected %q to contain "dagger"`, out)
+	}
+	if !semver.IsValid(fields[1]) {
+		return fmt.Errorf(`expected %q to contain valid semver`, out)
+	}
+	if version != nil {
+		if err := version(fields[1]); err != nil {
+			return err
+		}
+	}
+	currentPlatform := platforms.Format(platforms.DefaultSpec())
+	if fields[len(fields)-1] != currentPlatform {
+		return fmt.Errorf(`expected %q to contain the current platform %q`, out, currentPlatform)
+	}
+
+	return nil
 }
