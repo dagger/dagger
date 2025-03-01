a96b04604871a95164cfead418608acfdafa8d88
Alex Suraci <alex@dagger.io>
2025-03-01 14:43:50 -0500

    Simplify Simplify
    Tried this before but it didn't work out for some reason. Taking another
    swing at it now that I'm not on a time crunch.
    
    This fixes a test hang (Call/ArgTypes/SSH_Private_GitLab) and builds on
    the prior work to track creators in CreatorSpans.
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/dagui/db.go
+++ dagql/dagui/db.go
@@ -811,85 +811,22 @@ func (db *DB) MustCall(dig string) *callpbv1.Call {
 	return call
 }
 
-func (db *DB) litSize(lit *callpbv1.Literal) int {
-	switch x := lit.GetValue().(type) {
-	case *callpbv1.Literal_CallDigest:
-		return db.idSize(db.MustCall(x.CallDigest))
-	case *callpbv1.Literal_List:
-		size := 0
-		for _, lit := range x.List.GetValues() {
-			size += db.litSize(lit)
-		}
-		return size
-	case *callpbv1.Literal_Object:
-		size := 0
-		for _, lit := range x.Object.GetValues() {
-			size += db.litSize(lit.GetValue())
-		}
-		return size
-	}
-	return 1
-}
-
-func (db *DB) idSize(id *callpbv1.Call) int {
-	size := 0
-	for id := id; id != nil; id = db.MustCall(id.ReceiverDigest) {
-		size++
-		size += len(id.Args)
-		for _, arg := range id.Args {
-			size += db.litSize(arg.GetValue())
-		}
-		if id.ReceiverDigest == "" {
-			break
-		}
-	}
-	return size
-}
-
-func (db *DB) Simplify(call *callpbv1.Call, force bool) (smallest *callpbv1.Call) {
-	smallest = call
-	smallestSize := -1
-	if !force {
-		smallestSize = db.idSize(smallest)
-	}
-
-	creators, ok := db.OutputOf[call.Digest]
+func (db *DB) Simplify(call *callpbv1.Call, force bool) *callpbv1.Call {
+	creators, ok := db.CreatorSpans[call.Digest]
 	if !ok {
-		return smallest
+		return call
 	}
-	simplified := false
 
-loop:
-	for creatorDig := range creators {
-		if creatorDig == call.Digest {
-			// can't be simplified to itself
+	for _, creator := range creators.Order {
+		if creator.CallDigest == "" {
 			continue
 		}
-		creator := db.Call(creatorDig)
-		if creator == nil {
-			// creator not found? skip
-			continue
+		if creatorCall := db.Call(creator.CallDigest); creatorCall != nil {
+			return creatorCall // TODO: re-simplify?
 		}
-		for _, creatorArg := range creator.Args {
-			if creatorArg, ok := creatorArg.Value.Value.(*callpbv1.Literal_CallDigest); ok {
-				if creatorArg.CallDigest == call.Digest {
-					// can't be simplified to a call that references itself
-					// in it's argument - which would loop endlessly
-					continue loop
-				}
-			}
-		}
+	}
 
-		if size := db.idSize(creator); smallestSize == -1 || size < smallestSize {
-			smallest = creator
-			smallestSize = size
-			simplified = true
-		}
-	}
-	if simplified {
-		return db.Simplify(smallest, false)
-	}
-	return smallest
+	return call
 }
 
 // Function to check if a row is or contains a target row
