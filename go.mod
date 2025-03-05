479bd8293530afd52bb648b3f709ab98bcfdbb01
Justin Chadwell <me@jedevc.com>
2025-03-05 11:28:34 +0000

    chore: update go to 1.24.1 (#9775)
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/dagger/dagger
 
-go 1.24.0
+go 1.24
+
+toolchain go1.24.1
 
 require (
 	dagger.io/dagger v0.16.2
--- modules/go/main.go
+++ modules/go/main.go
@@ -23,7 +23,7 @@ func New(
 	source *dagger.Directory,
 	// Go version
 	// +optional
-	// +default="1.24.0"
+	// +default="1.24.1"
 	version string,
 	// Use a custom module cache
 	// +optional
