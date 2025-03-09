323de67865d5200fdfd3c4cf7c96a4e50e2240ea
Julián Cruciani <julian@dagger.io>
2025-03-08 17:52:37 -0800

    feat: emit module resolution path and git context as span attrs
    retrieves module source html URL as it allows us to emit the closest to what a module ref
    is: an HTTP url without the scheme.
    This way, we don't have to parse the URL in the API and duplicate the logic used in modulerefs.go
    for parsing URLs with different schemes
    
    Signed-off-by: Julián Cruciani <julian@dagger.io>

--- core/schema/modulesource_test.go
+++ core/schema/modulesource_test.go
@@ -406,3 +406,55 @@ type neverExistsFS struct {
 func (fs neverExistsFS) stat(ctx context.Context, path string) (*fsutiltypes.Stat, error) {
 	return nil, os.ErrNotExist
 }
+
+func TestModuleLocaLSource(t *testing.T) {
+	schema := &moduleSourceSchema{}
+	ctx := context.Background()
+
+	src := &core.ModuleSource{
+		Kind: core.ModuleSourceKindLocal,
+		Local: &core.LocalModuleSource{
+			ContextDirectoryPath: "/home/user/dagger-test-modules",
+		},
+	}
+
+	for _, tc := range []struct {
+		name           string
+		source         *core.ModuleSource
+		expectedResult string
+		expectError    bool
+		fn             func(ctx context.Context, source *core.ModuleSource, args struct{}) (string, error)
+	}{
+		{
+			name:           "Local module source commit",
+			source:         src,
+			expectedResult: "",
+			expectError:    false,
+			fn:             schema.moduleSourceCommit,
+		},
+		{
+			name:           "Local module source html repo url",
+			source:         src,
+			expectedResult: "",
+			expectError:    false,
+			fn:             schema.moduleSourceHTMLRepoURL,
+		},
+		{
+			name:           "Local module source version",
+			source:         src,
+			expectedResult: "",
+			expectError:    false,
+			fn:             schema.moduleSourceVersion,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			result, err := tc.fn(ctx, tc.source, struct{}{})
+			if tc.expectError {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, tc.expectedResult, result)
+			}
+		})
+	}
+}
