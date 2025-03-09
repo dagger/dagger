4c4355daa70a4b1ff02add17588874d9b8b6555a
Vikram Vaswani <vikram@dagger.io>
2025-03-09 21:24:27 +0530

    docs: Add page on module testing
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/sidebars.ts
+++ docs/sidebars.ts
@@ -119,6 +119,7 @@ module.exports = {
           items: [
             "api/module-structure",
             "api/remote-modules",
+            "api/module-tests",
             "api/daggerverse",
             {
               type: "link",
