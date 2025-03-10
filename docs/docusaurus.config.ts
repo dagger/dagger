cbbd8532dcef3a98413e21e92760871163e38dd2
Jason McCallister <jason@mccallister.dev>
2025-03-10 15:33:36 -0400

    use property, not name. Use correct size image
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -124,12 +124,15 @@ const config: Config = {
     sidebarCollapsed: false,
     metadata: [
       {
-        name: "description",
+        property: "description",
         content:
           "Dagger is an open-source runtime for composable workflows, powering AI agents and CI/CD with modular, repeatable, and observable pipelines.",
       },
-      { name: "og:image", content: `${url}/img/dagger-factory.jpg` },
-      { name: "twitter:image", content: `${url}/img/dagger-factory.jpg` },
+      { property: "og:image", content: `${url}/img/dagger-factory-share.jpg` },
+      {
+        property: "twitter:image",
+        content: `${url}/img/dagger-factory-share.jpg`,
+      },
     ],
     prism: {
       additionalLanguages: [
