9ea7c530028822873ddf2e55a88661a04a8b91c1
Jason McCallister <jason@mccallister.dev>
2025-03-10 14:09:50 -0400

    reuse url
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "https://docs.dagger.io",
+  url: url,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",
