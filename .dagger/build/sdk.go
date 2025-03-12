3fc4492f31a783e09fae876ff685c14ba565fa4d
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-03-12 23:03:36 +0700

    Update .dagger/build/sdk.go
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- .dagger/build/sdk.go
+++ .dagger/build/sdk.go
@@ -76,7 +76,6 @@ const TypescriptSDKTSXVersion = "4.15.6"
 func (build *Builder) typescriptSDKContent(ctx context.Context) (*sdkContent, error) {
 	tsxNodeModule := dag.Container(dagger.ContainerOpts{Platform: build.platform}).
 		From(tsdistconsts.DefaultNodeImageRef).
-		WithExec([]string{"npm", "install", "-g", "npm"}).
 		WithExec([]string{"npm", "install", "-g", fmt.Sprintf("tsx@%s", TypescriptSDKTSXVersion)}).
 		Directory("/usr/local/lib/node_modules/tsx")
 
