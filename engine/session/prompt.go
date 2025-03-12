921ee21b42e3de11e79f1292654d4d2bc5d850a1
Connor Braa <connor@dagger.io>
2025-03-12 13:58:45 -0700

    session attachable without actual prompting
    or the code that figures out the calling module for the prompt text
    
    Signed-off-by: Connor Braa <connor@dagger.io>

---
+++ engine/session/prompt.go
@@ -0,0 +1,49 @@
+package session
+
+import (
+	context "context"
+	"sync"
+	"time"
+
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	status "google.golang.org/grpc/status"
+)
+
+var promptMutex sync.Mutex
+
+type PromptAttachable struct {
+	rootCtx context.Context
+
+	UnimplementedGitCredentialServer
+}
+
+func NewPromptAttachable(rootCtx context.Context) PromptAttachable {
+	return PromptAttachable{
+		rootCtx: rootCtx,
+	}
+}
+
+func (p PromptAttachable) Register(srv *grpc.Server) {
+	RegisterPromptServer(srv, p)
+}
+
+func (s PromptAttachable) Prompt(ctx context.Context, req *PromptRequest) (*PromptResponse, error) {
+	ctx, cancel := context.WithTimeout(ctx, time.Minute)
+	defer cancel()
+
+	// Validate request
+	if req.Prompt == "" {
+		return nil, status.Errorf(codes.InvalidArgument, "Invalid input: prompt is required")
+	}
+
+	// Ensure we're displaying 1 prompt at a time
+	promptMutex.Lock()
+	defer promptMutex.Unlock()
+
+	// TODO: @vito: actually prompt the user via the frontend
+
+	return &PromptResponse{
+		Input: "yes",
+	}, nil
+}
