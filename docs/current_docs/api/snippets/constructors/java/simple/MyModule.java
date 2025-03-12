2a874f01683e3ff80b6f4ed51e13eaee0ce545d3
Yves Brissaud <gh@lgtd.io>
2025-03-12 15:56:41 +0100

    patch: Author: Yves Brissaud <gh@lgtd.io> Date:   2025-03-12 15:56:41 +0100
    directory filters
    
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- docs/current_docs/api/snippets/constructors/java/simple/MyModule.java
+++ docs/current_docs/api/snippets/constructors/java/simple/MyModule.java
@@ -1,23 +1,20 @@
 package io.dagger.modules.mymodule;
 
-import io.dagger.module.annotation.Default;
+import static io.dagger.client.Dagger.dag;
+
+import io.dagger.client.Container;
+import io.dagger.client.DaggerQueryException;
+import io.dagger.client.Directory;
 import io.dagger.module.annotation.Function;
+import io.dagger.module.annotation.Ignore;
 import io.dagger.module.annotation.Object;
+import java.util.concurrent.ExecutionException;
 
 @Object
 public class MyModule {
-  private String greeting;
-  private String name;
-
-  public MyModule() {}
-
-  public MyModule(@Default("Hello") String greeting, @Default("World") String name) {
-    this.greeting = greeting;
-    this.name = name;
-  }
-
   @Function
-  public String message() {
-    return greeting + " " + name;
+  public Container foo(@Ignore({"*", "!src/**/*.java", "!pom.xml"}) Directory source)
+      throws ExecutionException, DaggerQueryException, InterruptedException {
+    return dag().container().from("alpine:latest").withDirectory("/src", source).sync();
   }
 }
