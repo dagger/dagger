a9bc80adac615bf75ed243cb2501e256bf64b5c3
Yves Brissaud <gh@lgtd.io>
2025-03-12 15:56:41 +0100

    directory filters
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- docs/current_docs/api/snippets/fs-filters/pre-call/java/MyModule.java
+++ docs/current_docs/api/snippets/fs-filters/pre-call/java/MyModule.java
@@ -13,7 +13,7 @@ import java.util.concurrent.ExecutionException;
 @Object
 public class MyModule {
   @Function
-  public Container foo(@Ignore({"*", "!src/**/*.java", "!pom.xml"}) Directory source)
+  public Container foo(@Ignore({"*", "!**/*.java", "!pom.xml"}) Directory source)
       throws ExecutionException, DaggerQueryException, InterruptedException {
     return dag().container().from("alpine:latest").withDirectory("/src", source).sync();
   }
