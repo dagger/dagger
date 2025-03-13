fb8e6d862e243d2c28be3fab9e3bd1b01f9dfb7b
Yves Brissaud <gh@lgtd.io>
2025-03-13 11:36:39 +0100

    cover enum with integration tests
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- core/integration/module_java_test.go
+++ core/integration/module_java_test.go
@@ -288,6 +288,62 @@ func (JavaSuite) TestConstructor(_ context.Context, t *testctx.T) {
 	})
 }
 
+func (JavaSuite) TestEnum(_ context.Context, t *testctx.T) {
+	t.Run("can use an enum value", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := javaModule(t, c, "enums").
+			With(daggerCall("print", "--severity=LOW")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "LOW", out)
+	})
+
+	t.Run("can not use a value not defined in the enum", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		_, err := javaModule(t, c, "enums").
+			With(daggerCall("print", "--severity=FOO")).
+			Stdout(ctx)
+
+		require.Error(t, err)
+	})
+
+	t.Run("can return an enum value", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := javaModule(t, c, "enums").
+			With(daggerCall("from-string", "--severity=MEDIUM")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "MEDIUM", out)
+	})
+
+	t.Run("can return a list of enum values", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := javaModule(t, c, "enums").
+			With(daggerCall("get-severities")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "LOW\nMEDIUM\nHIGH\n", out)
+	})
+
+	t.Run("can read list of enum values", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := javaModule(t, c, "enums").
+			With(daggerCall("to-string", "--severities=MEDIUM,LOW")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "MEDIUM,LOW,", out)
+	})
+}
+
 func javaModule(t *testctx.T, c *dagger.Client, moduleName string) *dagger.Container {
 	t.Helper()
 	modSrc, err := filepath.Abs(filepath.Join("./testdata/modules/java", moduleName))
