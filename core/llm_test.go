54c1634db087aa9f7136fb93f496b63d9024f8e3
Marcos Nils <1578458+marcosnils@users.noreply.github.com>
2025-03-13 14:06:25 -0300

    llm: add basic tests to check llm config loading (#9861)
    it checks all the current supported variables are being parsed correctly
    as well as the fact that the file://.env is being parsed also
    
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

---
+++ core/llm_test.go
@@ -0,0 +1,129 @@
+package core
+
+import (
+	"context"
+	"testing"
+
+	"github.com/dagger/dagger/dagql"
+	"github.com/stretchr/testify/assert"
+	"github.com/vektah/gqlparser/v2/ast"
+)
+
+type LlmTestQuery struct {
+}
+
+func (LlmTestQuery) Type() *ast.Type {
+	return &ast.Type{
+		NamedType: "Query",
+		NonNull:   true,
+	}
+}
+
+type mockSecret struct {
+	Plaintext string `field:"true"`
+	uri       string
+}
+
+func (mockSecret) Type() *ast.Type {
+	return &ast.Type{
+		NamedType: "Secret",
+		NonNull:   true,
+	}
+}
+
+func TestLlmConfig(t *testing.T) {
+	q := LlmTestQuery{}
+
+	srv := dagql.NewServer(q)
+
+	vars := map[string]string{
+		"file://.env":                "",
+		"env://ANTHROPIC_API_KEY":    "anthropic-api-key",
+		"env://ANTHROPIC_BASE_URL":   "anthropic-base-url",
+		"env://ANTHROPIC_MODEL":      "anthropic-model",
+		"env://OPENAI_API_KEY":       "openai-api-key",
+		"env://OPENAI_AZURE_VERSION": "openai-azure-version",
+		"env://OPENAI_BASE_URL":      "openai-base-url",
+		"env://OPENAI_MODEL":         "openai-model",
+		"env://GEMINI_API_KEY":       "gemini-api-key",
+		"env://GEMINI_BASE_URL":      "gemini-base-url",
+		"env://GEMINI_MODEL":         "gemini-model",
+	}
+
+	dagql.Fields[LlmTestQuery]{
+		dagql.Func("secret", func(ctx context.Context, self LlmTestQuery, args struct {
+			URI string
+		}) (mockSecret, error) {
+			if _, ok := vars[args.URI]; !ok {
+				t.Errorf("uri not found: %s", args.URI)
+			}
+			return mockSecret{uri: args.URI}, nil
+		}),
+	}.Install(srv)
+
+	dagql.Fields[mockSecret]{
+		dagql.Func("plaintext", func(ctx context.Context, self mockSecret, _ struct{}) (string, error) {
+			return vars[self.uri], nil
+		}),
+	}.Install(srv)
+
+	ctx := context.Background()
+	r, err := NewLlmRouter(ctx, srv)
+	assert.NoError(t, err)
+	assert.Equal(t, "anthropic-api-key", r.AnthropicAPIKey)
+	assert.Equal(t, "anthropic-base-url", r.AnthropicBaseURL)
+	assert.Equal(t, "anthropic-model", r.AnthropicModel)
+	assert.Equal(t, "openai-api-key", r.OpenAIAPIKey)
+	assert.Equal(t, "openai-azure-version", r.OpenAIAzureVersion)
+	assert.Equal(t, "openai-base-url", r.OpenAIBaseURL)
+	assert.Equal(t, "openai-model", r.OpenAIModel)
+	assert.Equal(t, "gemini-api-key", r.GeminiAPIKey)
+	assert.Equal(t, "gemini-base-url", r.GeminiBaseURL)
+	assert.Equal(t, "gemini-model", r.GeminiModel)
+}
+
+func TestLlmConfigEnvFile(t *testing.T) {
+	q := LlmTestQuery{}
+
+	srv := dagql.NewServer(q)
+	dagql.Fields[LlmTestQuery]{
+		dagql.Func("secret", func(ctx context.Context, self LlmTestQuery, args struct {
+			URI string
+		}) (mockSecret, error) {
+			return mockSecret{uri: args.URI}, nil
+		}),
+	}.Install(srv)
+
+	dagql.Fields[mockSecret]{
+		dagql.Func("plaintext", func(ctx context.Context, self mockSecret, _ struct{}) (string, error) {
+			if self.uri == "file://.env" {
+				return `ANTHRIOPIC_API_KEY=anthropic-api-key
+ANTHROPIC_BASE_URL=anthropic-base-url
+ANTHROPIC_MODEL=anthropic-model
+ANTHROPIC_API_KEY=anthropic-api-key
+OPENAI_API_KEY=openai-api-key
+OPENAI_AZURE_VERSION=openai-azure-version
+OPENAI_BASE_URL=openai-base-url
+OPENAI_MODEL=openai-model
+GEMINI_API_KEY=gemini-api-key
+GEMINI_BASE_URL=gemini-base-url
+GEMINI_MODEL=gemini-model`, nil
+			}
+			return "", nil
+		}),
+	}.Install(srv)
+
+	ctx := context.Background()
+	r, err := NewLlmRouter(ctx, srv)
+	assert.NoError(t, err)
+	assert.Equal(t, "anthropic-api-key", r.AnthropicAPIKey)
+	assert.Equal(t, "anthropic-base-url", r.AnthropicBaseURL)
+	assert.Equal(t, "anthropic-model", r.AnthropicModel)
+	assert.Equal(t, "openai-api-key", r.OpenAIAPIKey)
+	assert.Equal(t, "openai-azure-version", r.OpenAIAzureVersion)
+	assert.Equal(t, "openai-base-url", r.OpenAIBaseURL)
+	assert.Equal(t, "openai-model", r.OpenAIModel)
+	assert.Equal(t, "gemini-api-key", r.GeminiAPIKey)
+	assert.Equal(t, "gemini-base-url", r.GeminiBaseURL)
+	assert.Equal(t, "gemini-model", r.GeminiModel)
+}
