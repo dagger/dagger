97913f5f55b5ab97534717c91fed8666bf3a36e7
Alex Suraci <alex@dagger.io>
2025-03-14 13:54:08 -0400

    undo docs regens
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -128,7 +128,15 @@ const config: Config = {
         content:
           "Dagger is an open-source runtime for composable workflows, powering AI agents and CI/CD with modular, repeatable, and observable pipelines.",
       },
-      { property: "og:image", content: `${url}/img/dagger-factory-share.jpg` },
+      {
+        name: "image",
+        property: "og:image",
+        content: `${url}/img/dagger-factory-share.jpg`,
+      },
+      {
+        name: "author",
+        content: "Dagger",
+      },
       {
         property: "twitter:image",
         content: `${url}/img/dagger-factory-share.jpg`,
