da4c19c42daa3714ecd71402366e812460d3deee
Justin Chadwell <me@jedevc.com>
2025-03-19 14:31:17 +0000

    tests: scrub redis memory overcommit warning
    Co-authored-by: Gerhard Lazu <gerhard@dagger.io>
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- dagql/idtui/golden_test.go
+++ dagql/idtui/golden_test.go
@@ -385,6 +385,12 @@ var scrubs = []scrubber{
 		"9.3 kB",
 		"X.X B",
 	},
+	// Memory overcommit warning for redis
+	{
+		regexp.MustCompile(`.+WARNING Memory overcommit.+\n`),
+		"# WARNING Memory overcommit must be enabled! Without it, a background save or replication may fail under low memory condition. Being disabled, it can also cause failures without low memory condition, see https://github.com/jemalloc/jemalloc/issues/1328. To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.\n",
+		"",
+	},
 }
 
 func TestScrubbers(t *testing.T) {
