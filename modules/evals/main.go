bd15eb1b03859e1633297c898a84f105f5207e38
Alex Suraci <suraci.alex@gmail.com>
2025-03-18 20:25:06 -0400

    support running evals across models
    Signed-off-by: Alex Suraci <suraci.alex@gmail.com>

--- modules/evals/main.go
+++ modules/evals/main.go
@@ -4,10 +4,12 @@ import (
 	"dagger/evals/internal/dagger"
 )
 
-type Evals struct{}
+type Evals struct {
+	Model string
+}
 
 func (m *Evals) UndoSingle() *dagger.Container {
-	return dag.Llm().
+	return dag.Llm(dagger.LlmOpts{Model: m.Model}).
 		WithQuery().
 		WithPrompt("give me a container for PHP 7 development").
 		Loop().
@@ -19,7 +21,7 @@ func (m *Evals) UndoSingle() *dagger.Container {
 }
 
 func (m *Evals) BuildMulti() *dagger.File {
-	return dag.Llm().
+	return dag.Llm(dagger.LlmOpts{Model: m.Model}).
 		SetDirectory("repo", dag.Git("https://github.com/vito/booklit").Head().Tree()).
 		SetContainer("ctr", dag.Container().From("golang")).
 		WithPrompt("Mount $repo into $ctr and build ./cmd/booklit").
