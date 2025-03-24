28c5edfd2101ef82ca796b70ce3a85784d9ad939
Connor Braa <connor@dagger.io>
2025-03-24 16:37:33 -0700

    bump to 0.17.1 and regen github configs
    Signed-off-by: Connor Braa <connor@dagger.io>

--- .github/main.go
+++ .github/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	daggerVersion      = "v0.17.0"
+	daggerVersion      = "v0.17.1"
 	upstreamRepository = "dagger/dagger"
 	ubuntuVersion      = "24.04"
 	defaultRunner      = "ubuntu-" + ubuntuVersion
