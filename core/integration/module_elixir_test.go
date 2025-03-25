db5cd4f63e6d319c734adcde4b46279aec489e82
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-03-25 18:40:43 +0700

    test(sdk/elixir): add integration tests and fix bugs (#9742)
    * test(sdk/elixir): add more integration tests to Elixir SDK
    
    * Add optional flag test and fix a bug that it is not working.
    * Add test default path.
    * Add test ignore path.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    * Update .dagger/build/sdk.go
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    * fix(sdk/elixir): remove cache volume in dagger_codegen
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    * fix(sdk/elixir): fix go lint
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    ---------
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- core/integration/module_elixir_test.go
+++ core/integration/module_elixir_test.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 	"testing"
 
+	"dagger.io/dagger"
 	"github.com/dagger/testctx"
 	"github.com/stretchr/testify/require"
 )
@@ -44,10 +45,10 @@ func (ElixirSuite) TestInit(ctx context.Context, t *testctx.T) {
 			With(daggerExec("init", "--name=bare", "--sdk=github.com/dagger/dagger/sdk/elixir"))
 
 		out, err := modGen.
-			With(daggerQuery(`{bare{containerEcho(stringArg:"hello"){stdout}}}`)).
+			With(daggerCall("container-echo", "--string-arg=hello", "stdout")).
 			Stdout(ctx)
 		require.NoError(t, err)
-		require.JSONEq(t, `{"bare":{"containerEcho":{"stdout":"hello\n"}}}`, out)
+		require.Equal(t, "hello\n", out)
 	})
 
 	t.Run("from alias", func(ctx context.Context, t *testctx.T) {
@@ -59,10 +60,10 @@ func (ElixirSuite) TestInit(ctx context.Context, t *testctx.T) {
 			With(daggerExec("init", "--name=bare", "--sdk=elixir"))
 
 		out, err := modGen.
-			With(daggerQuery(`{bare{containerEcho(stringArg:"hello"){stdout}}}`)).
+			With(daggerCall("container-echo", "--string-arg=hello", "stdout")).
 			Stdout(ctx)
 		require.NoError(t, err)
-		require.JSONEq(t, `{"bare":{"containerEcho":{"stdout":"hello\n"}}}`, out)
+		require.Equal(t, "hello\n", out)
 	})
 
 	t.Run("from alias with ref", func(ctx context.Context, t *testctx.T) {
@@ -74,9 +75,123 @@ func (ElixirSuite) TestInit(ctx context.Context, t *testctx.T) {
 			With(daggerExec("init", "--name=bare", "--sdk=elixir@main"))
 
 		out, err := modGen.
-			With(daggerQuery(`{bare{containerEcho(stringArg:"hello"){stdout}}}`)).
+			With(daggerCall("container-echo", "--string-arg=hello", "stdout")).
 			Stdout(ctx)
 		require.NoError(t, err)
-		require.JSONEq(t, `{"bare":{"containerEcho":{"stdout":"hello\n"}}}`, out)
+		require.Equal(t, "hello\n", out)
 	})
+}
+
+func (ElixirSuite) TestOptionalValue(ctx context.Context, t *testctx.T) {
+	t.Run("can run without a value", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("echo-else")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "default value if null", out)
+	})
+
+	t.Run("can set a value", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("echo-else", "--value=foo")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "foo", out)
+	})
+
+	// TODO: ensure that optional works with default value after the https://github.com/dagger/dagger/issues/9744 is resolved.
+}
+
+func (ElixirSuite) TestDefaultPath(ctx context.Context, t *testctx.T) {
+	t.Run("can set a path for a file", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("file-name", "--file=./mix.exs")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "mix.exs", out)
+	})
+
+	t.Run("can use a default path for a file", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("file-name")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "dagger.json", out)
+	})
+
+	t.Run("can use a default path for a dir", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("file-names")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Equal(t, "defaults.ex", out)
+	})
+}
+
+func (ElixirSuite) TestIgnore(ctx context.Context, t *testctx.T) {
+	t.Run("without ignore", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("files-no-ignore")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Contains(t, out, "dagger.json")
+		require.Contains(t, out, "mix.exs")
+	})
+
+	t.Run("with ignore", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("files-ignore")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.Contains(t, out, "dagger.json")
+		require.NotContains(t, out, "mix.exs")
+	})
+
+	t.Run("with negated ignore", func(ctx context.Context, t *testctx.T) {
+		c := connect(ctx, t)
+
+		out, err := elixirModule(t, c, "defaults").
+			With(daggerCall("files-neg-ignore")).
+			Stdout(ctx)
+
+		require.NoError(t, err)
+		require.NotContains(t, out, "dagger.json")
+		require.NotContains(t, out, "mix.exs")
+		require.Contains(t, out, "lib")
+	})
+}
+
+func elixirModule(t *testctx.T, c *dagger.Client, moduleName string) *dagger.Container {
+	t.Helper()
+	modSrc, err := filepath.Abs(filepath.Join("./testdata/modules/elixir", moduleName))
+	require.NoError(t, err)
+
+	sdkSrc, err := filepath.Abs("../../sdk/elixir")
+	require.NoError(t, err)
+
+	return goGitBase(t, c).
+		WithDirectory("modules/"+moduleName, c.Host().Directory(modSrc)).
+		WithDirectory("sdk/elixir", c.Host().Directory(sdkSrc)).
+		WithWorkdir("/work/modules/" + moduleName)
 }
