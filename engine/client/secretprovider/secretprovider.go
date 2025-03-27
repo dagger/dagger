0654ccc6d9f50c600923bc4ce599450ea8ad28f4
Rajat Jindal <rajatjindal83@gmail.com>
2025-03-27 21:29:28 +0530

    feat(engine/secret): add support for ttl for vault
    Signed-off-by: Rajat Jindal <rajatjindal83@gmail.com>

--- engine/client/secretprovider/secretprovider.go
+++ engine/client/secretprovider/secretprovider.go
@@ -4,13 +4,20 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"net/url"
 	"strings"
+	"time"
 
 	"github.com/moby/buildkit/session/secrets"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
+
+type dataWithTTL struct {
+	expiresAt time.Time
+	data      map[string]any
+}
 
 type SecretResolver func(context.Context, string) ([]byte, error)
 
@@ -19,19 +26,45 @@ var resolvers = map[string]SecretResolver{
 	"file":  fileProvider,
 	"cmd":   cmdProvider,
 	"op":    opProvider,
-	"vault": vaultProvider,
+	"vault": getVaultProvider(0), // no ttl for backward compatibility
 }
 
-func ResolverForID(id string) (SecretResolver, string, error) {
-	scheme, path, ok := strings.Cut(id, "://")
-	if !ok {
-		return nil, "", fmt.Errorf("parse %q: malformed id", id)
+func getSecretResolverWithTTL(scheme string, ttl time.Duration) (SecretResolver, error) {
+	if scheme == "vault" && ttl > 0 {
+		return getVaultProvider(ttl), nil
 	}
 
 	resolver, ok := resolvers[scheme]
 	if !ok {
-		return nil, "", fmt.Errorf("unsupported secret provider: %q", scheme)
+		return nil, fmt.Errorf("unsupported secret provider: %q", scheme)
 	}
+
+	return resolver, nil
+}
+
+func ResolverForID(id string) (SecretResolver, string, error) {
+	parsed, err := url.Parse(id)
+	if err != nil {
+		return nil, "", fmt.Errorf("parse %q: malformed id. err: %w", id, err)
+	}
+
+	scheme := parsed.Scheme
+	path := parsed.Path
+
+	var ttl time.Duration
+	ttlStr := strings.TrimSpace(parsed.Query().Get("ttl"))
+	if ttlStr != "" {
+		ttl, err = time.ParseDuration(ttlStr)
+		if err != nil {
+			return nil, "", fmt.Errorf("invalid ttl %q provided for secret %q. err: %w", ttlStr, id, err)
+		}
+	}
+
+	resolver, err := getSecretResolverWithTTL(scheme, ttl)
+	if err != nil {
+		return nil, "", err
+	}
+
 	return resolver, path, nil
 }
 
