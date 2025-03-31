2596a949ccd5f3eebd1209ff7a7d53a5a2c81e90
kpenfound <kyle@dagger.io>
2025-03-31 18:53:06 +0100

    clean up llm api page with new Env information
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/agents/snippets/coding-agent/go/main.go
+++ docs/current_docs/agents/snippets/coding-agent/go/main.go
@@ -11,17 +11,16 @@ func (m *CodingAgent) GoProgram(
 	// The programming assignment, e.g. "write me a curl clone"
 	assignment string,
 ) *dagger.Container {
-	result := dag.LLM().
-		WithToyWorkspace(dag.ToyWorkspace()).
-		WithPromptVar("assignment", assignment).
+	environment := dag.Env().
+		WithToyWorkspaceInput("before", dag.Workspace(), "these are the tools to complete the task").
+		WithStringInput("assignment", assignment, "this is the assignment, complete it").
+		WantToyWorkspaceOutput("after")
+
+	return dag.LLM().
+		WithEnv(environment).
 		WithPrompt(`
 			You are an expert go programmer. You have access to a workspace.
 			Use the default directory in the workspace.
-			Do not stop until the code builds.
-			Do not use the container.
-			Complete the assignment: $assignment
-			`).
-		ToyWorkspace().
-		Container()
-	return result
+			Do not stop until the code builds.`).
+		Env().Output("after").AsWorkspace().Container()
 }
