01c1c816779b6c12a81ee107e508997de39beb2f
kpenfound <kyle@dagger.io>
2025-03-31 18:59:57 +0100

    better toy-workspace input
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/agents/snippets/coding-agent/go/main.go
+++ docs/current_docs/agents/snippets/coding-agent/go/main.go
@@ -11,8 +11,9 @@ func (m *CodingAgent) GoProgram(
 	// The programming assignment, e.g. "write me a curl clone"
 	assignment string,
 ) *dagger.Container {
+	workspace := dag.ToyWorkspace()
 	environment := dag.Env().
-		WithToyWorkspaceInput("before", dag.Workspace(), "these are the tools to complete the task").
+		WithToyWorkspaceInput("before", workspace, "these are the tools to complete the task").
 		WithStringInput("assignment", assignment, "this is the assignment, complete it").
 		WantToyWorkspaceOutput("after")
 
