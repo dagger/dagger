9d31920366979f2600cbf5391c7fb9f9dbb4bb35
kpenfound <kyle@dagger.io>
2025-04-01 12:33:37 +0100

    (failing) update coding-agent java v0.18.0
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/agents/snippets/coding-agent/java/src/main/java/io/dagger/modules/codingagent/CodingAgent.java
+++ docs/current_docs/agents/snippets/coding-agent/java/src/main/java/io/dagger/modules/codingagent/CodingAgent.java
@@ -3,10 +3,10 @@ package io.dagger.modules.codingagent;
 import static io.dagger.client.Dagger.dag;
 
 import io.dagger.client.Container;
-import io.dagger.client.DaggerQueryException;
+import io.dagger.client.ToyWorkspace;
+import io.dagger.client.Env;
 import io.dagger.module.annotation.Function;
 import io.dagger.module.annotation.Object;
-import java.util.concurrent.ExecutionException;
 
 /** CodingAgent main object */
 @Object
@@ -14,18 +14,22 @@ public class CodingAgent {
   /** Write a Go program */
   @Function
   public Container goProgram(String assignment) {
+    ToyWorkspace workspace = dag.toyWorkspace();
+    Env environment = dag.env()
+        .withToyWorkspaceInput("before", workspace, "these are the tools to complete the task")
+        .withStringInput("assignment", assignment, "this is the assignment, complete it")
+        .withStringOutput("after", "the ToyWorkspace with the completed assignment");
     return dag()
       .llm()
-      .withToyWorkspace(dag.toyWorkspace())
-      .withPromptVar("assignment", assignment)
+      .withEnv(environment)
       .withPrompt("""
         You are an expert go programmer. You have access to a workspace.
         Use the default directory in the workspace.
         Do not stop until the code builds.
-        Do not use the container.
-        Complete the assignment: $assignment
         """)
-      .toyWorkspace()
+      .env()
+      .output("after")
+      .asToyWorkspace()
       .container();
   }
 }
