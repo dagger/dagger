cf0b7f80f52ec3320612c6be3b0472eeecbfa5c0
kpenfound <kyle@dagger.io>
2025-04-01 12:08:43 +0100

    udpate coding-agent python v0.18.0
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/agents/snippets/coding-agent/python/src/coding_agent/main.py
+++ docs/current_docs/agents/snippets/coding-agent/python/src/coding_agent/main.py
@@ -1,3 +1,4 @@
+
 import dagger
 from dagger import dag, function, object_type
 
@@ -5,20 +6,34 @@ from dagger import dag, function, object_type
 @object_type
 class CodingAgent:
     @function
-    def go_program(self, assignment: str) -> dagger.Container:
+    def go_program(
+        self,
+        assignment: str,
+    ) -> dagger.Container:
         """Write a Go program"""
-        result = (
+        workspace = dag.toy_workspace()
+        environment = (
+            dag.env()
+            .with_toy_workspace_input(
+                "before", workspace, "these are the tools to complete the task"
+            )
+            .with_string_input("assignment", assignment, "this is the assignment, complete it")
+            .with_toy_workspace_output(
+                "after", "the ToyWorkspace with the completed assignment"
+            )
+        )
+
+        return (
             dag.llm()
-            .with_toy_workspace(dag.toy_workspace())
-            .with_prompt_var("assignment", assignment)
-            .with_prompt("""
-                You are an expert go programmer. You have access to a workspace.
-                Use the default directory in the workspace.
-                Do not stop until the code builds.
-                Do not use the container.
-                Complete the assignment: $assignment
-                """)
-            .toy_workspace()
+            .with_env(environment)
+            .with_prompt(
+                """
+            You are an expert go programmer. You have access to a workspace.
+            Use the default directory in the workspace.
+            Do not stop until the code builds."""
+            )
+            .env()
+            .output("after")
+            .as_toy_workspace()
             .container()
         )
-        return result
