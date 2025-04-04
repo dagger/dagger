7af014ff27fde5523d246a933735886b049af2b6
Marcos Lilljedahl <marcosnils@gmail.com>
2025-04-04 12:03:17 -0300

    chore: bump dependencies to v0.18.2
    Signed-off-by: Marcos Lilljedahl <marcosnils@gmail.com>

--- sdk/go/engineconn/version.gen.go
+++ sdk/go/engineconn/version.gen.go
@@ -2,4 +2,4 @@
 
 package engineconn
 
-const CLIVersion = "0.18.1"
+const CLIVersion = "0.18.2"
--- sdk/rust/crates/dagger-sdk/src/core/version.rs
+++ sdk/rust/crates/dagger-sdk/src/core/version.rs
@@ -1,1 +1,1 @@
-pub const DAGGER_ENGINE_VERSION: &'static str = "0.18.1";
+pub const DAGGER_ENGINE_VERSION: &'static str = "0.18.2";
