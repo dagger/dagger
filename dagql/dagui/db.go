205216c71681e986b094863e5f8623670caf7252
Alex Suraci <alex@dagger.io>
2025-04-13 20:51:48 -0400

    tui: support making failures less spammy
    * encapsulating spans won't surface errors beneath them
    * support -1 verbosity level which keeps errors from accumulating
    
    unrelated, untested: show passthrough spans with --debug
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/dagui/db.go
+++ dagql/dagui/db.go
@@ -910,7 +910,7 @@ func (db *DB) CollectErrors(rows *RowsView) []*TraceTree {
 		if failed {
 			reveal[tree] = struct{}{}
 		}
-		if failed || tree.Span.IsUnset() {
+		if (failed || tree.Span.IsUnset()) && !tree.Span.Encapsulate {
 			for _, child := range tree.Children {
 				collect(child)
 			}
