89cae49e31241b569517ae40daccda036430d0d8
fivecut <fivecut@126.com>
2025-04-16 14:50:10 +0800

    chore: fix some function names in comment
    Signed-off-by: fivecut <fivecut@126.com>

--- .dagger/util/prerelease.go
+++ .dagger/util/prerelease.go
@@ -6,7 +6,7 @@ import (
 	"golang.org/x/mod/semver"
 )
 
-// prereleaseVariants takes a version with a prerelease, and returns variants
+// PrereleaseVariants takes a version with a prerelease, and returns variants
 // of it that should be aliased to the original one.
 // Example: v0.17.0-foo.1.2.3 -> [v0.17.0-foo.1.2.3, v0.17.0-foo.1.2, v0.17.0-foo.1, v0.17.0-foo]
 func PrereleaseVariants(version string) (results []string) {
