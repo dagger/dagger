13c29a1fdaa68bfdd30c8a66436ab7ebbcf7c905
Connor Braa <connor@dagger.io>
2025-04-28 11:56:27 -0700

    even more aggressively discard logs, we don't even want them showing on -vv because they're very red-herring-y
    Signed-off-by: Connor Braa <connor@dagger.io>

--- cmd/dagger/shell.go
+++ cmd/dagger/shell.go
@@ -216,15 +216,8 @@ func (h *shellCallHandler) Initialize(ctx context.Context) error {
 	}
 
 	// silently attempt to initialize llm to populate $agent.
-	// swallow errors, we'll try again and error if the user attempts to use it.
-	agentInitCtx, span := Tracer().Start(
-		ctx,
-		"attempting to set $agent",
-		telemetry.Encapsulated(),
-		telemetry.Encapsulate(),
-	)
-	_, _ = h.llm(agentInitCtx)
-	span.End()
+	// swallow errors, discard logs.
+	_, _ = h.llm(context.Background())
 
 	h.initwd = *wd
 	h.wd = h.initwd
