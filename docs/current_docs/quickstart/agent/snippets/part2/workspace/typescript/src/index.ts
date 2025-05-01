b664b036c759aeaa9073d008f6e9496f68f10d12
kpenfound <kyle@dagger.io>
2025-05-01 18:14:58 -0400

    test typescript
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/quickstart/agent/snippets/part2/workspace/typescript/src/index.ts
+++ docs/current_docs/quickstart/agent/snippets/part2/workspace/typescript/src/index.ts
@@ -1,4 +1,4 @@
-import { dag, object, func, Directory } from "@dagger.io/dagger"
+import { dag, object, func, Directory } from '@dagger.io/dagger'
 
 @object()
 export class Workspace {
@@ -9,49 +9,44 @@ export class Workspace {
   }
 
   /**
-  * Read a file in the Workspace
-  *
-  * @param path The path to the file in the workspace
-  */
+   * Read a file in the Workspace
+   *
+   * @param path The path to the file in the workspace
+   */
   @func()
-  async readFile(
-    path: string,
-  ): Promise<string> {
+  async readFile(path: string): Promise<string> {
     return await this.source.file(path).contents()
   }
 
   /**
-  * Write a file to the Workspace
-  *
-  * @param path The path to the file in the workspace
-  * @param contents The new contents of the file
-  */
+   * Write a file to the Workspace
+   *
+   * @param path The path to the file in the workspace
+   * @param contents The new contents of the file
+   */
   @func()
-  writeFile(
-    path: string,
-    contents: string,
-  ): Workspace {
+  writeFile(path: string, contents: string): Workspace {
     this.source = this.source.withNewFile(path, contents)
     return this
   }
 
   /**
-  * List all of the files in the Workspace
-  */
+   * List all of the files in the Workspace
+   */
   @func()
   async listFiles(): Promise<string> {
     return await dag
       .container()
-      .from("alpine:3")
-      .withDirectory("/src", this.source)
-      .withWorkdir("/src")
-      .withExec(["tree", "./src"])
+      .from('alpine:3')
+      .withDirectory('/src', this.source)
+      .withWorkdir('/src')
+      .withExec(['tree', './src'])
       .stdout()
   }
 
   /**
-  * Get the source code directory from the Workspace
-  */
+   * Get the source code directory from the Workspace
+   */
   @func()
   getSource(): Directory {
     return this.source
