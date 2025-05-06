f2e0b6117fc95df5b4386461e0e364403135286c
kpenfound <kyle@dagger.io>
2025-05-06 13:31:37 -0400

    test php flow
    Signed-off-by: kpenfound <kyle@dagger.io>

--- docs/current_docs/quickstart/agent/snippets/part2/workspace/php/src/Workspace.php
+++ docs/current_docs/quickstart/agent/snippets/part2/workspace/php/src/Workspace.php
@@ -12,50 +12,49 @@ use function Dagger\dag;
 #[Doc('A module for editing code')]
 class Workspace
 {
-    #[Doc('The source directory')]
-    public Directory $source;
+  #[Doc('The source directory')]
+  public Directory $source;
 
-    public function __construct(
-        Directory $source
-    ) {
-        $this->source = $source;
-    }
+  #[DaggerFunction]
+  public function __construct(Directory $source)
+  {
+    $this->source = $source;
+  }
 
-    #[DaggerFunction]
-    #[Doc('Read a file in the Workspace')]
-    public function readFile(
-        #[Doc('The path to the file in the workspace')]
-        string $path
-    ): string {
-        return $this->source->file($path)->contents();
-    }
+  #[DaggerFunction]
+  #[Doc('Read a file in the Workspace')]
+  public function readFile(#[Doc('The path to the file in the workspace')] string $path): string
+  {
+    return $this->source->file($path)->contents();
+  }
 
-    #[DaggerFunction]
-    #[Doc('Write a file to the Workspace')]
-    public function writeFile(
-        #[Doc('The path to the file in the workspace')]
-        string $path,
-        #[Doc('The new contents of the file')]
-        string $contents
-    ): Workspace {
-        $this->source = $this->source->withNewFile($path, $contents);
-        return $this;
-    }
+  #[DaggerFunction]
+  #[Doc('Write a file to the Workspace')]
+  public function writeFile(
+    #[Doc('The path to the file in the workspace')] string $path,
+    #[Doc('The new contents of the file')] string $contents
+  ): Workspace {
+    $this->source = $this->source->withNewFile($path, $contents);
+    return $this;
+  }
 
-    #[DaggerFunction]
-    #[Doc('List all of the files in the Workspace')]
-    public function listFiles(): string {
-        return dag()->container()
-            ->from('alpine:3')
-            ->withDirectory('/src', $this->source)
-            ->withWorkdir('/src')
-            ->withExec(['tree', './src'])
-            ->stdout();
-    }
+  #[DaggerFunction]
+  #[Doc('List all of the files in the Workspace')]
+  public function listFiles(): string
+  {
+    return dag()
+      ->container()
+      ->from('alpine:3')
+      ->withDirectory('/src', $this->source)
+      ->withWorkdir('/src')
+      ->withExec(['tree', './src'])
+      ->stdout();
+  }
 
-    #[DaggerFunction]
-    #[Doc('Get the source code directory from the Workspace')]
-    public function getSource(): Directory {
-        return $this->source;
-    }
+  #[DaggerFunction]
+  #[Doc('Get the source code directory from the Workspace')]
+  public function getSource(): Directory
+  {
+    return $this->source;
+  }
 }
