82da8b6abea4ee147965fa9bedf4c3d6994286f9
Alex Suraci <alex@dagger.io>
2025-05-07 19:17:24 -0400

    llm: keep LLM meta logs out of LLM <logs>
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- core/llm.go
+++ core/llm.go
@@ -534,6 +534,7 @@ func (llm *LLM) WithPrompt(
 		ctx, span := Tracer(ctx).Start(ctx, "LLM prompt", telemetry.Reveal(), trace.WithAttributes(
 			attribute.String(telemetry.UIActorEmojiAttr, "🧑"),
 			attribute.String(telemetry.UIMessageAttr, "sent"),
+			attribute.Bool("dagger.io/llm", true),
 		))
 		defer span.End()
 		stdio := telemetry.SpanStdio(ctx, InstrumentationLibrary,
@@ -737,7 +738,13 @@ func (llm *LLM) loop(ctx context.Context, dag *dagql.Server) error {
 		client := llm.Endpoint.Client
 		err = backoff.Retry(func() error {
 			var sendErr error
+			ctx, span := Tracer(ctx).Start(ctx, "LLM query", telemetry.Reveal(), trace.WithAttributes(
+				attribute.String(telemetry.UIActorEmojiAttr, "🤖"),
+				attribute.String(telemetry.UIMessageAttr, "received"),
+				attribute.Bool("dagger.io/llm", true),
+			))
 			res, sendErr = client.SendQuery(ctx, messagesToSend, tools)
+			telemetry.End(span, func() error { return sendErr })
 			if sendErr != nil {
 				var finished *ModelFinishedError
 				if errors.As(sendErr, &finished) {
