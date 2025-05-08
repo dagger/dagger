18b4a9bce24e8f8e9965d8aa8b061b547473677c
Vikram Vaswani <vikram@dagger.io>
2025-05-08 23:02:27 +0530

    Fixed linter
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

--- docs/current_docs/cookbook/snippets/load-image-host/typescript/index.ts
+++ docs/current_docs/cookbook/snippets/load-image-host/typescript/index.ts
@@ -5,14 +5,14 @@ class MyModule {
   @func()
   async load(docker: Socket, tag: string): Promise<Container> {
     // create a new container
-    const ctr = dag.container()
-      .from("alpine")
+    const ctr = dag.container().from("alpine")
       .withExec(["apk", "add", "git"])
 
     // create a new container from the docker CLI image
     // mount the Docker socket from the host
     // mount the newly-built container as a tarball
-    const dockerCli = dag.container()
+    const dockerCli = dag
+      .container()
       .from("docker:cli")
       .withUnixSocket("/var/run/docker.sock", docker)
       .withMountedFile("image.tar", ctr.asTarball())
@@ -23,9 +23,7 @@ class MyModule {
       .stdout()
 
     // add the tag to the image
-    const image = out.split(/:(.+)/)[1].trim();
-    return await dockerCli
-      .withExec(["docker", "tag", image, tag])
-      .sync()
+    const image = out.split(/:(.+)/)[1].trim()
+    return await dockerCli.withExec(["docker", "tag", image, tag]).sync()
   }
 }
