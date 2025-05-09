0183dd00e9795a951b6734b36e7cc82977150af6
Yves Brissaud <gh@lgtd.io>
2025-05-09 10:57:05 +0200

    core: allow to define a moduleTypeDefs function
    This is an optional new entry point on the module SDK.
    It's designed to perform the type definition from the module like
    the actual moduleRuntime when an empty string is passed as argument.
    
    If the function doesn't exist, the moduleRuntime is used instead.
    
    But this allows to decouple both of them so we can registry types
    of a module without the runnable module itself because to build it
    needs the types defined by the module
    
    Signed-off-by: Yves Brissaud <gh@lgtd.io>

--- core/sdk/module_runtime.go
+++ core/sdk/module_runtime.go
@@ -14,6 +14,31 @@ type runtimeModule struct {
 	mod *module
 }
 
+func (sdk *runtimeModule) HasModuleTypeDefs() bool {
+	_, ok := sdk.mod.funcs["moduleTypeDefs"]
+	return ok
+}
+
+func (sdk *runtimeModule) TypeDefs(
+	ctx context.Context,
+	deps *core.ModDeps,
+	source dagql.ObjectResult[*core.ModuleSource],
+) (inst dagql.ObjectResult[*core.Container], rerr error) {
+	if !sdk.HasModuleTypeDefs() {
+		return sdk.Runtime(ctx, deps, source)
+	}
+
+	ctx, span := core.Tracer(ctx).Start(ctx, "module SDK: load typedefs")
+	defer telemetry.End(span, func() error { return rerr })
+
+	schemaJSONFile, err := deps.SchemaIntrospectionJSONFile(ctx, []string{"Host"})
+	if err != nil {
+		return inst, fmt.Errorf("failed to get schema introspection json during %s module sdk runtime: %w", sdk.mod.mod.Self().Name(), err)
+	}
+
+	return sdk.callModuleFn(ctx, "moduleTypeDefs", source, schemaJSONFile)
+}
+
 func (sdk *runtimeModule) Runtime(
 	ctx context.Context,
 	deps *core.ModDeps,
@@ -22,19 +47,28 @@ func (sdk *runtimeModule) Runtime(
 	ctx, span := core.Tracer(ctx).Start(ctx, "module SDK: load runtime")
 	defer telemetry.End(span, func() error { return rerr })
 
-	dag, err := sdk.mod.dag(ctx)
-	if err != nil {
-		return inst, fmt.Errorf("failed to get dag for sdk module %s: %w", sdk.mod.mod.Self().Name(), err)
-	}
-
 	schemaJSONFile, err := deps.SchemaIntrospectionJSONFile(ctx, []string{"Host"})
 	if err != nil {
 		return inst, fmt.Errorf("failed to get schema introspection json during %s module sdk runtime: %w", sdk.mod.mod.Self().Name(), err)
 	}
 
+	return sdk.callModuleFn(ctx, "moduleRuntime", source, schemaJSONFile)
+}
+
+func (sdk *runtimeModule) callModuleFn(
+	ctx context.Context,
+	fnName string,
+	source dagql.ObjectResult[*core.ModuleSource],
+	schemaJSONFile dagql.Result[*core.File],
+) (inst dagql.ObjectResult[*core.Container], rerr error) {
+	dag, err := sdk.mod.dag(ctx)
+	if err != nil {
+		return inst, fmt.Errorf("failed to get dag for sdk module %s: %w", sdk.mod.mod.Self().Name(), err)
+	}
+
 	err = dag.Select(ctx, sdk.mod.sdk, &inst,
 		dagql.Selector{
-			Field: "moduleRuntime",
+			Field: fnName,
 			Args: []dagql.NamedInput{
 				{
 					Name:  "modSource",
