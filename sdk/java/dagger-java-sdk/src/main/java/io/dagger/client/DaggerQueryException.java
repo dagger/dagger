c7167260cc0e3ce6561c2ebd7d00db28955227e3
robertonav20 <roberto.nav20@gmail.com>
2025-05-15 00:19:01 +0000

    [#10398] DQE - added method to enanche error message
    Signed-off-by: robertonav20 <roberto.nav20@gmail.com>

--- sdk/java/dagger-java-sdk/src/main/java/io/dagger/client/DaggerQueryException.java
+++ sdk/java/dagger-java-sdk/src/main/java/io/dagger/client/DaggerQueryException.java
@@ -1,10 +1,21 @@
 package io.dagger.client;
 
-import io.smallrye.graphql.client.GraphQLError;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.stream.Collectors;
+import io.smallrye.graphql.client.GraphQLError;
 
 public class DaggerQueryException extends Exception {
+
+  private static final String ENANCHED_MESSAGE =
+      "Message: [%s]\nPath: [%s]\\Type Code: [%s]\nExit Code: [%s]\\Cmd: [%s]\n";
+  private static final String FULL_MESSAGE =
+      "Message: [%s]\nPath: [%s]\\Type Code: [%s]\nExit Code: [%s]\\Cmd: [%s]\\STDERR: [%s] \n";
+
+  protected static final String TYPE_KEY = "_type";
+  protected static final String EXIT_CODE_KEY = "exitCode";
+  protected static final String CMD_KEY = "cmd";
+  protected static final String STDERR_KEY = "stderr";
 
   private GraphQLError[] errors;
 
@@ -20,5 +31,30 @@ public class DaggerQueryException extends Exception {
 
   public GraphQLError[] getErrors() {
     return errors;
+  }
+
+  public String toEnanchedMessage() {
+    return Arrays.stream(errors)
+        .map(e -> String.format(ENANCHED_MESSAGE, e.getMessage(),
+            Arrays.stream(e.getPath()).reduce((a, b) -> ((String) a) + ((String) b)),
+            e.getExtensions().getOrDefault(TYPE_KEY, null),
+            e.getExtensions().getOrDefault(EXIT_CODE_KEY, null),
+            Arrays.stream(
+                (Object[]) e.getExtensions().getOrDefault(EXIT_CODE_KEY, Collections.emptyList()))
+                .reduce((a, b) -> ((String) a) + ((String) b))))
+        .collect(Collectors.joining("\n"));
+  }
+
+  public String toFullMessage() {
+    return Arrays.stream(errors)
+        .map(e -> String.format(FULL_MESSAGE, e.getMessage(),
+            Arrays.stream(e.getPath()).reduce((a, b) -> ((String) a) + ((String) b)),
+            e.getExtensions().getOrDefault(TYPE_KEY, null),
+            e.getExtensions().getOrDefault(EXIT_CODE_KEY, null),
+            Arrays.stream(
+                (Object[]) e.getExtensions().getOrDefault(CMD_KEY, Collections.emptyList()))
+                .reduce((a, b) -> ((String) a) + ((String) b)),
+            e.getExtensions().getOrDefault(STDERR_KEY, null)))
+        .collect(Collectors.joining("\n"));
   }
 }
