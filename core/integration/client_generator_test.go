ba142b8a8b76b34ab325857f152a183b48323fb5
Tom Chauveau <tom@dagger.io>
2025-05-21 20:24:46 +0200

    feat: add missmatch version test
    Signed-off-by: Tom Chauveau <tom@dagger.io>

--- core/integration/client_generator_test.go
+++ core/integration/client_generator_test.go
@@ -1338,3 +1338,54 @@ main()`)
 		})
 	}
 }
+
+func (ClientGeneratorTest) TestMissmatchDependencyVersion(ctx context.Context, t *testctx.T) {
+	c := connect(ctx, t)
+
+	moduleSrc := c.Container().From(golangImage).
+		WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
+		WithWorkdir("/work").
+		WithEnvVariable("_EXPERIMENTAL_DAGGER_CLI_BIN", "/bin/dagger").
+		With(nonNestedDevEngine(c)).
+		With(daggerNonNestedExec("init")).
+		With(daggerNonNestedExec("install", "github.com/shykes/hello@2d789671a44c4d559be506a9bc4b71b0ba6e23c9")).
+		WithExec([]string{"go", "mod", "init", "test.com/test"}).
+		WithNewFile("main.go", `package main
+		
+		import (
+			"context"
+			"fmt"
+			"os"
+		
+			"test.com/test/dagger"
+		)
+		
+		func main() {
+			ctx := context.Background()
+		
+			dag, err := dagger.Connect(ctx)
+      if err != nil {
+			  fmt.Println(err)
+				os.Exit(0)
+      }
+		
+			res, err := dag.Hello().Hello(ctx)
+			if err != nil {
+				panic(err)
+			}
+		
+			fmt.Println("result:", res)
+		}
+		`,
+		).
+		With(daggerClientInstall("go")).
+		WithExec([]string{"apk", "add", "jq"}).
+		// Update the dagger.json manually to not rettrigger the generation so we can verify that it triggers an error
+		// on execute
+		WithExec([]string{"sh", "-c", `sh -c 'cat dagger.json | jq '\''(.dependencies[] | select(.name == "hello") | .source) = "github.com/shykes/hello@main" | (.dependencies[] | select(.name == "hello") | .pin) = "main"'\'' > dagger.tmp && mv dagger.tmp dagger.json'`})
+
+	out, err := moduleSrc.With(daggerNonNestedRun("go", "run", "main.go")).Stdout(ctx)
+
+	require.NoError(t, err)
+	require.Contains(t, out, "error serving dependency hello: module hello")
+}
