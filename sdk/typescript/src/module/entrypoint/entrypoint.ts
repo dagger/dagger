03d225b0a4bc92c6145833cbe71affb5c9e14bab
Tom Chauveau <tom@dagger.io>
2025-05-26 14:31:59 +0200

    chore: serialize error object field in the returned error
    Signed-off-by: Tom Chauveau <tom@dagger.io>

--- sdk/typescript/src/module/entrypoint/entrypoint.ts
+++ sdk/typescript/src/module/entrypoint/entrypoint.ts
@@ -1,4 +1,5 @@
-import { dag } from "../../api/client.gen.js"
+import { dag, Error as DaggerError } from "../../api/client.gen.js"
+import type { JSON } from "../../api/client.gen.js"
 import { connection } from "../../connect.js"
 import { Executor, Args } from "../executor.js"
 import { scan } from "../introspector/index.js"
@@ -7,7 +8,7 @@ import { load } from "./load.js"
 import { register } from "./register.js"
 
 export async function entrypoint(files: string[]) {
-  // Start a Dagger session to get the cƒall context
+  // Start a Dagger session to get the call context
   await connection(
     async () => {
       const fnCall = dag.currentFunctionCall()
@@ -46,8 +47,8 @@ export async function entrypoint(files: string[]) {
             fnArgs: args,
           })
         } catch (e: unknown) {
-          await fnCall.returnError(dag.error(stringifyError(e)))
-          return
+          await fnCall.returnError(formatError(e))
+          process.exit(1)
         }
       }
 
@@ -69,7 +70,7 @@ export async function entrypoint(files: string[]) {
  * Take the error thrown by the user module and stringify it so it can
  * be returned to Dagger.
  *
- * If the error is an instande of Error, we stringify the message and the cause.
+ * If the error is an instance of Error, we stringify the message and the cause.
  * If the error is not an instance of Error (which is the case for unexpected errors happening
  * inside the code), we try to stringify it, if it fails we convert it to a string.
  *
@@ -77,19 +78,33 @@ export async function entrypoint(files: string[]) {
  * If the stringify fails, it will fails when calling `returnError` and should
  * still be displayed to the user and Cloud.
  */
-function stringifyError(e: unknown): string {
+function formatError(e: unknown): DaggerError {
   if (e instanceof Error) {
-    let error = `${e.name}: ${e.message}`
-    if (e.cause) {
-      error += `: ${e.cause}`
-    }
+    let error = dag.error(e.message)
+
+    Object.entries(e).map(([field, value]) => {
+      let serializedValue: string | null = null
+      if (value !== undefined && value !== null) {
+        try {
+          serializedValue = JSON.stringify(value)
+        } catch {
+          serializedValue = String(value)
+        }
+      }
+
+      if (serializedValue === null) {
+        return
+      }
+
+      error = error.withValue(field, serializedValue as JSON)
+    })
 
     return error
   }
 
   try {
-    return JSON.stringify(e)
+    return dag.error(JSON.stringify(e))
   } catch {
-    return String(e)
+    return dag.error(String(e))
   }
 }
