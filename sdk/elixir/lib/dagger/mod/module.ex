06d72da069653c2f79d76d4d66a32bc57ed9ccc2
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2025-05-31 14:19:50 +0700

    feat(sdk/elixir): add support for constructor function (#10446)
    * feat(sdk/elixir): add support for constructor function
    
    Once `init` function is define, the SDK turns that function into
    constructor function.
    
    Closes #10437
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    * refactor(sdk/elixir): refactoring tests
    
    Breakdown ObjectMod test module into multiple modules to make it easy to
    test.
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    * refactor(sdk/elixir): elimiate ObjectMod
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
    
    ---------
    
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- sdk/elixir/lib/dagger/mod/module.ex
+++ sdk/elixir/lib/dagger/mod/module.ex
@@ -28,6 +28,20 @@ defmodule Dagger.Mod.Module do
     |> Dagger.TypeDef.with_object(Helper.camelize(mod_name))
     |> then(&define_fields(&1, dag, module))
     |> then(&define_functions(&1, dag, module))
+    |> then(&define_constructor(&1, dag, module))
+  end
+
+  defp define_constructor(type_def, dag, module) do
+    case Enum.find(module.__object__(:functions), &init?/1) do
+      nil ->
+        type_def
+
+      {name, fun_def} ->
+        type_def
+        |> Dagger.TypeDef.with_constructor(
+          FunctionDef.to_dag_function(fun_def, name, module, dag)
+        )
+    end
   end
 
   defp define_fields(type_def, dag, module) do
@@ -39,8 +53,12 @@ defmodule Dagger.Mod.Module do
 
   defp define_functions(type_def, dag, module) do
     module.__object__(:functions)
+    |> Enum.reject(&init?/1)
     |> Enum.reduce(type_def, fn {name, fun_def}, type_def ->
       FunctionDef.define(fun_def, name, module, type_def, dag)
     end)
   end
+
+  defp init?({:init, _}), do: true
+  defp init?({_, _}), do: false
 end
