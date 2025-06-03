06f541245fac227cb2ecdea1209fad867b628d4b
Alex Couture-Beil <alex@mofo.ca>
2025-06-03 12:46:59 -0700

    chore: reuse connection in TestSymlinkCaching
    reuse the connection used in TestSymlinkCaching to prevent prunning from
    occuring while the test is running.
    
    Signed-off-by: Alex Couture-Beil <alex@mofo.ca>

--- core/integration/container_test.go
+++ core/integration/container_test.go
@@ -5109,34 +5109,29 @@ func (ContainerSuite) TestSymlink(ctx context.Context, t *testctx.T) {
 }
 
 func (ContainerSuite) TestSymlinkCaching(ctx context.Context, t *testctx.T) {
-	c1 := connect(ctx, t)
-	out1, err := c1.Container().
+	c := connect(ctx, t)
+	out1, err := c.Container().
 		From(alpineImage).
 		WithSymlink("bar", "foo").
 		WithExec([]string{"sh", "-c", "head -c 99 /dev/random | base64 -w0"}).
 		Stdout(ctx)
-	_ = c1.Close()
 
 	require.NoError(t, err)
 	require.Len(t, out1, 132) // test that 99 chars were randomly produced, this accounts for 4/3 times base64 bloat
 
-	c2 := connect(ctx, t)
-	out2, err := c2.Container().
+	out2, err := c.Container().
 		From(alpineImage).
 		WithSymlink("bar", "foo").
 		WithExec([]string{"sh", "-c", "head -c 99 /dev/random | base64 -w0"}).
 		Stdout(ctx)
-	_ = c2.Close()
 	require.NoError(t, err)
 	require.Equal(t, out1, out2)
 
-	c3 := connect(ctx, t)
-	out3, err := c3.Container().
+	out3, err := c.Container().
 		From(alpineImage).
 		WithSymlink("barf", "oo"). // Note the args here are different, and should bust the cache
 		WithExec([]string{"sh", "-c", "head -c 99 /dev/random | base64 -w0"}).
 		Stdout(ctx)
-	_ = c3.Close()
 	require.NoError(t, err)
 	require.NotEqual(t, out1, out3) // make sure the call to read from /dev/random was re-run
 	require.Len(t, out3, 132)
