1e82a8e469c752fae94bde5d2147d22af7bc2a34
Roberto Navarra <roberto.nav20@gmail.com>
2025-06-06 09:28:41 +0200

    Update MyModule.java
    Signed-off-by: Roberto Navarra <roberto.nav20@gmail.com>

--- docs/current_docs/api/snippets/constructors/java/simple/MyModule.java
+++ docs/current_docs/api/snippets/constructors/java/simple/MyModule.java
@@ -4,5 +4,15 @@ import java.util.concurrent.ExecutionExcep
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import io.dagger.client.Secret;
+import io.dagger.client.exception.DaggerExecException;
 import io.dagger.client.exception.DaggerQueryException;
 import io.dagger.module.annotation.Function;
+
+@Object
+public class MyModule {
+  @Function
+  public Container foo(@Ignore({"*", "!src/**/*.java", "!pom.xml"}) Directory source)
+      throws ExecutionException, DaggerExecException, DaggerQueryException, InterruptedException {
+    return dag().container().from("alpine:latest").withDirectory("/src", source).sync();
+  }
+}
