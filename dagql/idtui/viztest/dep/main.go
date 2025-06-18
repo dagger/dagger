a2d28d17bb4b0a0a027ce07a091c28b3ea054bc8
Alex Suraci <alex@dagger.io>
2025-06-18 09:15:58 -0400

    core: fix error origin bubbling from module functions (#10601)
    * add tests for error origins
    
    Signed-off-by: Alex Suraci <alex@dagger.io>
    
    * fix propagating module function error origins
    
    Signed-off-by: Alex Suraci <alex@dagger.io>
    
    ---------
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/idtui/viztest/dep/main.go
+++ dagql/idtui/viztest/dep/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"dagger/dep/internal/dagger"
+	"errors"
 )
 
 type Dep struct{}
@@ -25,4 +26,14 @@ func (*Dep) FileContents(ctx context.Context, files []*dagger.File) (string, err
 		s += c
 	}
 	return s, nil
+}
+
+// FailingFunction returns a simple error to test error origin stamping
+func (*Dep) FailingFunction() error {
+	return errors.New("this function always fails")
+}
+
+// FailingFunction returns a simple error to test error origin stamping
+func (*Dep) BubblingFunction(ctx context.Context) error {
+	return dag.NestedDep().FailingFunction(ctx)
 }
