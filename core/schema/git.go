4a7e345145470aa540e759f3307eade6008c14ff
Manuel Leonhardt <me@skycaptain.dev>
2025-06-18 14:07:34 +0200

    refactor: unwrap httpAuthUsername to buildin string type
    Signed-off-by: Manuel Leonhardt <me@skycaptain.dev>

--- core/schema/git.go
+++ core/schema/git.go
@@ -147,7 +147,7 @@ type gitArgs struct {
 	SSHKnownHosts string                        `name:"sshKnownHosts" default:""`
 	SSHAuthSocket dagql.Optional[core.SocketID] `name:"sshAuthSocket"`
 
-	HTTPAuthUsername dagql.Optional[dagql.String]  `name:"httpAuthUsername"`
+	HTTPAuthUsername string                        `name:"httpAuthUsername" default:""`
 	HTTPAuthToken    dagql.Optional[core.SecretID] `name:"httpAuthToken"`
 	HTTPAuthHeader   dagql.Optional[core.SecretID] `name:"httpAuthHeader"`
 }
@@ -185,7 +185,7 @@ func (s *gitSchema) git(ctx context.Context, parent dagql.Instance[*core.Query],
 
 	var (
 		sshAuthSock      dagql.Instance[*core.Socket]
-		httpAuthUsername dagql.String
+		httpAuthUsername string
 		httpAuthToken    dagql.Instance[*core.Secret]
 		httpAuthHeader   dagql.Instance[*core.Secret]
 	)
@@ -259,8 +259,8 @@ func (s *gitSchema) git(ctx context.Context, parent dagql.Instance[*core.Query],
 			return inst, fmt.Errorf("SSH URLs are not supported without an SSH socket")
 		}
 	case gitutil.HTTPProtocol, gitutil.HTTPSProtocol:
-		if args.HTTPAuthUsername.Valid {
-			httpAuthUsername = args.HTTPAuthUsername.Value
+		if args.HTTPAuthUsername != "" {
+			httpAuthUsername = args.HTTPAuthUsername
 		}
 
 		if args.HTTPAuthToken.Valid {
@@ -360,10 +360,10 @@ func (s *gitSchema) git(ctx context.Context, parent dagql.Instance[*core.Query],
 				},
 			}
 			// Omit blank username; adding it would change the selector hash and kill cache hits.
-			if httpAuthUsername.String() != "" {
+			if httpAuthUsername != "" {
 				selectArgs = append(selectArgs, dagql.NamedInput{
 					Name:  "httpAuthUsername",
-					Value: dagql.Opt(httpAuthUsername),
+					Value: dagql.Opt(dagql.NewString(httpAuthUsername)),
 				})
 			}
 			if args.KeepGitDir.Valid {
