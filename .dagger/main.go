0ac64ffd375016185ec1594d835ac2bec72c7bc0
Alex Suraci <alex@dagger.io>
2025-06-24 22:01:13 -0400

    lint: ignore all of viztest
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- .dagger/main.go
+++ .dagger/main.go
@@ -120,7 +120,7 @@ func (dev *DaggerDev) Lint(
 				if strings.HasPrefix(pkg, "core/integration/") {
 					continue
 				}
-				if strings.HasPrefix(pkg, "dagql/idtui/viztest/broken") {
+				if strings.HasPrefix(pkg, "dagql/idtui/viztest/") {
 					continue
 				}
 				if strings.HasPrefix(pkg, "modules/evaluator/") {
