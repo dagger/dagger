44bf06b7be0e358b5f0e0e9ad9157546b3481b66
Alex Couture-Beil <alex@mofo.ca>
2025-07-08 14:11:11 -0700

    chore: add WithStaticPath path func
    Signed-off-by: Alex Couture-Beil <alex@mofo.ca>

--- core/schema/container.go
+++ core/schema/container.go
@@ -569,7 +569,7 @@ func (s *containerSchema) Install() {
 			View(BeforeVersion("v0.12.0")).
 			Extend(),
 
-		dagql.NodeFunc("asTarball", DagOpFileWrapper(s.srv, s.asTarball, WithPathFn(s.asTarballPath))).
+		dagql.NodeFunc("asTarball", DagOpFileWrapper(s.srv, s.asTarball, WithStaticPath[*core.Container, containerAsTarballArgs]("container.tar"))).
 			Doc(`Package the container state as an OCI image, and return it as a tar archive`).
 			Args(
 				dagql.Arg("platformVariants").Doc(
@@ -1922,10 +1922,6 @@ type containerAsTarballArgs struct {
 	MediaTypes        core.ImageMediaTypes `default:"OCI"`
 
 	FSDagOpInternalArgs
-}
-
-func (s *containerSchema) asTarballPath(ctx context.Context, val *core.Container, _ containerAsTarballArgs) (string, error) {
-	return "container.tar", nil
 }
 
 func (s *containerSchema) asTarball(
