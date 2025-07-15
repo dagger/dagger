c8116a51bab74513c261ab6202a41ed96ec88ce7
Jason McCallister <jason@mccallister.dev>
2025-07-15 16:14:43 -0400

    fix the cli path
    Signed-off-by: Jason McCallister <jason@mccallister.dev>

--- docs/.dagger/main.go
+++ docs/.dagger/main.go
@@ -38,7 +38,7 @@ type Docs struct {
 
 const (
 	generatedSchemaPath           = "docs/docs-graphql/schema.graphqls"
-	generatedCliZenPath           = "docs/current_docs/reference/cli.mdx"
+	generatedCliZenPath           = "docs/current_docs/reference/cli/index.mdx"
 	generatedAPIReferencePath     = "docs/static/api/reference/index.html"
 	generatedDaggerJSONSchemaPath = "docs/static/reference/dagger.schema.json"
 	generatedEngineJSONSchemaPath = "docs/static/reference/engine.schema.json"
