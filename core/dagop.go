326f0d33e128fc1c4a902d2a026fc2f496076a71
Erik Sipsma <erik@sipsma.dev>
2025-07-18 13:38:08 -0700

    disable dag-op edge merges
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- core/dagop.go
+++ core/dagop.go
@@ -825,5 +825,6 @@ func newDagOpLLB(ctx context.Context, dagOp buildkit.CustomOp, id *call.ID, inpu
 		llb.WithCustomNamef("%s %s", dagOp.Name(), id.Name()),
 		buildkit.WithTracePropagation(ctx),
 		buildkit.WithPassthrough(),
+		llb.SkipEdgeMerge,
 	)
 }
