26b4c5201909ef2917b24fab2379a46935a257ba
Erik Sipsma <erik@sipsma.dev>
2025-07-18 12:25:22 -0700

    chore: bump internal tooling to v0.18.13
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- .github/main.go
+++ .github/main.go
@@ -9,7 +9,7 @@ import (
 )
 
 const (
-	daggerVersion      = "v0.18.12"
+	daggerVersion      = "v0.18.13"
 	upstreamRepository = "dagger/dagger"
 	ubuntuVersion      = "24.04"
 	defaultRunner      = "ubuntu-" + ubuntuVersion
