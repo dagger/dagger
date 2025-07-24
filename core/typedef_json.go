1f9e730ca0916cbaf43da9b2cd4a553d9841093d
Yves Brissaud <yves@dagger.io>
2025-07-24 11:37:00 +0200

    fix: constructor was not unmarshalled
    And this caused an issue where constructors for SDK (but also other constructors)
    were not callabled.
    
    Also be sure the constructor function name is set to an empty string
    using typedef function, like when using the one exposed on the schema.
    
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- core/typedef_json.go
+++ core/typedef_json.go
@@ -15,40 +15,40 @@ type TypeDefJSON struct {
 }
 
 // MarshalJSON implements custom JSON marshaling for TypeDef
-func (t TypeDef) MarshalJSON() ([]byte, error) {
+func (typeDef TypeDef) MarshalJSON() ([]byte, error) {
 	typeDefJSON := TypeDefJSON{
-		Kind:     string(t.Kind),
-		Optional: t.Optional,
+		Kind:     string(typeDef.Kind),
+		Optional: typeDef.Optional,
 	}
 
 	// Set the values field based on the Kind
-	switch t.Kind {
+	switch typeDef.Kind {
 	case TypeDefKindString, TypeDefKindInteger, TypeDefKindFloat, TypeDefKindBoolean, TypeDefKindVoid:
 		// Primitive types don't need values
 		typeDefJSON.Values = nil
 	case TypeDefKindList:
-		if t.AsList.Valid {
-			typeDefJSON.Values = t.AsList.Value
+		if typeDef.AsList.Valid {
+			typeDefJSON.Values = typeDef.AsList.Value
 		}
 	case TypeDefKindObject:
-		if t.AsObject.Valid {
-			typeDefJSON.Values = t.AsObject.Value
+		if typeDef.AsObject.Valid {
+			typeDefJSON.Values = typeDef.AsObject.Value
 		}
 	case TypeDefKindInterface:
-		if t.AsInterface.Valid {
-			typeDefJSON.Values = t.AsInterface.Value
+		if typeDef.AsInterface.Valid {
+			typeDefJSON.Values = typeDef.AsInterface.Value
 		}
 	case TypeDefKindInput:
-		if t.AsInput.Valid {
-			typeDefJSON.Values = t.AsInput.Value
+		if typeDef.AsInput.Valid {
+			typeDefJSON.Values = typeDef.AsInput.Value
 		}
 	case TypeDefKindScalar:
-		if t.AsScalar.Valid {
-			typeDefJSON.Values = t.AsScalar.Value
+		if typeDef.AsScalar.Valid {
+			typeDefJSON.Values = typeDef.AsScalar.Value
 		}
 	case TypeDefKindEnum:
-		if t.AsEnum.Valid {
-			typeDefJSON.Values = t.AsEnum.Value
+		if typeDef.AsEnum.Valid {
+			typeDefJSON.Values = typeDef.AsEnum.Value
 		}
 	}
 
@@ -56,22 +56,22 @@ func (t TypeDef) MarshalJSON() ([]byte, error) {
 }
 
 // UnmarshalJSON implements custom JSON unmarshaling for TypeDef
-func (t *TypeDef) UnmarshalJSON(data []byte) error {
+func (typeDef *TypeDef) UnmarshalJSON(data []byte) error {
 	var typeDefJSON TypeDefJSON
 	if err := json.Unmarshal(data, &typeDefJSON); err != nil {
 		return err
 	}
 
-	t.Kind = TypeDefKind(typeDefJSON.Kind)
-	t.Optional = typeDefJSON.Optional
+	typeDef.Kind = TypeDefKind(typeDefJSON.Kind)
+	typeDef.Optional = typeDefJSON.Optional
 
 	// Initialize all fields as null/invalid
-	t.AsList = dagql.Null[*ListTypeDef]()
-	t.AsObject = dagql.Null[*ObjectTypeDef]()
-	t.AsInterface = dagql.Null[*InterfaceTypeDef]()
-	t.AsInput = dagql.Null[*InputTypeDef]()
-	t.AsScalar = dagql.Null[*ScalarTypeDef]()
-	t.AsEnum = dagql.Null[*EnumTypeDef]()
+	typeDef.AsList = dagql.Null[*ListTypeDef]()
+	typeDef.AsObject = dagql.Null[*ObjectTypeDef]()
+	typeDef.AsInterface = dagql.Null[*InterfaceTypeDef]()
+	typeDef.AsInput = dagql.Null[*InputTypeDef]()
+	typeDef.AsScalar = dagql.Null[*ScalarTypeDef]()
+	typeDef.AsEnum = dagql.Null[*EnumTypeDef]()
 
 	// If there are no values, we're done (for primitive types)
 	if typeDefJSON.Values == nil {
@@ -85,13 +85,13 @@ func (t *TypeDef) UnmarshalJSON(data []byte) error {
 	}
 
 	// Set the appropriate field based on the Kind
-	switch t.Kind {
+	switch typeDef.Kind {
 	case TypeDefKindList:
 		var listTypeDef ListTypeDef
 		if err := json.Unmarshal(valuesJSON, &listTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal list values: %w", err)
 		}
-		t.AsList = dagql.NonNull(&listTypeDef)
+		typeDef.AsList = dagql.NonNull(&listTypeDef)
 	case TypeDefKindObject:
 		objectTypeDef := ObjectTypeDef{
 			Fields:    make([]*FieldTypeDef, 0),
@@ -100,7 +100,17 @@ func (t *TypeDef) UnmarshalJSON(data []byte) error {
 		if err := json.Unmarshal(valuesJSON, &objectTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal object values: %w", err)
 		}
-		t.AsObject = dagql.NonNull(&objectTypeDef)
+		// trick to have constructor working
+		var withConstructor struct {
+			Constructor *Function
+		}
+		if err := json.Unmarshal(valuesJSON, &withConstructor); err != nil {
+			return fmt.Errorf("failed to unmarshal constructor values: %w", err)
+		}
+		if withConstructor.Constructor != nil {
+			objectTypeDef.Constructor = dagql.NonNull(withConstructor.Constructor)
+		}
+		typeDef.AsObject = dagql.NonNull(&objectTypeDef)
 	case TypeDefKindInterface:
 		interfaceTypeDef := InterfaceTypeDef{
 			Functions: make([]*Function, 0),
@@ -108,7 +118,7 @@ func (t *TypeDef) UnmarshalJSON(data []byte) error {
 		if err := json.Unmarshal(valuesJSON, &interfaceTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal interface values: %w", err)
 		}
-		t.AsInterface = dagql.NonNull(&interfaceTypeDef)
+		typeDef.AsInterface = dagql.NonNull(&interfaceTypeDef)
 	case TypeDefKindInput:
 		inputTypeDef := InputTypeDef{
 			Fields: make([]*FieldTypeDef, 0),
@@ -116,13 +126,13 @@ func (t *TypeDef) UnmarshalJSON(data []byte) error {
 		if err := json.Unmarshal(valuesJSON, &inputTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal input values: %w", err)
 		}
-		t.AsInput = dagql.NonNull(&inputTypeDef)
+		typeDef.AsInput = dagql.NonNull(&inputTypeDef)
 	case TypeDefKindScalar:
 		var scalarTypeDef ScalarTypeDef
 		if err := json.Unmarshal(valuesJSON, &scalarTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal scalar values: %w", err)
 		}
-		t.AsScalar = dagql.NonNull(&scalarTypeDef)
+		typeDef.AsScalar = dagql.NonNull(&scalarTypeDef)
 	case TypeDefKindEnum:
 		enumTypeDef := EnumTypeDef{
 			Members: make([]*EnumMemberTypeDef, 0),
@@ -130,17 +140,17 @@ func (t *TypeDef) UnmarshalJSON(data []byte) error {
 		if err := json.Unmarshal(valuesJSON, &enumTypeDef); err != nil {
 			return fmt.Errorf("failed to unmarshal enum values: %w", err)
 		}
-		t.AsEnum = dagql.NonNull(&enumTypeDef)
+		typeDef.AsEnum = dagql.NonNull(&enumTypeDef)
 	default:
-		return fmt.Errorf("unknown TypeDefKind: %s", t.Kind)
+		return fmt.Errorf("unknown TypeDefKind: %s", typeDef.Kind)
 	}
 
 	return nil
 }
 
 // ToJSONString generates a JSON string representation of the TypeDef
-func (t TypeDef) ToJSONString() (string, error) {
-	jsonBytes, err := json.Marshal(t)
+func (typeDef TypeDef) ToJSONString() (string, error) {
+	jsonBytes, err := json.Marshal(typeDef)
 	if err != nil {
 		return "", fmt.Errorf("failed to marshal TypeDef to JSON: %w", err)
 	}
