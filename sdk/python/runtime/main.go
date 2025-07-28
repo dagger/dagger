b24f17abb116fe6ba832b19ef9c46e412e0d7cb2
Helder Correia <174525+helderco@users.noreply.github.com>
2025-07-28 12:11:49 +0000

    add python support
    Signed-off-by: Helder Correia <174525+helderco@users.noreply.github.com>

--- sdk/python/runtime/main.go
+++ sdk/python/runtime/main.go
@@ -18,6 +18,7 @@ const (
 	SDKGenPath            = "src/dagger/client/gen.py"
 	UserGenPath           = "src/dagger_gen.py"
 	SchemaPath            = "/schema.json"
+	TypeDefsPath          = "/module.json"
 	VenvPath              = "/opt/venv"
 	ProjectCfg            = "pyproject.toml"
 	PipCompileLock        = "requirements.lock"
@@ -177,6 +178,30 @@ func (m *PythonSdk) ModuleRuntime(
 		return nil, err
 	}
 	return m.WithInstall().Container, nil
+}
+
+// Container for executing the Python module runtime
+func (m *PythonSdk) ModuleTypeDefs(
+	ctx context.Context,
+	modSource *dagger.ModuleSource,
+	introspectionJSON *dagger.File,
+) (*dagger.Module, error) {
+	// Ignore introspection to avoid calling codegen first
+	ctr, err := m.ModuleRuntime(ctx, modSource, nil)
+	if err != nil {
+		return nil, err
+	}
+	mod_id, err := ctr.
+		WithEnvVariable("DAGGER_MODULE_FILE", TypeDefsPath).
+		WithExec([]string{RuntimeExecutablePath, "--register"}, dagger.ContainerWithExecOpts{
+			ExperimentalPrivilegedNesting: true,
+		}).
+		File(TypeDefsPath).
+		Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return dag.LoadModuleFromID(dagger.ModuleID(mod_id)), nil
 }
 
 // Common steps for the ModuleRuntime and Codegen functions
