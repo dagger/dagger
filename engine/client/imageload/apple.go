f9e71444002b7d1bb24a2ef2dae2e9135669e0ca
Justin Chadwell <me@jedevc.com>
2025-07-30 12:09:29 +0100

    feat: support more driver types
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- engine/client/imageload/apple.go
+++ engine/client/imageload/apple.go
@@ -12,41 +12,36 @@ import (
 	"go.opentelemetry.io/otel"
 )
 
-type Docker struct {
-	Cmd string
-}
+type Apple struct{}
 
 func init() {
-	register("docker-image", &Docker{"docker"})
-	register("podman-image", &Docker{"podman"})
-	register("finch-image", &Docker{"finch"})
-	register("nerdctl-image", &Docker{"nerdctl"})
+	register("apple-image", &Apple{})
 }
 
-func (loader Docker) Loader(ctx context.Context) (*Loader, error) {
+func (loader Apple) Loader(ctx context.Context) (*Loader, error) {
 	return &Loader{
 		TarballLoader: loader.loadTarball,
 	}, nil
 }
 
-func (loader Docker) loadTarball(ctx context.Context, name string, tarball io.Reader) (rerr error) {
+func (loader Apple) loadTarball(ctx context.Context, name string, tarball io.Reader) (rerr error) {
 	ctx, span := otel.Tracer("").Start(ctx, "load "+name)
 	defer telemetry.End(span, func() error { return rerr })
 
-	cmd := exec.CommandContext(ctx, loader.Cmd, "load")
+	cmd := exec.CommandContext(ctx, "container", "image", "load")
 	cmd.Stdin = tarball
 	stdout, _, err := traceexec.ExecOutput(ctx, cmd, telemetry.Encapsulated())
 	if err != nil {
 		return fmt.Errorf("docker load failed: %w", err)
 	}
 
-	result := regexp.MustCompile(`\b(sha256:[0-9a-f]+|\S+:\S+)\b`).FindStringSubmatch(stdout)
+	result := regexp.MustCompile("sha256:([0-9a-f]+)").FindStringSubmatch(stdout)
 	if len(result) == 0 {
 		return fmt.Errorf("unexpected output from docker load: %s", stdout)
 	}
 	imageID := result[1]
 
-	err = traceexec.Exec(ctx, exec.CommandContext(ctx, loader.Cmd, "tag", imageID, name), telemetry.Encapsulated())
+	err = traceexec.Exec(ctx, exec.CommandContext(ctx, "container", "image", "tag", imageID, name), telemetry.Encapsulated())
 	if err != nil {
 		return fmt.Errorf("docker tag failed: %w", err)
 	}
