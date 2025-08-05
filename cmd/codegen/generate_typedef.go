21b1865a9fe16961b77f2e849e346b24c74636f4
Yves Brissaud <yves@dagger.io>
2025-08-05 12:00:28 +0200

    fix: allow to dagger init a go module with self types
    In case of an empty module during the typedefs phase, generate
    the default template so that types from this template are returned
    and will be handled by codegen. That way, with a `dagger init --sdk=go`
    the types of this "empty" module are available from the start and don't
    require a `dagger develop` to be available.
    
    This also adds the introspection schema json file to the typedefs
    function of the generator. This allows to generate enough code
    so that the module initialisation can correctly load the packages
    and types and generates the right types.
    
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- cmd/codegen/generate_typedef.go
+++ cmd/codegen/generate_typedef.go
@@ -3,8 +3,6 @@ package main
 import (
 	"fmt"
 	"log/slog"
-	"path/filepath"
-	"strings"
 
 	"dagger.io/dagger/telemetry"
 	"github.com/dagger/dagger/cmd/codegen/generator"
@@ -20,36 +18,27 @@ func GenerateTypeDefs(cmd *cobra.Command, args []string) error {
 	ctx := cmd.Context()
 	ctx = telemetry.InitEmbedded(ctx, nil)
 
-	cfg := generator.Config{
-		Lang:      generator.SDKLang(lang),
-		OutputDir: outputDir,
+	cfg, err := getGlobalConfig(ctx)
+	if err != nil {
+		return fmt.Errorf("failed to get global configuration: %w", err)
+	}
+	defer cfg.Close()
+
+	moduleConfig := &generator.ModuleGeneratorConfig{
+		ModuleName: moduleName,
 	}
 
-	moduleConfig := &generator.ModuleGeneratorConfig{}
-
-	if moduleName != "" {
-		moduleConfig.ModuleName = moduleName
-
-		if modulePath == "" {
-			return fmt.Errorf("--module-name requires --module-source-path")
-		}
-		modPath, err := relativeTo(outputDir, modulePath)
-		if err != nil {
-			return err
-		}
-		if part, _, _ := strings.Cut(modPath, string(filepath.Separator)); part == ".." {
-			return fmt.Errorf("module path must be child of output directory")
-		}
-		moduleConfig.ModuleSourcePath = modPath
-		moduleParentPath, err := relativeTo(modulePath, outputDir)
-		if err != nil {
-			return err
-		}
-		moduleConfig.ModuleParentPath = moduleParentPath
-		//} else {
-		//	moduleConfig.ModuleName = filepath.Base(filepath.Clean(moduleConfig.OutputDir))
-		//	moduleConfig.ModuleSourcePath = "."
+	modPath, err := relativeTo(outputDir, modulePath)
+	if err != nil {
+		return err
 	}
+
+	moduleConfig.ModuleSourcePath = modPath
+	moduleParentPath, err := relativeTo(modulePath, outputDir)
+	if err != nil {
+		return err
+	}
+	moduleConfig.ModuleParentPath = moduleParentPath
 
 	cfg.ModuleConfig = moduleConfig
 
@@ -67,4 +56,6 @@ func init() {
 	// Specific typedefs generation flags
 	generateTypeDefsCmd.Flags().StringVar(&modulePath, "module-source-path", "", "path to source subpath of the module")
 	generateTypeDefsCmd.Flags().StringVar(&moduleName, "module-name", "", "name of module to generate code for")
+	generateTypeDefsCmd.MarkFlagRequired("module-name")
+	generateTypeDefsCmd.MarkFlagRequired("module-source-path")
 }
