595f81486c44c39387524acce9f5795c61c1283c
Yves Brissaud <yves@dagger.io>
2025-08-07 12:26:26 +0200

    refresh generated files
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- sdk/go/dagger.gen.go
+++ sdk/go/dagger.gen.go
@@ -7381,6 +7381,7 @@ type Module struct {
 	name        *string
 	serve       *Void
 	sync        *ModuleID
+	toJSON      *string
 }
 type WithModuleFunc func(r *Module) *Module
 
@@ -7474,6 +7475,16 @@ func (r *Module) Enums(ctx context.Context) ([]TypeDef, error) {
 	}
 
 	return convert(response), nil
+}
+
+// Load a module from a JSON string
+func (r *Module) FromJSON(json string) *Module {
+	q := r.query.Select("fromJSON")
+	q = q.Arg("json", json)
+
+	return &Module{
+		query: q,
+	}
 }
 
 // The generated files and directories made on top of the module source's context directory.
@@ -7666,6 +7677,19 @@ func (r *Module) Sync(ctx context.Context) (*Module, error) {
 	return &Module{
 		query: q.Root().Select("loadModuleFromID").Arg("id", id),
 	}, nil
+}
+
+// Return a JSON string representation of the module
+func (r *Module) ToJSON(ctx context.Context) (string, error) {
+	if r.toJSON != nil {
+		return *r.toJSON, nil
+	}
+	q := r.query.Select("toJSON")
+
+	var response string
+
+	q = q.Bind(&response)
+	return response, q.Execute(ctx)
 }
 
 // Retrieves the module with the given description
