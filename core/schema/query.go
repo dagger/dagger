687fa9656d4494a3f06de3730607e59c843c9786
Yves Brissaud <yves@dagger.io>
2025-08-19 09:28:47 +0200

    add flags to dagger develop
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- core/schema/query.go
+++ core/schema/query.go
@@ -45,6 +45,7 @@ func (s *querySchema) Install(srv *dagql.Server) {
 	core.TypeDefKinds.Install(srv)
 	core.ModuleSourceKindEnum.Install(srv)
 	core.ReturnTypesEnum.Install(srv)
+	core.ModuleSourceExperimentalFeatures.Install(srv)
 
 	dagql.MustInputSpec(PipelineLabel{}).Install(srv)
 	dagql.MustInputSpec(core.PortForward{}).Install(srv)
--- sdk/python/src/dagger/client/gen.py
+++ sdk/python/src/dagger/client/gen.py
@@ -323,6 +323,15 @@ class ImageMediaTypes(Enum):
     OCI = "OCIMediaTypes"
 
 
+class ModuleSourceExperimentalFeature(Enum):
+    """Experimental features of a module"""
+
+    SELF_CALLS_FEATURE = "SELF_CALLS_FEATURE"
+    """Self calls"""
+    SELF_CALLS = "SELF_CALLS_FEATURE"
+    """Self calls"""
+
+
 class ModuleSourceKind(Enum):
     """The kind of module source."""
 
@@ -8993,7 +9002,10 @@ class ModuleSource(Type):
         _ctx = self._select("withEngineVersion", _args)
         return ModuleSource(_ctx)
 
-    def with_experimental_features(self, features: list[str]) -> Self:
+    def with_experimental_features(
+        self,
+        features: list[ModuleSourceExperimentalFeature],
+    ) -> Self:
         """Enable the experimental features for the module source.
 
         Parameters
@@ -9120,9 +9132,20 @@ class ModuleSource(Type):
         _ctx = self._select("withoutDependencies", _args)
         return ModuleSource(_ctx)
 
-    def without_experimental_features(self) -> Self:
-        """Disable experimental features for the module source."""
-        _args: list[Arg] = []
+    def without_experimental_features(
+        self,
+        features: list[ModuleSourceExperimentalFeature],
+    ) -> Self:
+        """Disable experimental features for the module source.
+
+        Parameters
+        ----------
+        features:
+            The experimental features to disable.
+        """
+        _args = [
+            Arg("features", features),
+        ]
         _ctx = self._select("withoutExperimentalFeatures", _args)
         return ModuleSource(_ctx)
 
@@ -11548,6 +11571,7 @@ __all__ = [
     "ModuleConfigClientID",
     "ModuleID",
     "ModuleSource",
+    "ModuleSourceExperimentalFeature",
     "ModuleSourceID",
     "ModuleSourceKind",
     "NetworkProtocol",
