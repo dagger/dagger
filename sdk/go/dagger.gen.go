c97f687d8709e0719fd52ace087145e04abd6aee
Yves Brissaud <yves@dagger.io>
2025-08-26 17:02:25 +0200

    fix: remove unnecessary alias for ModuleSourceExperimentalFeature enum
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- sdk/go/dagger.gen.go
+++ sdk/go/dagger.gen.go
@@ -12459,8 +12459,8 @@ func (ModuleSourceExperimentalFeature) IsEnum() {}
 
 func (v ModuleSourceExperimentalFeature) Name() string {
 	switch v {
-	case ModuleSourceExperimentalFeatureSelfCallsFeature:
-		return "SELF_CALLS_FEATURE"
+	case ModuleSourceExperimentalFeatureSelfCalls:
+		return "SELF_CALLS"
 	default:
 		return ""
 	}
@@ -12491,8 +12491,6 @@ func (v *ModuleSourceExperimentalFeature) UnmarshalJSON(dt []byte) error {
 		*v = ""
 	case "SELF_CALLS":
 		*v = ModuleSourceExperimentalFeatureSelfCalls
-	case "SELF_CALLS_FEATURE":
-		*v = ModuleSourceExperimentalFeatureSelfCallsFeature
 	default:
 		return fmt.Errorf("invalid enum value %q", s)
 	}
@@ -12501,9 +12499,7 @@ func (v *ModuleSourceExperimentalFeature) UnmarshalJSON(dt []byte) error {
 
 const (
 	// Self calls
-	ModuleSourceExperimentalFeatureSelfCallsFeature ModuleSourceExperimentalFeature = "SELF_CALLS_FEATURE"
-	// Self calls
-	ModuleSourceExperimentalFeatureSelfCalls ModuleSourceExperimentalFeature = ModuleSourceExperimentalFeatureSelfCallsFeature
+	ModuleSourceExperimentalFeatureSelfCalls ModuleSourceExperimentalFeature = "SELF_CALLS"
 )
 
 // The kind of module source.
