6d53e824cef543dc7e078bd6d76217504eb86f00
Alex Suraci <alex@dagger.io>
2025-09-02 17:01:42 -0400

    lints
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/idtui/frontend_pretty.go
+++ dagql/idtui/frontend_pretty.go
@@ -1503,7 +1503,7 @@ func (fe *frontendPretty) handleEditlineKey(msg tea.KeyMsg) (cmd tea.Cmd) {
 	return cmd
 }
 
-//nolint:gocyclo
+//nolint:gocyclo // splitting this up doesn't feel more readable
 func (fe *frontendPretty) handleNavKey(msg tea.KeyMsg) tea.Cmd {
 	lastKey := fe.pressedKey
 	fe.pressedKey = msg.String()
