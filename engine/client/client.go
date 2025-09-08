8f6e1e4fee1d1688fca02cfbacebdd65077f4cec
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-09-08 08:39:37 +0300

    spelling: please
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- engine/client/client.go
+++ engine/client/client.go
@@ -1149,7 +1149,7 @@ func allCacheConfigsFromEnv() (cacheImportConfigs []*controlapi.CacheOptionsEntr
 func (c *Client) clientMetadata() engine.ClientMetadata {
 	sshAuthSock := os.Getenv("SSH_AUTH_SOCK")
 	// expand ~ into absolute path for consistent behavior with CLI
-	// ⚠️ When updating clientMetadata's logic, please also update setupNestedClient
+	// ⚠️ When updating clientMetadata's logic, please update setupNestedClient
 	// for consistent behavior of CLI inside nested execution
 	homeDir, err := os.UserHomeDir()
 	if err == nil {
