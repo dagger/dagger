c729fb27ec0a5f424494d9379747293817020dd5
Kyle Penfound <kyle@dagger.io>
2025-09-10 14:58:14 -0400

    Fix typescript introspection for blueprints (#11018)
    * add typescript blueprint test
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    * fix typescript loading for blueprint object names
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    * add test for python as well
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    * load all objects in typescript introspection unless we have our main class
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    * remove typescript Invalid module test since it is a valid path for blueprints. This is still checked by the CLI
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    * squish blueprint hello tests into one loop
    
    Signed-off-by: kpenfound <kyle@dagger.io>
    
    ---------
    
    Signed-off-by: kpenfound <kyle@dagger.io>

--- sdk/typescript/src/module/introspector/dagger_module/module.ts
+++ sdk/typescript/src/module/introspector/dagger_module/module.ts
@@ -110,29 +110,25 @@ export class DaggerModule {
       )
     }
 
-    const mainObjectNode = this.ast.findResolvedNodeByName(
-      this.name,
-      ts.SyntaxKind.ClassDeclaration,
-    )
-    if (!mainObjectNode) {
-      throw new IntrospectionError(
-        `could not find main object ${this.name} in module ${JSON.stringify(mainModule, null, 2) ?? ""} located at ${this.ast.files}`,
-      )
+    const classObjects = this.findClasses()
+    for (const classObject of classObjects) {
+      // This only applies to cloud. If this is the true main object, it is correct
+      // if this is a blueprint, the description will not matter in any situation
+      const mainFileContent = classObject.file.getFullText()
+      this.description = this.getDescription(mainFileContent)
+
+      const daggerObject = new DaggerObject(classObject.node, this.ast)
+      const objectName = classObject.node.name?.getText() || this.name
+
+      this.objects[objectName] = daggerObject
+      this.references[objectName] = {
+        kind: TypeDefKind.ObjectKind,
+        name: objectName,
+      }
+
+      this.resolveReferences(daggerObject.getReferences())
+      this.propagateReferences()
     }
-
-    const mainFileContent = mainObjectNode.file.getFullText()
-    this.description = this.getDescription(mainFileContent)
-
-    const mainDaggerObject = new DaggerObject(mainObjectNode.node, this.ast)
-
-    this.objects[this.name] = mainDaggerObject
-    this.references[this.name] = {
-      kind: TypeDefKind.ObjectKind,
-      name: this.name,
-    }
-
-    this.resolveReferences(mainDaggerObject.getReferences())
-    this.propagateReferences()
   }
 
   /**
@@ -361,6 +357,36 @@ export class DaggerModule {
     throw new IntrospectionError(
       `could not resolve type reference for ${reference} at ${AST.getNodePosition(typeAlias.node)}`,
     )
+  }
+
+  /**
+   * Find the classes in the AST. Returns only our main class if it exists
+   */
+  private findClasses(): ResolvedNodeWithSymbol<ts.SyntaxKind.ClassDeclaration>[] {
+    const allClassDeclarations = this.ast.findAllDeclarations(
+      ts.SyntaxKind.ClassDeclaration,
+    )
+
+    const allClasses: ResolvedNodeWithSymbol<ts.SyntaxKind.ClassDeclaration>[] =
+      []
+    for (const classDecl of allClassDeclarations) {
+      const convertedDecl =
+        classDecl as ResolvedNodeWithSymbol<ts.SyntaxKind.ClassDeclaration>
+
+      // Check if the class matches this.name and return only that if so
+      if (
+        convertedDecl.node.name &&
+        convertedDecl.node.name.getText() === this.name
+      ) {
+        return [convertedDecl]
+      }
+
+      // or we return all classes decorated with @object
+      if (this.ast.isNodeDecoratedWith(classDecl.node, OBJECT_DECORATOR)) {
+        allClasses.push(convertedDecl)
+      }
+    }
+    return allClasses
   }
 
   /**
