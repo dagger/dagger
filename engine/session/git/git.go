43b4e0d5c0feab60a059f15d4acc0440065f4bcc
Justin Chadwell <me@jedevc.com>
2025-09-12 17:18:45 +0100

    chore: remove deprecated client<->server interactions
    Signed-off-by: Justin Chadwell <me@jedevc.com>

--- engine/session/git/git.go
+++ engine/session/git/git.go
@@ -23,11 +23,4 @@ func NewGitAttachable(rootCtx context.Context) GitAttachable {
 
 func (s GitAttachable) Register(srv *grpc.Server) {
 	RegisterGitServer(srv, &s)
-
-	// dagger/dagger#9323 renamed the GitCredential attachable to Git
-	// it's easy to provide a fallback
-	// TODO: simplify when we break client<->server compat
-	serviceDesc := _Git_serviceDesc
-	serviceDesc.ServiceName = "GitCredential"
-	srv.RegisterService(&serviceDesc, &s)
 }
