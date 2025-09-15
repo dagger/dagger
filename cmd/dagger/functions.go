62a6ad56802c1f9cb33ef3d0a4f27e60a4b175f7
Alex Suraci <alex@dagger.io>
2025-09-15 21:33:09 +0900

    refactor: move summarizePatch to idtui
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- cmd/dagger/functions.go
+++ cmd/dagger/functions.go
@@ -13,12 +13,10 @@ import (
 	"strings"
 
 	"github.com/charmbracelet/huh"
-	"github.com/muesli/termenv"
 	"github.com/opencontainers/go-digest"
 	"github.com/sourcegraph/conc/pool"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"github.com/waigani/diffparser"
 
 	"dagger.io/dagger"
 	"dagger.io/dagger/querybuilder"
@@ -723,7 +721,7 @@ func handleChangesetResponse(ctx context.Context, dag *dagger.Client, response a
 			return fmt.Errorf("get patch contents: %w", err)
 		}
 
-		return summarizePatch(idtui.NewOutput(&summary), patch, -1)
+		return idtui.SummarizePatch(idtui.NewOutput(&summary), patch, -1)
 	})(); err != nil {
 		return err
 	}
@@ -771,134 +769,6 @@ func handleChangesetResponse(ctx context.Context, dag *dagger.Client, response a
 			slog.Error("failed to remove file; ignoring", "path", filePath, "error", err)
 		}
 	}
-
-	return nil
-}
-
-func summarizePatch(out *termenv.Output, patch string, maxWidth int) error {
-	diff, err := diffparser.Parse(patch)
-	if err != nil {
-		return fmt.Errorf("parse patch: %w", err)
-	}
-
-	totalAdded := 0
-	totalRemoved := 0
-
-	fmt.Fprintln(out)
-
-	var longestFilenameLen int
-	for _, f := range diff.Files {
-		filename := f.NewName
-		if filename == "" {
-			filename = f.OrigName
-		}
-		if filename == "" {
-			// FIXME: likely an empty file was created, which yields:
-			//
-			//   diff --git b/hey b/hey
-			//   new file mode 100644
-			//   index 0000000..e69de29
-			//
-			// our diff parsing package doesn't handle 'new file' and parse the
-			// filename
-			args := strings.Fields(f.DiffHeader)
-			lastArg := args[len(args)-1]
-			_, filename, _ = strings.Cut(lastArg, "/")
-			f.NewName = filename
-		}
-		if len(filename) > longestFilenameLen {
-			longestFilenameLen = len(filename)
-		}
-	}
-
-	var maxFilenameLen int
-	if maxWidth > 0 {
-		maxFilenameLen := max(maxWidth-20, 10) // Leave space for " | ", change count, and bars
-		if longestFilenameLen > maxFilenameLen {
-			longestFilenameLen = maxFilenameLen
-		}
-	}
-
-	for _, f := range diff.Files {
-		var removedLines, addedLines int
-		for _, h := range f.Hunks {
-			for _, l := range h.WholeRange.Lines {
-				switch l.Mode {
-				case diffparser.ADDED:
-					addedLines++
-				case diffparser.REMOVED:
-					removedLines++
-				}
-			}
-		}
-
-		totalAdded += addedLines
-		totalRemoved += removedLines
-
-		// Determine filename and color
-		var filenameColor termenv.Color
-		isAdded := f.OrigName == ""
-		isRemoved := f.NewName == ""
-
-		filename := f.NewName
-		if isRemoved {
-			filename = f.OrigName
-			filenameColor = termenv.ANSIRed
-		} else if isAdded {
-			filenameColor = termenv.ANSIGreen
-		} else {
-			filenameColor = termenv.ANSIYellow
-		}
-
-		// Shorten filename based on maxWidth
-		if maxFilenameLen > 0 {
-			if len(filename) > maxFilenameLen {
-				filename = "..." + filename[len(filename)-(maxFilenameLen-3):]
-			}
-		}
-
-		// Format line with colors
-		out.WriteString(out.String(filename).Foreground(filenameColor).String())
-		if len(filename) < longestFilenameLen {
-			out.WriteString(strings.Repeat(" ", longestFilenameLen-len(filename)))
-		}
-		out.WriteString(" | ")
-
-		// Show change indicator
-		if maxWidth > 0 {
-			// Simplified text form for constrained width
-			var parts []string
-			if addedLines > 0 {
-				parts = append(parts, out.String(fmt.Sprintf("+%d", addedLines)).Foreground(termenv.ANSIGreen).String())
-			}
-			if removedLines > 0 {
-				parts = append(parts, out.String(fmt.Sprintf("-%d", removedLines)).Foreground(termenv.ANSIRed).String())
-			}
-			out.WriteString(strings.Join(parts, ", "))
-		} else {
-			// Absolute bars representation
-			if addedLines > 0 {
-				out.WriteString(out.String(strings.Repeat("+", addedLines)).Foreground(termenv.ANSIGreen).String())
-			}
-			if removedLines > 0 {
-				out.WriteString(out.String(strings.Repeat("-", removedLines)).Foreground(termenv.ANSIRed).String())
-			}
-		}
-		out.WriteString("\n")
-	}
-
-	// Add summary line
-	fmt.Fprintf(out, "%d files changed, ", len(diff.Files))
-	if totalAdded > 0 {
-		out.WriteString(out.String(fmt.Sprintf("+%d", totalAdded)).Foreground(termenv.ANSIGreen).String())
-	}
-	if totalAdded > 0 && totalRemoved > 0 {
-		out.WriteString(" ")
-	}
-	if totalRemoved > 0 {
-		out.WriteString(out.String(fmt.Sprintf("-%d", totalRemoved)).Foreground(termenv.ANSIRed).String())
-	}
-	out.WriteString(" lines")
 
 	return nil
 }
