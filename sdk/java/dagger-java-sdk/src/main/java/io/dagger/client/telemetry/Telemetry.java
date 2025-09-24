5fdd94621acf796b7173fd8ff9ae7937cd82d7c8
robertonav20 <roberto.nav20@gmail.com>
2025-09-24 16:17:12 +0000

    [#10618] DT - added telemetry to java sdk
    Signed-off-by: robertonav20 <roberto.nav20@gmail.com>

---
+++ sdk/java/dagger-java-sdk/src/main/java/io/dagger/client/telemetry/Telemetry.java
@@ -0,0 +1,61 @@
+package io.dagger.client.telemetry;
+
+import io.opentelemetry.api.common.Attributes;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceState;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+
+public class Telemetry implements AutoCloseable {
+
+  private static final String TRACER_NAME = "dagger.io/sdk.java";
+
+  @Override
+  public void close() throws Exception {
+    TelemetryInitializer.close();
+  }
+
+  public <T> void trace(String name, Attributes attributes, TelemetrySupplier<T> supplier)
+      throws Exception {
+    TelemetryTracer tracer = new TelemetryTracer(TelemetryInitializer.init(), TRACER_NAME);
+    Context ctx = getContext();
+    try (Scope ignored = ctx.makeCurrent()) {
+      tracer.startActiveSpan(name, ctx, attributes, supplier);
+    }
+  }
+
+  private Context getContext() {
+    Context ctx = Context.current();
+
+    if (Span.current() != null && Span.current().getSpanContext().isValid()) {
+      return ctx;
+    }
+
+    String traceparent = System.getenv("TRACEPARENT");
+    if (traceparent == null || traceparent.isBlank()) {
+      return ctx;
+    }
+
+    try {
+      String[] parts = traceparent.split("-");
+      if (parts.length != 4) {
+        return ctx;
+      }
+
+      String traceId = parts[1];
+      String spanId = parts[2];
+      String traceFlags = parts[3];
+
+      SpanContext remoteContext =
+          SpanContext.createFromRemoteParent(
+              traceId, spanId, TraceFlags.fromHex(traceFlags, 0), TraceState.getDefault());
+
+      return ctx.with(Span.wrap(remoteContext));
+    } catch (Exception e) {
+      System.err.println("Failed to parse TRACEPARENT: " + e.getMessage());
+      return ctx;
+    }
+  }
+}
