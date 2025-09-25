d9224341389a40cb991c1dcb420285be823ee62c
vikram-dagger <112123850+vikram-dagger@users.noreply.github.com>
2025-09-25 22:20:11 +0530

    docs: Add PHP recipes to cookbook (services) (#11094)
    Signed-off-by: Vikram Vaswani <vikram@dagger.io>

---
+++ docs/current_docs/extending/modules/snippets/services/expose-dagger-services-to-host/php/src/MyModule.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DaggerModule;
+
+use Dagger\Attribute\{DaggerObject, DaggerFunction, Doc};
+use Dagger\Service;
+
+use function Dagger\dag;
+
+#[DaggerObject]
+class MyModule
+{
+    #[DaggerFunction]
+    #[Doc('Start and return an HTTP service')]
+    public function httpService(): Service
+    {
+        return dag()
+            ->container()
+            ->from('python')
+            ->withWorkdir('/srv')
+            ->withNewFile('index.html', 'Hello, world!')
+            ->withExposedPort(8080)
+            ->asService(args: ['python', '-m', 'http.server', '8080']);
+    }
+}
