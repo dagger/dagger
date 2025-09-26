52d25377b7ba46a18eabe7b24610fd3350c130a4
Alex Suraci <alex@dagger.io>
2025-09-26 16:34:45 +0900

    tui: fix Reveal taking precedence over Internal
    this snuck in with the giant batch of initial LLM related work, not
    entirely clear it's ever been helpful; the rest of the related commit
    has also been cleaned up by now
    
    https://github.com/dagger/dagger/commit/e118bb15f52e3d059de303ed4793735aab061068
    
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- dagql/dagui/opts.go
+++ dagql/dagui/opts.go
@@ -83,9 +83,6 @@ func (opts FrontendOpts) ShouldShow(db *DB, span *Span) bool {
 		// debug reveals all
 		return true
 	}
-	if span.Reveal {
-		return true
-	}
 	if opts.FocusedSpan == span.ID {
 		// prevent focused span from disappearing
 		return true
