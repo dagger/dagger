5993cd88f8e71f2bdb33f3a5c5100a6c8bebdeaa
Tibor Vass <teabee89@gmail.com>
2025-09-29 15:50:37 -0700

    go: allow vendor/* files through
    This allows to build Go apps that use vendoring, using Dagger's Go module.
    Useful when making local modifications to vendored libraries.
    
    Signed-off-by: Tibor Vass <teabee89@gmail.com>

--- cmd/dagger/.dagger/main.go
+++ cmd/dagger/.dagger/main.go
@@ -14,7 +14,7 @@ func New(
 
 	// +optional
 	// +defaultPath="/"
-	// +ignore=["*", ".*", "!cmd/dagger/*", "!**/go.sum", "!**/go.mod", "!**/*.go", "!**.graphql", "!.goreleaser*.yml", "!.changes", "!LICENSE", "!install.sh", "!install.ps1"]
+	// +ignore=["*", ".*", "!cmd/dagger/*", "!**/go.sum", "!**/go.mod", "!**/*.go", "!vendor/**/*", "!**.graphql", "!.goreleaser*.yml", "!.changes", "!LICENSE", "!install.sh", "!install.ps1"]
 	source *dagger.Directory,
 	// Base image for go build environment
 	// +optional
