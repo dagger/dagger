a460cd6efc5263115626499d1cecc56f9003262a
Marcos Nils <1578458+marcosnils@users.noreply.github.com>
2025-10-21 14:29:13 -0700

    fix callee ref parsing when using git@... refs (#11215)

---
+++ core/telemetry_test.go
@@ -0,0 +1,117 @@
+package core
+
+import (
+	"context"
+	"net/http"
+	"testing"
+
+	"github.com/containerd/containerd/content"
+	"github.com/dagger/dagger/auth"
+	"github.com/dagger/dagger/dagql"
+	"github.com/dagger/dagger/dagql/call"
+	"github.com/dagger/dagger/engine"
+	"github.com/dagger/dagger/engine/buildkit"
+	"github.com/dagger/dagger/engine/clientdb"
+	"github.com/dagger/dagger/engine/filesync"
+	"github.com/dagger/dagger/engine/server/resource"
+	bkcache "github.com/dagger/dagger/internal/buildkit/cache"
+	bkclient "github.com/dagger/dagger/internal/buildkit/client"
+	"github.com/dagger/dagger/internal/buildkit/executor/oci"
+	bksession "github.com/dagger/dagger/internal/buildkit/session"
+	"github.com/dagger/dagger/internal/buildkit/util/leaseutil"
+	"github.com/moby/locker"
+	"github.com/opencontainers/go-digest"
+	"github.com/stretchr/testify/require"
+	"github.com/vektah/gqlparser/v2/ast"
+	"github.com/zeebo/xxh3"
+)
+
+type mockServer struct {
+}
+
+func (ms *mockServer) ServeModule(ctx context.Context, mod *Module, includeDependencies bool) error {
+	return nil
+}
+func (ms *mockServer) CurrentModule(context.Context) (*Module, error) {
+	x3 := xxh3.New()
+	x3.WriteString("foo")
+	c := call.New().Append(&ast.Type{}, "caller1", "foo", &call.Module{}, 0, digest.NewDigest("xxh3", x3))
+	rs, err := dagql.NewResultForID(&ModuleSource{}, c)
+	if err != nil {
+		panic(err)
+	}
+
+	dn := dagql.Nullable[dagql.ObjectResult[*ModuleSource]]{
+		Valid: true,
+		Value: dagql.ObjectResult[*ModuleSource]{Result: rs},
+	}
+	return &Module{
+		Source: dn,
+	}, nil
+}
+func (ms *mockServer) CurrentFunctionCall(context.Context) (*FunctionCall, error) {
+	return nil, nil
+}
+func (ms *mockServer) CurrentServedDeps(context.Context) (*ModDeps, error) {
+	return &ModDeps{}, nil
+}
+func (ms *mockServer) MainClientCallerMetadata(context.Context) (*engine.ClientMetadata, error) {
+	return &engine.ClientMetadata{}, nil
+}
+func (ms *mockServer) NonModuleParentClientMetadata(context.Context) (*engine.ClientMetadata, error) {
+	return nil, nil
+}
+func (ms *mockServer) DefaultDeps(context.Context) (*ModDeps, error)           { return nil, nil }
+func (ms *mockServer) Cache(context.Context) (*dagql.SessionCache, error)      { return nil, nil }
+func (ms *mockServer) Server(context.Context) (*dagql.Server, error)           { return nil, nil }
+func (ms *mockServer) MuxEndpoint(context.Context, string, http.Handler) error { return nil }
+func (ms *mockServer) Secrets(context.Context) (*SecretStore, error)           { return nil, nil }
+func (ms *mockServer) Sockets(context.Context) (*SocketStore, error)           { return nil, nil }
+
+func (ms *mockServer) AddClientResourcesFromID(ctx context.Context, id *resource.ID, sourceClientID string, skipTopLevel bool) error {
+	return nil
+}
+func (ms *mockServer) Auth(context.Context) (*auth.RegistryAuthProvider, error) { return nil, nil }
+
+func (ms *mockServer) Buildkit(context.Context) (*buildkit.Client, error) { return nil, nil }
+
+func (ms *mockServer) Services(context.Context) (*Services, error) { return nil, nil }
+
+func (ms *mockServer) Platform() Platform               { return Platform{} }
+func (ms *mockServer) OCIStore() content.Store          { return nil }
+func (ms *mockServer) DNS() *oci.DNSConfig              { return nil }
+func (ms *mockServer) LeaseManager() *leaseutil.Manager { return nil }
+func (ms *mockServer) EngineLocalCacheEntries(context.Context) (*EngineCacheEntrySet, error) {
+	return nil, nil
+}
+
+func (ms *mockServer) PruneEngineLocalCacheEntries(context.Context, bool) (*EngineCacheEntrySet, error) {
+	return nil, nil
+}
+func (ms *mockServer) EngineLocalCachePolicy() *bkclient.PruneInfo { return nil }
+func (ms *mockServer) BuildkitCache() bkcache.Manager              { return nil }
+func (ms *mockServer) BuildkitSession() *bksession.Manager         { return nil }
+func (ms *mockServer) Locker() *locker.Locker                      { return nil }
+func (ms *mockServer) SecretSalt() []byte                          { return nil }
+func (ms *mockServer) FileSyncer() *filesync.FileSyncer            { return nil }
+func (ms *mockServer) ClientTelemetry(ctc context.Context, sessID, clientID string) (*clientdb.Queries, func() error, error) {
+	return nil, nil, nil
+}
+
+func TestParseCallerCalleeRefs(t *testing.T) {
+	pcID := call.New()
+	pcd := xxh3.New()
+	pcd.WriteString("foo")
+
+	mID := call.New()
+	md := xxh3.New()
+	md.WriteString("foo")
+	mID = mID.Append(&ast.Type{}, "callee1", "foo", nil, 0, digest.NewDigest("xxh3", md))
+
+	pcID = pcID.Append(&ast.Type{}, "VersionedGitSSH.hello", "foo", call.NewModule(mID, "versioned_git_ssh", "git@github.com:dagger/dagger-test-modules/versioned@main", "0cabe03cc0a9079e738c92b2c589d81fd560011f"), 0, digest.NewDigest("xxh3", pcd))
+	_, calleeRef := parseCallerCalleeRefs(t.Context(), &Query{Server: &mockServer{}}, pcID)
+
+	require.Equal(t, "github.com/dagger/dagger-test-modules/versioned", calleeRef.ref)
+	require.Equal(t, "0cabe03cc0a9079e738c92b2c589d81fd560011f", calleeRef.version)
+	require.Equal(t, "VersionedGitSSH.hello", calleeRef.functionName)
+}
