3d9546419f6d1f5db603c3b59cae04984e09a05a
Guillaume de Rouville <guillaume@dagger.io>
2025-10-23 00:55:16 -0700

    tempalte(go/sdk): add golden test for interfaces and object fields
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- cmd/codegen/generator/go/templates/object_test.go
+++ cmd/codegen/generator/go/templates/object_test.go
@@ -89,3 +89,87 @@ func TestObjectMethodDeprecated(t *testing.T) {
 
 	require.Equal(t, want, got)
 }
+
+func TestObjectFieldDeprecated(t *testing.T) {
+	schemaJSON := `
+    {
+      "description": "Test object with deprecated field",
+      "fields": [
+        {
+          "args": [],
+          "deprecationReason": "This field is deprecated and will be removed in future versions.",
+          "description": "",
+          "isDeprecated": true,
+          "name": "legacyField",
+          "type": {
+            "kind": "NON_NULL",
+            "ofType": {
+              "kind": "SCALAR",
+              "name": "String"
+            }
+          }
+        }
+      ],
+      "kind": "OBJECT",
+      "name": "Test"
+    }
+`
+
+	schema, object := loadSchemaFromTypeJSON(t, schemaJSON)
+	tmpl := parseTemplateFiles(t, schema, "_types/object.go.tmpl")
+	require.NotNil(t, tmpl)
+
+	got := renderTemplate(t, tmpl, object)
+
+	want := updateAndGetFixture(t, "testdata/object_field_deprecated.golden", got)
+
+	require.Equal(t, want, got)
+}
+
+func TestInterfaceMethodOptionalArgDeprecated(t *testing.T) {
+	schemaJSON := `
+    {
+      "description": "Test interface with deprecated method",
+      "fields": [
+        {
+          "args": [
+            {
+              "defaultValue": null,
+              "deprecationReason": "Not needed anymore.",
+              "description": "",
+              "isDeprecated": true,
+              "name": "bar",
+              "type": {
+                "kind": "SCALAR",
+                "name": "Int"
+              }
+            }
+          ],
+          "deprecationReason": "Use Bar instead.",
+          "description": "",
+          "isDeprecated": true,
+          "name": "foo",
+          "type": {
+            "kind": "NON_NULL",
+            "ofType": {
+              "kind": "SCALAR",
+              "name": "String"
+            }
+          }
+        }
+      ],
+      "kind": "INTERFACE",
+      "name": "TestFooer"
+    }
+`
+
+	schema, iface := loadSchemaFromTypeJSON(t, schemaJSON)
+	tmpl := parseTemplateFiles(t, schema, "_types/object.go.tmpl")
+	require.NotNil(t, tmpl)
+
+	got := renderTemplate(t, tmpl, iface)
+
+	want := updateAndGetFixture(t, "testdata/interface_method_optional_arg_deprecated.golden", got)
+
+	require.Equal(t, want, got)
+}
