0e103dd2abf6a333af0b6f4ef637b9c0d9618b30
Solomon Hykes <solomon@dagger.io>
2025-10-24 00:27:54 -0700

    dagger checks: prettier output
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- cmd/dagger/checks.go
+++ cmd/dagger/checks.go
@@ -3,7 +3,10 @@ package main
 import (
 	"context"
 	"fmt"
+	"strings"
 
+	"github.com/juju/ansiterm/tabwriter"
+	"github.com/muesli/termenv"
 	"github.com/spf13/cobra"
 
 	"dagger.io/dagger"
@@ -55,12 +58,18 @@ Examples:
 	},
 }
 
+// 'dagger checks -l'
 func listChecks(ctx context.Context, checksGroup *dagger.CheckGroup, cmd *cobra.Command) error {
 	checks, err := checksGroup.List(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to list checks: %w", err)
 	}
 
+	tw := tabwriter.NewWriter(cmd.OutOrStdout(), 0, 0, 3, ' ', tabwriter.DiscardEmptyColumns)
+	fmt.Fprintf(tw, "%s\t%s\n",
+		termenv.String("Name").Bold(),
+		termenv.String("Description").Bold(),
+	)
 	for _, check := range checks {
 		fullName, err := check.FullName(ctx)
 		if err != nil {
@@ -71,23 +80,32 @@ func listChecks(ctx context.Context, checksGroup *dagger.CheckGroup, cmd *cobra.
 		if err != nil {
 			return fmt.Errorf("failed to get check description: %w", err)
 		}
-
-		fmt.Fprintf(cmd.OutOrStdout(), "%s\t%s\n", cliName, description)
+		// Extract only the first line of the description
+		firstLine := description
+		if idx := strings.Index(description, "\n"); idx != -1 {
+			firstLine = description[:idx]
+		}
+		fmt.Fprintf(tw, "%s\t%s\n",
+			cliName,
+			firstLine,
+		)
 	}
-
-	return nil
+	return tw.Flush()
 }
 
+// 'dagger checks' (runs by default)
 func runChecks(ctx context.Context, checksGroup *dagger.CheckGroup, cmd *cobra.Command) error {
-	// Run the checks
-	runGroup := checksGroup.Run()
-
 	// Get the results
-	checks, err := runGroup.List(ctx)
+	checks, err := checksGroup.Run().List(ctx)
 	if err != nil {
 		return fmt.Errorf("failed to run checks: %w", err)
 	}
-
+	tw := tabwriter.NewWriter(cmd.OutOrStdout(), 0, 0, 3, ' ', tabwriter.DiscardEmptyColumns)
+	fmt.Fprintf(tw, "%s\t%s\t%s\n",
+		termenv.String("Name").Bold(),
+		termenv.String("Result").Bold(),
+		termenv.String("Message").Bold(),
+	)
 	for _, check := range checks {
 		fullName, err := check.FullName(ctx)
 		if err != nil {
@@ -103,7 +121,11 @@ func runChecks(ctx context.Context, checksGroup *dagger.CheckGroup, cmd *cobra.C
 		if err != nil {
 			return fmt.Errorf("failed to get check message: %w", err)
 		}
-
+		fmt.Fprintf(tw, "%s\t%s\\n",
+			fullName,
+			emoji,
+			message,
+		)
 		fmt.Fprintf(cmd.OutOrStdout(), "%s\t%s\t%s\n", fullName, emoji, message)
 	}
 
