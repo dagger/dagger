9ae8d007943665368c42c615256552461999ed43
Erik Sipsma <erik@sipsma.dev>
2025-10-30 10:26:06 -0700

    chore: bump dependencies to v0.19.4
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- sdk/elixir/lib/dagger/core/version.ex
+++ sdk/elixir/lib/dagger/core/version.ex
@@ -1,7 +1,7 @@
 defmodule Dagger.Core.Version do
   @moduledoc false
 
-  @dagger_cli_version "0.19.3"
+  @dagger_cli_version "0.19.4"
 
   def engine_version(), do: @dagger_cli_version
 end
--- sdk/go/engineconn/version.gen.go
+++ sdk/go/engineconn/version.gen.go
@@ -2,4 +2,4 @@
 
 package engineconn
 
-const CLIVersion = "0.19.3"
+const CLIVersion = "0.19.4"
