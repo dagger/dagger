a8cc6b9538459cdc69c16d9566dcf166e12ac839
Solomon Hykes <solomon@dagger.io>
2025-11-10 15:54:10 -0800

    CI monolith: playground: pass-through to cmd/engine
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- .dagger/playground.go
+++ .dagger/playground.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"context"
-
 	"github.com/dagger/dagger/.dagger/internal/dagger"
 )
 
 // Creates a complete end-to-end build environment with CLI and engine for interactive testing
 func (dev *DaggerDev) Playground(
-	ctx context.Context,
 	// Build from a custom base image
 	// +optional
 	base *dagger.Container,
@@ -18,23 +15,10 @@ func (dev *DaggerDev) Playground(
 	// Share cache globally
 	// +optional
 	sharedCache bool,
-) (*dagger.Container, error) {
-	if base == nil {
-		base = dag.Wolfi().Container().WithEnvVariable("HOME", "/root")
-	}
-	base = base.WithWorkdir("$HOME", dagger.ContainerWithWorkdirOpts{Expand: true})
-	svc := dag.DaggerEngine().Service("", dagger.DaggerEngineServiceOpts{
+) *dagger.Container {
+	return dag.DaggerEngine().Playground(dagger.DaggerEnginePlaygroundOpts{
+		Base:        base,
 		GpuSupport:  gpuSupport,
 		SharedCache: sharedCache,
 	})
-	endpoint, err := svc.Endpoint(ctx, dagger.ServiceEndpointOpts{Scheme: "tcp"})
-	if err != nil {
-		return nil, err
-	}
-	return base.
-			WithMountedFile("/usr/bin/dagger", dag.DaggerCli().Binary()).
-			WithEnvVariable("_EXPERIMENTAL_DAGGER_CLI_BIN", "/usr/bin/dagger").
-			WithServiceBinding("dagger-engine", svc).
-			WithEnvVariable("_EXPERIMENTAL_DAGGER_RUNNER_HOST", endpoint),
-		nil
 }
