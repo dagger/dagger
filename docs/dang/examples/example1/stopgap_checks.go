8d6364f1338ab153a99aea2e9b1214493920b55d
Solomon Hykes <solomon@dagger.io>
2025-11-11 03:07:03 -0800

    remove hidden examples interfering with engine tests
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- docs/dang/examples/example1/stopgap_checks.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// This file contains temporary code, to be removed once 'dagger checks' is merged and released.
-type MyCheckStatus string
-
-const (
-	CheckCompleted MyCheckStatus = "COMPLETED"
-	CheckSkipped   MyCheckStatus = "SKIPPED"
-)
