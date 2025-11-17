bfeb1a7cec3757a60e9e2c5bf176c1131e9af388
Vasek - Tom C <tom@dagger.io>
2025-11-17 18:39:36 +0100

    feat: add eager-mod-loading flag (#11413)
    As discussed with @shykes, with the lazy runtime loading addition,
    it became hard  to verify that your module and its dependencies
    compiles just by running dagger functions.
    
    This PR adds the flag `eager-runtime` to all modules
    commands so it disables lazy loading on module runtime.
    That way, running `dagger functions --eager-runtime`
    also let you verify that your module compiles properly.
    
    Add `EagerRuntime` in engine client metadata.
    
    Signed-off-by: Tom Chauveau <tom@dagger.io>

--- cmd/dagger/module.go
+++ cmd/dagger/module.go
@@ -58,7 +58,8 @@ var (
 
 	force bool
 
-	autoApply bool
+	autoApply    bool
+	eagerRuntime bool
 )
 
 const (
@@ -117,6 +118,9 @@ func moduleAddFlags(cmd *cobra.Command, flags *pflag.FlagSet, optional bool) {
 		defaultAllowLLM = strings.Split(allowLLMEnv, ",")
 	}
 	flags.StringSliceVar(&allowedLLMModules, "allow-llm", defaultAllowLLM, "List of URLs of remote modules allowed to access LLM APIs, or 'all' to bypass restrictions for the entire session")
+
+	// Add the eager module loading flag to disable lazy load on runtime.
+	flags.BoolVar(&eagerRuntime, "eager-runtime", false, "load module runtime eagerly")
 }
 
 func init() {
--- core/schema/modulesource.go
+++ core/schema/modulesource.go
@@ -2831,6 +2831,28 @@ func (s *moduleSourceSchema) runModuleDefInSDK(ctx context.Context, src, srcInst
 		mod.Deps = mod.Deps.Append(mod)
 	}
 
+	// Verify if lazy loading is disabled ; if so, load the runtime eagerly.
+	clientMetadata, err := engine.ClientMetadataFromContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get client metadata: %w", err)
+	}
+
+	if clientMetadata.EagerRuntime && !mod.Runtime.Valid {
+		runtime, err := runtimeImpl.Runtime(ctx, mod.Deps, srcInstContentHashed)
+		if err != nil {
+			return nil, err
+		}
+		mod.Runtime = dagql.NonNull(runtime)
+
+		// Force load the runtime to fill the cache
+		var runtimeRes dagql.ID[*core.Container]
+		if err = dag.Select(ctx, mod.Runtime.Value, &runtimeRes, dagql.Selector{
+			Field: "sync",
+		}); err != nil {
+			return nil, err
+		}
+	}
+
 	return mod, nil
 }
 
