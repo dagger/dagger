76aa8f5e9edc9560e84ac2435ec24156fa63f790
John Charman <john.charman@imhotek.net>
2026-01-13 17:14:27 +0000

    PHP: Support Getters
    Unconditionally add constructor if fields are present
    
    If the Root object has getters, with default values,
    Dagger will not initialize the root object before calling getters.
    
    This leads to Dagger returning uninitialized values from the root object.
    
    Use identical naming strategy for properties during serialization
    
    Add integration tests
    
    Signed-off-by: John Charman <john.charman@imhotek.net>

--- core/integration/testdata/modules/php/scalar-kind/src/ScalarKind.php
+++ core/integration/testdata/modules/php/scalar-kind/src/ScalarKind.php
@@ -8,6 +8,20 @@ use Dagger\Attribute\{DaggerFunction, DaggerObject};
 
 #[DaggerObject] class ScalarKind
 {
+    #[DaggerFunction] public bool $boolField = true;
+    #[DaggerFunction] public float $floatField = 3.14;
+    #[DaggerFunction] public int $intField = 1;
+    #[DaggerFunction] public string $stringField = 'Hello, field!';
+
+    #[DaggerFunction] public function setFields(): ScalarKind
+    {
+        $this->boolField = false;
+        $this->floatField = 1.618;
+        $this->intField = 2;
+        $this->stringField = 'HOWDY, FIELD!';
+        return $this;
+    }
+
     #[DaggerFunction] public function oppositeBool(bool $arg): bool
     {
         return !$arg;
