1073262874f277754125bde386cbd35797e9390b
Tom Chauveau <tom@dagger.io>
2026-01-14 10:55:46 -0800

    chore: fix python toolchain formatting
    Signed-off-by: Tom Chauveau <tom@dagger.io>

--- core/integration/testdata/checks/hello-with-checks-py/src/hello_with_checks_py/main.py
+++ core/integration/testdata/checks/hello-with-checks-py/src/hello_with_checks_py/main.py
@@ -12,20 +12,10 @@ class HelloWithChecksPy:
     @check
     async def passing_check(self) -> None:
         """Returns a passing check"""
-        await (
-            dag.container()
-            .from_(self.baseImage)
-            .with_exec(["sh", "-c", "exit 0"])
-            .sync()
-        )
+        await dag.container().from_(self.baseImage).with_exec(["sh", "-c", "exit 0"]).sync()
 
     @function
     @check
     async def failing_check(self) -> None:
         """Returns a failing check"""
-        await (
-            dag.container()
-            .from_(self.baseImage)
-            .with_exec(["sh", "-c", "exit 1"])
-            .sync()
-        )
+        await dag.container().from_(self.baseImage).with_exec(["sh", "-c", "exit 1"]).sync()
