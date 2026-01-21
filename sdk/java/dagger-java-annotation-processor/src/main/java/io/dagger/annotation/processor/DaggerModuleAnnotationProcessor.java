78c9264a4440e06645db9f30601202adc1aa8974
Yves Brissaud <yves@dagger.io>
2026-01-21 11:58:35 +0100

    fix: javaparser breaking change
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- sdk/java/dagger-java-annotation-processor/src/main/java/io/dagger/annotation/processor/DaggerModuleAnnotationProcessor.java
+++ sdk/java/dagger-java-annotation-processor/src/main/java/io/dagger/annotation/processor/DaggerModuleAnnotationProcessor.java
@@ -775,7 +775,7 @@ public class DaggerModuleAnnotationProcessor extends AbstractProcessor {
     if (javadocString == null) {
       return "";
     }
-    return StaticJavaParser.parseJavadoc(javadocString).getDescription().toText().trim();
+    return StaticJavaParser.parseJavadoc(javadocString, false).getDescription().toText().trim();
   }
 
   private String parseModuleDescription(Element element) {
@@ -805,7 +805,7 @@ public class DaggerModuleAnnotationProcessor extends AbstractProcessor {
   private String parseJavaDocDescription(Element element) {
     String javadocString = elementUtils.getDocComment(element);
     if (javadocString != null) {
-      return StaticJavaParser.parseJavadoc(javadocString).getDescription().toText().trim();
+      return StaticJavaParser.parseJavadoc(javadocString, false).getDescription().toText().trim();
     }
     return "";
   }
@@ -815,7 +815,7 @@ public class DaggerModuleAnnotationProcessor extends AbstractProcessor {
     if (javadocString == null) {
       return "";
     }
-    Javadoc javadoc = StaticJavaParser.parseJavadoc(javadocString);
+    Javadoc javadoc = StaticJavaParser.parseJavadoc(javadocString, false);
     Optional<JavadocBlockTag> blockTag =
         javadoc.getBlockTags().stream()
             .filter(tag -> tag.getType() == Type.PARAM)
