1e2a2bb4ee0a80fde5e4d92e1c5017d342c7468f
Yves Brissaud <yves@dagger.io>
2026-04-02 11:00:42 +0200

    fix: fmt java code
    Signed-off-by: Yves Brissaud <yves@dagger.io>

--- sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/annotation/Up.java
+++ sdk/java/dagger-java-sdk/src/main/java/io/dagger/module/annotation/Up.java
@@ -8,8 +8,8 @@ import java.lang.annotation.Target;
 /**
  * Annotation to mark a function as a service for {@code dagger up}.
  *
- * <p>Functions annotated with {@code @Up} will be discovered by {@code dagger up}
- * and their returned Service will be started and tunneled to the host.
+ * <p>Functions annotated with {@code @Up} will be discovered by {@code dagger up} and their
+ * returned Service will be started and tunneled to the host.
  */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
