1be6dd970840df1aa0ba3f2eb408fb30faed2c9a
Alex Suraci <alex@dagger.io>
2026-05-07 16:04:47 -0400

    DNM(SLOP): update + (AI) rewrite docs, to be deslopped
    Signed-off-by: Alex Suraci <alex@dagger.io>

--- docs/current_docs/extending/modules/snippets/interfaces/declarations/typescript/index.ts
+++ docs/current_docs/extending/modules/snippets/interfaces/declarations/typescript/index.ts
@@ -1,8 +1,7 @@
 import { func, object } from "@dagger.io/dagger"
 
 export interface Fooer {
-  // You can also declare it as a method signature (e.g., `foo(): Promise<string>`)
-  foo: (bar: number) => Promise<string>
+  foo(bar: number): Promise<string>
 }
 
 @object()
