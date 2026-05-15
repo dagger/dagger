a3c2ddc166c6051711c3c09bce594073d5e07214
Solomon Hykes <solomon@dagger.io>
2026-05-15 15:05:07 +0000

    test: convert legacy fixture setup

--- core/integration/changeset_test.go
+++ core/integration/changeset_test.go
@@ -782,43 +782,7 @@ func (ChangesetSuite) TestChangeset(ctx context.Context, t *testctx.T) {
 func (s ChangesetSuite) TestExport(ctx context.Context, t *testctx.T) {
 	c := connect(ctx, t)
 
-	modGen := c.Container().From(golangImage).
-		WithMountedFile(testCLIBinPath, daggerCliFile(t, c)).
-		WithWorkdir("/work").
-		With(daggerExec("module", "init", "--source=.", "--sdk=go", "test", ".")).
-		WithNewFile("main.go", `package main
-
-import (
-	"dagger/test/internal/dagger"
-)
-
-func New() *Test {
-	return &Test{
-		Dir: dag.Directory().
-			WithNewFile("foo.txt", "foo\nbar\nbaz").
-			WithNewFile("bar.txt", "hey").
-			WithNewDirectory("emptydir"),
-	}
-}
-
-type Test struct {
-	Dir *dagger.Directory
-}
-
-func (t *Test) Update() *dagger.Changeset {
-	return t.Dir.
-		WithNewFile("foo.txt", "foo\nbaz").
-		WithoutFile("bar.txt").
-		WithNewFile("baz.txt", "im new here").
-		WithoutDirectory("emptydir").
-		Changes(t.Dir)
-}
-
-func (t *Test) NoChanges() *dagger.Changeset {
-	return t.Dir.Changes(t.Dir)
-}
-`,
-		).
+	modGen := moduleEntrypointFixture(t, c, "test", "go/changeset-export").
 		With(daggerCall("dir", "-o", "./outdir"))
 
 	t.Run("export", func(ctx context.Context, t *testctx.T) {
