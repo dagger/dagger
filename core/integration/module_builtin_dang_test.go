930297bc30de71ff20209fb541f0f9eed0f6917b
Solomon Hykes <solomon@dagger.io>
2026-05-15 14:17:47 +0000

    test: convert dang dependency fixture
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>
    Signed-off-by: Yves Brissaud <yves@dagger.io>
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- core/integration/module_builtin_dang_test.go
+++ core/integration/module_builtin_dang_test.go
@@ -18,72 +18,7 @@ import (
 func (ModuleSuite) TestBuiltinDangDependencyModules(ctx context.Context, t *testctx.T) {
 	c := connect(ctx, t)
 
-	modGen := goGitBase(t, c).
-		WithWorkdir("/work").
-		WithExec([]string{"mkdir", "-p", "gochild", "pychild", "tschild", "dangchild"}).
-		With(withModInitAt("gochild", "go", `package main
-
-type Gochild struct{}
-
-func (m *Gochild) Value() string {
-	return "go"
-}
-`)).
-		With(withModInitAt("pychild", "python", `
-from dagger import function, object_type
-
-
-@object_type
-class Pychild:
-    @function
-    def value(self) -> str:
-        return "python"
-`)).
-		With(withModInitAt("tschild", "typescript", `
-import { object, func } from "@dagger.io/dagger"
-
-@object()
-export class Tschild {
-  @func()
-  value(): string {
-    return "typescript"
-  }
-}
-`)).
-		With(withModInitAt("dangchild", "dang", `
-type Dangchild {
-  pub value: String! {
-    "dang"
-  }
-}
-`)).
-		With(withModInit("dang", `
-type Test {
-  pub local: String! {
-    "local"
-  }
-
-  pub viaGo: String! {
-    gochild.value
-  }
-
-  pub viaPython: String! {
-    pychild.value
-  }
-
-  pub viaTypescript: String! {
-    tschild.value
-  }
-
-  pub viaDang: String! {
-    dangchild.value
-  }
-}
-`)).
-		With(daggerExec("module", "install", "./gochild")).
-		With(daggerExec("module", "install", "./pychild")).
-		With(daggerExec("module", "install", "./tschild")).
-		With(daggerExec("module", "install", "./dangchild"))
+	modGen := moduleEntrypointFixture(t, c, "test", "dang/dependency-modules")
 
 	for _, tc := range []struct {
 		name string
