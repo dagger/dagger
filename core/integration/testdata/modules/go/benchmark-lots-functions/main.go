7d1fb0d94dbf1ce73169744d74e72a88139b21a3
Solomon Hykes <solomon@dagger.io>
2026-05-15 17:14:45 +0000

    test: convert benchmark module fixtures
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>
    Signed-off-by: Yves Brissaud <yves@dagger.io>
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ core/integration/testdata/modules/go/benchmark-lots-functions/main.go
@@ -0,0 +1,403 @@
+package main
+
+type PotatoSack struct{}
+
+func (m *PotatoSack) Potato0() string {
+	return "potato #0"
+}
+
+func (m *PotatoSack) Potato1() string {
+	return "potato #1"
+}
+
+func (m *PotatoSack) Potato2() string {
+	return "potato #2"
+}
+
+func (m *PotatoSack) Potato3() string {
+	return "potato #3"
+}
+
+func (m *PotatoSack) Potato4() string {
+	return "potato #4"
+}
+
+func (m *PotatoSack) Potato5() string {
+	return "potato #5"
+}
+
+func (m *PotatoSack) Potato6() string {
+	return "potato #6"
+}
+
+func (m *PotatoSack) Potato7() string {
+	return "potato #7"
+}
+
+func (m *PotatoSack) Potato8() string {
+	return "potato #8"
+}
+
+func (m *PotatoSack) Potato9() string {
+	return "potato #9"
+}
+
+func (m *PotatoSack) Potato10() string {
+	return "potato #10"
+}
+
+func (m *PotatoSack) Potato11() string {
+	return "potato #11"
+}
+
+func (m *PotatoSack) Potato12() string {
+	return "potato #12"
+}
+
+func (m *PotatoSack) Potato13() string {
+	return "potato #13"
+}
+
+func (m *PotatoSack) Potato14() string {
+	return "potato #14"
+}
+
+func (m *PotatoSack) Potato15() string {
+	return "potato #15"
+}
+
+func (m *PotatoSack) Potato16() string {
+	return "potato #16"
+}
+
+func (m *PotatoSack) Potato17() string {
+	return "potato #17"
+}
+
+func (m *PotatoSack) Potato18() string {
+	return "potato #18"
+}
+
+func (m *PotatoSack) Potato19() string {
+	return "potato #19"
+}
+
+func (m *PotatoSack) Potato20() string {
+	return "potato #20"
+}
+
+func (m *PotatoSack) Potato21() string {
+	return "potato #21"
+}
+
+func (m *PotatoSack) Potato22() string {
+	return "potato #22"
+}
+
+func (m *PotatoSack) Potato23() string {
+	return "potato #23"
+}
+
+func (m *PotatoSack) Potato24() string {
+	return "potato #24"
+}
+
+func (m *PotatoSack) Potato25() string {
+	return "potato #25"
+}
+
+func (m *PotatoSack) Potato26() string {
+	return "potato #26"
+}
+
+func (m *PotatoSack) Potato27() string {
+	return "potato #27"
+}
+
+func (m *PotatoSack) Potato28() string {
+	return "potato #28"
+}
+
+func (m *PotatoSack) Potato29() string {
+	return "potato #29"
+}
+
+func (m *PotatoSack) Potato30() string {
+	return "potato #30"
+}
+
+func (m *PotatoSack) Potato31() string {
+	return "potato #31"
+}
+
+func (m *PotatoSack) Potato32() string {
+	return "potato #32"
+}
+
+func (m *PotatoSack) Potato33() string {
+	return "potato #33"
+}
+
+func (m *PotatoSack) Potato34() string {
+	return "potato #34"
+}
+
+func (m *PotatoSack) Potato35() string {
+	return "potato #35"
+}
+
+func (m *PotatoSack) Potato36() string {
+	return "potato #36"
+}
+
+func (m *PotatoSack) Potato37() string {
+	return "potato #37"
+}
+
+func (m *PotatoSack) Potato38() string {
+	return "potato #38"
+}
+
+func (m *PotatoSack) Potato39() string {
+	return "potato #39"
+}
+
+func (m *PotatoSack) Potato40() string {
+	return "potato #40"
+}
+
+func (m *PotatoSack) Potato41() string {
+	return "potato #41"
+}
+
+func (m *PotatoSack) Potato42() string {
+	return "potato #42"
+}
+
+func (m *PotatoSack) Potato43() string {
+	return "potato #43"
+}
+
+func (m *PotatoSack) Potato44() string {
+	return "potato #44"
+}
+
+func (m *PotatoSack) Potato45() string {
+	return "potato #45"
+}
+
+func (m *PotatoSack) Potato46() string {
+	return "potato #46"
+}
+
+func (m *PotatoSack) Potato47() string {
+	return "potato #47"
+}
+
+func (m *PotatoSack) Potato48() string {
+	return "potato #48"
+}
+
+func (m *PotatoSack) Potato49() string {
+	return "potato #49"
+}
+
+func (m *PotatoSack) Potato50() string {
+	return "potato #50"
+}
+
+func (m *PotatoSack) Potato51() string {
+	return "potato #51"
+}
+
+func (m *PotatoSack) Potato52() string {
+	return "potato #52"
+}
+
+func (m *PotatoSack) Potato53() string {
+	return "potato #53"
+}
+
+func (m *PotatoSack) Potato54() string {
+	return "potato #54"
+}
+
+func (m *PotatoSack) Potato55() string {
+	return "potato #55"
+}
+
+func (m *PotatoSack) Potato56() string {
+	return "potato #56"
+}
+
+func (m *PotatoSack) Potato57() string {
+	return "potato #57"
+}
+
+func (m *PotatoSack) Potato58() string {
+	return "potato #58"
+}
+
+func (m *PotatoSack) Potato59() string {
+	return "potato #59"
+}
+
+func (m *PotatoSack) Potato60() string {
+	return "potato #60"
+}
+
+func (m *PotatoSack) Potato61() string {
+	return "potato #61"
+}
+
+func (m *PotatoSack) Potato62() string {
+	return "potato #62"
+}
+
+func (m *PotatoSack) Potato63() string {
+	return "potato #63"
+}
+
+func (m *PotatoSack) Potato64() string {
+	return "potato #64"
+}
+
+func (m *PotatoSack) Potato65() string {
+	return "potato #65"
+}
+
+func (m *PotatoSack) Potato66() string {
+	return "potato #66"
+}
+
+func (m *PotatoSack) Potato67() string {
+	return "potato #67"
+}
+
+func (m *PotatoSack) Potato68() string {
+	return "potato #68"
+}
+
+func (m *PotatoSack) Potato69() string {
+	return "potato #69"
+}
+
+func (m *PotatoSack) Potato70() string {
+	return "potato #70"
+}
+
+func (m *PotatoSack) Potato71() string {
+	return "potato #71"
+}
+
+func (m *PotatoSack) Potato72() string {
+	return "potato #72"
+}
+
+func (m *PotatoSack) Potato73() string {
+	return "potato #73"
+}
+
+func (m *PotatoSack) Potato74() string {
+	return "potato #74"
+}
+
+func (m *PotatoSack) Potato75() string {
+	return "potato #75"
+}
+
+func (m *PotatoSack) Potato76() string {
+	return "potato #76"
+}
+
+func (m *PotatoSack) Potato77() string {
+	return "potato #77"
+}
+
+func (m *PotatoSack) Potato78() string {
+	return "potato #78"
+}
+
+func (m *PotatoSack) Potato79() string {
+	return "potato #79"
+}
+
+func (m *PotatoSack) Potato80() string {
+	return "potato #80"
+}
+
+func (m *PotatoSack) Potato81() string {
+	return "potato #81"
+}
+
+func (m *PotatoSack) Potato82() string {
+	return "potato #82"
+}
+
+func (m *PotatoSack) Potato83() string {
+	return "potato #83"
+}
+
+func (m *PotatoSack) Potato84() string {
+	return "potato #84"
+}
+
+func (m *PotatoSack) Potato85() string {
+	return "potato #85"
+}
+
+func (m *PotatoSack) Potato86() string {
+	return "potato #86"
+}
+
+func (m *PotatoSack) Potato87() string {
+	return "potato #87"
+}
+
+func (m *PotatoSack) Potato88() string {
+	return "potato #88"
+}
+
+func (m *PotatoSack) Potato89() string {
+	return "potato #89"
+}
+
+func (m *PotatoSack) Potato90() string {
+	return "potato #90"
+}
+
+func (m *PotatoSack) Potato91() string {
+	return "potato #91"
+}
+
+func (m *PotatoSack) Potato92() string {
+	return "potato #92"
+}
+
+func (m *PotatoSack) Potato93() string {
+	return "potato #93"
+}
+
+func (m *PotatoSack) Potato94() string {
+	return "potato #94"
+}
+
+func (m *PotatoSack) Potato95() string {
+	return "potato #95"
+}
+
+func (m *PotatoSack) Potato96() string {
+	return "potato #96"
+}
+
+func (m *PotatoSack) Potato97() string {
+	return "potato #97"
+}
+
+func (m *PotatoSack) Potato98() string {
+	return "potato #98"
+}
+
+func (m *PotatoSack) Potato99() string {
+	return "potato #99"
+}
