b4bb43e6cf92fefec0fabb3ab6e14ddfa08a9956
Solomon Hykes <solomon@dagger.io>
2026-05-15 16:59:37 +0000

    test: convert module definition fixtures
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

---
+++ core/integration/testdata/modules/go/description-multi/z.go
@@ -0,0 +1,11 @@
+// Test module, short description
+//
+// Long description, with full sentences.
+package main
+
+// Test object, short description
+type Test struct{}
+
+func (*Test) Foo() Foo {
+	return Foo{}
+}
