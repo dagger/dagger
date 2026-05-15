2aa2d7f2004ec7fa26f8bbeac919ce0922e62c1d
Solomon Hykes <solomon@dagger.io>
2026-05-15 17:14:45 +0000

    test: convert benchmark module fixtures
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

---
+++ core/integration/testdata/modules/go/benchmark-lots-of-deps/mod2/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "context"
+
+type Mod2 struct{}
+
+func (m *Mod2) Fn(ctx context.Context) (string, error) {
+	s := "mod2"
+	var depS string
+	_ = depS
+	var err error
+	_ = err
+	depS, err = dag.Mod0().Fn(ctx)
+	if err != nil {
+		return "", err
+	}
+	s += depS
+	return s, nil
+}
