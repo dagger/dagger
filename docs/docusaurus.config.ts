80f0ecad99ab8e69fd75751d19123e0502124740
Solomon Hykes <solomon@dagger.io>
2026-06-04 20:58:12 +0000

    Speed up docs production builds
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -39,6 +39,18 @@ const config: Config = {
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl,
 
+  future: {
+    experimental_faster: {
+      swcJsLoader: true,
+      swcJsMinimizer: true,
+      swcHtmlMinimizer: false,
+      lightningCssMinimizer: true,
+      mdxCrossCompilerCache: true,
+      rspackBundler: true,
+      rspackPersistentCache: true,
+    },
+  },
+
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   //organizationName: 'facebook', // Usually your GitHub org/user name.
