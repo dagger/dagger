b255e475f6360b86bce20aebfce270716ae5cb9c
Solomon Hykes <solomon@dagger.io>
2026-06-05 17:34:28 -0700

    docs: promote 1.0 beta docs to current
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- docs/current_docs/extending/modules/snippets/interfaces/usages/typescript/index.ts
+++
@@ -1,9 +0,0 @@
-import { dag, func, object } from "@dagger.io/dagger"
-
-@object()
-export class Usage {
-  @func()
-  async test(): Promise<string> {
-    return dag.myModule().foo(dag.example())
-  }
-}
