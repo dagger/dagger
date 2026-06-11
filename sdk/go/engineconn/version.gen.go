b32ce8f2470aa37a19de6785fb510b9ae1bfb6a4
Erik Sipsma <erik@sipsma.dev>
2026-06-10 18:12:16 -0700

    chore: bump dependencies to v0.21.6
    Signed-off-by: Erik Sipsma <erik@sipsma.dev>

--- sdk/go/engineconn/version.gen.go
+++ sdk/go/engineconn/version.gen.go
@@ -2,4 +2,4 @@
 
 package engineconn
 
-const CLIVersion = "0.21.5"
+const CLIVersion = "0.21.6"
--- sdk/rust/crates/dagger-sdk/src/core/version.rs
+++ sdk/rust/crates/dagger-sdk/src/core/version.rs
@@ -1,1 +1,1 @@
-pub const DAGGER_ENGINE_VERSION: &str = "0.21.5";
+pub const DAGGER_ENGINE_VERSION: &str = "0.21.6";
