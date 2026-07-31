e16e80987a1885701f51cb1feaf09a1d3b80fdd2
Solomon Hykes <solomon@dagger.io>
2026-07-31 16:56:35 +0200

    CI: move single-use dagger modules from toolchains/ to .dagger/modules/
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

---
+++ .dagger/modules/release/main.go
@@ -0,0 +1,508 @@
+// A module that encodes the official release process of the Dagger Engine
+package main
+
+import (
+	"bytes"
+	"cmp"
+	"context"
+	_ "embed"
+	"fmt"
+	"strings"
+	"text/template"
+	"time"
+
+	"dagger/release/internal/dagger"
+
+	sprig "github.com/go-task/slim-sprig/v3"
+	"golang.org/x/mod/semver"
+	"golang.org/x/sync/errgroup"
+)
+
+type Release struct {
+	Workspace *dagger.Workspace // +private
+}
+
+func New(ws *dagger.Workspace) *Release {
+	return &Release{Workspace: ws}
+}
+
+type ReleaseReport struct {
+	Ref     string
+	Commit  string
+	Version string
+
+	Date string
+
+	Artifacts []*ReleaseReportArtifact
+	FollowUps []*ReleaseReportFollowUp
+
+	Errors []*dagger.Error
+}
+
+type ReleaseReportArtifact struct {
+	Name string
+	Tag  string
+	Link string
+
+	Errors []*dagger.Error
+
+	Notify bool // +private
+}
+
+type ReleaseReportFollowUp struct {
+	Name string
+	Link string
+}
+
+//go:embed report.md.tmpl
+var reportTmpl string
+
+func (report *ReleaseReport) Markdown(ctx context.Context) (string, error) {
+	tmpl, err := template.New("").Funcs(sprig.FuncMap()).Parse(reportTmpl)
+	if err != nil {
+		return "", err
+	}
+
+	var result bytes.Buffer
+	err = tmpl.ExecuteTemplate(&result, "", struct {
+		*ReleaseReport
+		Context context.Context
+	}{report, ctx})
+	if err != nil {
+		return "", err
+	}
+	return result.String(), nil
+}
+
+func (report *ReleaseReport) notify(
+	ctx context.Context,
+	discordWebhook *dagger.Secret,
+) error {
+	for _, artifact := range report.Artifacts {
+		if !artifact.Notify {
+			continue
+		}
+
+		message := fmt.Sprintf("%s: https://github.com/dagger/dagger/releases/tag/%s", artifact.Name, artifact.Tag)
+		_, err := dag.Notify().Discord(ctx, discordWebhook, message)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (report *ReleaseReport) hasErrors() bool {
+	if len(report.Errors) > 0 {
+		return true
+	}
+	for _, artifact := range report.Artifacts {
+		if len(artifact.Errors) > 0 {
+			return true
+		}
+	}
+	return false
+}
+
+// +cache="session"
+func (r *Release) Publish( //nolint:gocyclo
+	ctx context.Context,
+	tag string,
+	commit string,
+
+	dryRun bool, // +optional
+
+	registryImage string, // +optional
+	registryUsername string, // +optional
+	registryPassword *dagger.Secret, // +optional
+
+	githubToken *dagger.Secret, // +optional
+	githubOrgName string, // +optional
+	githubHost string, // +optional
+	githubCaCert *dagger.File, // +optional
+
+	netlifyToken *dagger.Secret, // +optional
+	netlifyAPIURL string, // +optional
+	pypiToken *dagger.Secret, // +optional
+	pypiRepo string, // +optional
+	pypiURL string, // +optional
+	npmToken *dagger.Secret, // +optional
+	npmRegistryURL string, // +optional
+	hexAPIKey *dagger.Secret, // +optional
+	hexAPIURL string, // +optional
+	cargoRegistryToken *dagger.Secret, // +optional
+	cargoRegistryIndex string, // +optional
+	goSdkDestRemote string, // +optional
+	phpSdkDestRemote string, // +optional
+
+	awsAccessKeyID *dagger.Secret, // +optional
+	awsSecretAccessKey *dagger.Secret, // +optional
+	awsRegion string, // +optional
+	awsBucket string, // +optional
+	awsCloudfrontDistribution string, // +optional
+	awsEndpointURL string, // +optional
+	artefactsFQDN string, // +optional
+
+	helmRegistry string, // +optional
+
+	discordWebhook *dagger.Secret, // +optional
+) (*ReleaseReport, error) {
+	version := ""
+	if semver.IsValid(tag) {
+		version = tag
+	}
+
+	report := ReleaseReport{
+		Date:    time.Now().UTC().Format(time.RFC822),
+		Ref:     tag,
+		Commit:  commit,
+		Version: version,
+	}
+
+	artifact := &ReleaseReportArtifact{
+		Name:   "🚙 Engine",
+		Tag:    tag,
+		Notify: true,
+	}
+
+	tags := []string{tag, commit}
+	if semver.IsValid(version) && semver.Prerelease(version) == "" {
+		// this is a public release
+		tags = append(tags, "latest")
+	}
+	err := dag.EngineDev(dagger.EngineDevOpts{Ws: r.Workspace}).Publish(ctx, tags, dagger.EngineDevPublishOpts{
+		Image:            registryImage,
+		RegistryUsername: registryUsername,
+		RegistryPassword: registryPassword,
+		DryRun:           dryRun,
+	})
+	if err != nil {
+		artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+	}
+	report.Artifacts = append(report.Artifacts, artifact)
+
+	artifact = &ReleaseReportArtifact{
+		Name: "🚗 CLI",
+		Tag:  tag,
+	}
+	cliDevOpts := dagger.CliDevOpts{
+		// Dependencies don't inherit the workspace; forward it explicitly.
+		Ws: r.Workspace,
+	}
+	if version != "" {
+		cliDevOpts.Version = version
+	}
+	cliDev := dag.CliDev(cliDevOpts)
+	if !dryRun {
+		_, err := cliDev.
+			Publish(tag, commit, githubOrgName, dagger.CliDevPublishOpts{
+				GithubToken:        githubToken,
+				GithubHost:         githubHost,
+				GithubCaCert:       githubCaCert,
+				AwsAccessKeyID:     awsAccessKeyID,
+				AwsSecretAccessKey: awsSecretAccessKey,
+				AwsRegion:          awsRegion,
+				AwsBucket:          awsBucket,
+				ArtefactsFqdn:      artefactsFQDN,
+				AwsEndpointURL:     awsEndpointURL,
+			}).
+			Sync(ctx)
+		if err != nil {
+			artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+		}
+		err = cliDev.PublishMetadata(ctx, awsAccessKeyID, awsSecretAccessKey, awsRegion, awsBucket, awsCloudfrontDistribution, dagger.CliDevPublishMetadataOpts{
+			AwsEndpointURL: awsEndpointURL,
+		})
+		if err != nil {
+			artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+		}
+	} else {
+		if err := cliDev.ReleaseDryRun(ctx); err != nil {
+			artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+		}
+	}
+	report.Artifacts = append(report.Artifacts, artifact)
+
+	if report.hasErrors() {
+		// early-exit if engine / cli could not Publish
+		return &report, nil
+	}
+
+	isPrerelease := semver.IsValid(version) && semver.Prerelease(version) != ""
+	if isPrerelease {
+		// early-exit if this is a pre-release
+		return &report, nil
+	}
+
+	if semver.IsValid(version) {
+		artifact = &ReleaseReportArtifact{
+			Name: "📖 Docs",
+			Link: "https://docs.dagger.io",
+		}
+		if !dryRun {
+			if err := dag.DocsDev().Publish(ctx, netlifyToken, dagger.DocsDevPublishOpts{
+				APIURL: netlifyAPIURL,
+			}); err != nil {
+				artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+			}
+		}
+		report.Artifacts = append(report.Artifacts, artifact)
+	}
+
+	components := []struct {
+		name    string
+		path    string
+		tag     string
+		link    string
+		dev     bool
+		release func(context.Context) error
+		dryRun  func(context.Context) error
+	}{
+		{
+			name: "🐹 Go SDK",
+			path: "sdk/go/",
+			tag:  "sdk/go/",
+			link: "https://pkg.go.dev/dagger.io/dagger@" + cmp.Or(version, "main"),
+			dev:  true,
+			release: func(ctx context.Context) error {
+				return dag.GoSDKDev(dagger.GoSDKDevOpts{Ws: r.Workspace}).Release(ctx, tag, dagger.GoSDKDevReleaseOpts{
+					GithubToken: githubToken,
+					DestRemote:  goSdkDestRemote,
+				})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.GoSDKDev(dagger.GoSDKDevOpts{Ws: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "🐍 Python SDK",
+			path: "sdk/python/",
+			tag:  "sdk/python/",
+			link: "https://pypi.org/project/dagger-io/" + strings.TrimPrefix(version, "v"),
+			release: func(ctx context.Context) error {
+				return dag.PythonSDKDev(dagger.PythonSDKDevOpts{Ws: r.Workspace}).Release(ctx, tag, dagger.PythonSDKDevReleaseOpts{
+					PypiRepo:  pypiRepo,
+					PypiURL:   pypiURL,
+					PypiToken: pypiToken,
+				})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.PythonSDKDev(dagger.PythonSDKDevOpts{Ws: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "⬢ TypeScript SDK",
+			path: "sdk/typescript/",
+			tag:  "sdk/typescript/",
+			link: "https://www.npmjs.com/package/@dagger.io/dagger/v/" + strings.TrimPrefix(version, "v"),
+			release: func(ctx context.Context) error {
+				return dag.TypescriptSDKDev(dagger.TypescriptSDKDevOpts{Ws: r.Workspace}).Release(ctx, tag, dagger.TypescriptSDKDevReleaseOpts{
+					NpmToken:       npmToken,
+					NpmRegistryURL: npmRegistryURL,
+				})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.TypescriptSDKDev(dagger.TypescriptSDKDevOpts{Ws: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "🧪 Elixir SDK",
+			path: "sdk/elixir/",
+			tag:  "sdk/elixir/",
+			link: "https://hex.pm/packages/dagger/" + strings.TrimPrefix(version, "v"),
+			release: func(ctx context.Context) error {
+				return dag.ElixirSDKDev(dagger.ElixirSDKDevOpts{Ws: r.Workspace}).Publish(ctx, tag, dagger.ElixirSDKDevPublishOpts{
+					HexAPIKey: hexAPIKey,
+					HexAPIURL: hexAPIURL,
+				})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.ElixirSDKDev(dagger.ElixirSDKDevOpts{Ws: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "⚙️ Rust SDK",
+			path: "sdk/rust/",
+			tag:  "sdk/rust/",
+			link: "https://crates.io/crates/dagger-sdk/" + strings.TrimPrefix(version, "v"),
+			release: func(ctx context.Context) error {
+				return dag.RustSDKDev(dagger.RustSDKDevOpts{Workspace: r.Workspace}).
+					Release(ctx, tag, cargoRegistryToken, dagger.RustSDKDevReleaseOpts{
+						CargoRegistryIndex: cargoRegistryIndex,
+					})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.RustSDKDev(dagger.RustSDKDevOpts{Workspace: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "🐘 PHP SDK",
+			path: "sdk/php/",
+			tag:  "sdk/php/",
+			link: "https://packagist.org/packages/dagger/dagger#" + cmp.Or(version, "dev-main"),
+			dev:  true,
+			release: func(ctx context.Context) error {
+				return dag.PhpSDKDev(dagger.PhpSDKDevOpts{Ws: r.Workspace}).Release(ctx, tag, dagger.PhpSDKDevReleaseOpts{
+					GithubToken: githubToken,
+					Dest:        phpSdkDestRemote,
+				})
+			},
+			dryRun: func(ctx context.Context) error {
+				return dag.PhpSDKDev(dagger.PhpSDKDevOpts{Ws: r.Workspace}).ReleaseDryRun(ctx)
+			},
+		},
+		{
+			name: "☸️ Helm Chart",
+			path: "helm/dagger/",
+			tag:  "helm/chart/",
+			link: "https://github.com/dagger/dagger/pkgs/container/dagger-helm",
+			// Helm publishing was inlined from helm-dev so release owns the
+			// package/push step without reintroducing that module dependency.
+			release: func(ctx context.Context) error {
+				return r.helmPublish(ctx, tag, githubToken, helmRegistry, false)
+			},
+			dryRun: func(ctx context.Context) error {
+				return r.helmReleaseDryRun(ctx)
+			},
+		},
+	}
+	artifacts := make([]*ReleaseReportArtifact, len(components))
+	var eg errgroup.Group
+	for i, component := range components {
+		if component.dev || semver.IsValid(version) {
+			// FIXME: use parallel
+			eg.Go(func() error {
+				target := ""
+				if semver.IsValid(version) {
+					target = strings.TrimSuffix(component.tag, "/") + "/" + version
+				}
+
+				artifact := &ReleaseReportArtifact{
+					Name:   component.name,
+					Tag:    target,
+					Link:   component.link,
+					Notify: true,
+				}
+				artifacts[i] = artifact
+
+				// FIXME: cleanup & simplify
+				func() (rerr error) {
+					defer func() {
+						if rerr != nil {
+							artifact.Notify = false
+							artifact.Errors = append(artifact.Errors, dag.Error(rerr.Error()))
+						}
+					}()
+					if dryRun {
+						if component.dryRun != nil {
+							if err := component.dryRun(ctx); err != nil {
+								return err
+							}
+						}
+					} else {
+						if component.release != nil {
+							if err := component.release(ctx); err != nil {
+								return err
+							}
+						}
+					}
+					return nil
+				}()
+
+				if semver.IsValid(version) {
+					notes := dag.Changelog().LookupEntry(component.path, version)
+					repo := "https://github.com/dagger/dagger"
+					if githubHost != "" {
+						repo = "https://" + githubHost + "/dagger/dagger"
+					}
+					if err := r.githubRelease(ctx, repo, commit, target, notes, githubToken, githubHost, githubCaCert, dryRun); err != nil {
+						artifact.Errors = append(artifact.Errors, dag.Error(err.Error()))
+						return nil
+					}
+				}
+
+				return nil
+			})
+		}
+	}
+	if err := eg.Wait(); err != nil {
+		return nil, err
+	}
+	for _, artifact := range artifacts {
+		if artifact == nil {
+			continue
+		}
+		report.Artifacts = append(report.Artifacts, artifact)
+	}
+
+	if semver.IsValid(version) {
+		report.FollowUps = append(report.FollowUps, &ReleaseReportFollowUp{
+			Name: "❄️ Nix",
+			Link: "https://github.com/dagger/nix",
+		})
+
+		report.FollowUps = append(report.FollowUps, &ReleaseReportFollowUp{
+			Name: "🍺 Homebrew Tap",
+			Link: "https://github.com/dagger/homebrew-tap",
+		})
+		report.FollowUps = append(report.FollowUps, &ReleaseReportFollowUp{
+			Name: "🍺 Homebrew Core",
+			Link: "https://github.com/Homebrew/homebrew-core/pulls?q=is%3Apr+in%3Atitle+dagger+" + strings.TrimPrefix(version, "v"),
+		})
+		report.FollowUps = append(report.FollowUps, &ReleaseReportFollowUp{
+			Name: "🪟 Winget pkgs",
+			Link: "https://github.com/microsoft/winget-pkgs/pulls?q=is%3Apr+in%3Atitle+dagger+" + strings.TrimPrefix(version, "v"),
+		})
+
+		report.FollowUps = append(report.FollowUps, &ReleaseReportFollowUp{
+			Name: "🌌 Daggerverse",
+			Link: "https://github.com/dagger/dagger.io/pulls?q=author%3Adagger-ci+is%3Apr+in%3Atitle+dgvs+" + strings.TrimPrefix(version, "v"),
+		})
+	}
+
+	if semver.IsValid(version) && discordWebhook != nil {
+		if err := report.notify(ctx, discordWebhook); err != nil {
+			report.Errors = append(report.Errors, dag.Error(err.Error()))
+		}
+	}
+
+	return &report, nil
+}
+
+// +cache="session"
+func (r Release) Notify(
+	ctx context.Context,
+	// GitHub repository URL
+	repository string,
+	// The target tag for the release
+	// e.g. sdk/typescript/v0.14.0
+	target string,
+	// Name of the component to release
+	name string,
+	// Discord webhook
+	// +optional
+	discordWebhook *dagger.Secret,
+
+	// Whether to perform a dry run without creating the release
+	// +optional
+	dryRun bool,
+) error {
+	githubRepo, err := githubRepo(repository, "")
+	if err != nil {
+		return err
+	}
+	if dryRun {
+		return nil
+	}
+
+	if discordWebhook != nil {
+		message := fmt.Sprintf("%s: https://github.com/%s/releases/tag/%s", name, githubRepo, target)
+		_, err = dag.Notify().Discord(ctx, discordWebhook, message)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
