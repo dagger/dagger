c503522c8bc22efd4dc04bb73ea7c66dedcfcbb0
Guillaume de Rouville <guillaume@dagger.io>
2026-09-09 16:28:21 -0700

    fix(core): preserve legacy output nesting defaults
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- core/schema/container.go
+++ core/schema/container.go
@@ -1685,6 +1685,7 @@ func (s *containerSchema) stdoutLegacy(ctx context.Context, parent dagql.ObjectR
 		var ctr dagql.ObjectResult[*core.Container]
 		if err := srv.Select(ctx, parent, &ctr, dagql.Selector{
 			Field: "withExec",
+			View:  "v0.12.0",
 			Args: []dagql.NamedInput{
 				{
 					Name:  "args",
@@ -1726,6 +1727,7 @@ func (s *containerSchema) stderrLegacy(ctx context.Context, parent dagql.ObjectR
 		var ctr dagql.ObjectResult[*core.Container]
 		if err := srv.Select(ctx, parent, &ctr, dagql.Selector{
 			Field: "withExec",
+			View:  "v0.12.0",
 			Args: []dagql.NamedInput{
 				{
 					Name:  "args",
