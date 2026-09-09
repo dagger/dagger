5d8433358cc5b63fc3e12d13b4eabf568028208c
Guillaume de Rouville <guillaume@dagger.io>
2026-09-09 16:26:16 -0700

    fix(core): preserve legacy terminal nesting behavior
    Signed-off-by: Guillaume de Rouville <guillaume@dagger.io>

--- core/schema/container.go
+++ core/schema/container.go
@@ -4723,6 +4723,7 @@ func (s *containerSchema) terminalLegacy(
 	err = srv.Select(ctx, ctr, new(dagql.Result[*core.Container]),
 		dagql.Selector{
 			Field: "terminal",
+			View:  "v0.12.0",
 			Args:  inputs,
 		},
 	)
