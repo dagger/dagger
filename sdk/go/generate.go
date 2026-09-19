7e883455d732c81eb93a0f20b6d819aa0b534c3b
Solomon Hykes <solomon@dagger.io>
2026-09-18 19:07:57 -0700

    sdk/go: generate core bindings with dagger-go-sdk-codegen
    Generate sdk/go/core and sdk/go/dag with the core subcommand of
    dagger-go-sdk-codegen (github.com/dagger/go-sdk), installed as a tool in
    the root go.mod. The root go.mod replaces dagger.io/dagger with ./sdk/go,
    so the tool builds against this checkout.
    
    Remove the Go library mode of cmd/codegen. generate-library stays for
    TypeScript.
    
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- sdk/go/generate.go
+++
@@ -1,6 +0,0 @@
-package dagger
-
-// TODO: generate with dagger-go-sdk-codegen (github.com/dagger/go-sdk), run by
-// //go:generate:container against a dev engine. Then remove generate-library
-// and go-client-dev's generate function. Blocked on #14178.
-//go:generate go -C ../../ run ./cmd/codegen generate-library --output ./sdk/go
