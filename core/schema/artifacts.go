3dfc49df861b9fe60f3b2442a67fdb8d45817b18
Solomon Hykes <solomon@dagger.io>
2026-09-18 20:24:48 -0700

    fix: preserve collection selections in artifact unions
    Signed-off-by: Solomon Hykes <solomon@dagger.io>

--- core/schema/artifacts.go
+++ core/schema/artifacts.go
@@ -159,7 +159,15 @@ func (*artifactsSchema) withArtifacts(ctx context.Context, parent *core.Artifact
 	if err != nil {
 		return nil, err
 	}
-	return parent.WithArtifacts(other.Self())
+	left, err := expandArtifacts(ctx, parent)
+	if err != nil {
+		return nil, err
+	}
+	right, err := expandArtifacts(ctx, other.Self())
+	if err != nil {
+		return nil, err
+	}
+	return left.WithArtifacts(right)
 }
 func (*artifactsSchema) filterPath(_ context.Context, parent *core.Artifacts, args struct{ Path []string }) (*core.Artifacts, error) {
 	return parent.FilterPath(args.Path), nil
@@ -250,6 +258,9 @@ func (*artifactsSchema) one(ctx context.Context, parent *core.Artifacts, _ struc
 func (*artifactsSchema) uri(_ context.Context, parent *core.Artifacts, _ struct{}) (string, error) {
 	var workspaceID uint64
 	for i, artifact := range parent.Entries {
+		if len(artifact.DimensionKeys) > 0 {
+			return "", fmt.Errorf("a selection with resolved collection keys has no single DAG address; use the individual artifact addresses")
+		}
 		id, err := artifact.Workspace.ID()
 		if err != nil {
 			return "", err
